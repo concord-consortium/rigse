c639d3ad73363f082bf4de034a092cda2124fe5a
Stephen Bannasch <stephen.bannasch@gmail.com>
2009-01-28 01:17:59 -0500

    update vendor/gems

--- db/schema.rb
+++ db/schema.rb
@@ -9,46 +9,83 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20090127214650) do
+ActiveRecord::Schema.define(:version => 20090127220427) do
+
+  create_table "activities", :force => true do |t|
+    t.integer  "user_id"
+    t.string   "title"
+    t.text     "context"
+    t.text     "opportunities"
+    t.text     "objectives"
+    t.text     "procedures_opening"
+    t.text     "procedures_engagement"
+    t.text     "procedures_closure"
+    t.text     "assessment"
+    t.text     "reflection"
+    t.string   "uuid",                  :limit => 36
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "assessment_targets", :force => true do |t|
-    t.integer "knowledge_statement_id"
-    t.integer "unifying_theme_id"
-    t.integer "number"
-    t.string  "target"
-    t.string  "grade_span"
+    t.integer  "knowledge_statement_id"
+    t.integer  "unifying_theme_id"
+    t.integer  "number"
+    t.string   "target"
+    t.string   "grade_span"
+    t.string   "uuid",                   :limit => 36
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "big_ideas", :force => true do |t|
-    t.integer "unifying_theme_id"
-    t.string  "idea"
+    t.integer  "unifying_theme_id"
+    t.string   "idea"
+    t.string   "uuid",              :limit => 36
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "domains", :force => true do |t|
-    t.string "name"
-    t.string "key"
+    t.string   "name"
+    t.string   "key"
+    t.string   "uuid",       :limit => 36
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "expectation_stems", :force => true do |t|
-    t.integer "grade_span_expectation_id"
-    t.string  "stem"
+    t.integer  "grade_span_expectation_id"
+    t.string   "stem"
+    t.string   "uuid",                      :limit => 36
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "expectations", :force => true do |t|
-    t.integer "expectation_stem_id"
-    t.string  "ordinal"
-    t.string  "expectation"
+    t.integer  "expectation_stem_id"
+    t.string   "ordinal"
+    t.string   "expectation"
+    t.string   "uuid",                :limit => 36
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "grade_span_expectations", :force => true do |t|
-    t.integer "assessment_target_id"
-    t.string  "grade_span"
+    t.integer  "assessment_target_id"
+    t.string   "grade_span"
+    t.string   "uuid",                 :limit => 36
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "knowledge_statements", :force => true do |t|
-    t.integer "domain_id"
-    t.integer "number"
-    t.string  "statement"
+    t.integer  "domain_id"
+    t.integer  "number"
+    t.string   "statement"
+    t.string   "uuid",       :limit => 36
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "open_id_authentication_associations", :force => true do |t|
@@ -75,7 +112,8 @@ ActiveRecord::Schema.define(:version => 20090127214650) do
   end
 
   create_table "roles", :force => true do |t|
-    t.string "name"
+    t.string  "title"
+    t.integer "position"
   end
 
   create_table "roles_users", :id => false, :force => true do |t|
@@ -94,14 +132,18 @@ ActiveRecord::Schema.define(:version => 20090127214650) do
   add_index "sessions", ["updated_at"], :name => "index_sessions_on_updated_at"
 
   create_table "unifying_themes", :force => true do |t|
-    t.string "name"
-    t.string "key"
+    t.string   "name"
+    t.string   "key"
+    t.string   "uuid",       :limit => 36
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "users", :force => true do |t|
     t.string   "login",                     :limit => 40
     t.string   "identity_url"
-    t.string   "name",                      :limit => 100, :default => ""
+    t.string   "first_name",                :limit => 100, :default => ""
+    t.string   "last_name",                 :limit => 100, :default => ""
     t.string   "email",                     :limit => 100
     t.string   "crypted_password",          :limit => 40
     t.string   "salt",                      :limit => 40
