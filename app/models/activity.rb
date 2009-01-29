fada4bf99f356fc02d1be95f1bb560daeb768a98
Stephen Bannasch <stephen.bannasch@gmail.com>
2009-01-29 13:13:44 -0500

    more edits to default activity and template

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -4,41 +4,49 @@ class Activity < ActiveRecord::Base
 
   default_value_for :procedures_opening, <<HEREDOC
   <h3>Procedures</h3>  
-  <p>What activities will you and your students do and how are they connected to the objectives?</p>
+  <p><em>What activities will you and your students do and how are they connected to the objectives?</em></p>
   <p></p>
   <h4>What will you be doing?</h4>
-  <p>How do you activate and assess students’ prior knowledge and connect it to this new learning?</li>
+  <p><em>How do you activate and assess students’ prior knowledge and connect it to this new learning?</em></li>
   <p></p>
-  <p>How do you get students engaged in this lesson?</li>
+  <p><em>How do you get students engaged in this lesson?</em></li>
   <p></p>
-  <h4>Students will discuss the following driving question:</h4>
-  <p>Key components:</p>
+  <h4>What will the students be doing?</h4>
+  <p><em>Students will discuss the following driving question:</em></p>
   <p></p>
-  <p>Starting conditions:</p>
+  <p><em>Key components:</p>
   <p></p>
-  <p>Ability to change variables:</p>
+  <p><em>Starting conditions:</p>
+  <p></p>
+  <p><em>Ability to change variables:</p>
   <p></p>
 HEREDOC
 
   default_value_for :procedures_engagement, <<HEREDOC
   <h3>Engagement</h3>  
-  <p>What questions can you pose to encourage students to take risks and to deepen students’ understanding? </p>
+  <h4>What will you be doing?</h4>
+  <p><em>What questions can you pose to encourage students to take risks and to deepen students’ understanding?</em></p>
   <p></p>
-  <p>How do you facilitate student discourse?</p>
+  <p><em>How do you facilitate student discourse?</em></p>
   <p></p>
-  <p>How do you facilitate the lesson so that all students are active learners and reflective during this lesson?</p>
+  <p><em>How do you facilitate the lesson so that all students are active learners and reflective during this lesson?</em></p>
   <p></p>
-  <p>How do you monitor students’ learning throughout this lesson?</p>
+  <p><em>How do you monitor students’ learning throughout this lesson?</em></p>
   <p></p>
-  <p>What formative assessment is imbedded in the lesson?</p>
+  <p><em>What formative assessment is imbedded in the lesson?</em></p>
+  <p></p>
+  <h4>What will the students be doing?</h4>
   <p></p>
 HEREDOC
 
   default_value_for :procedures_closure, <<HEREDOC
   <h3>Closure</h3>  
-  <p>What kinds of questions do you ask to get meaningful student feedback? </p>
+  <h4>What will you be doing?</h4>
+  <p><em>What kinds of questions do you ask to get meaningful student feedback?</em></p>
   <p></p>
-  <p>What opportunities do you provide for students to share their understandings of the task(s)?</p>
+  <p><em>What opportunities do you provide for students to share their understandings of the task(s)?</em></p>
+  <p></p>
+  <h4>What will the students be doing?</h4>
   <p></p>
 HEREDOC
 
