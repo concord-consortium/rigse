b901d12bf862060f2eb71e3b94b371c9deeeaaa0
Noah Paessel <knowuh@gmail.com>
2009-02-02 17:26:44 -0500

    mapping table expectations_stems_grade_span_expectation had an id field.     I did not know that I had to explicitly tell active record NOT to incude     an ide field.  See article:     http://wiki.rubyonrails.org/rails/pages/has_and_belongs_to_many

---
+++ db/migrate/20090202222045_redo_expectation_to_grade_span_exp.rb
@@ -0,0 +1,19 @@
+class RedoExpectationToGradeSpanExp < ActiveRecord::Migration
+  def self.up
+    drop_table  :expectation_stems_grade_span_expectations
+    
+    create_table :expectation_stems_grade_span_expectations, :id => false do |t|
+      t.integer  :grade_span_expectation_id
+      t.integer  :expectation_stem_id
+    end
+  end
+
+  def self.down
+    drop_table :expectation_stems_grade_span_expectations
+    
+    create_table :expectation_stems_grade_span_expectations  do |t|
+      t.integer  :grade_span_expectation_id
+      t.integer  :expectation_stem_id
+    end
+  end
+end
