aec353f0d8b8a0a3dd599de3650844063c1708da
Noah Paessel <knowuh@gmail.com>
2009-02-06 17:10:51 -0500

    Changes to the model of gses -> expectations -> expectation_stems etc. Migration changes to support model changes.

--- app/models/expectation_stem.rb
+++ app/models/expectation_stem.rb
@@ -1,6 +1,5 @@
 class ExpectationStem < ActiveRecord::Base
   belongs_to              :user
   has_many                :expectations
-  has_and_belongs_to_many :grade_span_expectations
   acts_as_replicatable
 end
