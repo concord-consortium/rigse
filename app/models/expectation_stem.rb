054c747bbe149baafd5d4c7e7eacd7eea5cff4f5
Stephen Bannasch <stephen.bannasch@gmail.com>
2009-02-05 21:46:08 -0500

    oops ... fix last commit: 5e76599e5

--- app/models/expectation_stem.rb
+++ app/models/expectation_stem.rb
@@ -1,6 +1,6 @@
 class ExpectationStem < ActiveRecord::Base
   belongs_to              :user
   has_many                :expectations
-  has_many                :grade_span_expectations
+  has_and_belongs_to_many :grade_span_expectations
   acts_as_replicatable
 end
