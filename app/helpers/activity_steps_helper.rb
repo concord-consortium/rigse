dd455ef7fb19e25985dc964ccb83be37fe5847db
Noah Paessel <knowuh@gmail.com>
2009-03-16 16:14:09 -0400

    added helper methods to DRY up the view templates for various steps

--- app/helpers/activity_steps_helper.rb
+++ app/helpers/activity_steps_helper.rb
@@ -1,15 +1,37 @@
 module ActivityStepsHelper
   
-  def form_for_step(act_step)
+  # def form_for_step(act_step)
+  #   type = act_step.step_type
+  #   # form_html = act.step.form_htm
+  #   case type
+  #   when 'Xhtml'
+  #     act_step.step.name
+  #   when 'MultipleChoice'
+  #     act_step.step.prompt
+  #   when 'OpenResponse'
+  #     act_step.step.prompt
+  #   end
+  # end
+  
+  def html_for_step(act_step, mode="edit")
+    @act_step = act_step
+    @step = act_step.step
     type = act_step.step_type
-    case type
-    when 'Xhtml'
-      act_step.step.name
-    when 'MultipleChoice'
-      act_step.step.prompt
-    when 'OpenResponse'
-      act_step.step.prompt
+    partial = "activity_steps/#{mode}_#{type.downcase}"
+    html = "could not render partial (#{partial})"
+    begin
+      html = render(:partial => partial, :object => @act_step)
+    rescue => e
+      html = "#{html} : #{e}"
     end
+    return html
   end
+
+  def link_to_delete (act_step) 
+    @word = 'word'
+    @delete_step = act_step
+    render(:partial => 'activity_steps/delete', :object => @act_step)
+  end
+  
   
 end
