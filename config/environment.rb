7d4dd37f7c162f1c06b0beda45f6281abbcf79b7
Stephen Bannasch <stephen.bannasch@gmail.com>
2009-03-16 09:52:42 -0400

    remove gems currently in vendor/gems the rails 2.3 code for gems integration has been re-worked

--- config/environment.rb
+++ config/environment.rb
@@ -30,7 +30,7 @@ Rails::Initializer.run do |config|
   config.gem "capistrano-ext", :lib => "capistrano"
   config.gem 'rubyist-aasm', :lib => 'aasm', :source => 'http://gems.github.com', :version => '2.0.2'
   config.gem 'mislav-will_paginate', :version => '2.3.6', :lib => 'will_paginate', :source => 'http://gems.github.com'
-# config.gem "haml", :version => '2.0.7'  
+  config.gem "haml", :version => '2.0.7'  
   config.gem "RedCloth", :version => '4.1.1'
   config.gem "uuidtools"
   config.gem "hpricot"
