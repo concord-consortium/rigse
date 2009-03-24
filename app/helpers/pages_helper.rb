e44302b3bed306430976d9bdc410f9053b9024bb
Noah Paessel <knowuh@gmail.com>
2009-03-24 18:48:16 -0400

    Some of the link-to-remote (ajax) calls are too big to fit neatly on a single haml line.  Reading best practices, they suggest that those be broken out into a helper for the calling page.
    I made one for delete, and we could make others for edit and save I
    think.

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,35 +1,14 @@
 module PagesHelper
   
-  # def form_for_step(element)
-  #   type = element.step_type
-  #   # form_html = act.step.form_htm
-  #   case type
-  #   when 'Xhtml'
-  #     element.step.name
-  #   when 'MultipleChoice'
-  #     element.step.prompt
-  #   when 'OpenResponse'
-  #     element.step.prompt
-  #   end
-  # end
-  
-  def html_for_element(element, mode="edit")
-    partial = "pages/#{mode}_#{element.embeddable_type.downcase}"
-    html = "could not render partial (#{partial})"
-    begin
-      html = render(:partial => partial, :object => element)
-    rescue => e
-      html = "#{html} : #{e}"
-    end
-    return html
-  end
-
-  def link_to_delete (element) 
-    render(:partial => 'pages/delete', :object => element)
-  end
-  
-  def link_to_save (element) 
-    render(:partial => 'pages/save', :object => element)
+  def link_to_ajax_delete(page,component)
+    link_to_remote 'delete',  
+      :confirm => "Delete this item?", 
+      :html => {:class => 'delete'}, 
+      :url => { 
+        :action => 'delete_element', 
+        :dom_id =>page.element_for(component).dom_id, 
+        :element_id => page.element_for(component).id
+      }
   end
   
 end
