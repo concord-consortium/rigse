f4f5abf617050c6fa8ca4541148f81ddf54ac442
Noah Paessel <knowuh@gmail.com>
2009-03-27 09:33:25 -0400

    users controller had lots of new bugs, related to commit 2c94b3d54eac638478da4471fa43a9aea489da07
    Account creation was broken. see grab: http://skitch.com/noahpaessel/bju64/rites-authoring-template
    
    views/user/new.html.erb was actually haml, and should have been named new.html.haml
    
    After renaming, then some of the partials stopped working:
    
    >>>
     Haml::SyntaxError in Users#new
    
    Showing app/views/users/_password.html.haml where line #5 raised:
    
    Invalid tag: "%= f.label :password".
    
    Extracted source (around line #5):
    
    2:   %legend
    3:     Password
    4:   %li
    5:     %= f.label :password
    6:     %= f.password_field :password
    7:   %li
    8:     %= f.label :password_confirmation, 'Confirm Password'
    <<<<
    
    
    So, I edited _password.html.haml and changed %= instances to just =
    
    Then another controller error:
    
    ActionController::DoubleRenderError in UsersController#create
    
    Render and/or redirect were called multiple times in this action. Please note that you may only call render OR redirect, and at most once per action. Also note that neither redirect nor render terminate execution of the action, so if you want to exit an action after redirecting, you need to do something like "redirect_to(...) and return".
    
    RAILS_ROOT: /Users/npaessel/lab/ruby/rigse

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,7 @@ class UsersController < ApplicationController
   def create
     logout_keeping_session!
     create_new_user(params[:user])
-    redirect_to(root_path)
+    # redirect_to(root_path) # (no need to redirect here, the above controller did it.)
   end
 
   # GET /users/1/edit
@@ -61,16 +61,18 @@ class UsersController < ApplicationController
       @user.register!
     end
     if @user.errors.empty?
+      # will redirect:
       successful_creation(@user)
     else
+      # will redirect:
       failed_creation
     end
   end
   
   def successful_creation(user)
-    redirect_back_or_default(root_path)
     flash[:notice] = "Thanks for signing up!"
     flash[:notice] << " We're sending you an email with your activation code."
+    redirect_back_or_default(root_path)
   end
   
   def failed_creation(message = 'Sorry, there was an error creating your account')
