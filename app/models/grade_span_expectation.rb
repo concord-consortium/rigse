b7026b719eb7696057621228869b4c767835ad3c
Noah Paessel <knowuh@gmail.com>
2009-04-02 13:12:26 -0400

    Fixed bug on import with this default scope:
    undefined method `grade_span LIKE '%9-11%'=' for #<GradeSpanExpectation:0xb6a60354>
    /web/rites.concord.org/releases/20090402170801/vendor/rails/activerecord/lib/active_record/attribute_methods.rb:255:in `method_missing'
    /web/rites.concord.org/releases/20090402170801/vendor/rails/activerecord/lib/active_record/base.rb:2440:in `send'
    /web/rites.concord.org/releases/20090402170801/vendor/rails/activerecord/lib/active_record/base.rb:2440:in `initialize'
    /web/rites.concord.org/releases/20090402170801/vendor/rails/activerecord/lib/active_record/base.rb:2440:in `each'
    /web/rites.concord.org/releases/20090402170801/vendor/rails/activerecord/lib/active_record/base.rb:2440:in `initialize'
    /web/rites.concord.org/releases/20090402170801/lib/parser.rb:292:in `new'

--- app/models/grade_span_expectation.rb
+++ app/models/grade_span_expectation.rb
@@ -5,7 +5,17 @@ class GradeSpanExpectation < ActiveRecord::Base
   has_many                :unifying_themes, :through => :assessment_target
   acts_as_replicatable
   
-  default_scope :conditions => "grade_span LIKE '%9-11%'"
+  # this was causing errors on otto when running setup-from-scratch:
+  # here is error-output
+        # undefined method `grade_span LIKE '%9-11%'=' for #<GradeSpanExpectation:0xb6a60354>
+        # /web/rites.concord.org/releases/20090402170801/vendor/rails/activerecord/lib/active_record/attribute_methods.rb:255:in `method_missing'
+        # /web/rites.concord.org/releases/20090402170801/vendor/rails/activerecord/lib/active_record/base.rb:2440:in `send'
+        # /web/rites.concord.org/releases/20090402170801/vendor/rails/activerecord/lib/active_record/base.rb:2440:in `initialize'
+        # /web/rites.concord.org/releases/20090402170801/vendor/rails/activerecord/lib/active_record/base.rb:2440:in `each'
+        # /web/rites.concord.org/releases/20090402170801/vendor/rails/activerecord/lib/active_record/base.rb:2440:in `initialize'
+        # /web/rites.concord.org/releases/20090402170801/lib/parser.rb:292:in `new'
+  #
+  #:default_scope :conditions => "grade_span LIKE '%9-11%'"
   
   include Changeable
   
