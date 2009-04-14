393cd31365582a3afdb7810911a62d10d8cae2f4
Noah Paessel <knowuh@gmail.com>
2009-04-14 13:25:18 -0400

    dropdown javascript

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,29 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+var dropdown_for = function(menu_dom_id,drop_down_dom_id) {
+    var menu = $(menu_dom_id);
+    var drop_down = $(drop_down_dom_id);
+    var menu_width = menu.getDimensions().width
+    var drop_down_width = drop_down.getDimensions().width
+    var left_offset = (drop_down_width - menu_width) / -2
+    var top_offset = menu.getDimensions().height
+    var options = { setWidth: false, setHeight: false, offsetLeft:left_offset, offsetTop: top_offset};
+    drop_down.clonePosition(menu,options);
+    drop_down.observe('mouseout', function(event) {
+      var mouse_over_element;  
+      if( event.toElement ) {
+         mouse_over_element = event.toElement;
+      }
+      else if(event.relatedTarget) {
+        mouse_over_element = event.relatedTarget;
+      }
+      if(!mouse_over_element.descendantOf(drop_down)) {
+         drop_down.setStyle({left: "-1000px"});
+         drop_down.stopObserving();
+      }
+    });
+    drop_down.observe('click', function(event) {
+       drop_down.setStyle({left: "-1000px"});
+       drop_down.stopObserving();
+    });
+  }
