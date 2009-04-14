553d56a213fda197cf2277b5f8edf691a55922ef
Noah Paessel <knowuh@gmail.com>
2009-04-14 16:04:24 -0400

    Trying to improve the behavior of the dropdowns when the mouseout event is missed. The problem is if the mouse takes this path: :trigger for drop down ->   :in drop-down -> 	:mouseout to trigger
    This causes the drop down to flash off and then back on again.
    
    This still needs work.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -8,22 +8,22 @@ var dropdown_for = function(menu_dom_id,drop_down_dom_id) {
     var left_offset = (drop_down_width - menu_width) / -2
     var top_offset = menu.getDimensions().height
     var options = { setWidth: false, setHeight: false, offsetLeft:left_offset, offsetTop: top_offset};
+
     drop_down.clonePosition(menu,options);
     drop_down.observe('mouseout', function(event) {
-      var mouse_over_element;  
-      if( event.toElement ) {
-         mouse_over_element = event.toElement;
-      }
-      else if(event.relatedTarget) {
-        mouse_over_element = event.relatedTarget;
-      }
-      if(!mouse_over_element.descendantOf(drop_down)) {
-         drop_down.setStyle({left: "-1000px"});
-         drop_down.stopObserving();
+      var mouse_over_element = event.relatedTarget;
+      if(!mouse_over_element.descendantOf(drop_down) || event.toElement == menu) {
+        hide()
       }
     });
+  
     drop_down.observe('click', function(event) {
        drop_down.setStyle({left: "-1000px"});
        drop_down.stopObserving();
     });
+    
+    function hide() {
+      drop_down.setStyle({left: "-1000px"});
+      drop_down.stopObserving();
+    }
   }
