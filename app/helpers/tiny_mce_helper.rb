5017b031ea5724a97f0d540754b485e6477de7c1
Stephen Bannasch <stephen.bannasch@gmail.com>
2009-04-24 19:15:23 -0400

    expand tinymce init string to experiment more easily

--- app/helpers/tiny_mce_helper.rb
+++ app/helpers/tiny_mce_helper.rb
@@ -1,7 +1,21 @@
 module TinyMceHelper
 
   def mce_init_string
-    'tinyMCE.init({mode:"textareas",theme:"advanced",theme_advanced_toolbar_location:"top",theme_advanced_buttons1:"justifyleft,justifycenter,justifyright,justifyfull,|,bold,italic,underline,strikethrough,|,fontselect,fontsizeselect|,bullist,numlist,hr|,undo,redo,link,unlink,image",theme_advanced_toolbar_align:"left",theme_advanced_buttons2:"",theme_advanced_buttons:"",theme_advanced_statusbar_location:"bottom",theme_advanced_resizing:true});' 
+    <<HEREDOC
+tinyMCE.init({
+  mode : "textareas",
+  theme : "advanced",
+  theme_advanced_layout_manager : "SimpleLayout",
+  theme_advanced_buttons1     : "justifyleft,justifycenter,justifyright,justifyfull,|,bold,italic,underline,strikethrough,",
+  theme_advanced_buttons1_add : "|,fontselect,fontsizeselect|,bullist,numlist,hr|,undo,redo,link,unlink,image",
+  theme_advanced_buttons2 : "",
+  theme_advanced_buttons3 : "",
+  theme_advanced_toolbar_location : "top",
+  theme_advanced_toolbar_align : "left",
+  theme_advanced_statusbar_location : "bottom",
+  theme_advanced_resizing : true
+});
+HEREDOC
   end
 
 end
