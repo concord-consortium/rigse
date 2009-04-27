6124a9c71153af706102f3c5fcb8ed63870c0e4e
Noah Paessel <knowuh@gmail.com>
2009-04-27 17:12:01 -0400

    Can now add / remove columns to dataTables
    Helper class to make editing data-tables easier.
    New sass styling for data_table. (maybe we want to break out components somehow?)
    simplified dom id for form, so I can observe it.

---
+++ app/helpers/data_table_helper.rb
@@ -0,0 +1,34 @@
+module DataTableHelper
+  
+  #
+  #
+  #
+  def remove_empty_headings
+    <<-EOF_JS
+    $$('.js_target').each(function (t) {
+      if (t.value =='') {
+        t.remove();
+      }
+    })
+    $('column_names').value = $$('.js_target').pluck('value').join(',');
+    $('column_count').value = $$('.js_target').size()
+    EOF_JS
+  end
+  
+  #
+  # 
+  #
+  def link_add_column(link_text, dom_id="heading_list") 
+    link_to_function link_text,"$('#{dom_id}').insert('#{escape_javascript data_table_heading_tag()}')"
+  end
+  
+  #
+  # Probably would be good to extract this to a haml partial
+  #
+  def data_table_heading_tag(heading = "new column")
+    <<-EOF_HTML
+      <input type="text" size="16" name="heading" class="js_target" value="#{heading}"/>
+    EOF_HTML
+  end
+  
+end
