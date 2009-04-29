27249e3723e54987465998282af68c053989308d
Noah Paessel <knowuh@gmail.com>
2009-04-29 16:15:36 -0400

    Added support for "anyone can change things created by anonymous" to the changeable rules. Changed brown color for highlighting to something lighter. Experiments with tinyMCE

--- app/helpers/tiny_mce_helper.rb
+++ app/helpers/tiny_mce_helper.rb
@@ -3,17 +3,17 @@ module TinyMceHelper
   def mce_init_string
     <<HEREDOC
 tinyMCE.init({
-  mode : "textareas",
   theme : "advanced",
-  theme_advanced_layout_manager : "SimpleLayout",
-  theme_advanced_buttons1     : "justifyleft,justifycenter,justifyright,justifyfull,|,bold,italic,underline,strikethrough,",
-  theme_advanced_buttons1_add : "|,fontselect,fontsizeselect|,bullist,numlist,hr|,undo,redo,link,unlink,image",
+  mode : "textareas",
+  theme_advanced_resizing : true,
+  theme_advanced_toolbar_location : "top",
+  theme_advanced_buttons1     : "justifyleft,justifycenter,justifyfull,",
+  theme_advanced_buttons1_add : "|,bold,italic,underline,|,fontselect,fontsizeselect,|,bullist,numlist,|link,image",
   theme_advanced_buttons2 : "",
   theme_advanced_buttons3 : "",
-  theme_advanced_toolbar_location : "top",
-  theme_advanced_toolbar_align : "left",
   theme_advanced_statusbar_location : "bottom",
-  theme_advanced_resizing : true
+  theme_advanced_path : false,
+  height : "70"
 });
 HEREDOC
   end
--- lib/changeable.rb
+++ lib/changeable.rb
@@ -5,8 +5,13 @@ module Changeable
   def changeable?(user)
     if user.roles.find_by_title('admin') || user.roles.find_by_title('manager')
       true
-    elsif self.respond_to?(:user) && self.user == user
-      true
+    elsif self.respond_to?(:user) 
+      if self.user == user
+        true
+      # any one can change an anonymous item(??)
+      elsif self.user.anonymous?
+        true
+      end
     elsif self == user
       true
     else
