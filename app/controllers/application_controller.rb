7aaaecb7e64f7da79288f87efe411f71eedd0b58
Noah Paessel <knowuh@gmail.com>
2009-04-29 17:54:23 -0400

    Disabled live validation on password reset form. Changed action of password reset form. (to match what RESTFUL authentications generator creates) Friendlier more useful error messages in password controller added redirect_back_or(path) application_controller method.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -31,5 +31,12 @@ class ApplicationController < ActionController::Base
     end
   end
 
+
+  def redirect_back_or(path)
+    redirect_to :back
+  rescue ActionController::RedirectBackError
+    redirect_to path
+  end
+
 end
 
--- app/controllers/passwords_controller.rb
+++ app/controllers/passwords_controller.rb
@@ -27,22 +27,20 @@ class PasswordsController < ApplicationController
 
   def update_after_forgetting
     @user = Password.find_by_reset_code(params[:reset_code]).user
-    
     if @user.update_attributes(params[:user])
-      flash[:notice] = 'Password was successfully updated.'
+      flash[:notice] = "Password for #{@user.login} was successfully updated."
       redirect_to login_path
     else
-      flash[:notice] = 'EPIC FAIL!'
+      flash[:error] = 'Password could not be updated'
       redirect_to :action => :reset, :reset_code => params[:reset_code]
     end
   end
   
   def update
     @password = Password.find(params[:id])
-
     if @password.update_attributes(params[:password])
       flash[:notice] = 'Password was successfully updated.'
-      redirect_to(@password)
+      redirect_back_or investigations_url
     else
       render :action => :edit
     end
