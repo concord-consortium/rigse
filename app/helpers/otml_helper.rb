e974560a09efd8ec3b656081c485573b7b232e08
Stephen Bannasch <stephen.bannasch@gmail.com>
2009-05-05 14:53:55 -0400

    add :title option to ot_bundles helper

--- app/helpers/otml_helper.rb
+++ app/helpers/otml_helper.rb
@@ -84,7 +84,7 @@ module OtmlHelper
     else
       left_nav_panel_width = 0
     end
-    render :partial => "otml/ot_view_bundle", :locals => { :view_entries => view_entries, :left_nav_panel_width => left_nav_panel_width }
+    render :partial => "otml/ot_view_bundle", :locals => { :view_entries => view_entries, :left_nav_panel_width => left_nav_panel_width, :title => 'RITES sample' }
   end
 
   def ot_script_engine_bundle
