d3a97ea497b2c678ff0b94e4cc0b89fc46e5e7a1
Noah Paessel <knowuh@gmail.com>
2009-05-06 17:42:23 -0400

    Because in place editing is a div, it can have a minimum width, and thus be edited even when there is no existing value.

--- vendor/plugins/in_place_editing/lib/in_place_macros_helper.rb
+++ vendor/plugins/in_place_editing/lib/in_place_macros_helper.rb
@@ -70,7 +70,7 @@ module InPlaceMacrosHelper
   # Renders the value of the specified object and method with in-place editing capabilities.
   def in_place_editor_field(object, method, tag_options = {}, in_place_editor_options = {})
     instance_tag = ::ActionView::Helpers::InstanceTag.new(object, method, self)
-    tag_options = {:tag => "span",
+    tag_options = {:tag => "div",
                    :id => "#{object}_#{method}_#{instance_tag.object.id}_in_place_editor",
                    :class => "in_place_editor_field"}.merge!(tag_options)
     in_place_editor_options[:url] = in_place_editor_options[:url] || url_for({ :action => "set_#{object}_#{method}", :id => instance_tag.object.id })
