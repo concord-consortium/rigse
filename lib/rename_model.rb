928125e11e7f564504247b772d7e55484d203d20
Noah Paessel <knowuh@gmail.com>
2009-05-08 15:26:23 -0400

    model renaming tool. TODO: generate migration here too?

--- lib/rename_model.rb
+++ lib/rename_model.rb
@@ -14,11 +14,14 @@ def rename(old_name,new_name)
   # the correct ordering of 'each'...
   replacements = [
     {
-      :old => old_name.tableize.pluralize,
-      :new => new_name.tableize.pluralize
-    },{
       :old => old_name.tableize,
       :new => new_name.tableize
+    },{
+      :old => old_name.classify.pluralize,
+      :new => new_name.classify.pluralize
+    },{
+      :old => old_name.tableize.singularize,
+      :new => new_name.tableize.singularize
     },{
       :old => old_name.classify,
       :new => new_name.classify
@@ -28,19 +31,29 @@ def rename(old_name,new_name)
     }
   ]
   replacements.each do |replacement|
+    puts "replacing #{replacement[:old]} => #{replacement[:new]} "
     Dir.glob(File.join("../app/**","*.#{types}")) do |filename| 
+      # look for files containting our replacement text:
+      found_path =  exec "grep #{replacement[:old]} -l #{filename}"
+      if (found_path.size > 0)
+        # replace:
+        exec "sed -i '' 's/#{replacement[:old]}/#{replacement[:new]}/g' #{filename}"
+      end
+      
+      # rename matching patterns:
       if filename =~ /#{replacement[:old]}/
         exec "mv #{filename} #{filename.gsub(replacement[:old],replacement[:new])}"
       end
-      exec "sed -i -n 's/#{replacement[:old]}/#{replacement[:new]}/g' #{filename}"
     end
+    # we should change the routes file too:
+    puts exec "sed -i '' 's/#{replacement[:old]}/#{replacement[:new]}/g' ../config/routes.rb"
   end
 end
 
 
 def exec(command)
-  puts command
-  puts %x[#{command}]
+  #return command
+  return %x[#{command}]
 end
 
 rename ('Investigation','Activity')
