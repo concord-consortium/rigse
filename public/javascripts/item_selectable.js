a0f03296e1d9b4c0d668ff25079c477972112270
Noah Paessel <knowuh@gmail.com>
2009-05-11 18:30:17 -0400

    Experiment with double-click to edit xhtml page elements. (more to come) Continuing working on the idea of a 'selectable' list interface. Studies in a toned-down action-menu behaviors. (more to do)

--- public/javascripts/item_selectable.js
+++ public/javascripts/item_selectable.js
@@ -1,16 +1,34 @@
 var selected_class = 'selected';
 var unselected_class = 'selectable';
-var get_selectable = function(element) { return (element.hasClassName(unselected_class) || element.up("." + unselected_class) || false) }
+var get_selectable = function(element) { return (element.hasClassName(unselected_class) || element.hasClassName(selected_class) || element.up("." + unselected_class)) || element.up("." + selected_class)}
+var is_selected = function(element) { return element.hasClassName(selected_class)}
 
 var item_select = function(event) {
-  item_deselect();
   element = event.element()
-  console.log("clicked");
   selected = get_selectable(element)
   if (selected) {
-    console.log("way clicked");
-    selected.addClassName(selected_class);
-    selected.removeClassName(unselected_class);
+    console.log("clicked");
+    if (selected.hasClassName(selected_class)) {
+      var type = '';
+      var id = '';
+    
+      selected.identify().gsub(/item_([\w|_]+)_(\d+)/, function(match){
+        type = match[1];
+        id = match[2];
+      });
+      console.log('edit-click on class='+type+' id='+id);
+      var edit_dom_id = "form_"   +type+ "_" +id;
+      var show_dom_id = "display_"+type+ "_" +id;
+      console.log(edit_dom_id);
+      $(edit_dom_id).show();
+      $(show_dom_id).hide();
+    }
+    else {
+      item_deselect();
+      selected.addClassName(selected_class);
+      selected.removeClassName(unselected_class);
+      // selected.down('.action_menu').show();
+    }
   }
 }
 
@@ -18,6 +36,7 @@ var item_deselect = function() {
   $$("." + selected_class).each(function(element){
     element.removeClassName(selected_class);
     element.addClassName(unselected_class);
+    // element.down('.action_menu').hide();
   });
 }
 
