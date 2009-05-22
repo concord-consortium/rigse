c4f3e12b47c1f8baa6b8216f17fb0d49f32918b7
Noah Paessel <knowuh@gmail.com>
2009-05-22 16:36:30 -0400

    a list of types pages accept for paste content

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,6 +1,12 @@
 module PagesHelper
 
+  
   def element_types
     [DataCollector,DrawingTool,OpenResponse,Xhtml,MultipleChoice,DataTable,MwModelerPage,NLogoModel]
   end
+
+  def page_acceptable_types
+    element_types.map {|t| t.name.underscore}
+  end
+  
 end
