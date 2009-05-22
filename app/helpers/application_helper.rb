f5d6669a9dd4023e9917196a83fd80109be5ddbc
Noah Paessel <knowuh@gmail.com>
2009-05-22 16:36:09 -0400

    paste_link_for helper method (copy and pasting functionality)

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -146,6 +146,21 @@ module ApplicationHelper
         })
   end
   
+  def paste_link_for(component_id,acceptable_types,options={})
+    clipboard_data_type  = options[:clipboard_data_type] || cookies[:clipboard_data_type]
+    clipboard_data_id    = options[:clipboard_data_id]   || cookies[:clipboard_data_id]
+    
+    return "paste (nothing in clipboard)" unless clipboard_data_type
+    
+    if acceptable_types.include?(clipboard_data_type) 
+      url = url_for :action => 'paste', :method=> 'post', :clipboard_data_type => clipboard_data_type, :clipboard_data_id => clipboard_data_id, :id =>component_id
+      return link_to_remote ("paste #{clipboard_data_type}:#{clipboard_data_id}", :url => url)
+    end
+    return "cant paste (#{clipboard_data_type}:#{clipboard_data_id}) here"
+  end
+  
+  
+  
   def run_link_for(component, prefix='')
     component_display_name = component.class.display_name.downcase
     name = component.name
@@ -332,5 +347,5 @@ module ApplicationHelper
       end
     end
   end
-  
+
 end
