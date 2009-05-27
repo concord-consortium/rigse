f7defd0b6255d5d2439ceda228be343a4b832ddb
Aaron Unger <aunger@concord.org>
2009-05-27 11:25:04 -0400

    More files that were missed in the first commit.

---
+++ db/migrate/20090526205055_create_raw_otmls.rb
@@ -0,0 +1,19 @@
+class CreateRawOtmls < ActiveRecord::Migration
+  def self.up
+    create_table :raw_otmls do |t|
+      
+      t.integer   "user_id"
+      t.string    "uuid",        :limit => 36
+      t.string    "name"
+      t.text      "description"
+      
+      t.text :content
+
+      t.timestamps
+    end
+  end
+
+  def self.down
+    drop_table :raw_otmls
+  end
+end
