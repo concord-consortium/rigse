29d368f93578ca362c696c2ea78fac674e57e854
Noah Paessel <knowuh@gmail.com>
2009-05-28 11:51:42 -0400

    Don't rely on console log! (its firebug specific, and will silently throw exceptions elsewhere)

--- public/javascripts/item_selectable.js
+++ public/javascripts/item_selectable.js
@@ -1,10 +1,12 @@
 var selected_class = 'item_selected';
 var unselected_class = 'item_selectable';
+var rites_document;
 
 var is_selected = function(element) { return element.hasClassName(selected_class)}
 var selected = function(toggle_element)         { return (readCookie(id_for_toggle(toggle_element)) == "true");    }
 var set_cookie = function(toggle_element)         { createCookie(id_for_toggle(toggle_element),'true'); }
 var remove_cookie = function(toggle_element)      { eraseCookie(id_for_toggle(toggle_element)); }
+
 
 var get_selectable = function(element) { 
   element = $(element)
@@ -22,7 +24,7 @@ var item_select = function(event) {
   element = event.element();
   element = $(element); // extend
   selected = get_selectable(element)
-  console.log(selected)
+
   if (selected) {
     var type = '';
     var id = '';
@@ -33,8 +35,8 @@ var item_select = function(event) {
     });
     selected.addClassName(selected_class);
     selected.removeClassName(unselected_class);
-    document.selected_type=type;
-    document.selected_id=id;
+    rites_document.selected_type=type;
+    rites_document.selected_id=id;
   }
   update_links();
 }
@@ -43,17 +45,17 @@ var item_deselect = function() {
   $$("." + selected_class).each(function(element){
     element.removeClassName(selected_class);
     element.addClassName(unselected_class);
-    document.selected_type=null;
-    document.selected_id=null;
+    rites_document.selected_type=null;
+    rites_document.selected_id=null;
   });
 }
 
 var update_links = function() {
-  if(document.selected_type !=null) {
+  if(rites_document.selected_type !=null) {
     var template = new Template('<a>copy #{type}:#{id}</a>');
     $('copy_link').addClassName('copy_enabled');
     $('copy_link').observe('click',copy);
-    $('copy_link').update(template.evaluate({type:document.selected_type, id:document.selected_id}));    
+    $('copy_link').update(template.evaluate({type:rites_document.selected_type, id:rites_document.selected_id}));    
   }
   else {
     $('copy_link').addClassName('copy_disabled');
@@ -64,21 +66,21 @@ var update_links = function() {
 
 var copy = function() {
   var template = new Template('#{type}:#{id} is now in your clipboard.');
-  createCookie('clipboard_data_type',document.selected_type); 
-  createCookie('clipboard_data_id',document.selected_id);
-  alert(template.evaluate({type:document.selected_type, id:document.selected_id}));
+  createCookie('clipboard_data_type',rites_document.selected_type); 
+  createCookie('clipboard_data_id',rites_document.selected_id);
+  alert(template.evaluate({type:rites_document.selected_type, id:rites_document.selected_id}));
   // replace the paste button, much harder?
   new Ajax.Updater({ 
-      onCreate: 'show_wait()',
-      onComplete: 'hide_wait()',
+      // onCreate: 'show_wait()',
+      // onComplete: 'hide_wait()',
       success: 'paste_link' 
     }, 
     'paste_link', {
       parameters: { 
         authenticity_token:AUTH_TOKEN,
         container_id: container_id,
-        clipboard_data_type: document.selected_type, 
-        clipboard_data_id:document.selected_id
+        clipboard_data_type: rites_document.selected_type, 
+        clipboard_data_id:rites_document.selected_id
       }
   });
 }
