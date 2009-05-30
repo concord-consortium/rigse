4d66efc56abc97dcef77ac11ed4cd580313c4bcd
Stephen Bannasch <stephen.bannasch@gmail.com>
2009-05-30 12:36:26 -0400

    fix syntax error in vendor_interfaces_conroller

--- app/controllers/vendor_interfaces_controller.rb
+++ app/controllers/vendor_interfaces_controller.rb
@@ -17,10 +17,10 @@ class VendorInterfacesController < ApplicationController
     if request.xhr?
       render :partial => 'vendor_interface', :locals => { :vendor_interface => @vendor_interface }
     else
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.xml  { render :xml => @vendor_interface }
+      respond_to do |format|
+        format.html # show.html.erb
+        format.xml  { render :xml => @vendor_interface }
+      end
     end
   end
 
