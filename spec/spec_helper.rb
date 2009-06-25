fc5711255f19b2476629b0d3d5301202c935fc8d
Stephen Bannasch <stephen.bannasch@gmail.com>
2009-06-25 01:33:44 -0400

    update rspec scripts from gem v1.2.7
    See:
    http://wiki.github.com/dchelimsky/rspec/configgem-for-rails
    
    also enabling shoulda-style macros for rspec with:
    
    require 'remarkable_rails'

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,12 +1,14 @@
 # This file is copied to ~/spec when you run 'ruby script/generate rspec'
 # from the project root directory.
-ENV["RAILS_ENV"] = "test"
-require File.expand_path(File.dirname(__FILE__) + "/../config/environment")
-require 'spec'
+ENV["RAILS_ENV"] ||= 'test'
+require File.dirname(__FILE__) + "/../config/environment" unless defined?(RAILS_ROOT)
+require 'spec/autorun'
 require 'spec/rails'
+require 'remarkable_rails'
 
-include AuthenticatedTestHelper
-include AuthenticatedSystem
+# Requires supporting files with custom matchers and macros, etc,
+# in ./support/ and its subdirectories.
+Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each {|f| require f}
 
 Spec::Runner.configure do |config|
   # If you're not using ActiveRecord you should remove these
@@ -45,6 +47,6 @@ Spec::Runner.configure do |config|
   # config.mock_with :rr
   #
   # == Notes
-  # 
-  # For more information take a look at Spec::Example::Configuration and Spec::Runner
+  #
+  # For more information take a look at Spec::Runner::Configuration and Spec::Runner
 end
