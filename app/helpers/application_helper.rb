f74682f07c9c29baa76de06c17c47375fe7df511
Noah Paessel <knowuh@gmail.com>
2009-06-26 13:54:19 -0400

    simplified teacher_only? and corrected a typo there.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -503,16 +503,20 @@ module ApplicationHelper
   end
   
   #
-  # is a component viewable only by teacher
+  # is a component viewable only by teacher?
+  # cascading logic.
+  # TODO: generic container-based method-forwarding mechanism
   #
   def teacher_only?(thing)
-    if (thing.respond_to?("teacher_ony?") && thing.teacher_only?)
+    if (thing.respond_to?("teacher_only?") && thing.teacher_only?)
       return true;
     end
     if (thing.respond_to?("parent"))
-      while ((thing = thing.parent()) != nil) 
-        if (thing.teacher_only?)
-          return true
+      while (thing = thing.parent)
+        if (thing.respond_to?("teacher_only?"))
+          if thing.teacher_only? 
+            return true
+          end
         end
       end
     end
