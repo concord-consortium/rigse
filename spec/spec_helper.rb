0f61a799d0677c5fe0c713694400cfda39046b88
Stephen Bannasch <stephen.bannasch@gmail.com>
2009-06-26 16:40:01 -0400

    more customizations for spec_helper.rb
    These:
    
      include AuthenticatedTestHelper
      include AuthenticatedSystem
    
    are used in the user spec tests.
    
    also make sure all the customization are bracketed by 
    comments lines so they are easy to find

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,7 +4,18 @@ ENV["RAILS_ENV"] = 'test'
 require File.dirname(__FILE__) + "/../config/environment" unless defined?(RAILS_ROOT)
 require 'spec/autorun'
 require 'spec/rails'
+
+# customizations ...
+
+# http://remarkable.rubyforge.org/
+# http://www.nomedojogo.com/2008/11/18/shoulda-for-rspec-is-remarkable/
 require 'remarkable_rails'
+
+# for testing authentication
+include AuthenticatedTestHelper
+include AuthenticatedSystem
+
+# end of customizations
 
 # Requires supporting files with custom matchers and macros, etc,
 # in ./support/ and its subdirectories.
@@ -14,7 +25,7 @@ Spec::Runner.configure do |config|
   # If you're not using ActiveRecord you should remove these
   # lines, delete config/database.yml and disable :active_record
   # in your config/boot.rb
-  config.use_transactional_fixtures = true
+  config.use_transactional_fixtures = false
   config.use_instantiated_fixtures  = false
   config.fixture_path = RAILS_ROOT + '/spec/fixtures/'
 
