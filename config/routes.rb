6b5431282fee7b968238a6f65d0e0839a79f18f7
Aaron Unger <aunger@concord.org>
2009-07-07 15:45:05 -0400

    Looks like the user and session routes didn't actually get deleted from the routes.rb.

--- config/routes.rb
+++ config/routes.rb
@@ -153,30 +153,8 @@ ActionController::Routing::Routes.draw do |map|
   }
 
   map.resources :images
- 
-  # Restful Authentication Rewrites
-  map.logout '/logout', :controller => 'sessions', :action => 'destroy'
-  map.login '/login', :controller => 'sessions', :action => 'new'
-  map.register '/register', :controller => 'users', :action => 'create'
-  map.signup '/signup', :controller => 'users', :action => 'new'
-  map.activate '/activate/:activation_code', :controller => 'users', :action => 'activate', :activation_code => nil
-  map.forgot_password '/forgot_password', :controller => 'passwords', :action => 'new'
-  map.change_password '/change_password/:reset_code', :controller => 'passwords', :action => 'reset'
-  map.open_id_complete '/opensession', :controller => "sessions", :action => "create", :requirements => { :method => :get }
-  map.open_id_create '/opencreate', :controller => "users", :action => "create", :requirements => { :method => :get }
   
   map.admin '/admin', :controller =>'admin/users'
-  
-  # Restful Authentication Resources
-  map.resources :users, :member => { 
-    :preferences => [:get, :put], 
-    :interface => :get,
-    :suspend   => :put,
-    :unsuspend => :put,
-    :purge     => :delete }
-    
-  map.resources :passwords
-  map.resource :session
   
   # Home Page
   map.home '/readme', :controller => 'home', :action => 'readme'
