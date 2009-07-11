dc2cc312768144fd4930e36eb6c78d4689a5f482
Stephen Bannasch <stephen.bannasch@gmail.com>
2009-07-10 23:47:13 -0400

    RUN rake gems:install, haml >= 2.2.0
    There are some very handy new features in haml v2.2.0.
    
    You can continue to use ruby style attributes or an
    html style that looks like this:
    
      %a(href="http://haml-lang.com" title="Haml home page")
    
    You can use #{} to interpret Ruby strings right in
    the middle of plain text:
    
      You originally logged in as: <b>#{@original_user.name}</b>.
    
    see the rest of the changelog here:
    
      http://haml-lang.com/docs/yardoc/HAML_CHANGELOG.md.html

--- config/environment.rb
+++ config/environment.rb
@@ -34,7 +34,7 @@ Rails::Initializer.run do |config|
   config.gem "capistrano-ext", :lib => "capistrano"
   config.gem 'rubyist-aasm', :lib => 'aasm', :source => 'http://gems.github.com', :version => '>=2.0.2'
   config.gem 'mislav-will_paginate', :version => '2.3.6', :lib => 'will_paginate', :source => 'http://gems.github.com'
-  # config.gem "haml", :version => '>= 2.0.7'  
+  config.gem "haml", :version => '>= 2.2.0'  
   config.gem "RedCloth", :version => '>= 4.1.1'
   config.gem "uuidtools", :version => '>= 2.0.0'
   config.gem "spreadsheet"  #see http://spreadsheet.rubyforge.org/
