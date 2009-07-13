43b776c7994fe5006285d9559e4a01c6c2ac16bf
Stephen Bannasch <stephen.bannasch@gmail.com>
2009-07-13 14:57:12 -0400

    remove 'Rites' from plugin reloading references
    This relates to code disabled in this commit:
    
     b5af71f6ad4fe6c3c38e3214f83ab0911aeb23f3
    
    in the rites_portal engine.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -17,9 +17,9 @@ config.action_controller.perform_caching             = false
 config.action_mailer.raise_delivery_errors = false
 
 # see http://nhw.pl/wp/2009/01/07/reloading-your-plugin-in-development-mode
-ActiveSupport::Dependencies.explicitly_unloadable_constants << 'RitesPortal'
+ActiveSupport::Dependencies.explicitly_unloadable_constants << 'Portal'
 ActiveSupport::Dependencies.explicitly_unloadable_constants << 'User'
-ActiveSupport::Dependencies.load_once_paths.delete_if {|p| p =~ /vendor\/plugins\/rites_portal/ }
+ActiveSupport::Dependencies.load_once_paths.delete_if {|p| p =~ /vendor\/plugins\/portal/ }
   
 # textmate-footnotes
 # Rails errors displayed in browser are generated with links to textmate
