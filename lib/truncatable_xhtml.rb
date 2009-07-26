dd5f2c09cfa2494c292fbde27e4e22ce18f62721
Stephen Bannasch <stephen.bannasch@gmail.com>
2009-07-26 15:16:16 -0400

    fix extract_first_text method in TruncatableXhtml
    Refactored TruncatableXhtml methods to properly
    extract first text fragment from more kinds of
    xhtml strings.
    
    RAILS_ENV=production rake rigse:convert:copy_truncated_xhtml_into_name

--- lib/truncatable_xhtml.rb
+++ lib/truncatable_xhtml.rb
@@ -3,17 +3,43 @@ module TruncatableXhtml
   # Extracts and soft truncates text from an xhtml document stored
   # in the an attribute for models this module is included in.
   #
-  def truncate_from_xhtml(attribute, limit=24, soft_limit=8)
+  def truncate_from_xhtml(xhtml, limit=24, soft_limit=8, logging=false)
+    return '' unless xhtml && !xhtml.empty?
+    print "#{self.id}: " if logging
+    extracted_text = extract_first_text(xhtml)
+    truncated_text = soft_truncate(extracted_text, limit, soft_limit)
+    puts "#{truncated_text}" if logging
+    truncated_text
+  end
+  #
+  # Finds the first text element that has non-whitespace
+  # using a depth first search of the xhtml tree.
+  #
+  def extract_first_text(xhtml)
+    texts = extract_text_from_elements(xhtml)
+    texts.empty? ? '' : texts[0]
+  end
+  #
+  # Extract an array of the text content of elements with text.
+  # return and empty array if:
+  #
+  #   the xhtml string can't be parsed
+  #   there are no text elements with non-whitespace characters 
+  #
+  def extract_text_from_elements(xhtml)
+    return '' if xhtml.empty?
     begin
-      child = Hpricot.XML(attribute).children.first
-      while child.kind_of? Hpricot::Elem
-        child = child.children.first
+      doc = Hpricot.XML(xhtml)
+      text_contents = []
+      doc.children.each do |child| 
+        child.traverse_text do |text_element| 
+          t = text_element.to_s.strip
+          text_contents << t unless t.empty?
+        end
       end
-      extracted_text = child.to_s.gsub(/\s*\n/, ' ')
-      soft_truncate(extracted_text, limit, soft_limit)
     rescue ArgumentError
-      ''
     end
+    text_contents
   end
   #
   # Truncates a string to length characters with an additional 
@@ -23,16 +49,18 @@ module TruncatableXhtml
   # total length of (length + soft_limit) in an attempt to
   # complete the last word.
   #
+  # "Will someone else try to match the graph?"
+  #
   def soft_truncate(string, length, soft_limit=nil)
     return '' unless string
     string.strip!
     return string if string.length <= length
     firstpart = string[0..(length-1)]
     afterpart = string[length..-1]
-    starts_with_whitespace = afterpart[/^\s/]
+    does_not_start_with_whitespace = afterpart[/^\S/]
     afterpart.strip!
     return firstpart if afterpart.empty?
-    return firstpart << ' ...' unless soft_limit || starts_with_whitespace
+    return firstpart << ' ...' unless soft_limit && does_not_start_with_whitespace
     afterword = afterpart[/(\S+)/][0..(soft_limit-1)]
     afterword << ' ...' if afterpart.length > afterword.length
     firstpart << afterword
