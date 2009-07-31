6440c94a276b8499c9e7300c4b3b080b5dfed959
Stephen Bannasch <stephen.bannasch@gmail.com>
2009-07-30 21:58:58 -0400

    remove recent MIGRATION -- attr was already in schema
    I had added the enable_default_users attribute to
    the migration after developing the initial form and
    forgot to run my copy down and then back up.
    
    I looked in the running db schema to see if the attr
    existed. When it wasn't there I created the new 
    migration without checkin more closely.

--- db/migrate/20090731014058_add_default_users_enabled_to_admin_project.rb
+++
@@ -1,9 +0,0 @@
-class AddDefaultUsersEnabledToAdminProject < ActiveRecord::Migration
-  def self.up
-    add_column :admin_projects, :enable_default_users, :boolean
-  end
-
-  def self.down
-    remove_column :admin_projects, :enable_default_users
-  end
-end
