419df19d682d70b27eb4deb33938213d25f611da
Stephen Bannasch <stephen.bannasch@gmail.com>
2009-08-03 02:47:33 -0400

    remove the migrations in main app that ran portal engine migrations

--- db/migrate/20090704075811_portal_to_version_20090704075501.rb
+++
@@ -1,9 +0,0 @@
-class PortalToVersion20090704075501 < ActiveRecord::Migration
-  def self.up
-    Engines.plugins["portal"].migrate(20090704075501)
-  end
-
-  def self.down
-    Engines.plugins["portal"].migrate(20090701153613)
-  end
-end
--- db/migrate/20090801065519_portal_to_version_20090801065156.rb
+++
@@ -1,9 +0,0 @@
-class PortalToVersion20090801065156 < ActiveRecord::Migration
-  def self.up
-    Engines.plugins["portal"].migrate(20090801065156)
-  end
-
-  def self.down
-    Engines.plugins["portal"].migrate(20090722180312)
-  end
-end
