00fc8f4721c09b5bd26e129927128531d8c9e30c
Stephen Bannasch <stephen.bannasch@gmail.com>
2009-08-03 04:19:23 -0400

    custom inflector for clazzes: routes generate correctly

--- config/initializers/inflections.rb
+++ config/initializers/inflections.rb
@@ -8,3 +8,6 @@
 #   inflect.irregular 'person', 'people'
 #   inflect.uncountable %w( fish sheep )
 # end
+ActiveSupport::Inflector.inflections do |inflect|
+    inflect.irregular 'clazz', 'clazzes'
+end
