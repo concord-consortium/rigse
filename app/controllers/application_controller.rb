f3cee9adbccfc3c6b2dbfe0ff04214455220748b
Stephen Bannasch <stephen.bannasch@gmail.com>
2009-08-03 02:42:04 -0400

    add the code from the portal's application _controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,7 +12,17 @@ class ApplicationController < ActionController::Base
   
   before_filter :setup_container
   before_filter :setup_project
+
+  include AuthenticatedSystem
+  include RoleRequirementSystem
   
+  helper :all # include all helpers, all the time
+  filter_parameter_logging :password, :password_confirmation
+    
+  before_filter :check_user
+  before_filter :original_user
+  before_filter :portal_resources
+
   protected
   
   def setup_container
@@ -40,6 +50,39 @@ class ApplicationController < ActionController::Base
       end
     rescue ActiveRecord::RecordNotFound
     end
+  end
+
+  private
+
+  # setup the portal_teacher and student instance variables
+  def portal_resources
+    @portal_teacher = current_user.portal_teacher
+    @portal_student = current_user.portal_student
+  end
+  
+  # Accesses the user that this session originally logged in as.
+  def original_user
+    if session[:original_user_id]
+      @original_user ||=  User.find(session[:original_user_id])
+    else
+      @original_user = current_user
+    end
+  end
+
+
+  def check_user
+    if logged_in?
+      self.current_user = current_user
+    else
+      self.current_user = User.anonymous 
+    end
+  end
+
+
+  def redirect_back_or(path)
+    redirect_to :back
+  rescue ActionController::RedirectBackError
+    redirect_to path
   end
 
 end
