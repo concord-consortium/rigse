e1df8b5e2742f315ff939a632cceec75101a8ee8
Stephen Bannasch <stephen.bannasch@gmail.com>
2009-08-03 02:46:47 -0400

    remove initial load of engine, remove ref to Myswl::Error Rails 2.3.3 is much more strict about autoloaded constants

--- config/environment.rb
+++ config/environment.rb
@@ -9,7 +9,6 @@ RAILS_GEM_VERSION = '2.3.2' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
-require File.join(File.dirname(__FILE__), '../vendor/plugins/engines/boot.rb')
 
 Rails::Initializer.run do |config|
   # Settings in config/environments/* take precedence over those specified here.
@@ -125,7 +124,8 @@ begin
       User.suspend_default_users
     end
   end
-rescue Mysql::Error => e
+rescue StandardError => e
+# rescue Mysql::Error => e
   puts "e"
 end
 
