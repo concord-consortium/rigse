6bc50f2fe5248d6bcea9f7312c1d581a17dbd688
Stephen Bannasch <stephen.bannasch@gmail.com>
2009-08-03 04:33:47 -0400

    MIGRATION: remove table: portal_sds_configs

---
+++ db/migrate/20090803082306_remove_sds_configs.rb
@@ -0,0 +1,16 @@
+class RemoveSdsConfigs < ActiveRecord::Migration
+  def self.up
+    drop_table :portal_sds_configs
+  end
+
+  def self.down
+    create_table :portal_sds_configs do |t|
+      t.integer :configurable_id
+      t.string  :configurable_type
+      
+      t.integer :sds_id
+
+      t.timestamps
+    end
+  end
+end
