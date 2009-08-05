cc81643ae8cceb609b97f11de53926e0e8fd20d5
Stephen Bannasch <stephen.bannasch@gmail.com>
2009-08-05 01:25:58 -0400

    metal apps must return 404 to continue filter chain
    Also close database connections.
    
    See: 
    http://blog.codefront.net/2009/06/15/activerecord-rails-metal-too-many-connections/

--- app/metal/bundle_content.rb
+++ app/metal/bundle_content.rb
@@ -9,17 +9,19 @@ class BundleContent
   POST_BODY = 'rack.input'.freeze
   
   def self.call(env)
-    bundle_logger_id = env["PATH_INFO"][/\/dataservice\/bundle_loggers\/(\d+)\/bundle_contents\.bundle/, 1]
-    if bundle_logger_id
-      bundle_logger = Dataservice::BundleLogger.find(bundle_logger_id)
-      if bundle_logger
+    if bundle_logger_id = env["PATH_INFO"][/\/dataservice\/bundle_loggers\/(\d+)\/bundle_contents\.bundle/, 1]
+      if bundle_logger = Dataservice::BundleLogger.find(bundle_logger_id)
         bundle_content = bundle_logger.bundle_contents.create(:body => env[POST_BODY].read)
       end
     end
     if bundle_content
-      [200, {"Content-Type" => "text/xml"}, ['Created']]
+      [201, {}, []]
     else
-      [400, {"Content-Type" => "text/xml"}, ["Bad Request"]]
+      [404, { 'Content-Type' => 'text/html' }, ['Not Found']]
     end
+  ensure
+    # Release the connections back to the pool.
+    # see: http://blog.codefront.net/2009/06/15/activerecord-rails-metal-too-many-connections/
+    ActiveRecord::Base.clear_active_connections!      
   end
 end
