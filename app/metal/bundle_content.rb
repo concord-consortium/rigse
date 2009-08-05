603da82e8a40b882268342c19efd02f25826a55d
Stephen Bannasch <stephen.bannasch@gmail.com>
2009-08-05 11:43:08 -0400

    add Content-MD5 digest to bundle_content response

--- app/metal/bundle_content.rb
+++ app/metal/bundle_content.rb
@@ -11,17 +11,25 @@ class BundleContent
   def self.call(env)
     if bundle_logger_id = env["PATH_INFO"][/\/dataservice\/bundle_loggers\/(\d+)\/bundle_contents\.bundle/, 1]
       if bundle_logger = Dataservice::BundleLogger.find(bundle_logger_id)
-        bundle_content = bundle_logger.bundle_contents.create(:body => env[POST_BODY].read)
+        if bundle_content = bundle_logger.bundle_contents.create(:body => env[POST_BODY].read)
+          digest = Digest::MD5.hexdigest(bundle_content.body)
+        end
       end
     end
     if bundle_content
-      [201, {'Content-Type' => 'text/xml', 'Last-Modified' => bundle_content.created_at.httpdate, 'Content-Length' => '0' }, []]
+      [201, 
+        { 'Content-Type' => 'text/xml', 
+          'Last-Modified' => bundle_content.created_at.httpdate, 
+          'Content-Length' => '0',
+          'Content-MD5' => digest },
+        []
+      ]
     else
       [404, { 'Content-Type' => 'text/html' }, ['Not Found']]
     end
   ensure
-    # Release the connections back to the pool.
+    # If we accessed ActiveRecord then release the connections back to the pool. 
     # see: http://blog.codefront.net/2009/06/15/activerecord-rails-metal-too-many-connections/
-    ActiveRecord::Base.clear_active_connections!      
+    ActiveRecord::Base.clear_active_connections! if bundle_logger_id
   end
 end
