e5b59861c10420a14d5f7856d6822e60ccef8d67
Stephen Bannasch <stephen.bannasch@gmail.com>
2009-08-05 12:31:41 -0400

    remove has_many :latest ... associations from bundle and console models

--- app/models/dataservice/bundle_logger.rb
+++ app/models/dataservice/bundle_logger.rb
@@ -3,7 +3,6 @@ class Dataservice::BundleLogger < ActiveRecord::Base
   
   has_one  :learner, :class_name => "Portal::Learner"
   has_many :bundle_contents, :class_name => "Dataservice::BundleContent", :order => :position, :dependent => :destroy
-  has_many :latest_bundle_content, :class_name => "Dataservice::BundleContent", :order => :position, :dependent => :destroy, :limit => 1
 
   OPEN_ELEMENT_EPORTFOLIO = "<sailuserdata:EPortfolio xmi:version=\"2.0\" xmlns:xmi=\"http://www.omg.org/XMI\" xmlns:sailuserdata=\"sailuserdata\">\n"
   CLOSE_ELEMENT_EPORTFOLIO = "\n</sailuserdata:EPortfolio>"
