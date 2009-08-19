36b0c2a51629d237ce13e801012e20ff463124e9
Stephen Bannasch <stephen.bannasch@gmail.com>
2009-08-19 05:04:38 -0400

    special case when migration has not been run

--- app/models/admin/project.rb
+++ app/models/admin/project.rb
@@ -126,13 +126,15 @@ class Admin::Project < ActiveRecord::Base
       project.snapshot_enabled = snapshot_enabled
       project.save!
       active_grades = APP_CONFIG[:active_grades]
-      Portal::Grade.find(:all).each do |grade|
-        if (active_grades & [grade.name.to_i]).empty?
-          grade.active = false
-        else
-          grade.active = true
+      if ActiveRecord::Base.connection.table_exists?('portal_grades')
+        Portal::Grade.find(:all).each do |grade|
+          if (active_grades & [grade.name.to_i]).empty?
+            grade.active = false
+          else
+            grade.active = true
+          end
+          grade.save!
         end
-        grade.save!
       end
       project
     end
