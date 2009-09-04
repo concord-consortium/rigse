bf340f8f0812aa4b44155eda8955263b955bb36c
Noah Paessel <knowuh@gmail.com>
2009-09-04 15:56:35 -0400

    Fixes related to making a more prominent link for OSX JWS users: Mac OSX fix should NOT just be a popup...where can it BE FOUND AGAIN?!? see: http://screencast.com/t/BEFlgLggveIL

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -60,54 +60,54 @@ focus_first_field = function() {
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
 dropdown_for = function(menu_dom_id,drop_down_dom_id) {
-    var menu = $(menu_dom_id);
-    var drop_down = $(drop_down_dom_id);
-    var menu_width = menu.getDimensions().width
-    var drop_down_width = drop_down.getDimensions().width
+  var menu = $(menu_dom_id);
+  var drop_down = $(drop_down_dom_id);
+  var menu_width = menu.getDimensions().width
+  var drop_down_width = drop_down.getDimensions().width
 
-    drop_down.hide();
-    drop_down.show();
-    drop_down.setStyle({
-      'z-index': 2000
-    });
-    
-    if (drop_down_width < menu_width) {
-      drop_down.setStyle({
-        width: menu_width+"px"
-      }); 
-      drop_down_width = menu_width;
-    }
-    
-    var left_offset = (drop_down_width - menu_width) / -2
-    //var left_offset = 0;
-    var top_offset = menu.getDimensions().height
-    var options = { setWidth: false, setHeight: false, offsetLeft:left_offset, offsetTop: top_offset};
-
-    drop_down.clonePosition(menu,options);
-    
-    menu.observe('mouseout',function(event) {
-      var mouse_over_element = event.relatedTarget;
-      if(mouse_over_element && mouse_over_element != drop_down) {
-        hide();
-      }
-    });
-    
-    drop_down.observe('mouseout', function(event) {
-      var mouse_over_element = event.relatedTarget;
-      if(mouse_over_element && (!mouse_over_element.descendantOf(drop_down) || event.toElement == menu)) {
-        hide()
-      }
-    });
+  drop_down.hide();
+  drop_down.show();
+  drop_down.setStyle({
+    'z-index': 2000
+  });
   
-    drop_down.observe('click', function(event) {
-      hide();
-    });
-    
-    function hide() {
-      drop_down.setStyle({left: "-1000px"});
-      drop_down.stopObserving();
-    }
+  if (drop_down_width < menu_width) {
+    drop_down.setStyle({
+      width: menu_width+"px"
+    }); 
+    drop_down_width = menu_width;
   }
+  
+  var left_offset = (drop_down_width - menu_width) / -2
+  //var left_offset = 0;
+  var top_offset = menu.getDimensions().height
+  var options = { setWidth: false, setHeight: false, offsetLeft:left_offset, offsetTop: top_offset};
+
+  drop_down.clonePosition(menu,options);
+  
+  menu.observe('mouseout',function(event) {
+    var mouse_over_element = event.relatedTarget;
+    if(mouse_over_element && mouse_over_element != drop_down) {
+      hide();
+    }
+  });
+  
+  drop_down.observe('mouseout', function(event) {
+    var mouse_over_element = event.relatedTarget;
+    if(mouse_over_element && (!mouse_over_element.descendantOf(drop_down) || event.toElement == menu)) {
+      hide()
+    }
+  });
+
+  drop_down.observe('click', function(event) {
+    hide();
+  });
+  
+  function hide() {
+    drop_down.setStyle({left: "-1000px"});
+    drop_down.stopObserving();
+  };
+};
 
 
 show_alert = function(elem, force) {
@@ -119,6 +119,21 @@ show_alert = function(elem, force) {
       new Effect.Opacity('wrapper', { from: 0.25, to: 1.0, duration: 0.25 });
       elem.stopObserving();
     });
-  }
-}
+  };
+};
+
+show_mac_content = function(element_id) {
+  var el_id = element_id || 'macintosh_content';
+  if (navigator.appVersion.indexOf("Mac")!=-1) {
+    if ($(el_id)) {
+      $(el_id).show();
+    }
+  };
+};
+  
+document.observe("dom:loaded", function() {
+  show_mac_content();
+});
+
+
 
