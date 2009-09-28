fd91429cf075e49438e071a0f8ecea03f0a18436
Stephen Bannasch <stephen.bannasch@gmail.com>
2009-09-28 16:40:17 -0400

    new technique to skip observers when creating app

--- config/initializers/observers.rb
+++ config/initializers/observers.rb
@@ -1,6 +1,21 @@
-# Don't startup observers if we are running db:migrate -- they'll throw 
-# an error accessing the models when running db:migrate from scratch
-unless ( File.basename($0) == "rake" && ARGV.include?("db:migrate") ) 
+# Don't startup observers if we are building the app for the first time because the
+# tables haven't been created yet. The classes being observed will throw 
+# errors accessing the models when running db:migrate from scratch
+#
+# This is one way to test to see if we got here running rake db:migrate:
+#
+#   unless ( File.basename($0) == "rake" && ARGV.include?("db:migrate") ) 
+# 
+# but it won't help when running a task like this the first time:
+#
+#   rake rigse:setup:new_rites_app
+#
+# So I'm using this method to find out if the standard "ActiveRecord::Base" 
+# connection in the connection_pool is working:
+#
+#   ActiveRecord::Base.connection_handler.connection_pools["ActiveRecord::Base"].connected?
+#
+if ActiveRecord::Base.connection_handler.connection_pools["ActiveRecord::Base"].connected?
   ActiveRecord::Base.observers = :user_observer, :investigation_observer
   ActiveRecord::Base.instantiate_observers
 end
