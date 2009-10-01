b9cf52d858c98175d3db44d746240fb9ee270649
Noah Paessel <knowuh@gmail.com>
2009-10-01 18:18:53 -0400

    First stab at adding the sakai callback.

--- app/controllers/sakai_link_controller.rb
+++ app/controllers/sakai_link_controller.rb
@@ -1,9 +1,16 @@
 class SakaiLinkController < ApplicationController
+  require 'soap/wsdlDriver'
+  WSDL = 'http://moleman.concord.org/sakai-axis/SakaiSigning.jws?wsdl'
+  DRIVER = SOAP::WSDLDriverFactory.new(WSDL).create_rpc_driver
 
   def index
     @site = params[:site]
     @username = params[:username]
     @params = params
+    @query_string = request.query_string
+    @response = DRIVER.testsign(@query_string)
+    @params[:query_string] = @query_string
+    @params[:response] = @response
     # render views/sakai_link.html.haml
   end
 end
