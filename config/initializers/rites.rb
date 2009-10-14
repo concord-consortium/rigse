e6db25ae5a921a47c25bbd2f78b4bd769f2c37ca
Noah Paessel <knowuh@gmail.com>
2009-10-14 16:39:32 -0400

    Error message clarifying that factory girl should be creating the required resources.

--- config/initializers/rites.rb
+++ config/initializers/rites.rb
@@ -31,9 +31,9 @@
 #   => [Mysql::Error, StandardError, ...
 #
 
-if ENV['RAILS_ENV']== 'test'
-  puts "Test environmnent detected, using factory girl resources"
-else
+# if ENV['RAILS_ENV']== 'test'
+#   puts "Test environmnent detected, using factory girl resources"
+# else
   begin
     ActiveRecord::Base.connection_handler.connection_pools["ActiveRecord::Base"].connection
     puts "running Admin::Project.create_or_update_default_project_from_settings_yml"
@@ -42,5 +42,6 @@ else
     puts e
     puts "the database or some required models in the database don't exist ... run migrations, load a database schema, or create resources"
     puts "not running Admin::Project.create_or_update_default_project_from_settings_yml"
+    puts "(In a test environment factory girl should create these resources for you)"
   end
-end
+# end
