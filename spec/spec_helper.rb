063b6ac449b7c8712356b1c2f41dbf129cc6f0ca
Stephen Bannasch <stephen.bannasch@gmail.com>
2009-10-14 20:25:05 -0400

    updated default spec and cucumber files
    removed script/spec_server
    
    see:
    
    Date: Tue, 23 Jun 2009 19:15:17 -0500From: David Chelimsky <dchelimsky@gmail.com>To: rspec-users <rspec-users@rubyforge.org>Subject: [rspec-users] spec_server is dead! Long live spork!
    
    Just a heads up that the rspec-rails-1.2.7.1 release deprecatesspec_server in favor of Tim Harper's spork gem.  http://github.com/timcharper/sporkI've already removed spec_server from the rspec-rails git repo, so itwill no longer be part of the rspec-rails gem beginning with the nextrelease.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,7 @@
 # This file is copied to ~/spec when you run 'ruby script/generate rspec'
 # from the project root directory.
-ENV["RAILS_ENV"] = 'test'
-require File.dirname(__FILE__) + "/../config/environment" unless defined?(RAILS_ROOT)
+ENV["RAILS_ENV"] ||= 'test'
+require File.expand_path(File.join(File.dirname(__FILE__),'..','config','environment'))
 require 'spec/autorun'
 require 'spec/rails'
 
@@ -17,9 +17,12 @@ include AuthenticatedSystem
 
 # end of customizations
 
+# Uncomment the next line to use webrat's matchers
+#require 'webrat/integrations/rspec-rails'
+
 # Requires supporting files with custom matchers and macros, etc,
 # in ./support/ and its subdirectories.
-Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each {|f| require f}
+Dir[File.expand_path(File.join(File.dirname(__FILE__),'support','**','*.rb'))].each {|f| require f}
 
 Spec::Runner.configure do |config|
   # If you're not using ActiveRecord you should remove these
