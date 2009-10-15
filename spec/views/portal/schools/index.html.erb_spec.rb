bea2446c8b91eeaecc24841293ddb127f1a4bd18
Stephen Bannasch <stephen.bannasch@gmail.com>
2009-10-14 20:32:08 -0400

    remove spec test that reference SchoolsHelper
    SchoolsHelper doesn't exist

--- spec/views/portal/schools/index.html.erb_spec.rb
+++ spec/views/portal/schools/index.html.erb_spec.rb
@@ -1,7 +1,7 @@
 require File.expand_path(File.dirname(__FILE__) + '/../../../spec_helper')
 
 describe "/portal_schools/index.html.erb" do
-  include Portal::SchoolsHelper
+  # include Portal::SchoolsHelper
 
   before(:each) do
     assigns[:portal_schools] = [
