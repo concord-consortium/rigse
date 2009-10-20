928c2dec7a6e7bb8ba94e22b2bf8f7ce9102c0eb
Noah Paessel <knowuh@gmail.com>
2009-10-20 17:56:35 -0400

    Continuing to flesh out the details of the import process.

--- lib/rinet_data.rb
+++ lib/rinet_data.rb
@@ -4,10 +4,34 @@ require 'arrayfields'
 class RinetData
   include RinetCsvFields  # definitions for the fields we use when parsing.
     
-  @@districts = %w{07 16}
+  # @@districts = %w{07 16}
+  @@districts = %w{07}
   @@csv_files = %w{students staff courses enrollments staff_assignments staff_sakai student_sakai}
   @@local_dir = "#{RAILS_ROOT}/rinet_data/districts/csv"
 
+
+  @@csv_files.each do |csv_file|
+    if csv_file =~/_sakai/
+      ## 
+      ## Create a Chaching Hash Mapfor sakai login info
+      ## for the *_sakai csv files
+      ##
+      eval <<-END_EVAL
+        def #{csv_file}_map
+          if @#{csv_file}_map
+            return @#{csv_file}_map
+          end
+          @#{csv_file}_map = {}
+          # hash_it
+          @parsed_data[:#{csv_file}].each do |auth_tokens|
+            @#{csv_file}_map[auth_tokens[0]] = auth_tokens[1]
+          end
+          return @#{csv_file}_map
+        end
+      END_EVAL
+    end
+  end
+  
   def initialize
     @rinet_data_config = YAML.load_file("#{RAILS_ROOT}/config/rinet_data.yml")[RAILS_ENV].symbolize_keys
   end
@@ -33,35 +57,157 @@ class RinetData
     puts
   end
 
-  def parse_csv_files(date_time_key='current')
+  def parse_csv_files(date_time_key='test')
     if @parsed_data
-      @parsed_data
+      @parsed_data # cached data.
     else
       @parsed_data = {}
       @@districts.each do |district|
-        puts
-        local_dir_path = "#{@@local_dir}/#{district}/#{date_time_key}"
-        if File.exists?(local_dir_path)
-          parsed_csvs = {}
-          @@csv_files.each do |csv_file|
-            local_path = "#{local_dir_path}/#{csv_file}.csv"
-            fields = FIELD_DEFINITIONS["#{csv_file}_fields".to_sym]
-            parsed_csvs[csv_file] = []
-            FasterCSV.foreach(local_path) do |row|
-              row.fields = fields
-              parsed_csvs[csv_file] << row
-            end
-          end
-          @parsed_data[district] = parsed_csvs
-        else
-          puts "no data folder found: #{local_dir_path}"
-        end
+        _parse_csv_files("#{@@local_dir}/#{district}/#{date_time_key}",@parsed_data)
       end
     end
     # Data is now available in this format
     # @data['07']['staff'][0][:EmailAddress]
+    # lets add login info
+    # join_students_sakai
+    #  join_staff_sakai
     @parsed_data
   end
+
+  def _parse_csv_files(local_dir_path,existing_data={})
+    @parsed_data = existing_data
+    puts "working on #{local_dir_path}"
+    if File.exists?(local_dir_path)
+      
+      @@csv_files.each do |csv_file|
+        local_path = "#{local_dir_path}/#{csv_file}.csv"
+        fields = FIELD_DEFINITIONS["#{csv_file}_fields".to_sym]
+        @parsed_data[csv_file.to_sym] = []
+        FasterCSV.foreach(local_path) do |row|
+          row.fields = fields
+          @parsed_data[csv_file.to_sym] << row
+        end
+      end
+    else
+      puts "no data folder found: #{local_dir_path}"
+    end
+  end
+  
+  def join_students_sakai 
+    @parsed_data[:students].each do |student|
+      begin
+        student[:login] = student_sakai_map[student[:SASID]]
+      rescue
+        puts "couldn't map student #{student[:Firstname]} #{student[:Lastname]}"
+      end
+    end
+  end
+  
+  def join_staff_sakai
+    @parsed_data[:staff].each do |staff_member|
+      begin
+        staff_member[:login] = staff_sakai_map[staff_member[:TeacherCertNum]]
+      rescue 
+        puts "couldn't map staff #{staff_member[:Firstname]} #{staff_member[:Lastname]}"
+        puts "ERROR WAS: #{$!}"
+      end
+    end
+  end
+  
+  def school_for(row)
+    school = Portal::Nces06School.find(:conditions => {:SEASCH => row[:SchoolNumber]});
+    unless school
+      puts "could not find school for: #{row[:SchoolNumber]}"
+      # TODO, create one with a special name? Throw exception?
+    end
+    school
+  end
+  
+  def district_for(row)
+    district = Portal::Nces06District.find(:conditions => {:STID => row[:District]});
+    unless district
+      puts "could not find distrcit for: #{row[:District]}"
+      # TODO, create one with a special name? Throw exception?
+    end
+    district
+  end
+  
+  def create_or_update_user(row)
+    # try to cache the data here in memory:
+    unless row[:rites_user_id]
+      email = row[:EmailAddress].gsub(/\s+/,"").size > 4 ? row[:EmailAddress].gsub(/\s+/,"") : "#{User::NO_EMAIL_STRING}#{row[:login]}@fakehost.com"
+      params = {
+        :login  => row[:login],
+        :password => row[:Password] || row[:Birthdate],
+        :password_confirmation => row[:Password] || row[:Birthdate],
+        :first_name => row[:Firstname],
+        :last_name  => row[:Lastname],
+        :email => email
+      }
+      user = User.find_or_create_by_login(params)
+      user.save!
+      row[:rites_user_id]=user.id
+      user.unsuspend! if user.state == 'suspended'
+      unless user.state == 'active'
+        user.register!
+        user.activate!
+      end
+      user.roles.clear
+    end
+    user
+  end
+  
+  def create_portal_teachers(row)
+    # try and cache our data
+    unless row[:rites_teacher_id]
+      user = create_or_update_user(row)
+      teacher = Portal::Teacher.create_or_find_by_user(user)
+      teacher.save!
+      row[:rites_user_id]=teacher.id
+      # how do we find out the teacher grades?
+      # teacher.grades << grade_9
+    
+      # add the teacher to the school
+      school = school_for(row)
+      if (school)
+        school.members << teacher
+        # do we need to worry about dupes?
+        # school.members.unique!
+      end
+    end
+  end
+  
+  def create_portal_student(row)
+    unless row[:rites_student_id]
+      user = create_or_update_user(row)
+      student = user.portal_student
+      unless student
+        student = Portal::Student.create(:user => user)
+        student.save!
+        user.portal_student=student;
+      end
+      row[:rites_student_id] = student.id
+      # how do we find out the students grade?
+      # student.grade = ??
+    
+      # add the student to the school
+      school = school_for(row)
+      if (school)
+        school.members << student
+        # do we need to worry about dupes?
+        # school.members.unique!
+      end
+    end
+  end
+  
+  def create_portal_courses(row)
+  
+  end
+  
+  def create_portal_class(row)
+    
+  end
+  
   
   
 end
