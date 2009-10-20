44727d216e3635a17b0b4c811f0e706477479cf1
Noah Paessel <knowuh@gmail.com>
2009-10-20 17:55:43 -0400

    Spec Test for rinet_data importer (place-holder)

---
+++ spec/importers/rinet_data.spec.rb
@@ -0,0 +1,13 @@
+require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
+
+describe GseParser do
+  before(:all) do
+  
+  end
+
+  it "should import csv files"
+  it "should create find valid NCES districts"
+  it "should create valid portal_clazzes"
+  it "should create valid portal_schools"
+
+end
