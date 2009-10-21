14cd2d9e26176901b142a76e4977c95d47534c1f
Noah Paessel <knowuh@gmail.com>
2009-10-21 16:43:03 -0400

    Factory changes: Unique Names for NCES districts and schools

--- factories/portal_nces06_schools.rb
+++ factories/portal_nces06_schools.rb
@@ -1,4 +1,7 @@
+Factory.sequence(:nces_school_name) { |n| "factory generated nces school ##{n}" }
+  
 Factory.define :portal_nces06_school, :class => Portal::Nces06School do |f|
+  f.SCHNAM {Factory.next :nces_school_name}
   f.association :nces_district, :factory => :portal_nces06_district
 end
 
