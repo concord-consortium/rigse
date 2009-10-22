cddc2a84acb62157ba33b300e61ca6d5b8350637
Stephen Bannasch <stephen.bannasch@gmail.com>
2009-10-22 03:50:45 -0400

    add mock_project to spec_controller_helper.rb

--- spec/controllers/spec_controller_helper.rb
+++ spec/controllers/spec_controller_helper.rb
@@ -1,3 +1,18 @@
 def will_paginate_params
-  {:limit=>20, :offset=>0, :include=>{}}
+  {:limit=>30, :offset=>0, :include=>{}}
 end
+
+def mock_project
+  @mock_project ||= mock_model(Admin::Project,
+    :maven_jnlp_server => mock_model( MavenJnlp::MavenJnlpServer),
+    :maven_jnlp_family => mock_model(MavenJnlp::MavenJnlpFamily,
+      :update_snapshot_jnlp_url => :jnlp_url,
+      :snapshot_jnlp_url => mock_model(MavenJnlp::VersionedJnlpUrl,
+        :versioned_jnlp => mock_model(MavenJnlp::VersionedJnlp)
+      )
+    ),
+    :jnlp_version_str => :jnlp_version_str,
+    :snapshot_enabled => :snapshot_enabled
+  )
+end
+
