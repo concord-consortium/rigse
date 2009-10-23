997a1f1098d6be24ce96498d4d41fc3a438c8b3c
Noah Paessel <knowuh@gmail.com>
2009-10-23 16:22:30 -0400

    Changes to prevent factory girl from pre-loading all our models. It delays FactoryGirls loading of the model classes it needs to introspect until after the fork of spork.
    For more of an explanation see:
    
    see http://github.com/timcharper/spork/issues/#issue/9

--- config/initializers/spork.rb
+++ config/initializers/spork.rb
@@ -1,6 +1,9 @@
 # Attempt to keep model access down when running spork.  I can't get this 
 # to work in our setup tho.  - Noah Oct. 23
 # see http://groups.google.com/group/sporkgem/browse_thread/thread/24e6342e6629d371/c37df459c6bf7fca?lnk=gst&q=has_many_polymorphs#c37df459c6bf7fca
+if defined? Spork
+  Spork.trap_class_method(Factory, :find_definitions)
+end
 # class Rails::Initializer
 #   def after_initialize_with_trap
 #     Spork.trap_class_method(HasManyPolymorphs, :autoload)
