59412565a528b553f62edb540e7888e52154a7f7
Stephen Bannasch <stephen.bannasch@gmail.com>
2009-10-23 01:18:29 -0400

    updates to spec_helper from newer rails_spec scripts

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,10 +1,11 @@
 # This file is copied to ~/spec when you run 'ruby script/generate rspec'
 # from the project root directory.
-ENV["RAILS_ENV"] = 'test'
-require File.dirname(__FILE__) + "/../config/environment" unless defined?(RAILS_ROOT)
+ENV["RAILS_ENV"] ||= 'test'
+require File.expand_path(File.join(File.dirname(__FILE__),'..','config','environment'))
 require 'spec/autorun'
 require 'spec/rails'
 
+#
 # customizations ...
 
 # http://remarkable.rubyforge.org/
@@ -15,17 +16,17 @@ require 'remarkable_rails'
 include AuthenticatedTestHelper
 include AuthenticatedSystem
 
-# end of customizations
+def generate_uuid
+  UUIDTools::UUID.timestamp_create.to_s
+end
 
-# Requires supporting files with custom matchers and macros, etc,
-# in ./support/ and its subdirectories.
-Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each {|f| require f}
+# end of customizations
 
 Spec::Runner.configure do |config|
   # If you're not using ActiveRecord you should remove these
   # lines, delete config/database.yml and disable :active_record
   # in your config/boot.rb
-  config.use_transactional_fixtures = false
+  config.use_transactional_fixtures = true
   config.use_instantiated_fixtures  = false
   config.fixture_path = RAILS_ROOT + '/spec/fixtures/'
 
