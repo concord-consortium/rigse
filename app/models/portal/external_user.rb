708de1a7436d9c0bb9ea10c98c7cd755321ab96b
Stephen Bannasch <stephen.bannasch@gmail.com>
2009-10-23 23:30:17 -0400

    fix table name for portal_external_user

--- app/models/portal/external_user.rb
+++ app/models/portal/external_user.rb
@@ -1,5 +1,5 @@
 class Portal::ExternalUser < ActiveRecord::Base
-  set_table_name :portal_external_user
+  set_table_name :portal_external_users
   
   belongs_to :external_user_domain
   belongs_to :user
