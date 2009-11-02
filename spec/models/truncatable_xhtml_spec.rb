e3e86bcfd31b2caf42f865ec805defa9191c4d0a
Noah Paessel <knowuh@gmail.com>
2009-11-02 16:05:36 -0500

    Better tests -- cleaner way to test multiple patterns.

--- spec/models/truncatable_xhtml_spec.rb
+++ spec/models/truncatable_xhtml_spec.rb
@@ -9,16 +9,14 @@ require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 describe TruncatableXhtml do
 
   before(:each) do
-    @styled_content = "<p style=\"ANYTHING\">something styled</p>"
-    @unstyled_content ="<p>something without a style= entity.</p>"
-    @xhtml_to_be_fixed = Xhtml.create({
-      :name => "xhtml",
-      :content => @styled_content
-    });
-    @xhtml_not_to_be_fixed = Xhtml.create({
-      :name => "xhtml",
-      :content => @unstyled_content
-    })
+    @replacement_examples = {
+      "<p style=\"ANYTHING\">something styled</p>" =>  "<p>something styled</p>",
+      "&nbsp;&nbsp;&nbsp;" => " "
+    }
+    @non_replacement_examples = [
+      "<p> nothing wrong with this paragraph </p>",
+      "<h1> this paragraph is fine</h1>" 
+    ]
   end
   
   describe "truncate xhtml entities" do
@@ -30,13 +28,23 @@ describe TruncatableXhtml do
   
   describe "replace xhtml entities" do 
     it "should replace unwanted entities when present" do
-      @xhtml_to_be_fixed.should respond_to(:replace_offensive_html)
-      @xhtml_to_be_fixed.replace_offensive_html.content.should_not eql @styled_content
-      @xhtml_to_be_fixed.replace_offensive_html.content.should eql "<p >something styled</p>"
+      @replacement_examples.each_pair do |original, expected|
+        xhtml = Xhtml.create({
+         :name =>"testing",
+         :content => original
+        })
+        xhtml.replace_offensive_html.content.should_not eql original
+        xhtml.replace_offensive_html.content.should eql expected
+      end
     end
     it "should leave non offending xhtml alone" do
-      @xhtml_not_to_be_fixed.should respond_to(:replace_offensive_html)
-      @xhtml_not_to_be_fixed.replace_offensive_html.content.should eql @unstyled_content
+      @non_replacement_examples.each do |content|
+        xhtml = Xhtml.create({
+         :name => "testing",
+         :content => content
+        })
+        xhtml.replace_offensive_html.content.should eql content
+      end
     end
   end
 end
