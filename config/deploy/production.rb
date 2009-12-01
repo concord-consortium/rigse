1d84a783c836ae384840ae92975e7c935bd99ea7
Stephen Bannasch <stephen.bannasch@gmail.com>
2009-12-01 15:44:18 -0500

    enable the db roles for production and itsisu_pro

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -11,7 +11,7 @@ set :branch, "production"
 
 set :domain, "bumblebeeman.concord.org"
 server domain, :app, :web
-# role :db, domain, :primary => true
+role :db, domain, :primary => true
 # DISABLE SOME OF THE DESTRUCTIVE DB TASKS
 namespace :db do
 #  desc 'Loads the production database in db/production_data.sql on the remote server'
