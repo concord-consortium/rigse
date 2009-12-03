f5a90cbcf049ea0252f12e18cde417f6766457b9
Noah Paessel <knowuh@gmail.com>
2009-12-03 17:06:32 -0500

    Fixed pending examples fir rinet_data_spec.rb
    Root cause of failures was ExternalUserDominError
    which would happen in the event of bad rinet_data.yml
    file.

--- spec/importers/rinet_data_spec.rb
+++ spec/importers/rinet_data_spec.rb
@@ -76,14 +76,13 @@ describe RinetData do
     end
     
     before(:each) do
-      #FIXME: ExternalUserDomain::ExternalUserDomainError on following
-      #@rinet_data = RinetData.new(:district_data_root_dir => @district_data_root_dir)
+      @rinet_data = RinetData.new(:district_data_root_dir => @district_data_root_dir)
     end
     
     it "should be resilient in the event that it can not connect to the sftp server"
     
     it "should report a reasonable error message in the event that it can not connect to the sftp server" do
-      pending "Broken example"
+      #pending "Broken example"
       Net::SFTP.stub(:start).and_raise(NoMethodError.new('SFTP.start failed', 'random message'))
       @rinet_data.should_receive('log_message') do |a, b|
         a.should =~ @failed_connection_log
@@ -95,7 +94,7 @@ describe RinetData do
     end
     
     it "should be resilient in the event that a local/remote directory/file does not exist" do
-      pending "Broken example"
+      #pending "Broken example"
       sftp = double('mock_sftp')
       sftp.stub(:download!).and_raise(RuntimeError.new('open the test file to download: no such file'))
       proc = lambda { @rinet_data.get_csv_files_for_district('test07', sftp) }
@@ -103,13 +102,20 @@ describe RinetData do
     end
     
     it "should report an error in the event that a remote directory/file does not exist" do
-      pending "Broken example"
+      #pending "Broken example"
       sftp = double('mock_sftp')
       sftp.stub(:download!).and_raise(RuntimeError.new('open the test file to download: no such file'))
       @rinet_data.should_receive('log_message').at_least(:once) do |a, b |
         a.should =~ @no_file_log
       end
       @rinet_data.get_csv_files_for_district('test07', sftp)
+    end
+  end
+  
+  describe "exceptions that should be thrown" do
+    it "should throw MissingDistrictFolderError when trying to load non-existant district data" do
+      rd = RinetData.new
+      lambda {rd.parse_csv_files_for_district('fake')}.should raise_error RinetData::MissingDistrictFolderError
     end
   end
   
@@ -120,7 +126,7 @@ describe RinetData do
       @initial_students = Portal::Student.find(:all)
       @initial_courses = Portal::Course.find(:all)
       @initial_clazzes = Portal::Clazz.find(:all)
-      #run_importer #FIXME: ExternalUserDomain::ExternalUserDomainError
+      run_importer #FIXME: ExternalUserDomain::ExternalUserDomainError
     end
     
     #  require 'ruby-prof'
@@ -135,7 +141,6 @@ describe RinetData do
     # end
     
     it "should have parsed data" do
-      pending "Broken example"
       @rinet_data_importer.parsed_data.should_not be_nil
       %w{students staff courses enrollments staff_assignments staff_sakai student_sakai}.each do |data_file|
         @rinet_data_importer.parsed_data[data_file.to_sym].should_not be_nil
@@ -144,24 +149,20 @@ describe RinetData do
     
     describe "parsing should tolerate broken input" do
       it "should tolerate csv input with blank lines" do
-        pending "Broken example"
         @rinet_data_importer.add_csv_row(:students,"")
       end
       it "should tolerate csv input with blank fields" do
-        pending "Broken example"
         csv_student_with_blank_fields = "Garcia,Raquel, ,,,1000139715,07113,07,0,CTP,2009-09-01,0--,230664,Y,N,,10316"
         @rinet_data_importer.add_csv_row(:students,csv_student_with_blank_fields)
       end
       
       it "should tolerate csv input with missing fields" do
-        pending "Broken example"
         csv_student_with_missing_commas = "Garcia,,,,1000139715,"
         @rinet_data_importer.add_csv_row(:students,csv_student_with_missing_commas)
       end
       
       # try creating a student with a bad login
       it "should not throw an error failing validations for users" do
-        pending "Broken example"
         student_row = {
           :Firstname => "bad",
           :Lastname => "student",
@@ -181,10 +182,10 @@ describe RinetData do
         @initial_students = Portal::Student.find(:all)
         @initial_courses = Portal::Course.find(:all)
         @initial_clazzes = Portal::Clazz.find(:all)
-        #run_importe #FIXME: ExternalUserDomain::ExternalUserDomainError
+        run_importer #FIXME: ExternalUserDomain::ExternalUserDomainError
       end
       it "should log an error if an enrollment is missing a valid student" do
-        pending "Broken example"
+        #pending "Broken example"
         @logger.should_receive(:error).with(/student not found/)
         # 007 is not a real student SASID
         csv_enrollment_with_bad_student_id = "007,GYM,1,FY,07,2009-09-01,07113,0"
@@ -193,7 +194,7 @@ describe RinetData do
       end
       
       it "should log an error if an enrollment is for a non existing course" do
-        pending "Broken example" 
+        #pending "Broken example" 
         @logger.should_receive(:error).with(/course not found/)
         # SPYING_101 is not a real course:
         csv_enrollment_with_bad_course_id = "1000139715,SPYING_101,1,FY,07,2009-09-01,07113,0"
@@ -202,7 +203,7 @@ describe RinetData do
       end
       
       it "should log an error if a staff assignment is missing a teacher" do
-        pending "Broken example"
+        #pending "Broken example"
         @logger.should_receive(:error).with(/teacher .* not found/)
         # 007 is not a real teacher:
         csv_assignment_with_bad_teacher_id = "007,GYM,1,FY,07,2009-09-01,07113"
@@ -211,7 +212,7 @@ describe RinetData do
       end
       
       it "should log an error if a staff ssignment is missing course information" do
-        pending "Broken example"
+        #pending "Broken example"
         @logger.should_receive(:error).with(/course not found/)
         # SPYING_101 is not a real course:
         csv_assignment_with_bad_course_id = "48404,SPYING_101,1,FY,07,2009-09-01,07113"
@@ -230,25 +231,25 @@ describe RinetData do
       @initial_students = Portal::Student.find(:all)
       @initial_courses = Portal::Course.find(:all)
       @initial_clazzes = Portal::Clazz.find(:all)
-      #run_importer #FIXME: ExternalUserDomain::ExternalUserDomainError
+      run_importer #FIXME: ExternalUserDomain::ExternalUserDomainError
     end
     it "should create new teachers" do
-      pending "Broken example"
+      #pending "Broken example"
       Portal::Teacher.find(:all).should be_more_than(@initial_teachers)
     end
   
     it "should create new students" do
-      pending "Broken example"
+      #pending "Broken example"
       Portal::Student.find(:all).should be_more_than(@initial_students)
     end
   
     it "should create new users" do
-      pending "Broken example"
+      #pending "Broken example"
       User.find(:all).should be_more_than(@initial_users)
     end
   
     it "new teachers should be teaching at a valid NCES school" do
-      pending "Broken example"
+      #pending "Broken example"
       teachers = Portal::Teacher.find(:all) - @initial_teachers
       teachers.each do |teacher|
         teacher.should be_in_nces_school
@@ -256,7 +257,7 @@ describe RinetData do
     end
   
     it "new students should be enrolled in valid NCES school" do
-      pending "Broken example"
+      #pending "Broken example"
       students = Portal::Student.find(:all) - @initial_students
       students.each do |student|
         student.should be_in_nces_school
@@ -264,7 +265,7 @@ describe RinetData do
     end
   
     it "should create new courses" do
-      pending "Broken example"
+      #pending "Broken example"
       Portal::Course.find(:all).should be_more_than(@initial_courses)
       courses = Portal::Course.find(:all) - @initial_courses
       courses.each do |course|
@@ -273,7 +274,7 @@ describe RinetData do
     end
   
     it "should create classes with students,teachers,names, and start_times" do
-      pending "Broken example"
+      #pending "Broken example"
       current_clazzes = Portal::Clazz.find(:all)
       current_clazzes.should be_more_than(@initial_clazzes)
       current_clazzes = current_clazzes - @initial_clazzes
@@ -288,7 +289,7 @@ describe RinetData do
     end
 
     it "should not create courses without clazzes" do
-      pending "Broken example"
+      #pending "Broken example"
       courses = Portal::Course.find(:all)
       courses.each do |course| 
         course.clazzes.should_not be_empty
@@ -296,7 +297,7 @@ describe RinetData do
     end
     
     it "should not create courses without schools" do
-      pending "Broken example"
+      #pending "Broken example"
       courses = Portal::Course.find(:all)
       courses.each do |course| 
         course.school.should_not be_nil
@@ -312,45 +313,45 @@ describe RinetData do
       @initial_students = Portal::Student.find(:all)
       @initial_courses = Portal::Course.find(:all)
       @initial_clazzes = Portal::Clazz.find(:all)
-      #run_importer #FIXME: ExternalUserDomain::ExternalUserDomainError
+      run_importer #FIXME: ExternalUserDomain::ExternalUserDomainError
     end
     it "should not create duplicate courses" do
-      pending "Broken example"
+      #pending "Broken example"
       courses = Portal::Course.find(:all)
       courses.map! { |course| "#{course.school_id}-#{course.name}" }
       courses.size.should eql(courses.uniq.size)
     end
     
     it "when the same import is rerun, there should be no new students" do
-      pending "Broken example"
+      #pending "Broken example"
       current_students = Portal::Student.find(:all)
       run_importer # run the import again.
       Portal::Student.find(:all).should eql(current_students)
     end
   
     it "when the same import is rerun, there should be no new teachers" do
-      pending "Broken example"
+      #pending "Broken example"
       current_teachers = Portal::Teacher.find(:all)
       run_importer # run the import again.
       Portal::Teacher.find(:all).should eql(current_teachers)
     end
   
     it "when the same import is rerun, there should be no new classes" do
-      pending "Broken example"
+      #pending "Broken example"
       current_clazzes = Portal::Clazz.find(:all)
       run_importer # run the import again.
       Portal::Clazz.find(:all).should eql(current_clazzes)
     end
   
     it "when the same import is rerun, there should be no new courses" do
-      pending "Broken example"
+      #pending "Broken example"
       current_courses = Portal::Course.find(:all)
       run_importer # run the import again.
       Portal::Course.find(:all).should eql(current_courses)
     end
     
     it "when the same import is rerun, there should be no new users" do
-      pending "Broken example"
+      #pending "Broken example"
       current_courses = Portal::Course.find(:all)
       run_importer # run the import again.
       Portal::Course.find(:all).should eql(current_courses)
@@ -369,26 +370,26 @@ describe RinetData do
     end
     describe "district 01, and 02 contain 3 and 4 students each, with one duplicate, for a total of 6 unique students" do
       it "when students are added from the first district 3 new students are created, then 3 more are created for district 02" do
-        pending "Broken example"
+        #pending "Broken example"
         run_importer(:districts => ['01'])
         Portal::Student.find(:all).size.should eql(@initial_students.size + 3)
         run_importer(:districts => ['02'])
         Portal::Student.find(:all).size.should eql(@initial_students.size + 6)
       end
       it "when students are imported from districts [02,01] in one batch, all six new students get created at once" do
-        pending "Broken example"
+        #pending "Broken example"
         run_importer(:districts => ['02','01'])
         Portal::Student.find(:all).size.should eql(@initial_students.size + 6)
       end
       it "when students are imported from districts [01,02] in one batch, all six new students get created at once" do
-        pending "Broken example"
+        #pending "Broken example"
         run_importer(:districts => ['01','02'])
         Portal::Student.find(:all).size.should eql(@initial_students.size + 6)
       end
     end
     
     it "GYM is imported from district 01, and PHYSICS is imported from district 02. Both should be in Active Record tables." do
-      pending "Broken example"
+      #pending "Broken example"
       run_importer(:districts => ['01','02'])
       ["GYM","PHYSICS"].each do | name |
         Portal::Clazz.count(:conditions=>{:name => name}).should be 1
@@ -397,7 +398,7 @@ describe RinetData do
     end
     
     it "ART and MATH exist in both distrcits, but are unique courses" do
-      pending "Broken example"
+      #pending "Broken example"
       run_importer(:districts => ['01','02'])
       ["ART","MATH"].each do | name |
         Portal::Clazz.count(:conditions=>{:name => name}).should be 2
@@ -408,7 +409,7 @@ describe RinetData do
   
   describe "when student enrollments change in CSV, those changes *ARE* reflected in the rites portal" do
     it "when students are added to the the ART class in csv for day two of district 1, they should be added on the rites site too" do
-      pending "Broken example"
+      #pending "Broken example"
       run_importer(:districts => ['01'])
       Portal::Clazz.find_by_name("ART").students.size.should be(1)
       run_importer(:districts => ['01_day_two'])
@@ -421,30 +422,30 @@ describe RinetData do
   
   describe "check_start_date validation method works" do
     before(:each) do
-      #@rinet_data_importer = RinetData.new #FIXME: ExternalUserDomain::ExternalUserDomainError
+      @rinet_data_importer = RinetData.new #FIXME: ExternalUserDomain::ExternalUserDomainError
     end
     it "should not return nil when parsing a start_date like: '2008-08-15'" do
-      pending "Broken example"
+      #pending "Broken example"
       @rinet_data_importer.check_start_date("2008-08-15").should_not be_nil
     end
 
     it "should not return nil when parsing a start_date like: '9/1/2009'" do
-      pending "Broken example"
+      #pending "Broken example"
       @rinet_data_importer.check_start_date("9/1/2009").should_not be_nil
     end
 
     it "should return nil when parsing a start_date like: 'abc'" do
-      pending "Broken example"
+      #pending "Broken example"
       @rinet_data_importer.check_start_date("abc").should be_nil
     end
     
     it "should return nil when parsing a start_date like: ''" do
-      pending "Broken example"
+      #pending "Broken example"
       @rinet_data_importer.check_start_date("").should be_nil
     end
 
     it "should return nil when parsing a start_date like: nil" do
-      pending "Broken example"
+      #pending "Broken example"
       @rinet_data_importer.check_start_date(nil).should be_nil
     end
   end
