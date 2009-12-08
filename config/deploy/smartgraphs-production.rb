619c69e1e6457f5485d623669e99f02c2d96274a
Aaron Unger <aunger@concord.org>
2009-12-08 14:07:51 -0500

    Smargraphs: Link a static index page in production.

--- config/deploy/smartgraphs-production.rb
+++ config/deploy/smartgraphs-production.rb
@@ -13,6 +13,14 @@ set :domain, "smartgraphs.concord.org"
 server domain, :app, :web
 role :db, domain, :primary => true
 
+after 'deploy:symlink', 'copy_index'
+
+task :copy_index, :roles => :web do
+  run "cp #{deploy_to}/shared/index.html #{current_path}/public/"
+  sudo "chown root.root #{current_path}/public/index.html"
+  sudo "chmod 444 #{current_path}/public/index.html"
+end
+
 # DISABLE SOME OF THE DESTRUCTIVE DB TASKS
 namespace :db do
 #  desc 'Loads the production database in db/production_data.sql on the remote server'
