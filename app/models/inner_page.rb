199d5d85c98f15ae461e1559b9366b5caf2ed6c1
Noah Paessel <knowuh@gmail.com>
2009-12-15 14:15:33 -0500

    Innerpages duplicate method to handle complexities
    A simple deep_clone would not have worked here,
    so I had to roll my own.

--- app/models/inner_page.rb
+++ app/models/inner_page.rb
@@ -101,4 +101,27 @@ class InnerPage < ActiveRecord::Base
   end
   
   
+  #
+  # Duplicate: try and create a deep clone of this innerpage with all of its sub_pages
+  #
+  def duplicate
+    @copy = self.deep_clone :no_duplicates => true, :never_clone => [:uuid, :updated_at,:created_at]
+    @copy.static_page = self.static_page.duplicate
+    self.sub_pages.each do |page| 
+      copy_of_page = page.duplicate
+      @copy.sub_pages  << copy_of_page
+      copy_of_page.save
+    end
+    @copy.save
+    @copy
+  end
+  
+  #
+  # used to choose a different partial for printing.
+  # see application_helper.rb#render_show_partial_for
+  #
+  def print_partial_name
+    return "print"
+  end
+  
 end
