73a2ceadb37f1719e20a024bef25482c37c1fc3a
Noah Paessel <knowuh@gmail.com>
2009-12-16 18:47:23 -0500

    WIP: Initial has_settings work.
    has_settings allows models to have an array of 
    hash_like settings parameters.
    
    Probably could add some other goodness.

---
+++ vendor/plugins/has_settings/lib/has_settings.rb
@@ -0,0 +1,56 @@
+module CC
+  module HAS
+    module Settings
+  
+      def self.included(base)
+        base.extend(ClassMethods)
+      end
+  
+      module ClassMethods
+        def has_settings
+          has_many :settings, :as => :scope
+          send :include, CC::HAS::Settings::InstanceMethods
+        end
+      end
+      
+      
+      module InstanceMethods
+        def print_settings
+          settings.each { |s| puts "#{s.name}: #{s.value}"}
+        end
+        
+        def get_setting(name)
+          setting = settings.detect { |s| s.name.to_s == name.to_s}
+        end
+        
+        def get_value(name)
+          setting = get_setting(name)
+          if setting
+            return setting.value
+          end
+          return nil
+        end
+        
+        def put_setting(name,value)
+          name = name.to_s
+          setting = get_setting(name)
+          if setting
+            if setting.value != value
+              setting.value=value
+              setting.save
+            end
+          else
+            setting =  Setting.create(:name=>name.to_s,:value=>value);
+            self.settings << setting
+          end
+          setting
+        end
+        alias get get_value
+        alias put put_setting
+        
+      end #instance Methods
+    end # Module Settings
+  end # Module HAS
+end # Module CC
+
+ActiveRecord::Base.send :include, CC::HAS::Settings
---
+++ vendor/plugins/has_settings/tasks/has_settings_tasks.rake
@@ -0,0 +1,4 @@
+# desc "Explaining what the task does"
+# task :has_settings do
+#   # Task goes here
+# end
