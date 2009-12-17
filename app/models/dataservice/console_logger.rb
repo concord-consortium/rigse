b1b3096b4e1f3267162df3368116e1e3d6ff3f36
Stephen Bannasch <stephen.bannasch@gmail.com>
2009-12-17 18:49:51 -0500

    bring ConsoleLogger in-line with BundleLogger
    Update ConsoleLogger so the logs can be viewed in the
    view system. There is a great deal of identical code in
    the Bundle and Console loggers which should be refactored
    to be more dry.

--- app/models/dataservice/console_logger.rb
+++ app/models/dataservice/console_logger.rb
@@ -3,10 +3,45 @@ class Dataservice::ConsoleLogger < ActiveRecord::Base
   
   has_one  :learner, :class_name => "Portal::Learner"
   has_many :console_contents, :class_name => "Dataservice::ConsoleContent", :order => :position, :dependent => :destroy
+  has_one :last_non_empty_console_content, 
+    :class_name => "Dataservice::ConsoleContent",
+    :conditions => "empty is null and valid_xml is not null",
+    :order => 'position DESC' 
 
+  include Changeable
+
+  # pagination default
+  cattr_reader :per_page
+  @@per_page = 5
+  
+  self.extend SearchableModel
+  
+  @@searchable_attributes = %w{updated_at}
+  
   class <<self
+    def searchable_attributes
+      @@searchable_attributes
+    end
+
     def display_name
       "Dataservice::ConsoleLogger"
+    end
+  end
+
+  # for the view system ...
+  def user
+    nil
+  end
+  
+  def name
+    if learner = self.learner
+      user = learner.student.user
+      name = user.name
+      login = user.login
+      runnable_name = (learner.offering.runnable ? learner.offering.runnable.name : "invalid offering runnable")
+      "#{user.login}: (#{user.name}), #{runnable_name}, #{self.bundle_contents.count} sessions"
+    else
+      "no associated learner"
     end
   end
 
