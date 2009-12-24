c91db260b78b99ef749906859885a194d1c74744
Stephen Bannasch <stephen.bannasch@gmail.com>
2009-12-23 21:18:13 -0500

    new model JnlpTestingAdaptor
    similar to JnlpAdaptor, an @jnlp_testing_adaptor is setup
    in the application_controller.

---
+++ app/models/jnlp_testing_adaptor.rb
@@ -0,0 +1,40 @@
+class JnlpTestingAdaptor
+  
+  attr_reader :jnlp
+  
+  def initialize
+    @jnlp_family = MavenJnlp::MavenJnlpFamily.find_by_name("gui-testing")
+    @jnlp = @jnlp_family.update_snapshot_jnlp_url.versioned_jnlp
+  end
+  
+  def resource_jars
+    @jnlp.jars.collect do |jar|
+      if jar.main
+        [jar.href, jar.version_str, true]
+      else
+        [jar.href, jar.version_str]
+      end
+    end
+  end
+  
+  def linux_native_jars
+    @jnlp.native_libraries.find_all_by_os('linux').collect { |nl| [nl.href, nl.version_str] }
+  end
+
+  def macos_native_jars
+    @jnlp.native_libraries.find_all_by_os('mac_os_x').collect { |nl| [nl.href, nl.version_str] }
+  end
+  
+  def windows_native_jars
+    @jnlp.native_libraries.find_all_by_os('windows').collect { |nl| [nl.href, nl.version_str] }
+  end
+
+  def system_properties
+    jnlp_properties = @jnlp.properties.collect { |prop| [prop.name, prop.value] }
+    custom_properties = [
+     ['otrunk.view.export_image', 'true'],
+     ['otrunk.view.status', 'true']
+    ]
+    jnlp_properties + custom_properties
+  end
+end
