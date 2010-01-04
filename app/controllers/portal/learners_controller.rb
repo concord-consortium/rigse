1a80d13e09fc7383df1f1db3f0c943437de1bf16
Stephen Bannasch <stephen.bannasch@gmail.com>
2010-01-04 12:59:19 -0500

    implement Portal::Learner controller and view
    available for admin users only.

--- app/controllers/portal/learners_controller.rb
+++ app/controllers/portal/learners_controller.rb
@@ -1,90 +1,104 @@
 class Portal::LearnersController < ApplicationController
-  # GET /portal_learners
-  # GET /portal_learners.xml
+
+  before_filter :admin_only
+  
+  protected  
+
+  def admin_only
+    unless current_user.has_role?('admin') || request.format == :bundle
+      flash[:notice] = "Please log in as an administrator" 
+      redirect_to(:home)
+    end
+  end
+  
+  public
+
+  # GET /portal/learners
+  # GET /portal/learners.xml
   def index
-    @learners = Portal::Learner.all
+    @portal_learners = Portal::Learner.search(params[:search], params[:page], nil)
 
     respond_to do |format|
       format.html # index.html.erb
-      format.xml  { render :xml => @learners }
+      format.xml  { render :xml => @portal_learners }
     end
   end
 
-  # GET /portal_learners/1
-  # GET /portal_learners/1.xml
+  # GET /portal/learners/1
+  # GET /portal/learners/1.xml
   def show
-    @learner = Portal::Learner.find(params[:id])
+    @portal_learner = Portal::Learner.find(params[:id])
     
-    @learner.console_logger = Dataservice::ConsoleLogger.create! unless @learner.console_logger
-    @learner.bundle_logger = Dataservice::BundleLogger.create! unless @learner.bundle_logger
+    @portal_learner.console_logger = Dataservice::ConsoleLogger.create! unless @portal_learner.console_logger
+    @portal_learner.bundle_logger = Dataservice::BundleLogger.create! unless @portal_learner.bundle_logger
     
     respond_to do |format|
       format.html # show.html.erb
       format.config { render :partial => 'shared/learn', 
-        :locals => { :runnable => @learner.offering.runnable, 
-                     :console_logger => @learner.console_logger, 
-                     :bundle_logger => @learner.bundle_logger,
+        :locals => { :runnable => @portal_learner.offering.runnable, 
+                     :console_logger => @portal_learner.console_logger, 
+                     :bundle_logger => @portal_learner.bundle_logger,
                      :session_id => (params[:session] || request.env["rack.session.options"][:id]) } }            
       
-      format.xml  { render :xml => @learner }
+      format.xml  { render :xml => @portal_learner }
     end
   end
 
-  # GET /portal_learners/new
-  # GET /portal_learners/new.xml
+  # GET /portal/learners/new
+  # GET /portal/learners/new.xml
   def new
-    @learner = Portal::Learner.new
+    @portal_learner = Portal::Learner.new
 
     respond_to do |format|
       format.html # new.html.erb
-      format.xml  { render :xml => @learner }
+      format.xml  { render :xml => @portal_learner }
     end
   end
 
-  # GET /portal_learners/1/edit
+  # GET /portal/learners/1/edit
   def edit
-    @learner = Portal::Learner.find(params[:id])
+    @portal_learner = Portal::Learner.find(params[:id])
   end
 
-  # POST /portal_learners
-  # POST /portal_learners.xml
+  # POST /portal/learners
+  # POST /portal/learners.xml
   def create
-    @learner = Portal::Learner.new(params[:learner])
+    @portal_learner = Portal::Learner.new(params[:learner])
 
     respond_to do |format|
-      if @learner.save
+      if @portal_learner.save
         flash[:notice] = 'Portal::Learner was successfully created.'
-        format.html { redirect_to(@learner) }
-        format.xml  { render :xml => @learner, :status => :created, :location => @learner }
+        format.html { redirect_to(@portal_learner) }
+        format.xml  { render :xml => @portal_learner, :status => :created, :location => @portal_learner }
       else
         format.html { render :action => "new" }
-        format.xml  { render :xml => @learner.errors, :status => :unprocessable_entity }
+        format.xml  { render :xml => @portal_learner.errors, :status => :unprocessable_entity }
       end
     end
   end
 
-  # PUT /portal_learners/1
-  # PUT /portal_learners/1.xml
+  # PUT /portal/learners/1
+  # PUT /portal/learners/1.xml
   def update
-    @learner = Portal::Learner.find(params[:id])
+    @portal_learner = Portal::Learner.find(params[:id])
 
     respond_to do |format|
-      if @learner.update_attributes(params[:learner])
+      if @portal_learner.update_attributes(params[:learner])
         flash[:notice] = 'Portal::Learner was successfully updated.'
-        format.html { redirect_to(@learner) }
+        format.html { redirect_to(@portal_learner) }
         format.xml  { head :ok }
       else
         format.html { render :action => "edit" }
-        format.xml  { render :xml => @learner.errors, :status => :unprocessable_entity }
+        format.xml  { render :xml => @portal_learner.errors, :status => :unprocessable_entity }
       end
     end
   end
 
-  # DELETE /portal_learners/1
-  # DELETE /portal_learners/1.xml
+  # DELETE /portal/learners/1
+  # DELETE /portal/learners/1.xml
   def destroy
-    @learner = Portal::Learner.find(params[:id])
-    @learner.destroy
+    @portal_learner = Portal::Learner.find(params[:id])
+    @portal_learner.destroy
 
     respond_to do |format|
       format.html { redirect_to(portal_learners_url) }
