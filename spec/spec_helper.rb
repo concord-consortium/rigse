db907451031236892f463f25258401debd1ce4bc
Stephen Bannasch <stephen.bannasch@gmail.com>
2010-01-06 21:32:16 -0500

    remove duplicated setting of RAILS_ENV

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -11,7 +11,6 @@ Spork.prefork do
   # This file is copied to ~/spec when you run 'ruby script/generate rspec'
   # from the project root directory.
   
-  ENV["RAILS_ENV"] ||= 'test'
   require File.expand_path(File.join(File.dirname(__FILE__),'..','config','environment'))
   
   require 'spec/autorun'
