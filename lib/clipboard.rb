concord-consortium/rigse

commit 1e4808e57e03768bd373e0e1e0388d1732475900

second attempt: dry up clipboard string conversion

lib/clipboard.rb
@@ -1,30 +1,29 @@
-def clipboard_scope_char
-  '__'
-end
-def class_scope_char
-  '/'
-end
-
-class String
-
-  def clipboardify
-    self.gsub(class_scope_char, clipboard_scope_char)
-  end
-
-  def de_clipboardify
-    self.gsub(clipboard_scope_char, class_scope_char)
-  end
-
-  def clipboardify!
-    self.gsub!(class_scope_char, clipboard_scope_char)
-  end
-  
-  def de_clipboardify!
-    self.gsub!(clipboard_scope_char, class_scope_char)
-  end
-end
-
 module Clipboard
+  
+  module StringExtensions
+      def clipboard_scope_char
+        '__'
+      end
+      def class_scope_char
+        '/'
+      end
+    
+      def clipboardify
+        self.gsub(class_scope_char, clipboard_scope_char)
+      end
+
+      def de_clipboardify
+        self.gsub(clipboard_scope_char, class_scope_char)
+      end
+
+      def clipboardify!
+        self.gsub!(class_scope_char, clipboard_scope_char)
+      end
+
+      def de_clipboardify!
+        self.gsub!(clipboard_scope_char, class_scope_char)
+      end
+  end
   
   def get_clipboard_object(clipboard_data_type, clipboard_data_id)
     results = nil
@@ -83,5 +82,8 @@ module Clipboard
     end
     return "<span class='copy_paste_disabled'>cant paste #{clipboard_data_type} #{name} here</span>"
   end
-  
+end
+
+class String
+  include Clipboard::StringExtensions
 end

commit e8fdeb72e49acaa91828370b55576c2354d19c6b

commit 1e4808e5 introduced a bug.
What I am trying to do is cleaning extend string
maybe there is no super clean way.

Revert "second attempt: dry up clipboard string conversion"

This reverts commit 1e4808e57e03768bd373e0e1e0388d1732475900.

lib/clipboard.rb
@@ -1,29 +1,30 @@
-module Clipboard
-  
-  module StringExtensions
-      def clipboard_scope_char
-        '__'
-      end
-      def class_scope_char
-        '/'
-      end
-    
-      def clipboardify
-        self.gsub(class_scope_char, clipboard_scope_char)
-      end
+def clipboard_scope_char
+  '__'
+end
+def class_scope_char
+  '/'
+end
 
-      def de_clipboardify
-        self.gsub(clipboard_scope_char, class_scope_char)
-      end
+class String
 
-      def clipboardify!
-        self.gsub!(class_scope_char, clipboard_scope_char)
-      end
-
-      def de_clipboardify!
-        self.gsub!(clipboard_scope_char, class_scope_char)
-      end
+  def clipboardify
+    self.gsub(class_scope_char, clipboard_scope_char)
   end
+
+  def de_clipboardify
+    self.gsub(clipboard_scope_char, class_scope_char)
+  end
+
+  def clipboardify!
+    self.gsub!(class_scope_char, clipboard_scope_char)
+  end
+  
+  def de_clipboardify!
+    self.gsub!(clipboard_scope_char, class_scope_char)
+  end
+end
+
+module Clipboard
   
   def get_clipboard_object(clipboard_data_type, clipboard_data_id)
     results = nil
@@ -82,8 +83,5 @@ module Clipboard
     end
     return "<span class='copy_paste_disabled'>cant paste #{clipboard_data_type} #{name} here</span>"
   end
-end
-
-class String
-  include Clipboard::StringExtensions
+  
 end