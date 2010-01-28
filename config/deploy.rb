4b0fa16c74a3c7812642f858fda578f83ca315e5
Seong-June Kim <jkim707@gmail.com>
2010-01-27 20:49:43 -0500

    New cap stage: sparks-dev

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,7 @@
 set :stages, %w(
   development staging production seymour 
   itsisu-dev itsisu-staging itsisu-production fall2009 
-  smartgraphs-production smartgraphs-staging sg-dev)
+  smartgraphs-production smartgraphs-staging sg-dev sparks-dev)
 set :default_stage, "development"
 # require File.expand_path("#{File.dirname(__FILE__)}/../vendor/gems/capistrano-ext-1.2.1/lib/capistrano/ext/multistage")
 require 'capistrano/ext/multistage'
