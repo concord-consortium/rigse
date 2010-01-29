b7cf4c7ff52384271782df4056640a3d967bcab0
Noah Paessel <knowuh@gmail.com>
2010-01-29 15:57:27 -0500

    Tag defaults should include newly created tags.

--- lib/tag_defaults.rb
+++ lib/tag_defaults.rb
@@ -12,6 +12,16 @@ module TagDefaults
         :subject_areas => "Earth Science, Space Science, Life Science, Physics, Biology, Chemestry".split(",")
       }
       
+      # look through our records and update our defaults...
+      self.tag_types.each do |type|
+        if (@@default_tags[type])
+          @@default_tags[type] = @@default_tags[type] | self.tag_counts_on(type).map { |c| c.name}
+        else
+          @@default_tags[type] = self.tag_counts_on(type).map { |c| c.name}
+        end
+      end
+      
+      # class methods
       def self.add_tag(scope,tag)
         unless @@default_tags[scope]
           @@default_tags[scope] = []
@@ -22,12 +32,31 @@ module TagDefaults
 
       def self.available_tags(scope)
         if scope
-          return @@default_tags[scope].map { |i| i.strip}
+          if @@default_tags[scope] && @@default_tags[scope].size > 0
+            return @@default_tags[scope].map { |i| i.strip}
+          else
+            return []
+          end
         end
         return @@default_tags.values.flatten.uniq.map { |i| i.strip}
       end
+    
+      # after we are saved, add our tags.
+      after_save :update_available_tags
+
     }
   end
   
+  # check for new tags in this taggable thing
+  def update_available_tags
+    self.tag_types.each do |type|
+      if (@@default_tags[type])
+        @@default_tags[type] = @@default_tags[type] | self.tag_counts_on(type).map { |c| c.name}
+      else
+        @@default_tags[type] = self.tag_counts_on(type).map { |c| c.name}
+      end
+    end
+  end
+
   
 end
