1423db52931e671bd9db1a5c9de583d23a427a6a
Stephen Bannasch <stephen.bannasch@gmail.com>
2010-02-01 17:54:09 -0500

    Saveable::Sparks::MeasuringResistance models

---
+++ app/models/saveable/sparks/measuring_resistance.rb
@@ -0,0 +1,11 @@
+class Saveable::Sparks::MeasuringResistance < ActiveRecord::Base
+  set_table_name "saveable_sparks_measuring_resistance"
+
+  belongs_to :learner, :class_name => 'Portal::Learner'
+
+  has_many :reports, :order => :position, :class_name => "Saveable::Sparks::MeasuringResistanceReports"
+
+  def answer
+    self.reports.last.answer
+  end
+end
