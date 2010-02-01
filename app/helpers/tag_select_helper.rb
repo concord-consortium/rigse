217ee4695b4e6f15a8c1acd41693f983c521b59a
Noah Paessel <knowuh@gmail.com>
2010-02-01 14:48:48 -0500

    Multi-checkbox helper for tags. (has_many checkbx)

---
+++ app/helpers/tag_select_helper.rb
@@ -0,0 +1,24 @@
+module TagSelectHelper
+
+  def tag_selector(tagged_object,object_name,tag_symbol,opts={})
+    label = opts[:label] || tag_symbol.to_s.humanize
+    clazz = tagged_object.class
+    
+    result_string =<<-DONE
+    <div class="field_set">
+      <label>#{label}</label>
+      <br/>
+    DONE
+    
+    clazz.available_tags("#{tag_symbol.to_s}s".to_sym).each do |tag|
+      set = tagged_object.send "#{tag_symbol}_list".to_sym
+      result_string << check_box_tag("#{object_name}[#{tag_symbol.to_s}_list][]", tag,set.include?(tag))
+      # result_string << check_box_tag("#{object_name}[#{tag_symbol.to_s}_list][]", tag,set.include? tag)
+      result_string << "#{tag}<br/>\n"
+    end
+
+    result_string << "</div>"
+    result_string
+  end
+  
+end
