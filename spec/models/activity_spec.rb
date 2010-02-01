bf0db18c183505b2fa0a9fcdf33fbd0fe087cebf
Noah Paessel <knowuh@gmail.com>
2010-02-01 16:14:18 -0500

    initial has_pedigree model with model specs

--- spec/models/activity_spec.rb
+++ spec/models/activity_spec.rb
@@ -12,7 +12,11 @@ describe Activity do
     Activity.create!(@valid_attributes)
   end
   
-  describe "should be publishable" do
+  
+  ##
+  ## TODO: Move to publishable_spec ?
+  ##
+  describe "should be publishable and" do
     before (:each) do
       @activity = Activity.create!(@valid_attributes)
     end
@@ -33,67 +37,70 @@ describe Activity do
     end
   end
   
-  describe "should be taggable" do
+  ##
+  ## TODO: Move to taggable_spec ?
+  ##
+  describe "should be taggable and" do
     
     before (:each) do
       @activity = Activity.create!(@valid_attributes)
     end
     
-    it "should allow the tagging of grade_levels" do
+    it "allow the tagging of grade_levels" do
       @activity.grade_level_list = "1-3, 4-6, 7-9, 10-12"
       @activity.save
       @activity.reload
       @activity.grade_level_list.should include("1-3")
     end
-    it "should allow the tagging of subject_areas" do
+    it "allow the tagging of subject_areas" do
       @activity.subject_area_list = "Physics"
       @activity.save
       @activity.reload
       @activity.subject_area_list.should include("Physics")
     end
     
-    it "shulld allow the tagging of units" do
+    it "allow the tagging of units" do
       @activity.unit_list = "Heat and Temperature"
       @activity.save
       @activity.reload
       @activity.unit_list.should include("Heat and Temperature")
     end
     
-    it "should allow free-form tags" do
+    it "allow free-form tags" do
       @activity.tag_list = "xxx,yyy,zzz,dog"
       @activity.save
       @activity.reload
       @activity.tag_list.should include("dog")
     end
     
-    it "should allow searching by grade_level" do
+    it "allow searching by grade_level" do
       @activity.grade_level_list = "1-3"
       @activity.save
-      found = Activity.tagged_with("1-3", :on => :grade_level)
+      found = Activity.tagged_with("1-3", :on => :grade_levels)
       found.should have(1).activity
-      found = Activity.tagged_with("1-6", :on => :grade_level)
+      found = Activity.tagged_with("1-6", :on => :grade_levels)
       found.should have(0).activities
     end
     
-    it "should allow searching by units" do
+    it "allow searching by units" do
       @activity.unit_list = "Heat and Electricity"
       @activity.save
-      found = Activity.tagged_with("Heat and Electricity", :on => :unit)
+      found = Activity.tagged_with("Heat and Electricity", :on => :units)
       found.should have(1).activity
-      found = Activity.tagged_with("Heat and Water", :on => :unit)
+      found = Activity.tagged_with("Heat and Water", :on => :units)
       found.should have(0).activities
     end
     
-    it "should allow searching by subject_area" do
+    it "allow searching by subject_area" do
       @activity.subject_area_list = "Math"
       @activity.save
-      found = Activity.tagged_with("Math", :on => :subject_area)
+      found = Activity.tagged_with("Math", :on => :subject_areas)
       found.should have(1).activity
-      found = Activity.tagged_with("Art", :on => :subject_area)
+      found = Activity.tagged_with("Art", :on => :subject_areas)
       found.should have(0).activities
     end
     
-    it "should allow searching by tag" do
+    it "allow searching by tag" do
       @activity.tag_list = "probe"
       @activity.save
       found = Activity.tagged_with("probe")
@@ -103,30 +110,48 @@ describe Activity do
     end
   end
   
-  #
-  # Kind of testing HasPedigree and 
-  # 
-  describe "A pedigree" do
+  ##
+  ## TODO: Move to has_pedigree_spec ?
+  ##
+  describe "has a pedigrees that" do
     before (:each) do
-      @a = Activity.create!(@valid_attributes)
-      @a_a = Activity.create!(@valid_attributes)
-      @a_b= Activity.create!(@valid_attributes)
+      @a     = Factory(:activity, {:name => "A"})
+      @a_a   = Factory(:activity, {:name => "A_A"})
+      @a_b   = Factory(:activity, {:name => "A_B"})
+      @a_a_a = Factory(:activity, {:name => "A_A_A"})
       @a.descendants << @a_a
       @a.descendants << @a_b
+      @a_a.descendants << @a_a_a
       @a.reload
       @a_a.reload
       @a_b.reload
+      @a_a_a.reload
     end
     
-    it "has ancestors" do
+    it "have ancestors" do
       @a.ancestor.should be_nil
-      @a_a.ancestor.should be(@a)
-      @a_b.ancestor.should be(@b)
+      @a_a.ancestor.id.should equal(@a.id)
+      @a_b.ancestor.id.should equal(@a.id)
+      @a_a_a.ancestor.id.should equal(@a_a.id)
+      @a_a_a.ancestor.id.should_not equal(@a.id)
     end
     
+    it "have descendants" do
+      @a.descendants.should have(2).items
+      @a.descendants.should include(@a_b)
+      @a.descendants.should include(@a_a)
+      @a_a.descendants.should have(1).items
+      @a_a.descendants.should include(@a_a_a)
+      @a_a.descendants.should_not include(@a_b)
     end
-    it "has descendants" do
-      @a.descendants.should_have(2).items
-    end    
     
+    it "have a pedigree" do
+      @a_a.pedigree.should include(@a)
+      @a_b.pedigree.should include(@a)
+      @a_a_a.pedigree.should include(@a)
+      @a_a_a.pedigree.should include(@a_a)
+      @a_a_a.pedigree.should_not include(@a_b)
+    end
+  end
+  
 end
