967005bdbc86460bb09110f254a218768923b36b
Stephen Bannasch <stephen.bannasch@gmail.com>
2010-02-01 22:59:23 -0500

    simplify conditionals with NOT_USING_JNLPS constant

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -348,12 +348,12 @@ module ApplicationHelper
     if as_name
       link_text << " as #{as_name}"
     end
-    if APP_CONFIG[:runnables_use] && APP_CONFIG[:runnables_use] == 'browser'
+    if NOT_USING_JNLPS
       url = polymorphic_url(component, :format => APP_CONFIG[:runnable_mime_type], :params => params)      
     else
       url = polymorphic_url(component, :format => :jnlp, :params => params)
     end
-    if APP_CONFIG[:runnables_use] && APP_CONFIG[:runnables_use] == 'browser'
+    if NOT_USING_JNLPS
       run_button_for(component) + link_to(link_text, url)
     else
       run_button_for(component) +
