8cb18c6e7dbf9218c563a52888ff839f4ddadc40
Aaron Unger <aunger@concord.org>
2010-02-03 15:09:06 -0500

    Change the open response matcher so that it will match an empty-string answer.

--- app/models/dataservice/bundle_content.rb
+++ app/models/dataservice/bundle_content.rb
@@ -75,7 +75,7 @@ class Dataservice::BundleContent < ActiveRecord::Base
     end
   end
   
-  OR_MATCHER = /open_response_(\d+).*?<OTText text="(.+?)" \/>/m
+  OR_MATCHER = /open_response_(\d+).*?<OTText text="(.*?)" \/>/m
   
   def extract_open_responses
     learner = self.bundle_logger.learner
