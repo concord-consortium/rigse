6abf703c600394e572c11ebfedc51d398a5d9a28
Stephen Bannasch <stephen.bannasch@gmail.com>
2010-02-04 13:18:43 -0500

    cleaned up view of open_response reports

--- app/models/saveable/open_response.rb
+++ app/models/saveable/open_response.rb
@@ -10,10 +10,17 @@ class Saveable::OpenResponse < ActiveRecord::Base
   #   :class_name => "Saveable::OpenResponseAnswer",
   #   :order => 'position DESC' 
   
-  delegate :prompt, :to => :open_response, :class_name => 'Embeddable::OpenResponse'
+  [:prompt, :name].each { |m| delegate m, :to => :open_response, :class_name => 'Embeddable::OpenResponse' }
   
   def answer
-    answers = self.answers
-    answers.empty? ? '' : answers.last.answer
+    if answered?
+      answers.last.answer
+    else
+      "not answered"
+    end
+  end
+  
+  def answered?
+    answers.length > 0
   end
 end
