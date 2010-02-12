658dc6e73b201d711cccdca0e97782f13591a52b
Aaron Unger <aunger@concord.org>
2010-02-12 10:32:02 -0500

    Use the background job plugin to process bundle_contents

--- app/models/dataservice/bundle_content_observer.rb
+++ app/models/dataservice/bundle_content_observer.rb
@@ -1,6 +1,8 @@
 class Dataservice::BundleContentObserver < ActiveRecord::Observer
   def after_create(bundle_content)
-    bundle_content.extract_saveables
+    # first schedule the bundle processing
+    cmd = "nice #{RAILS_ROOT}/script/runner 'Dataservice::BundleContent.find(#{bundle_content.id}).extract_saveables'"
+    jobs = ::Bj.submit cmd, :tag => 'bundle_content_processing'
   end
 
   def after_save(user)
