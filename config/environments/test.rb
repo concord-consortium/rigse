43e0ea627344a6889c3006bf913de3482ee9095d
Aaron Unger <aunger@concord.org>
2010-03-22 16:24:08 -0400

    Add support for running tests in Hudson.

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -47,3 +47,5 @@ config.gem "autotest-rails", :lib => false,          :version => "= 4.1.0"
 # Adds new rspec matchers for models and controllers
 # as well as better support for I18n, collections, creating custom matchers 
 config.gem "remarkable_rails", :lib => false,        :version => ">= 3.1.11"
+
+config.gem "ci_reporter", :lib => false
