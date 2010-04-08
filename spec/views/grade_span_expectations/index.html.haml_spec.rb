74070efa1011d71168720f7a2de15d1db5bfea81
Noah Paessel <knowuh@gmail.com>
2010-04-08 15:06:30 -0400

    Fix for view spec test, with better mocking.
    see:
    http://www.screencast.com/t/ZGY0YWQ0
    
    Also: bad view paths were fixed.

--- spec/views/grade_span_expectations/index.html.haml_spec.rb
+++ spec/views/grade_span_expectations/index.html.haml_spec.rb
@@ -1,17 +1,28 @@
 require File.expand_path(File.dirname(__FILE__) + '/../../spec_helper')
-
 describe "/grade_span_expectations/index.html.haml" do
   
   before(:each) do
-    assigns[:grade_span_expectations] = [
-      stub_model(GradeSpanExpectation),
-      stub_model(GradeSpanExpectation)
-    ]
+    target = mock('target', :number => 1, :description => "nothing here", :knowledge_statement => nil, :unifying_themes => [])
+    domain = mock('domain')
+    canned_responses = {
+      :assessment_target => target,
+      :domain => domain,
+      :expectations => [],
+      :gse_key => "GSE_KEY",
+      :grade_span => "k-12",
+      :number => 1
+    }
+    @gses = [
+      mock_model(RiGse::GradeSpanExpectation,canned_responses),
+      mock_model(RiGse::GradeSpanExpectation,canned_responses)
+    ];
+    RiGse::GradeSpanExpectation.stub!(:paginate).and_return(@gses.paginate)
+    assigns[:grade_span_expectations] = @gses.paginate
   end
 
   it "should render list of grade_span_expectations" do
-    pending "Broken example"
-    render "/grade_span_expectations/index.html.haml"
+    render "/ri_gse/grade_span_expectations/index.html.haml"
+    # TODO: Assert something about what we just rendered!
   end
 end
 
