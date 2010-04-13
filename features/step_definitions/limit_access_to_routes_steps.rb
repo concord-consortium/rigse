a48bfc25600f4382c82a08d2532105db36f00674
Noah Paessel <knowuh@gmail.com>
2010-04-13 12:02:33 -0400

    capybara-ified these step definitions.

--- features/step_definitions/limit_access_to_routes_steps.rb
+++ features/step_definitions/limit_access_to_routes_steps.rb
@@ -3,28 +3,15 @@ Given /^I am not logged in$/ do
   # controller.current_user = User.anonymous
 end
 
-Given /^[Ii] am logged in as(\s*the\s*)?[aA]dmin(\s*user\s*)$/ do |x,y|
-  visit 'login'
-  fill_in('login', :with => 'admin')
-  fill_in('password', :with => 'password')
-  click_button('Login')
-end
-
-
-Then /^There should be a valid admin user$/ do
-  admin = User.find_by_login('admin')
-  admin.should_not be_nil
-  admin.authenticated?("password").should be true
-end
-
 # dont use webrat for these, because of sessions:
 When /^I visit the route (.+)$/ do |route|
-  get route
+  visit route
 end
 
 Then /^I should be redirected (.+)$/ do |named_route|
   response.should redirect_to(named_route)
 end
+
 Then /^I should not be redirected (.+)$/ do |named_route|
   response.should_not redirect_to(named_route)
 end
--- features/support/paths.rb
+++ features/support/paths.rb
@@ -21,12 +21,11 @@ module NavigationHelpers
       "/admin/projects/#{Admin::Project.default_project.id}/edit"
     when /the current project show page/  
       "/admin/projects/#{Admin::Project.default_project.id}/show"
-    # Add more mappings here.
-    # Here is an example that pulls values out of the Regexp:
-    #
-    #   when /^(.*)'s profile page$/i
-    #     user_profile_path(User.find_by_login($1))
 
+    # accept paths too:
+    when /\/[\S+\/]+/
+      page_name
+      
     else
       raise "Can't find mapping from \"#{page_name}\" to a path.\n" +
         "Now, go and add a mapping in #{__FILE__}"
