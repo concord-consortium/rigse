36aae4d9a701f241f991e9390590ff13e3692b94
Noah Paessel <knowuh@gmail.com>
2010-04-12 22:16:26 -0400

    email spec required for some signup stories.
    (not sure why this stopped working when capybara was installed)

--- config/environments/cucumber.rb
+++ config/environments/cucumber.rb
@@ -27,3 +27,4 @@ config.gem 'capybara',         :lib => false, :version => '>=0.3.0' unless File.
 config.gem 'rspec',            :lib => false, :version => '>=1.3.0' unless File.directory?(File.join(Rails.root, 'vendor/plugins/rspec'))
 config.gem 'rspec-rails',      :lib => false, :version => '>=1.3.2' unless File.directory?(File.join(Rails.root, 'vendor/plugins/rspec-rails'))
 config.gem "factory_girl",                    :version => "= 1.2.3"
+config.gem 'email_spec',                      :version => "= 0.3.5"
