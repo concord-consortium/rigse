d467e78d24f7a805e4ec1f77754500848b7027ba
Stephen Bannasch <stephen.bannasch@gmail.com>
2010-04-22 12:01:55 -0400

    Admin::Project factory uses data from Admin::Project
    This should now create a valid admin project
    admin_project = Factory.create(:admin_project)
    Admin::Project.create_or_update_default_project_from_settings_yml

--- factories/admin_projects.rb
+++ factories/admin_projects.rb
@@ -1,11 +1,13 @@
 Factory.define :admin_project, :class => Admin::Project do |f|
+  name, url = Admin::Project.default_project_name_url
+  
   f.user  { |p| Factory.next(:admin_user) } 
-  f.name  'RITES Investigations' 
-  f.url  'http://localhost:3000/' 
-  f.states_and_provinces  %w{RI MA} 
+  f.name  name 
+  f.url  url 
+  f.states_and_provinces  APP_CONFIG[:states_and_provinces] 
   f.maven_jnlp_family { |p| Factory(:maven_jnlp_maven_jnlp_family) }
   f.maven_jnlp_server { |p| p.maven_jnlp_family.maven_jnlp_server  }
   f.jnlp_version_str  '0.1.0-20091013.161730' 
   f.snapshot_enabled  0 
-  f.enable_default_users  1
+  f.enable_default_users  APP_CONFIG[:enable_default_users]
 end
