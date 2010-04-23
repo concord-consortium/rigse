0b11af7f98fdd44d1c2f091864cf86ced33ec022
Stephen Bannasch <stephen.bannasch@gmail.com>
2010-04-23 00:01:43 -0400

    simplify requiring of spec_helper

--- spec/models/page_element_spec.rb
+++ spec/models/page_element_spec.rb
@@ -1,7 +1,7 @@
-require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
+require 'spec_helper'
 
 describe PageElement do
-  
+
   before(:each) do
     @page_element = Factory :page_element
     @user = Factory :user
