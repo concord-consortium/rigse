ddfb93b3a83a46b721eecff9cdbf5192e4bc31f1
Stephen Bannasch <stephen.bannasch@gmail.com>
2010-04-24 00:27:14 -0400

    new Kernel method: suppress_warnings
    To enable selective supression of warnings from Ruby such as when
    redefining the constant: REST_AUTH_SITE_KEY when running spec tests
    
      suppress_warnings { REST_AUTH_SITE_KEY = 'sitekeyforrunningtests' }
    
    See: http://mentalized.net/journal/2010/04/02/suppress_warnings_from_ruby/

--- config/initializers/00_core_extensions.rb
+++ config/initializers/00_core_extensions.rb
@@ -1,3 +1,6 @@
+# Recursively converts the keys in a Hash to symbols.
+# Also converts the keys in any Array elements which are 
+# Hashes to symbols.
 module HashExtensions
   def recursive_symbolize_keys
     inject({}) do |acc, (k,v)|
@@ -18,3 +21,19 @@ module HashExtensions
   end
 end
 Hash.send(:include, HashExtensions)
+
+# To enable selective supression of warnings from Ruby such as when
+# redefining the constant: REST_AUTH_SITE_KEY when running spec tests
+# See: http://mentalized.net/journal/2010/04/02/suppress_warnings_from_ruby/
+# 
+#   suppress_warnings { REST_AUTH_SITE_KEY = 'sitekeyforrunningtests' }
+#
+module Kernel
+  def suppress_warnings
+    original_verbosity = $VERBOSE
+    $VERBOSE = nil
+    result = yield
+    $VERBOSE = original_verbosity
+    return result
+  end
+end
