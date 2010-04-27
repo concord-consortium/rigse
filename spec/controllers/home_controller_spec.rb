a604a3b45e37d0cce904bbf8d8c6ae152a013176
Stephen Bannasch <stephen.bannasch@gmail.com>
2010-04-26 23:19:47 -0400

    need to login/out for current_user to work
    Running a controller test goes through application_controller
    and runs this method:
    
      def portal_resources
        @portal_teacher = current_user.portal_teacher
        @portal_student = current_user.portal_student
      end
    
    In order for current_user to return something reasonable
    either a regular or anonymous user needs to exist
    and be logged in.

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -3,14 +3,14 @@ require 'spec_helper'
 describe HomeController do
 
   before(:each) do
-    #mock_project #FIXME mock_project is undefined!
+    generate_default_project_and_jnlps_with_mocks
+    logout_user
     Admin::Project.should_receive(:default_project).and_return(@mock_project)
   end
-  
-  
+
   #Delete this example and add some real ones
   it "should use HomeController" do
-    pending "Broken example"
+    get :index
     controller.should be_an_instance_of(HomeController)
   end
 
