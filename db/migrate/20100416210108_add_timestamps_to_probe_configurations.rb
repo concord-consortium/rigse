01c01ea9ccb54dac31fd7340037fd0f11a8c4fb6
Stephen Bannasch <stephen.bannasch@gmail.com>
2010-04-29 02:33:12 -0400

    add note to run rake task to probe timestamp migration

--- db/migrate/20100416210108_add_timestamps_to_probe_configurations.rb
+++ db/migrate/20100416210108_add_timestamps_to_probe_configurations.rb
@@ -7,6 +7,14 @@ class AddTimestampsToProbeConfigurations < ActiveRecord::Migration
       add_column table, :created_at, :datetime
       add_column table, :updated_at, :datetime
     end
+    puts "***"
+    puts "*** Run: rake db:backup:load_probe_configurations"
+    puts "*** to reload the updated probe configuration fixtures"
+    puts "*** with time stamps into the updated db."
+    puts "***"
+    puts "*** Run: cap import:reload_probe_configurations"
+    puts "*** to do this on a remote server."
+    puts "***"
   end
 
   def self.down
