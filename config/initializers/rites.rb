05f469c2c640999c90c26bfab4a35a03159ecfb5
Stephen Bannasch <stephen.bannasch@gmail.com>
2010-04-29 11:25:01 -0400

    rites initializer creates RUNNING_TESTS constant
    RUNNING_TESTS will be true if RAILS_ENV is either 
    'cucumber' or 'test'

--- config/initializers/rites.rb
+++ config/initializers/rites.rb
@@ -30,8 +30,9 @@
 #   Mysql::Error.ancestors
 #   => [Mysql::Error, StandardError, ...
 #
+RUNNING_TESTS = RAILS_ENV == 'test' || RAILS_ENV == 'cucumber'
 
-if RAILS_ENV == 'test' || RAILS_ENV == 'cucumber' || $PROGRAM_NAME =~ /rake/
+if RUNNING_TESTS || $PROGRAM_NAME =~ /rake/
   puts "Executing rake task or running in test/cucumber env: skipping Admin::Project.create_or_update_default_project_from_settings_yml"
 else
   begin
