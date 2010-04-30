4899f7b8b2a2a0a7bfe034c01206a6d4be377154
Stephen Bannasch <stephen.bannasch@gmail.com>
2010-04-29 23:42:39 -0400

    rites now has specific cap stages, rites-dev, etc ...
    I've removed the original cap stages: development, staging,
    and production and replaced them with rites-specific stages:
    
      cap rites-dev deploy
      cap rites-staging deploy
      cap rites-production deploy
    
    These new cap stages are identical to the removed stages.
    
    After this push to master. I'll cherry-pick this commit
    into staging and production.
    
    I'll then create and push three new branches:
    
     rites-dev        based on master
     rites-staging    based on staging
     rites-production based on production

--- config/deploy/rites-dev.rb
+++ config/deploy/rites-dev.rb
@@ -1,6 +1,6 @@
-# development branch
+# RITES development branch
 set :domain, "rites.dev.concord.org"
 set :deploy_to, "/web/rites.concord.org"
 server domain, :app, :web
 role :db, domain, :primary => true
-set :branch, "master"
+set :branch, "rites-dev"
