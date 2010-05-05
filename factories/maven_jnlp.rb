a86d719d68560079c6d0646ef2ff3057c8465a16
Stephen Bannasch <stephen.bannasch@gmail.com>
2010-05-05 19:12:52 -0400

    MavenJnlp::VersionedJnlp factory uses default version
    When creating a MavenJnlp::VersionedJnlp with the factory
    use the version supplied by Admin::Project.default_jnlp_info.
    
    Working on debugging this error running cucumber features
    on hudson:
    
      can't clone NilClass (TypeError)
      ./app/models/maven_jnlp/maven_jnlp_family.rb:49:in `clone'
    
    Which is generated by running either of these features:
    
      features/admin_configures_vendor_interfaces.feature
      features/teacher_registers.feature
    
    I am not having the problem locally.

--- factories/maven_jnlp.rb
+++ factories/maven_jnlp.rb
@@ -16,10 +16,11 @@ Factory.define :maven_jnlp_maven_jnlp_family, :class => MavenJnlp::MavenJnlpFami
 end
 
 Factory.define :maven_jnlp_versioned_jnlp_url, :class => MavenJnlp::VersionedJnlpUrl do |f|
+  server, family, version = Admin::Project.default_jnlp_info
   f.path  '/dev/org/concord/maven-jnlp/all-otrunk-snapshot/all-otrunk-snapshot-0.1.0-20091013.161730.jnlp'
   f.url  'http://jnlp.concord.org/dev/org/concord/maven-jnlp/all-otrunk-snapshot/all-otrunk-snapshot-0.1.0-20091013.161730.jnlp'
   f.date_str  '20091013.161730'
-  f.version_str  '0.1.0-20091013.161730'
+  f.version_str  version
   f.association :maven_jnlp_family, :factory => :maven_jnlp_maven_jnlp_family
 end
 
