6ce27b2c58f010868b103d96a41e0417aad3b926
Aaron Unger <aunger@concord.org>
2010-05-06 12:57:53 -0400

    Be sure to symlink the has resources when deploying.

--- config/deploy/has-production.rb
+++ config/deploy/has-production.rb
@@ -12,3 +12,12 @@ set :branch, "has-production"
 set :domain, "seymour.concord.org"
 server domain, :app, :web
 role :db, domain, :primary => true
+
+namespace :deploy do
+  desc "link in the has resources folder"
+  task :has_resource_symlink do
+    run "ln -nfs #{shared_path}/public/resources #{release_path}/public/resources"
+  end
+end
+
+after 'deploy:update_code', 'deploy:has_resource_symlink'
