bd350e53a73254e07bc27efbacf5f7c6231cec9b
Aaron Unger <aunger@concord.org>
2010-05-12 15:52:14 -0400

    Move extended_group_by into core_extensions.

--- config/core_extensions.rb
+++ config/core_extensions.rb
@@ -49,3 +49,21 @@ module StringExtensions
 end
 String.send(:include, StringExtensions)
 
+# An extended group_by which will group at multiple depths
+# Ex:
+# >> ["aab","abc","aba","abd","aac","ada"].extended_group_by([lambda {|e| e.first}, lambda {|e| e.first(2)}])
+# => {"a"=>{"aa"=>["aab", "aac"], "ab"=>["abc", "aba", "abd"], "ad"=>["ada"]}}
+module Enumerable
+  def extended_group_by(lambdas)
+    lamb = lambdas.shift
+    result = lamb ? self.group_by{|e| lamb.call(e)} : self
+    if lambdas.size > 0
+      final = {}
+      temp = result.map{|k,v| {k => v.extended_group_by(lambdas.clone)}}
+      temp.each {|r| final.merge!(r) }
+      result = final
+    end
+    return result
+  end
+end
+
