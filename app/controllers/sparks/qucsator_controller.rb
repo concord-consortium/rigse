2912a01d8388461eac6fc105009a184d59b93d9f
Seong-June Kim <jkim707@gmail.com>
2010-06-15 17:33:48 -0400

    Get URL of qucsator from APP_CONFIG

--- app/controllers/sparks/qucsator_controller.rb
+++ app/controllers/sparks/qucsator_controller.rb
@@ -6,7 +6,8 @@ class Sparks::QucsatorController < ApplicationController
   def solve
     puts "#{'*' * 72}\n" * 4
     puts "params=#{params.inspect}"
-    res = Net::HTTP.post_form(URI.parse('http://localhost/qucs/index.php'),
+    puts "url=#{APP_CONFIG[:qucs_url]}"
+    res = Net::HTTP.post_form(URI.parse(APP_CONFIG[:qucs_url]),
       { :qucs => params['qucs'] })
     puts "res=#{res.body.inspect}"
     render :inline => res.body
