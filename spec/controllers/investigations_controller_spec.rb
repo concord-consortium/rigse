17ddad034ddc46296cc8bf664371ba0aa7ce253a
Charlie H <cmhonig@gmail.com>
2010-06-23 14:26:39 -0400

    Changes for story 'only authors should have duplicate link'

---
+++ spec/controllers/investigations_controller_spec.rb
@@ -0,0 +1,41 @@
+require 'spec_helper'
+#include ApplicationHelper
+
+describe InvestigationsController do
+  integrate_views
+
+  before(:each) do
+    generate_default_project_and_jnlps_with_mocks
+    Admin::Project.stub!(:default_project).and_return(@mock_project)
+    
+    @admin_user = Factory.create(:user, { :email => "test@test.com", :password => "password", :password_confirmation => "password" })
+    @admin_user.add_role("admin")
+    
+    stub_current_user :admin_user
+    
+    @investigation = Factory.create(:investigation, {
+      :name => "test investigation",
+      :description => "new decription"
+    })
+    Investigation.stub!(:find).and_return(@investigation)
+  end
+
+  it "should display a 'duplicate' link for authorized users" do
+    @investigation.should_receive(:duplicateable?).with(@logged_in_user).and_return(true)
+
+    get :show, :id => @investigation.id
+    
+    #@response.body.should include(duplicate_link_for(@investigation))
+    assert_select("a[href=?]", duplicate_investigation_url(@investigation), { :text => "duplicate", :count => 1 })
+  end
+  
+  it "should not display a 'duplicate' link for unauthorized users" do
+    @investigation.should_receive(:duplicateable?).with(@logged_in_user).and_return(false)
+
+    get :show, :id => @investigation.id
+    
+    #@response.body.should_not include(duplicate_link_for(@investigation))
+    assert_select("a[href=?]", duplicate_investigation_url(@investigation), { :text => "duplicate", :count => 0 })
+  end
+
+end
