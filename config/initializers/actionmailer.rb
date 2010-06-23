9df1da5cc60d7aa20e4283c97d3df2286d88862f
Aaron Unger <aunger@concord.org>
2010-06-23 13:11:12 -0600

    Change the way config/mailer.yml is read so that it supports configurations other than smtp.

--- config/initializers/actionmailer.rb
+++ config/initializers/actionmailer.rb
@@ -1,6 +1,15 @@
 if File.exists?("#{RAILS_ROOT}/config/mailer.yml")
   require "action_mailer"
   c = YAML::load(File.open("#{RAILS_ROOT}/config/mailer.yml"))
-  ActionMailer::Base.delivery_method = c[:delivery_method]
-  ActionMailer::Base.smtp_settings = c[:smtp]
+  c.each do |key,val|
+    if key == :smtp || key == 'smtp'
+      key = :smtp_settings
+    end
+    begin
+      ActionMailer::Base.send("#{key}=".to_sym, val)
+    rescue Exception => e
+      $stderr.puts "Problem processing key '#{key}' in config/mailer.yml"
+      $stderr.puts "#{e}"
+    end
+  end
 end
