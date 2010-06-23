ed96c43e7eb12e75237379a85e1c48d08770742f
Charlie H <cmhonig@gmail.com>
2010-06-23 16:07:43 -0400

    Changes for story 'Change usernames to first initial, last name'

--- spec/models/portal/student_spec.rb
+++ spec/models/portal/student_spec.rb
@@ -27,5 +27,22 @@ describe Portal::Student do
       @student.should have(1).clazzes
     end
   end
+  
+  it "should generate a user name by first initial and last name" do
+    Portal::Student.generate_user_login("test", "user").should == "tuser"
+    
+    first_name = "Nametest"
+    last_name  = "Testuser"
+    @student.user = Factory.create(:user, {
+      :first_name => first_name,
+      :last_name => last_name,
+      :login => Portal::Student.generate_user_login(first_name, last_name),
+      :password => "password",
+      :password_confirmation => "password",
+      :email => "test@test.com"
+    })
+    @student.user.login.should == "ntestuser"
+    Portal::Student.generate_user_login(@student.first_name, @student.last_name).should == @student.user.login + "2"
+  end
 
 end
