70ed1ae99d2a59e397616219394b592b676023d1
Stephen Bannasch <stephen.bannasch@gmail.com>
2010-07-19 11:34:09 -0400

    jnlps used only when :runnables_use == 'otrunk_jnlp'
    I'll be adding new runnable types which also don't use 
    jnlps so switch the logic in this initialization to more
    easily cover those cases.
    
    These constants defined here are used in helpers, models, 
    controllers, tests, factories, and rake tasks.

--- config/initializers/01_load_config.rb
+++ config/initializers/01_load_config.rb
@@ -6,5 +6,5 @@ APP_CONFIG[:use_gse] = true if APP_CONFIG[:use_gse] == nil
 USING_RITES = APP_CONFIG[:theme] && (APP_CONFIG[:theme] == 'default' || APP_CONFIG[:theme] == 'rites')
 NOT_USING_RITES = !USING_RITES
 
-NOT_USING_JNLPS = APP_CONFIG[:runnables_use] && APP_CONFIG[:runnables_use] == 'browser'
+NOT_USING_JNLPS = APP_CONFIG[:runnables_use] && APP_CONFIG[:runnables_use] != 'otrunk_jnlp'
 USING_JNLPS = !NOT_USING_JNLPS
