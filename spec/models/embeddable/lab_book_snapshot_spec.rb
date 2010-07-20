671ef45ff72c3a04bb9e5b1169523d2658182a8f
Noah Paessel <knowuh@gmail.com>
2010-07-20 14:01:06 -0400

    Send update events to investigations
    Note that many embeddables dont do this.
    see gist: http://gist.github.com/483303
    
    Also note that the tests for send_update_events are??

---
+++ spec/models/embeddable/lab_book_snapshot_spec.rb
@@ -0,0 +1,20 @@
+require File.expand_path(File.dirname(__FILE__) + '/../../spec_helper')
+
+describe Embeddable::LabBookSnapshot do
+
+  # TODO: I am not happy with how flakey send_updates_to is
+  # It took a while to finagle this test. It should probably be
+  # looked at closer.
+  it "should send updates to investigations when changed" do
+    investigation = mock("investigation")
+    investigations = [investigation]
+    snapshot = Embeddable::LabBookSnapshot.new(:name => "button", :target_element_type => "fake", :target_element_id => 3)
+    
+    snapshot.should_receive(:save)
+    snapshot.should_receive(:investigations).and_return(investigations)
+    investigation.should_receive(:update_attribute).and_return(true)
+    snapshot.update_attributes({:name => "button changed"})
+    snapshot.destroy
+  end
+  
+end
