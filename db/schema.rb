d6cb9bdf705aa31e806a38e5435ecf9129f1066e
Stephen Bannasch <stephen.bannasch@gmail.com>
2010-10-20 14:12:47 -0400

    increase field size for dataservice_bundle_contents body and otml

--- db/schema.rb
+++ db/schema.rb
@@ -122,10 +122,10 @@ ActiveRecord::Schema.define(:version => 20100702164051) do
   create_table "dataservice_bundle_contents", :force => true do |t|
     t.integer  "bundle_logger_id"
     t.integer  "position"
-    t.text     "body",             :limit => 16777215
+    t.text     "body",             :limit => 2147483647
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.text     "otml",             :limit => 16777215
+    t.text     "otml",             :limit => 2147483647
     t.boolean  "processed"
     t.boolean  "valid_xml"
     t.boolean  "empty"
