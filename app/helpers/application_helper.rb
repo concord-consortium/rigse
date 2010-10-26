07daff1ba71ee79e919051c6f0744f4319639294
Aaron Unger <aunger@concord.org>
2010-10-26 10:33:12 -0400

    Add question filtering to the learner report.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -586,20 +586,24 @@ module ApplicationHelper
   end
   
   def learner_report_summary(learner, opts = {})
-    options = { :omit_delete => true, :omit_edit => true, :hide_component_name => true }
+    options = { :omit_delete => true, :omit_edit => true, :hide_component_name => true, :hide_statistics => true }
     options.update(opts)
-    reportUtil = Report::Util.factory(learner.offering)
-    questions = reportUtil.embeddables
-    answered  = reportUtil.saveables(:learner => learner, :answered => true)
+    unless options[:hide_statistics]
+      reportUtil = Report::Util.factory(learner.offering)
+      questions = reportUtil.embeddables
+      answered  = reportUtil.saveables(:learner => learner, :answered => true)
+    end
     capture_haml do
       haml_tag :div, :class => 'action_menu' do
         haml_tag :div, :class => 'action_menu_header_left' do
           haml_concat title_for_component(learner.offering, options)
         end
       end
-      haml_tag :div do
-        haml_tag :p do
-          haml_concat("#{questions.size} questions, #{answered.size} have been answered")
+      unless options[:hide_statistics]
+        haml_tag :div do
+          haml_tag :p do
+            haml_concat("#{questions.size} questions, #{answered.size} have been answered")
+          end
         end
       end
     end
