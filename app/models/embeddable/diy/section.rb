192dc9d73a66b3c4ad9188dde56d0c9fc20f5228
Noah Paessel <knowuh@gmail.com>
2010-11-03 16:06:14 -0400

    MIGRATE: Add DIY section model

---
+++ app/models/embeddable/diy/section.rb
@@ -0,0 +1,25 @@
+class Embeddable::Diy::Section < Embeddable::Embeddable
+  set_table_name "embeddable_diy_sections"
+    # t.integer user_id
+    # t.string  uuid, 
+    # t.string  name
+    # t.text    description
+    # t.boolean has_question
+    # t.text    content
+    # t.text    default_response
+    # t.timestamps
+
+  @@searchable_attributes = %w{name description content}
+  
+  self.extend SearchableSection
+  class <<self
+    def searchable_attributes
+      @@searchable_attributes
+    end
+  end
+  
+  def self.display_name
+    self.name 
+  end
+  
+end
