2db92e44e2b0721c490cc9621793c67e78041a32
Noah Paessel <knowuh@gmail.com>
2010-11-04 21:31:41 -0400

    drawing tool should inherit from Embeddable.
    DRY embeddables when you find them like this in the wild.

--- app/models/embeddable/embeddable.rb
+++ app/models/embeddable/embeddable.rb
@@ -33,4 +33,40 @@ class Embeddable::Embeddable < ActiveRecord::Base
     end
   end
 
+  def _dis_enable_targets(page)
+    results = []
+    if page.nil?
+      results = self.page_elements
+    else
+      page_element = self.page_elements.detect{ |elm| elm.page == page}
+      (results << page_element) unless page_element.nil?
+    end
+    results
+  end
+  
+  def enable(page=nil)
+    _dis_enable_targets(page).each do |target|
+      target.is_enabled = true
+      target.save
+    end
+  end
+
+  def disable(page=nil)
+    _dis_enable_targets(page).each do |target|
+      target.is_enabled = false
+      target.save
+    end
+  end
+
+  def toggle_enabled(page=nil)
+    _dis_enable_targets(page).each do |target|
+      if target.is_enabled
+        target.is_enabled = false
+      else
+        target.is_enabled = true
+      end
+      target.save
+    end
+  end
+
 end
