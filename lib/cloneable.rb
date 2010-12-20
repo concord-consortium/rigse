9c56bf4017a729d5dce344f75b560f683849861c
Noah Paessel <knowuh@gmail.com>
2010-12-20 13:21:45 -0500

    deep_clone defaults.

--- lib/cloneable.rb
+++ lib/cloneable.rb
@@ -11,7 +11,7 @@ module Cloneable
     end
   end
 
-  def clone(options = {})
+  def deep_clone(options = {})
     begin
       new_assocs = self.class.cloneable_associations
     rescue
@@ -19,11 +19,14 @@ module Cloneable
       throw("cloneable class #{self.class.name} fails #cloneable_associations {$!}")
     end
     if new_assocs.size > 0
+      options[:except] ||= []
       options[:include] ||= []
       if options[:include].kind_of? Hash
         options[:include] = Array(options[:include])
       end
       options[:include] += new_assocs
+      options[:use_dictionary] = true # prevent duplicates
+      options[:except] += ['uuid','id','update_at','created_at']
     end
     # invokes on superclas (possilby up to Object#clone)
     super(options)
