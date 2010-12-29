6f32d63f882e7892f850f441f96a88edc534d950
Noah Paessel <knowuh@gmail.com>
2010-12-29 18:43:34 -0500

    Strange default name behavior cleanup.

--- spec/models/embeddable/data_collector_spec.rb
+++ spec/models/embeddable/data_collector_spec.rb
@@ -1,23 +1,24 @@
 require 'spec_helper'
 describe Embeddable::DataCollector do
-  
+
   describe "When there are existing probes" do
     before(:all) do
       @fake_probe= mock_model(Probe::ProbeType,
-        :name => 'fake')
+        :name => 'fake', :id => 1)
       Probe::ProbeType.stub!(:find_by_name => @fake_probe)
       Probe::ProbeType.stub!(:find => @fake_probe)
     end
 
     it_should_behave_like 'a cloneable model'
+
     it "should create a new instance given valid attributes" do
       data_collector = Embeddable::DataCollector.new
-      data_collector.save 
+      data_collector.save
       data_collector.probe_type.should_not be_nil
       data_collector.probe_type.id.should_not be_nil
       data_collector.should be_valid
     end
-    
+
     it "should use a good default and valid probe_type" do
       data_collector = Embeddable::DataCollector.create
       data_collector.probe_type.should_not be_nil
@@ -30,8 +31,39 @@ describe Embeddable::DataCollector do
       data_collector.save
       data_collector.should_not be_valid
     end
+
+    describe "Embeddable::DataCollector.prototype_by_type_and_calibration" do
+      it "should find and use an existing datacollector prototype with a known probeType" do
+        @fake_probe_a = mock_model(Probe::ProbeType, :name => "type a", :id => 2)
+        moc_data_collector = mock_model(Embeddable::DataCollector,
+                               :probe_type => @fake_probe_a)
+        prototypes = mock(:find => moc_data_collector)
+        Embeddable::DataCollector.stub(:prototypes => prototypes)
+
+        proto = Embeddable::DataCollector.prototype_by_type_and_calibration(@fake_probe_a,nil)
+        proto.probe_type.should == @fake_probe_a
+      end
+      
+      it "should create a datacollector with the given probeType, when an existing prototype cant be found" do
+        @fake_probe_a = mock_model(Probe::ProbeType, :name => "type a", :id => 2)
+        prototypes = mock(:find => nil)
+        Embeddable::DataCollector.stub(:prototypes => prototypes)
+        proto = Embeddable::DataCollector.prototype_by_type_and_calibration(@fake_probe_a,nil)
+        proto.probe_type.should == @fake_probe_a
+      end
+
+      it "should use the name of the probeType for the name of the dataCollector when making a new prototype" do
+        @fake_probe_a = mock_model(Probe::ProbeType, :name => "type a", :id => 2)
+        prototypes = mock(:find => nil)
+        Embeddable::DataCollector.stub(:prototypes => prototypes)
+        proto = Embeddable::DataCollector.prototype_by_type_and_calibration(@fake_probe_a,nil)
+        proto.probe_type.should == @fake_probe_a
+        proto.name.should match @fake_probe_a.name
+      end
+    end
+
   end
-  
+
   describe "When no probes exist" do
     # todo: whats the behavior here?
     before(:all) do
@@ -39,7 +71,7 @@ describe Embeddable::DataCollector do
       Probe::ProbeType.stub!(:find => nil)
       Probe::ProbeType.stub!(:default => nil)
     end
-    
+
     it "should fail validation" do
       data_collector = Embeddable::DataCollector.create
       data_collector.should_not be_valid
@@ -50,6 +82,5 @@ describe Embeddable::DataCollector do
       data_collector.should_not be_valid
       data_collector.errors.on(:probe_type).should include(Embeddable::DataCollector::MISSING_PROBE_MESSAGE)
     end
-
   end
 end
