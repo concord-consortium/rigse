16530fe58cdfb5c044a676df6185b857c7d4b492
Matt Venables <mattv@cantinaconsulting.com>
2011-01-05 15:30:09 -0500

    Force capybara version 0.3.9
    The cucumber_rails v0.3.2 gem does not support capybara 0.4.0, which is causing some of the tests to fail.  Hopefully this change will fix those.

--- features/support/env.rb
+++ features/support/env.rb
@@ -16,7 +16,7 @@ require 'cucumber/web/tableish'
 require 'capybara/rails'
 require 'capybara/cucumber'
 require 'capybara/session'
-# require 'cucumber/rails/capybara_javascript_emulation' # Lets you click links with onclick javascript handlers without using @culerity or @javascript
+require 'cucumber/rails/capybara_javascript_emulation' # Lets you click links with onclick javascript handlers without using @culerity or @javascript
 
 require 'email_spec'
 require 'email_spec/cucumber'
