f7b7b8cb067712b104b9ea69e977bbe3a1610761
Matt Venables <mattv@cantinaconsulting.com>
2011-01-24 16:43:35 -0500

    Add basic search for reports controller

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -9,10 +9,22 @@ class ReportsController < ApplicationController
       redirect_to :action => 'index' and return
     end
     
+    search_options = {
+      :name => params[:name], 
+      :portal_clazz_id => params[:portal_clazz_id],
+      :grade_span => params[:grade_span],
+      :domain_id => params[:domain_id],
+      :paginate => !params[:print_mode],  # send params[:print_mode] for single-page view
+      :page => params[:page] || 1
+    }
+    
     if params[:id] == "investigations"
       @report_type = Investigation.display_name
+      @records = Investigation.search_list(search_options)
+      
     elsif params[:id] == "resource_pages"
       @report_type = ResourcePage.display_name
+      @records = ResourcePage.search_list(search_options)
     end
     
   end
