5cd337b6810a2c5cfc084588a64bc34ff8bc9546
Stephen Bannasch <stephen.bannasch@gmail.com>
2011-01-25 14:30:19 -0500

    host name remains genome-dynamics.dev.concord.org

--- config/deploy/genomedynamics-dev.rb
+++ config/deploy/genomedynamics-dev.rb
@@ -2,13 +2,13 @@
 #  Application
 #############################################################
 
-set :deploy_to, "/web/genomedynamics.dev.concord.org"
+set :deploy_to, "/web/genome-dynamics.dev.concord.org"
 set :branch, "genomedynamics-dev"
 
 #############################################################
 #  Servers
 #############################################################
 
-set :domain, "genomedynamics.dev.concord.org"
+set :domain, "genome-dynamics.dev.concord.org"
 server domain, :app, :web
 role :db, domain, :primary => true
