1bfcac725ff6a16cbd5454c6f52c4335d9190b05
Matt Venables <mattv@cantinaconsulting.com>
2011-01-28 09:47:53 -0500

    Update cap attachment upload/download tasks
    Updated the tasks to use rsync and moved the tasks to the db namespace:
    cap db:fetch_remote_attachemnts
    cap db:push_local_attachments

--- config/deploy.rb
+++ config/deploy.rb
@@ -143,13 +143,20 @@ namespace :db do
     upload("config/initializers/site_keys.rb", "#{deploy_to}/shared/config/initializers/site_keys.rb", :via => :sftp)
   end
 
-end
-
-namespace :paperclip do 
   desc "Pulls Paperclip images"
-  task :fetch_attachments, :roles => :web do 
-    download "#{shared_path}/system/attachments", "public/system/attachments/", :via => :sftp, :recursive => true
+  task :fetch_remote_attachments, :roles => :web do 
+    remote_dir  = "#{shared_path}/system/attachments/"
+    local_dir   = "public/system/attachments/"
+    run_locally "rsync --recursive --rsh=ssh --compress --human-readable --progress #{domain}:#{remote_dir} #{local_dir}"
   end
+  
+  desc "Pulls Paperclip images"
+  task :push_local_attachments, :roles => :web do 
+    remote_dir  = "#{shared_path}/system/attachments/"
+    local_dir   = "public/system/attachments/"
+    run_locally "rsync --recursive --rsh=ssh --compress --human-readable --progress #{local_dir} #{domain}:#{remote_dir}"
+  end
+
 end
 
 namespace :deploy do
