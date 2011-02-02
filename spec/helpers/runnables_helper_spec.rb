3405543b6df02a0e49396a1382d9490945aca721
Dan Barron <dan@cantina.co>
2011-02-02 17:26:41 -0500

    added test for .run_link_for

--- spec/helpers/runnables_helper_spec.rb
+++ spec/helpers/runnables_helper_spec.rb
@@ -11,26 +11,32 @@ describe RunnablesHelper do
   end
 
   describe ".run_button_for" do
-    it "should render a run button for a specified page" do
+    it "should render a run button for a specified component" do
       helper.run_button_for(@page).should == "<a href=\"http://test.host/pages/1002.jnlp\" class=\"run_link rollover\" title=\"Run the Page: 'Foo' as a Java Web Start application. The first time you do this it may take a while to startup as the Java code is downloaded and saved on your hard drive.\"><img alt=\"Run the Page: 'Foo' as a Java Web Start application. The first time you do this it may take a while to startup as the Java code is downloaded and saved on your hard drive.\" src=\"/images/run.png?1295966927\" /></a>"
     end
   end
 
   describe ".preview_button_for" do
-    it "should render a preview button for a specified page" do
+    it "should render a preview button for a specified component" do
       helper.preview_button_for(@page).should == "<a href=\"http://test.host/pages/1004.jnlp\" class=\"run_link rollover\" title=\"Preview the Page: 'Foo' as a Java Web Start application. The first time you do this it may take a while to startup as the Java code is downloaded and saved on your hard drive.\"><img alt=\"Preview the Page: 'Foo' as a Java Web Start application. The first time you do this it may take a while to startup as the Java code is downloaded and saved on your hard drive.\" src=\"/images/preview.png?1295966927\" /></a>"
     end
   end
 
   describe ".teacher_preview_button_for" do
-    it "should render a preview button in techer mode for a given page" do
+    it "should render a preview button in techer mode for a given component" do
       helper.teacher_preview_button_for(@page).should == "<a href=\"http://test.host/pages/1006.jnlp?teacher_mode=true\" class=\"run_link rollover\" title=\"Preview the Page 'Foo' as a Teacher. The first time you do this it may take a while to startup as the Java code is downloaded and saved on your hard drive.\"><img alt=\"Preview the Page 'Foo' as a Teacher. The first time you do this it may take a while to startup as the Java code is downloaded and saved on your hard drive.\" src=\"/images/teacher_preview.png?1295966927\" /></a>"
     end
   end
 
   describe ".preview_link_for" do
-    it "should do something" do
+    it "should render a preview link for a given component" do
       helper.preview_link_for(@page).should == "<a href=\"http://test.host/pages/1008.jnlp\" class=\"run_link rollover\" title=\"Preview the Page: 'Foo' as a Java Web Start application. The first time you do this it may take a while to startup as the Java code is downloaded and saved on your hard drive.\"><img alt=\"Preview the Page: 'Foo' as a Java Web Start application. The first time you do this it may take a while to startup as the Java code is downloaded and saved on your hard drive.\" src=\"/images/preview.png?1295966927\" /></a><a href=\"http://test.host/pages/1008.jnlp\" class=\"run_link\" title=\"Preview the page: 'Foo' as a Java Web Start application. The first time you do this it may take a while to startup as the Java code is downloaded and saved on your hard drive.\">preview </a>"
+    end
+  end
+
+  describe ".run_link_for" do
+    it "should render a run link for a given component" do
+      helper.run_link_for(@page).should == "<a href=\"http://test.host/pages/1010.jnlp\" class=\"run_link rollover\" title=\"Run the Page: 'Foo' as a Java Web Start application. The first time you do this it may take a while to startup as the Java code is downloaded and saved on your hard drive.\"><img alt=\"Run the Page: 'Foo' as a Java Web Start application. The first time you do this it may take a while to startup as the Java code is downloaded and saved on your hard drive.\" src=\"/images/run.png?1295966927\" /></a><a href=\"http://test.host/pages/1010.jnlp\" class=\"run_link\" title=\"run the page: 'Foo' as a Java Web Start application. The first time you do this it may take a while to startup as the Java code is downloaded and saved on your hard drive.\">run </a>"
     end
   end
 end
