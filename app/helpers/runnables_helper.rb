3adc1bd0cc0cc8ec5349007192a9352aae2a2ecc
Dan Barron <dan@cantina.co>
2011-02-03 17:41:00 -0500

    more DRYing up of runnables helpers

--- app/helpers/runnables_helper.rb
+++ app/helpers/runnables_helper.rb
@@ -3,35 +3,42 @@ include JnlpHelper
 include Clipboard
 
 module RunnablesHelper
-  def run_text(display_name, component_name, run_as)
-    "Run the #{display_name}: '#{component_name}' as a #{run_as}. The first time you do this it may take a while to startup as the Java code is downloaded and saved on your hard drive."
+  def run_text(component, run_as = "Java Web Start application")
+    "Run the #{component.class.display_name}: '#{component.name}' as a #{run_as}. The first time you do this it may take a while to startup as the Java code is downloaded and saved on your hard drive."
   end
 
-  def preview_text(display_name, component_name, run_as)
-    "Preview the #{display_name}: '#{component_name}' as a #{run_as}. The first time you do this it may take a while to startup as the Java code is downloaded and saved on your hard drive."
+  def preview_text(component, run_as = "Java Web Start application")
+    "Preview the #{component.class.display_name}: '#{component.name}' as a #{run_as}. The first time you do this it may take a while to startup as the Java code is downloaded and saved on your hard drive."
   end
 
   def run_button_for(component)
     url = polymorphic_url(component, :format => :jnlp, :params => current_user.extra_params)
     link_button("run.png",  url,
                 :class => "run_link rollover",
-                :title => run_text(component.class.display_name, component.name, "Java Web Start application"))
+                :title => run_text(component))
   end
 
-  def preview_button_for(component)
-    url = polymorphic_url(component, :format => :jnlp, :params => current_user.extra_params)
-    link_button("preview.png",  url,
+  def preview_button_for(component, url_params = nil, img = "preview.png", run_as = nil)
+    unless url_params
+      url_params = current_user.extra_params
+    end
+
+    if run_as
+      title = preview_text(component, run_as)
+    else
+      title = preview_text(component)
+    end
+
+    url = polymorphic_url(component, :format => :jnlp, :params => url_params)
+    link_button(img,  url,
                 :class => "run_link rollover",
-                :title => preview_text(component.class.display_name, component.name, "Java Web Start application"))
+                :title => title)
   end
 
   def teacher_preview_button_for(component)
     url_params = current_user.extra_params
     url_params[:teacher_mode] = true
-    url = polymorphic_url(component, :format => :jnlp, :params => url_params)
-    link_button("teacher_preview.png",  url,
-                :class => "run_link rollover",
-                :title => preview_text(component.class.display_name, component.name, "Teacher"))
+    preview_button_for(component, url_params, "teacher_preview.png", "Teacher")
   end
 
   def preview_link_for(component, as_name=nil, params={})
@@ -46,7 +53,7 @@ module RunnablesHelper
     preview_button_for(component) +
       link_to(link_text, url,
               :class => "run_link",
-              :title => preview_text(component.class.display_name, component.name, "Java Web Start application"))
+              :title => preview_text(component))
   end
 
   def run_link_for(component, as_name=nil, params={})
@@ -59,17 +66,13 @@ module RunnablesHelper
 
     if NOT_USING_JNLPS
       url = polymorphic_url(component, :format => APP_CONFIG[:runnable_mime_type], :params => params)
-    else
-      url = polymorphic_url(component, :format => :jnlp, :params => params)
-    end
-
-    if NOT_USING_JNLPS
       run_button_for(component) + link_to(link_text, url, :popup => true)
     else
+      url = polymorphic_url(component, :format => :jnlp, :params => params)
       run_button_for(component) +
         link_to(link_text, url,
                 :class => 'run_link',
-                :title => run_text(component.class.display_name, component.name, "Java Web Start application"))
+                :title => run_text(component))
     end
   end
 end
