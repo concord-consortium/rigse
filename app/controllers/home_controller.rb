aa5948f5c0e6cb86c4c93d4542556c3689ae91d3
Dan Barron <dan@cantina.co>
2011-02-07 14:23:02 -0500

    added jnlpLaunchable type

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,23 +1,21 @@
 class HomeController < ApplicationController
-  
   def readme
     @readme = ReadMe.new
     render :action => "readme", :layout => "readme"
   end
-  
-  def pick_signup    
+
+  def pick_signup
   end
-  
+
   def about
   end
-  
+
   # @template is a reference to the View template object
   def name_for_clipboard_data
     render :text=> @template.clipboard_object_name(params)
   end
-  
+
   def missing_installer
     @os = params['os']
   end
-  
 end
