c1f58d8a8d5e12ecad1a0c59dac2129f1f9ef6f9
Dan Barron <dan@cantina.co>
2011-02-22 17:04:24 -0500

    added custom matcher to avoid cachebuster code at the end of images in runnables_helper links

--- spec/helpers/runnables_helper_spec.rb
+++ spec/helpers/runnables_helper_spec.rb
@@ -2,6 +2,7 @@ require 'spec_helper'
 
 include ApplicationHelper
 describe RunnablesHelper do
+  include RunnablesLinkMatcher
   before :each do
     @anonymous_user = mock_model(User, :roles => ["guest"], :anonymous? => true, :name => "guest")
     @anonymous_user.stub!(:extra_params).and_return({})
@@ -12,39 +13,66 @@ describe RunnablesHelper do
 
   describe ".run_button_for" do
     it "should render a run button for a specified component" do
-      helper.run_button_for(@resource_page).should == "<a href=\"http://test.host/resource_pages/#{@resource_page.id}.jnlp\" class=\"run_link rollover\" title=\"Run the Resource Page: 'Foo' as a Java Web Start application. The first time you do this it may take a while to startup as the Java code is downloaded and saved on your hard drive.\"><img alt=\"Run the Resource Page: 'Foo' as a Java Web Start application. The first time you do this it may take a while to startup as the Java code is downloaded and saved on your hard drive.\" src=\"/images/run.png?1295966927\" /></a>"
+      helper.run_button_for(@resource_page).should be_link_like("http://test.host/resource_pages/#{@resource_page.id}.jnlp",
+                                                                "run_link rollover",
+                                                                "/images/run.png")
     end
   end
 
   describe ".preview_button_for" do
     it "should render a preview button for a specified component" do
-      helper.preview_button_for(@resource_page).should == "<a href=\"http://test.host/resource_pages/#{@resource_page.id}.jnlp\" class=\"run_link rollover\" title=\"Preview the Resource Page: 'Foo' as a Java Web Start application. The first time you do this it may take a while to startup as the Java code is downloaded and saved on your hard drive.\"><img alt=\"Preview the Resource Page: 'Foo' as a Java Web Start application. The first time you do this it may take a while to startup as the Java code is downloaded and saved on your hard drive.\" src=\"/images/preview.png?1295966927\" /></a>"
+      helper.preview_button_for(@resource_page).should be_link_like("http://test.host/resource_pages/#{@resource_page.id}.jnlp",
+                                                                    "run_link rollover",
+                                                                    "/images/preview.png")
     end
   end
 
   describe ".teacher_preview_button_for" do
     it "should render a preview button in techer mode for a given component" do
-      helper.teacher_preview_button_for(@resource_page).should == "<a href=\"http://test.host/resource_pages/#{@resource_page.id}.jnlp?teacher_mode=true\" class=\"run_link rollover\" title=\"Preview the Resource Page: 'Foo' as a Teacher. The first time you do this it may take a while to startup as the Java code is downloaded and saved on your hard drive.\"><img alt=\"Preview the Resource Page: 'Foo' as a Teacher. The first time you do this it may take a while to startup as the Java code is downloaded and saved on your hard drive.\" src=\"/images/teacher_preview.png?1297111962\" /></a>"
+      #helper.teacher_preview_button_for(@resource_page).should == "<a href=\"http://test.host/resource_pages/#{@resource_page.id}.jnlp?teacher_mode=true\" class=\"run_link rollover\" title=\"Preview the Resource Page: 'Foo' as a Teacher. The first time you do this it may take a while to startup as the Java code is downloaded and saved on your hard drive.\"><img alt=\"Preview the Resource Page: 'Foo' as a Teacher. The first time you do this it may take a while to startup as the Java code is downloaded and saved on your hard drive.\" src=\"/images/teacher_preview.png?1297111962\" /></a>"
+      helper.teacher_preview_button_for(@resource_page).should be_link_like("http://test.host/resource_pages/#{@resource_page.id}.jnlp",
+                                                                             "run_link rollover",
+                                                                             "/images/teacher_preview.png")
     end
   end
 
   describe ".preview_link_for" do
     it "should render a preview link for a given component" do
-      helper.preview_link_for(@resource_page).should == "<a href=\"http://test.host/resource_pages/#{@resource_page.id}.jnlp\" class=\"run_link rollover\" title=\"Preview the Resource Page: 'Foo' as a Java Web Start application. The first time you do this it may take a while to startup as the Java code is downloaded and saved on your hard drive.\"><img alt=\"Preview the Resource Page: 'Foo' as a Java Web Start application. The first time you do this it may take a while to startup as the Java code is downloaded and saved on your hard drive.\" src=\"/images/preview.png?1295966927\" /></a><a href=\"http://test.host/resource_pages/#{@resource_page.id}.jnlp\" title=\"Preview the Resource Page: 'Foo' as a Java Web Start application. The first time you do this it may take a while to startup as the Java code is downloaded and saved on your hard drive.\">preview </a>"
+      helper.preview_link_for(@resource_page).should be_link_like("http://test.host/resource_pages/#{@resource_page.id}.jnlp",
+                                                                  "run_link rollover",
+                                                                  "/images/preview.png",
+                                                                  "preview")
     end
 
     it "should render a preview link for a given component with parameters" do
-      helper.preview_link_for(@resource_page, nil, {:link_text => "run Jeff's Leiderhosen"}).should == "<a href=\"http://test.host/resource_pages/#{@resource_page.id}.jnlp\" class=\"run_link rollover\" title=\"Preview the Resource Page: 'Foo' as a Java Web Start application. The first time you do this it may take a while to startup as the Java code is downloaded and saved on your hard drive.\"><img alt=\"Preview the Resource Page: 'Foo' as a Java Web Start application. The first time you do this it may take a while to startup as the Java code is downloaded and saved on your hard drive.\" src=\"/images/preview.png?1295966927\" /></a><a href=\"http://test.host/resource_pages/#{@resource_page.id}.jnlp\" title=\"Preview the Resource Page: 'Foo' as a Java Web Start application. The first time you do this it may take a while to startup as the Java code is downloaded and saved on your hard drive.\">run Jeff's Leiderhosen</a>"
+      link_text = "run Jeff's Leiderhosen"
+      helper.preview_link_for(@resource_page,
+                              nil,
+                              {:link_text => link_text}).
+                              should be_link_like("http://test.host/resource_pages/#{@resource_page.id}.jnlp",
+                                                  "run_link rollover",
+                                                  "/images/preview.png",
+                                                  link_text)
     end
   end
 
   describe ".run_link_for" do
     it "should render a run link for a given component" do
-      helper.run_link_for(@resource_page).should == "<a href=\"http://test.host/resource_pages/#{@resource_page.id}.jnlp\" class=\"run_link rollover\" title=\"Run the Resource Page: 'Foo' as a Java Web Start application. The first time you do this it may take a while to startup as the Java code is downloaded and saved on your hard drive.\"><img alt=\"Run the Resource Page: 'Foo' as a Java Web Start application. The first time you do this it may take a while to startup as the Java code is downloaded and saved on your hard drive.\" src=\"/images/run.png?1295966927\" /></a><a href=\"http://test.host/resource_pages/#{@resource_page.id}.jnlp\" title=\"Run the Resource Page: 'Foo' as a Java Web Start application. The first time you do this it may take a while to startup as the Java code is downloaded and saved on your hard drive.\">run </a>"
+      helper.run_link_for(@resource_page).should be_link_like("http://test.host/resource_pages/#{@resource_page.id}.jnlp",
+                                                              "run_link rollover",
+                                                              "/images/run.png",
+                                                              "run")
     end
 
     it "should render a run link for a given component with parameters" do
-      helper.run_link_for(@resource_page, nil, {:link_text => "run Biscuits"}).should == "<a href=\"http://test.host/resource_pages/#{@resource_page.id}.jnlp\" class=\"run_link rollover\" title=\"Run the Resource Page: 'Foo' as a Java Web Start application. The first time you do this it may take a while to startup as the Java code is downloaded and saved on your hard drive.\"><img alt=\"Run the Resource Page: 'Foo' as a Java Web Start application. The first time you do this it may take a while to startup as the Java code is downloaded and saved on your hard drive.\" src=\"/images/run.png?1295966927\" /></a><a href=\"http://test.host/resource_pages/#{@resource_page.id}.jnlp\" title=\"Run the Resource Page: 'Foo' as a Java Web Start application. The first time you do this it may take a while to startup as the Java code is downloaded and saved on your hard drive.\">run Biscuits</a>"
+      link_text = "run Biscuits"
+      helper.run_link_for(@resource_page,
+                          nil,
+                          {:link_text => "run Biscuits"}).
+                          should be_link_like("http://test.host/resource_pages/#{@resource_page.id}.jnlp",
+                                               "run_link rollover",
+                                               "/images/run.png",
+                                               link_text)
     end
 
     it "should render a link for a resource page" do
