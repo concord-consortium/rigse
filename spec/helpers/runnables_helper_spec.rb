957ba19bd7e162976a95754c02dd29f8a8879724
Dan Barron <dan@cantina.co>
2011-02-22 17:21:54 -0500

    whoops, forgot to add the new matcher to one test

--- spec/helpers/runnables_helper_spec.rb
+++ spec/helpers/runnables_helper_spec.rb
@@ -29,7 +29,6 @@ describe RunnablesHelper do
 
   describe ".teacher_preview_button_for" do
     it "should render a preview button in techer mode for a given component" do
-      #helper.teacher_preview_button_for(@resource_page).should == "<a href=\"http://test.host/resource_pages/#{@resource_page.id}.jnlp?teacher_mode=true\" class=\"run_link rollover\" title=\"Preview the Resource Page: 'Foo' as a Teacher. The first time you do this it may take a while to startup as the Java code is downloaded and saved on your hard drive.\"><img alt=\"Preview the Resource Page: 'Foo' as a Teacher. The first time you do this it may take a while to startup as the Java code is downloaded and saved on your hard drive.\" src=\"/images/teacher_preview.png?1297111962\" /></a>"
       helper.teacher_preview_button_for(@resource_page).should be_link_like("http://test.host/resource_pages/#{@resource_page.id}.jnlp",
                                                                              "run_link rollover",
                                                                              "/images/teacher_preview.png")
@@ -84,7 +83,9 @@ describe RunnablesHelper do
 
     it "should render a link for a JNLP type" do
       page = mock_model(Page, :name => "Fun With Hippos")
-      helper.run_link_for(page).should == "<a href=\"http://test.host/pages/#{page.id}\" class=\"run_link rollover\" title=\"Run the Page: 'Fun With Hippos' as a Java Web Start application. The first time you do this it may take a while to startup as the Java code is downloaded and saved on your hard drive.\"><img alt=\"Run the Page: 'Fun With Hippos' as a Java Web Start application. The first time you do this it may take a while to startup as the Java code is downloaded and saved on your hard drive.\" src=\"/images/run.png?1295966927\" /></a><a href=\"http://test.host/pages/#{page.id}\" onclick=\"window.open(this.href);return false;\">run </a>"
+      helper.run_link_for(page).should be_link_like("http://test.host/pages/#{page.id}",
+                                                    "run_link rollover",
+                                                    "/images/run.png")
     end
   end
 end
