214c8a958d38bf7add6e04865119cafb052db420
Dan Barron <dan@cantina.co>
2011-02-23 17:08:44 -0500

    more whistepsace

--- app/controllers/admin/projects_controller.rb
+++ app/controllers/admin/projects_controller.rb
@@ -1,44 +1,44 @@
 class Admin::ProjectsController < ApplicationController
-  
+
   before_filter :admin_only, :except => [:index, :edit, :update]
   before_filter :admin_or_manager, :only => [:index, :edit, :update]
   # before_filter :setup_object, :except => [:index]
   # before_filter :render_scope, :only => [:show]
 
   # editing / modifying / deleting require editable-ness
-  # before_filter :can_edit, :except => [:index,:show,:print,:create,:new,:duplicate,:export] 
+  # before_filter :can_edit, :except => [:index,:show,:print,:create,:new,:duplicate,:export]
   # before_filter :can_create, :only => [:new, :create,:duplicate]
-  # 
+  #
   # in_place_edit_for :activity, :name
   # in_place_edit_for :activity, :description
-  
-  protected 
+
+  protected
 
   def admin_only
     unless current_user.has_role?('admin')
-      flash[:notice] = "Please log in as an administrator" 
+      flash[:notice] = "Please log in as an administrator"
       redirect_to(:home)
     end
   end
-  
+
   def admin_or_manager
     if current_user.has_role?('admin')
       @admin_role = true
     elsif current_user.has_role?('manager')
       @manager_role = true
     else
-      flash[:notice] = "Please log in as an administrator or manager" 
+      flash[:notice] = "Please log in as an administrator or manager"
       redirect_to(:home)
     end
   end
-  
+
   public
-  
+
   # GET /admin/projects
   # GET /admin/projects.xml
   def index
     default_project = Admin::Project.default_project
-    
+
     if @manager_role
       @admin_projects = [default_project].paginate
     else
@@ -73,7 +73,7 @@ class Admin::ProjectsController < ApplicationController
   def new
     @admin_project = Admin::Project.new
     @scope = nil
-    
+
 
     respond_to do |format|
       format.html # new.html.erb
@@ -84,7 +84,7 @@ class Admin::ProjectsController < ApplicationController
   # GET /admin/projects/1/edit
   def edit
     @admin_project = Admin::Project.find(params[:id])
-    
+
     # Pull in the current theme default home page content, if it isn't set in the project.
     # This feels hackish, but there is no way to do this without fudging the controller_path if the
     # _project_info partial contains a nested render without a path, which it does (_project_summary).
@@ -94,11 +94,11 @@ class Admin::ProjectsController < ApplicationController
       self.class.instance_variable_set(:@controller_path, "home")
       render_to_string :partial => "home/project_info"
       self.class.instance_variable_set(:@controller_path, saved_path)
-      
+
       @admin_project.home_page_content = @template.instance_variable_get(:@content_for_project_info)
       @template.instance_variable_set(:@content_for_project_info, nil)
     end
-    
+
     if request.xhr?
       render :partial => 'remote_form', :locals => { :admin_project => @admin_project }
     end
@@ -152,7 +152,7 @@ class Admin::ProjectsController < ApplicationController
       format.xml  { head :ok }
     end
   end
-  
+
   def update_form
     if request.xhr?
       @admin_project = Admin::Project.new(params[:admin_project])
@@ -165,5 +165,4 @@ class Admin::ProjectsController < ApplicationController
       render :nothing => true
     end
   end
-  
 end
