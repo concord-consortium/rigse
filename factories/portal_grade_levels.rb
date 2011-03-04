4dd84fbfafbaee30768592929462eaf89669c2dd
Dan Barron <dan@cantina.co>
2011-03-04 11:31:07 -0500

    no cucumber failures

--- factories/portal_grade_levels.rb
+++ factories/portal_grade_levels.rb
@@ -1,4 +1,6 @@
 Factory.define :portal_grade_level, :class => Portal::GradeLevel do |f|
+  f.name "test"
+  f.association :grade, :factory => :portal_grade
 end
 
 Factory.define :full_portal_grade_level, :parent => :portal_grade_level  do |f|
