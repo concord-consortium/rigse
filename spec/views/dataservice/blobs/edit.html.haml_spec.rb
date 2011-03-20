4b5ac68b4a5869afc6ec26c65a64fb80ff1890b1
Scott Cytacki <scott@concord.org>
2011-03-20 10:08:53 -0400

    isolated views/dataservice/blobs spec test more

--- spec/views/dataservice/blobs/edit.html.haml_spec.rb
+++ spec/views/dataservice/blobs/edit.html.haml_spec.rb
@@ -4,9 +4,8 @@ describe "/dataservice/blobs/edit.html.haml" do
   include Dataservice::BlobsHelper
 
   before(:each) do
-    generate_default_project_and_jnlps_with_mocks
-    logout_user
-    login_admin
+    # cut off the edit_menu_for helper which traverses lots of other code
+    template.stub!(:edit_menu_for).and_return("edit menu")
     assigns[:dataservice_blob] = @dataservice_blob = stub_model(Dataservice::Blob,
       :new_record? => false, :id => 1, :token => "8ad04a50ba96463d80407cd119173b86", :content => ""
     )
--- spec/views/dataservice/blobs/show.html.haml_spec.rb
+++ spec/views/dataservice/blobs/show.html.haml_spec.rb
@@ -3,10 +3,12 @@ require 'spec_helper'
 describe "/dataservice/blobs/show.html.haml" do
   include Dataservice::BlobsHelper
   before(:each) do
-    generate_default_project_and_jnlps_with_mocks
-    logout_user
-    login_admin
-    assigns[:dataservice_blob] = @dataservice_blob = stub_model(Dataservice::Blob, :id => 1, :token => "8ad04a50ba96463d80407cd119173b86", :content => "")
+    # cut off the show_menu_for helper which traverses lots of other code
+    template.stub!(:show_menu_for).and_return("show menu")
+
+    # :changeable? => true prevents a current_user lookup, but will test if editing links correctly wrap the passed block
+    assigns[:dataservice_blob] = @dataservice_blob = stub_model(Dataservice::Blob, :id => 1, :token => "8ad04a50ba96463d80407cd119173b86", 
+      :content => "", :changeable? => true)
   end
 
   it "renders attributes in <p>" do
