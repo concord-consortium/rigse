71eee2c2121edf310ca5ccc28da131320d28a150
Noah Paessel <knowuh@gmail.com>
2011-03-21 11:54:10 -0400

    refactor M/V seperation.

--- public/javascripts/student_workgroup.js
+++ public/javascripts/student_workgroup.js
@@ -1,10 +1,10 @@
 //(function()
-  console.log("loading");
   var add_learner_button = null;
   var learners_list = null;
   var learners_dropdown = null;
   var add_button = null;
   var learners = null;
+  var collaborators = null;
   var password_panel = null;
   var login_button = null;
   var login_user_name = null;
@@ -12,7 +12,6 @@
 
   // startup
   var initialize_workgroup_ui = function() {
-    console.log("here we go");
     add_learner_button = $('add_learner');
     learners_container = $('learners_container');
     learners_list = $('learners_list');
@@ -23,107 +22,119 @@
     login_button =   $('login_button');
     login_user_name = $('login_user_name');
     password_field =  $('password_field');
-    add_button.observe('click',add_learner);
+    add_button.observe('click',function() {add_learner(selected_learner()); });
     learners = get_learners();
+    collaborators = [];
     learners.each (function(l) { add_learner_to_dropdown(l); });
-    check_visibility();
+    update_ui();
   };
 
-  var do_add_learner = function(learner) {
+  var add_learner = function(learner) {
     if(learner.have_consent) {
-      add_learner_to_list(learner);
-      remove_learner_from_dropdown(learner);
-      check_visibility();
+      learners = learners.without(learner);
+      learners = learners.sortBy(function(e) {return e.name;} );
+      collaborators.push(learner);
+      collaborators = collaborators.sortBy(function(e) {return e.name;} );
+      update_ui();
+    }
+    else {
+      ask_password(learner,function() {
+        add_learner(learner);
+      });
     }
   };
 
-  var add_learner = function(button) {
+  var remove_learner = function(learner) {
+    collaborators = collaborators.without(learner).sortBy(function(e) {return e.name;} );
+    learners.push(learner);
+    learners = learners.sortBy(function(e) {return e.name;} );
+    update_ui();
+  };
+
+  var selected_learner = function() {
     selected = learners_dropdown.select('option').find(function(ele){return !!ele.selected});
     var learner = learners.detect(function(l) {
       return l.id == parseInt(selected.value);
     });
-    if (learner.have_consent) {
-      do_add_learner(learner);
-    }
-    else {
-      ask_password(learner, function() {
-        do_add_learner(learner);
-      });
-    }
+    return learner;
   };
+
 
   // return learners in this learners class
   var get_learners = function() {
     // var learners = ajax.get('blah')
     var _learners = [
-      { name: 'John Doe',      id: 1, in_group: false, have_consent: false},
-      { name: 'Jane Doe',      id: 2, in_group: false, have_consent: false},
-      { name: 'Mark Schard',   id: 3, in_group: false, have_consent: false},
-      { name: 'Linda Low',     id: 4, in_group: false, have_consent: false},
-      { name: 'Steve Austin',  id: 5, in_group: false, have_consent: false},
-      { name: 'Dexter Tron',   id: 6, in_group: false, have_consent: false},
-      { name: 'Abraham Leaf',  id: 7, in_group: false, have_consent: false},
-      { name: 'Ada Paessel',   id: 8, in_group: false, have_consent: false},
-      { name: 'Linden Paessel', id: 9, in_group: false, have_consent: false},
-      { name: 'Dan Damian',    id: 10, in_group: false, have_consent: false}
+      { name: 'John Doe',      id: 1,  have_consent: false},
+      { name: 'Jane Doe',      id: 2,  have_consent: false},
+      { name: 'Mark Schard',   id: 3,  have_consent: false},
+      { name: 'Linda Low',     id: 4,  have_consent: false},
+      { name: 'Steve Austin',  id: 5,  have_consent: false},
+      { name: 'Dexter Tron',   id: 6,  have_consent: false},
+      { name: 'Abraham Leaf',  id: 7,  have_consent: false},
+      { name: 'Ada Paessel',   id: 8,  have_consent: false},
+      { name: 'Linden Paessel', id: 9, have_consent: false},
+      { name: 'Dan Damian',    id: 10, have_consent: false}
     ]
     _learners = _learners.sortBy(function(e) {return e.name;} );
     return _learners;
   };
 
 
-  var learner_id_for = function(learner,scope) {
-    return "_LEARNER_" + scope + learner.id;
+  var learner_id_for = function(learner) {
+    return "_LEARNER_" + learner.id;
   };
 
-  var add_learner_to_list = function(learner) {
-    learner.in_group = true;
+  var add_learner_to_container = function(learner) {
     var learner_el = new Element('li',
       {
         'class': 'learner',
-        id: learner_id_for(learner,'container'),
+        id: learner_id_for(learner),
         value: learner.i,
       }).update(learner.name);
     var delete_button = new Element('span', {
-      'class': 'delete',
-      id: learner_id_for(learner,'remove'),
+      'class': 'delete'
     }).update('remove');
     learners_list.insert({ bottom: learner_el});
     learner_el.insert({bottom: delete_button});
-    delete_button.observe('click',function() {remove_learner_from_container(learner);});
+    delete_button.observe('click',function() {remove_learner(learner);});
   };
 
   var remove_learner_from_container = function(learner) {
-    learner.in_group = false;
-    $(learner_id_for(learner,'container')).remove();
-    add_learner_to_dropdown(learner);
-    var selected = learners.select(function(e) { return e.in_group == true});
-    check_visibility();
+    var learner_dom = $(learner_id_for(learner));
+    if (learner_dom) {
+      learner_dom.remove();
+    }
   };
- 
-  var check_visibility = function() {
-    var selected = learners.select(function(e) { return e.in_group == true});
-    var not_selected = learners.select(function(e) { return e.in_group == false});
-    if (selected.size() < 1) { 
-      learners_container.hide(); 
+
+
+  var update_ui = function() {
+    learners.each(function(learner) {
+      remove_learner_from_container(learner);
+      add_learner_to_dropdown(learner);
+    });
+    collaborators.each(function(learner){
+      remove_learner_from_dropdown(learner);
+      add_learner_to_container(learner);
+    });
+    if (collaborators.size() < 1) {
+      learners_container.hide();
     }
     else {
       learners_container.show();
     }
-    if (not_selected.size() < 1) { 
-      add_group.hide(); 
+    if (learners.size() < 1) {
+      add_group.hide();
     }
     else {
       add_group.show();
     }
   };
-  
+
   var add_learner_to_dropdown = function (learner) {
-    learner.in_group = false;
     var learner_el = new Element('option',
       {
         'class': 'learner',
-        id: learner_id_for(learner,'dropdown'),
+        id: learner_id_for(learner),
         value: learner.id
       }).update(learner.name);
     learners_dropdown.insert({ bottom: learner_el})
@@ -131,9 +142,12 @@
 
   var remove_learner_from_dropdown = function (learner) {
     var id = learner_id_for(learner,"dropdown");
-    $(id).remove();
+    var learner_dom = $(id);
+    if (learner_dom) {
+      $(id).remove();
+    }
   };
-  
+
   var check_password = function(learner,passwd) {
     if (passwd == 'password') {
       return true;
@@ -163,9 +177,7 @@
   }
 
   document.observe("dom:loaded", function() {
-    console.log("Yup here we go");
     initialize_workgroup_ui();
-    console.log("Yup there we went");
   });
 
 //}());
