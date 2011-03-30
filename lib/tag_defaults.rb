85edbb8e9718621b1e1fcf79225f655735148879
Dan Barron <dan@cantina.co>
2011-03-30 15:11:38 -0400

    more activity stuff

--- lib/tag_defaults.rb
+++ lib/tag_defaults.rb
@@ -4,59 +4,172 @@ module TagDefaults
   ## Called when a class is extended by this module
   ##
   def self.included(clazz)
-    ## add before_save hooks
-    clazz.class_eval {
-      @@default_tags = {
+    clazz.extend(ClassMethods)
+    #clazz.class_eval do
+      #after_save :update_available_tags
+    #end
+  end
+  # class methods
+  module ClassMethods
+    def found_tags
+      results = {}
+      self.tag_types.each do |type|
+        results[type] ||= []
+        results[type] = results[type] +  self.tag_counts_on(type).map { |tc| tc.name }
+        results[type] = results[type].uniq.sort
+      end
+      results
+    end
+    
+    def default_tags
+      {
         :units => "Crystals, Global Warming, Earthquakes, Water Cycle, Solar System, Weather".split(","),
         :grade_levels => "Elementary, Middle School, High School".split(","),
         :subject_areas => "Earth Science, Space Science, Life Science, Physics, Biology, Chemestry".split(",")
       }
-      
-      # look through our records and update our defaults...
-      self.tag_types.each do |type|
-        if (@@default_tags[type])
-          @@default_tags[type] = @@default_tags[type].map { |c| c.strip } | self.tag_counts_on(type).map { |c| c.name.strip}
+    end
+
+    def add_tag(scope,tag)
+      unless self.default_tags[scope]
+        self.default_tags[scope] = []
+      end
+      self.default_tags[scope] << tag.strip
+      self.default_tags[scope].uniq!
+      self.default_tags[scope].sort!
+      self.default_tags[scope]
+    end
+
+    def available_tags(scope=nil)
+      if scope
+        if self.default_tags[scope] && self.default_tags[scope].size > 0
+          return self.default_tags[scope].map { |i| i.strip}
         else
-          @@default_tags[type] = self.tag_counts_on(type).map { |c| c.name.strip}
+          return []
         end
       end
-      
-      # class methods
-      def self.add_tag(scope,tag)
-        unless @@default_tags[scope]
-          @@default_tags[scope] = []
-        end
-        @@default_tags[scope] << tag.strip
-        @@default_tags[scope].uniq!
-      end
+      return self.default_tags.values.flatten.uniq.map { |i| i.strip}
+    end
 
-      def self.available_tags(scope)
-        if scope
-          if @@default_tags[scope] && @@default_tags[scope].size > 0
-            return @@default_tags[scope].map { |i| i.strip}
-          else
-            return []
-          end
+    def find_by_bin(grade_level,subject_area)
+      results = {}
+      found = self.published.tagged_with(grade_level, :on => :grade_level).tagged_with(subject_area, :on => :subject_area)
+      found.each do |act|
+        act.units.each do |unit|
+          results[unit] ||= []
+          results[unit] << act
         end
-        return @@default_tags.values.flatten.uniq.map { |i| i.strip}
-      end
-    
-      # after we are saved, add our tags.
-      after_save :update_available_tags
-
-    }
-  end
-  
-  # check for new tags in this taggable thing
-  def update_available_tags
-    self.tag_types.each do |type|
-      if (@@default_tags[type])
-        @@default_tags[type] = @@default_tags[type] | self.tag_counts_on(type).map { |c| c.name.strip}
-      else
-        @@default_tags[type] = self.tag_counts_on(type).map { |c| c.name.strip}
       end
     end
+
+    def list_bins(user=nil)
+      grade_levels  = self.grade_level_counts
+      subject_areas = self.subject_area_counts
+      units = self.unit_counts.map { |u| u.name }
+      results = []
+      grade_levels.sort{|a,b| a.name <=> b.name}.each_with_index do |grade_level,i|
+        subject_areas.sort{|a,b| a.name <=> b.name}.each_with_index do |subject,j|
+          query = self.published
+          query = query.tagged_with(grade_level.name, :on => :grade_levels)
+          query = query.tagged_with(subject.name, :on => :subject_areas)
+          unit_listing = units.map do |u| 
+            u_query = query.tagged_with(u, :on => :units)
+            { :name => u,
+              :count => u_query.count,
+              :activities => u_query
+            }
+          end
+          unit_listing.reject! { |u| u[:count] < 1 }
+          record = {
+            :key          => "#{grade_level.name}#{subject.name}".gsub(/\s+/,"").downcase,
+            :name         => "#{grade_level.name} #{subject.name}",
+            :classes      => "unit-navigation level#{i+1}",
+            :grade_level  => grade_level.name,
+            :subject_area => subject.name,
+            :query        => query,
+            :units        => unit_listing,
+            :count        => query.count
+          }
+          results << record
+        end
+        if user
+          users = self.find(:all, :conditions => {:user_id => user.id});
+          remainder = users.clone
+          unit_listing = units.map do |u| 
+            unit_activities = users.select { |a| a.unit_list && a.unit_list.include?(u) }
+             { 
+              :name => u,
+              :count => unit_activities.size,
+              :activities => unit_activities
+            }
+            remainder = remainder - unit_activities
+          end
+
+          if remainder.size > 0
+            unit_record = {
+              :name => "no assigned unit",
+              :count => remainder.size,
+              :activities => remainder
+            }
+            unit_listing << [unit_record]
+          end
+
+          record = {
+            :key          => "users_own",
+            :name         => "Your activities",
+            :classes      => "unit-navigation level#{i+1}",
+            :units        => unit_listing,
+            :count        => users.size,
+            :query        => users
+          } 
+          results << record 
+        end
+      end
+      results.each
+      results.sort {|a,b| a[:key] <=> b[:key] }.uniq
+    end
+  
+    #def keys(tags=self.tag_types)
+      #results = []
+      #_keys = tags.each do |t| 
+        #counts = self.send("#{t.to_s.singularize}_counts".to_sym)
+        #results << counts.map { |count| count.name }
+      #end
+
+      #_keys = results.compact
+      ## [["1", "2", "3"], ["1", "2", "3", "4"], ["A","B"], ["a","b"]] 
+        ##
+        ##
+      ## [ "1-1-A-a", "1-1-A-b", "1-1-B-a", "1-1-B-b", ]
+      ## [1,2] [a,b] => [1-a, 1-b, 2-a, 2 -b]
+      #index = _keys.size() -1
+      #while index > 0
+        #_keys[index-1].map! do |k|
+          #_keys[index].flatten.map do |kk|
+            #"#{k}-#{kk}"
+          #end
+        #end
+        #_keys[index-1].flatten!
+        #index = index - 1
+      #end
+      #_keys[0]
+    #end
   end
 
-  
+  def random_tags
+    self.grade_level_list = self.class.default_tags[:grade_levels].rand
+    self.unit_list = self.class.default_tags[:units].rand
+    self.subject_area_list = self.class.default_tags[:subject_areas].rand
+  end
+
+  def keys
+    results = []
+    self.grade_level_list.each do |grade|
+      self.subject_area_list.each do |subject|
+        self.unit_list.each do |unit|
+          results << "#{grade} #{subject} #{unit}"
+        end
+      end
+    end
+    results
+  end
 end
