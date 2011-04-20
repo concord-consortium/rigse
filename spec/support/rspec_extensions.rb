dcf85e01561f9669d52022cc433579151bb4a674
Aaron Unger <aunger@concord.org>
2011-04-20 11:38:01 -0400

    Rspec extensions should also capture mock errors (e.g. foo.should_receive(:bar) )

--- spec/support/rspec_extensions.rb
+++ spec/support/rspec_extensions.rb
@@ -1,11 +1,15 @@
 module Spec
   module Example
     module FailsInThemes
+      # wrapping mocks only works with the rspec mock framework right now...
       def fails_in_themes(themes = {})
+        $previous_mockspace = $rspec_mocks
+        $rspec_mocks = Spec::Mocks::Space.new
         if block_given?
           error = nil
           begin
             yield
+            verify_mocks_for_rspec
             error = nil
           rescue Exception => e
             error = e
@@ -23,6 +27,10 @@ module Spec
           # require a block to be passed in
           raise "No block passed for fails_in_themes"
         end
+      ensure
+        teardown_mocks_for_rspec
+        $rspec_mocks = $previous_mockspace
+        $previous_mockspace = nil
       end
     end
 
