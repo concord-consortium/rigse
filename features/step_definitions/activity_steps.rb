1855e8acb9f6af8ad0983e421adb5a7e07b5ad8c
Scott Cytacki <scytacki@concord.org>
2011-04-28 01:00:33 -0400

    handle case where activity.description is required

--- features/step_definitions/activity_steps.rb
+++ features/step_definitions/activity_steps.rb
@@ -17,9 +17,9 @@ end
 #Table: | activity | section   | page   | multiple_choices |
 Given /^the following activities with multiple choices exist:$/ do |activity_table|
   activity_table.hashes.each do |hash|
-    activity = Activity.find_or_create_by_name(hash['activity'])
+    activity = Activity.find_or_create_by_name(hash['activity'], :description => hash['activity'])
     activity.user = Factory(:user)
-    activity.save
+    activity.save.should be_true
     section = Section.find_or_create_by_name(hash['section'])
     page = Page.find_or_create_by_name(hash['page'])
     mcs = hash['multiple_choices'].split(",").map{ |q| Embeddable::MultipleChoice.find_by_prompt(q.strip) }
