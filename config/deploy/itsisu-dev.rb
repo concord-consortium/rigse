5349a5edd7cc6406db3366c2d603ca927a215ae9
Scott Cytacki <scytacki@concord.org>
2011-05-10 10:57:48 -0400

    allow and prevent db uploading correctly

--- config/deploy/itsisu-dev.rb
+++ config/deploy/itsisu-dev.rb
@@ -13,18 +13,18 @@ server domain, :app, :web
 role :db, domain, :primary => true
 
 namespace :db do
- desc 'Loads the production database in db/production_data.sql on the remote server'
- task :remote_db_load, :roles => :db, :only => { :primary => true } do
-   puts "This task is disabled for the production environment"
- end
-  
- desc 'Uploads db/production_data.sql to the remote production environment from your local machine'
- task :remote_db_upload, :roles => :db, :only => { :primary => true } do  
-   puts "This task is disabled for the production environment"
- end
-
- desc 'Uploads, inserts, and then cleans up the production data dump'
- task :push_remote_db do
-   puts "This task is disabled for the production environment"
- end
+ # desc 'Loads the production database in db/production_data.sql on the remote server'
+ # task :remote_db_load, :roles => :db, :only => { :primary => true } do
+ #   puts "This task is disabled for the production environment"
+ # end
+ #  
+ # desc 'Uploads db/production_data.sql to the remote production environment from your local machine'
+ # task :remote_db_upload, :roles => :db, :only => { :primary => true } do  
+ #   puts "This task is disabled for the production environment"
+ # end
+ # 
+ # desc 'Uploads, inserts, and then cleans up the production data dump'
+ # task :push_remote_db do
+ #   puts "This task is disabled for the production environment"
+ # end
 end
