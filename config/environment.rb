3a093838b4e6f6ceabe237bf49632f2a6124e79c
Scott Cytacki <scytacki@concord.org>
2011-05-22 20:57:41 -0400

    switch session config to the more modern location

--- config/environment.rb
+++ config/environment.rb
@@ -72,19 +72,6 @@ Rails::Initializer.run do |config|
   # Run "rake -D time" for a list of tasks for finding time zone names. Comment line to use default local time.
   config.time_zone = 'UTC'
 
-  # Your secret key for verifying cookie session data integrity.
-  # If you change this key, all old sessions will become invalid!
-  # Make sure the secret is at least 30 characters and all random, 
-  # no regular words or you'll be exposed to dictionary attacks.
-  config.action_controller.session = {
-    :session_key => '_bort_session',
-    :secret      => 'a3e2a51a371bb964a4250c21f8d083f9ddb224d455171dcba55518e74af43366e52e3f239773f90aed0ab6caf6554f051504ce7232599d066150dbabff0f1654'
-  }
-
-  # Use the database for sessions instead of the cookie-based default,
-  # which shouldn't be used to store highly confidential information
-  # (create the session table with "rake db:sessions:create")
-  
   # Set the default location for page caching
   config.action_controller.page_cache_directory = RAILS_ROOT + '/public'
 
@@ -116,8 +103,6 @@ Rails::Initializer.run do |config|
     end
   end
 
-
-  config.action_controller.session_store = :active_record_store
 
   # config.after_initialize do
   #   opts = config.has_many_polymorphs_options
