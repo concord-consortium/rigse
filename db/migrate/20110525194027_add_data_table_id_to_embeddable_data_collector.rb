6c01ecbfa58a85891c0bcb1853d5b04452a36435
Noah Paessel <knowuh@gmail.com>
2011-06-03 18:49:43 -0400

    Guard against null entities.

--- db/migrate/20110525194027_add_data_table_id_to_embeddable_data_collector.rb
+++ db/migrate/20110525194027_add_data_table_id_to_embeddable_data_collector.rb
@@ -3,11 +3,14 @@ class AddDataTableIdToEmbeddableDataCollector < ActiveRecord::Migration
     add_column :embeddable_data_collectors, :data_table_id, :integer
     Embeddable::DataTable.find(:all).each do |dt|
       unless dt.data_collector_id.nil?
-        collector = Embeddable::DataCollector.find(dt.data_collector_id)
-        if collector
-          collector.data_table_id = dt.id
-          collector.save
-          puts "inverted one data table <-> data collector relationship"
+        begin
+          collector = Embeddable::DataCollector.find(dt.data_collector_id)
+          if collector
+            collector.data_table_id = dt.id
+            collector.save
+            puts "inverted one data table <-> data collector relationship"
+          end
+        rescue
         end
       end
     end
@@ -16,13 +19,16 @@ class AddDataTableIdToEmbeddableDataCollector < ActiveRecord::Migration
   def self.down
     Embeddable::DataCollector.find(:all).each do |dc|
       unless dc.data_table_id.nil?
-        table = Embeddable::DataTable.find(dc.data_table_id)
-        if table
-          if table.data_collector_id != dc.id
-            table.data_collector_id = dc.id
-            table.save
-            puts "reverted one data table <-> data collector relationship"
+        begin
+          table = Embeddable::DataTable.find(dc.data_table_id)
+          if table
+            if table.data_collector_id != dc.id
+              table.data_collector_id = dc.id
+              table.save
+              puts "reverted one data table <-> data collector relationship"
+            end
           end
+        rescue
         end
       end
     end
