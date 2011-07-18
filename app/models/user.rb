5478ca0eba2c061d3f1407188207b595605ff23c
Noah Paessel <knowuh@gmail.com>
2011-07-18 15:19:24 -0400

    Make external_id accessable.

--- app/models/user.rb
+++ app/models/user.rb
@@ -160,7 +160,7 @@ class User < ActiveRecord::Base
   # HACK HACK HACK -- how to do attr_accessible from here?
   # prevents a user from submitting a crafted form that bypasses activation
   # anything else you want your user to change should be added here.
-  attr_accessible :login, :email, :first_name, :last_name, :password, :password_confirmation, :vendor_interface_id
+  attr_accessible :login, :email, :first_name, :last_name, :password, :password_confirmation, :vendor_interface_id, :external_id
 
   # Authenticates a user by their login name and unencrypted password.  Returns the user or nil.
   def self.authenticate(login, password)
