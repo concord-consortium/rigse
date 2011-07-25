8f3edcd255c3bf9374ee9c8c0574429348c7b040
Noah Paessel <knowuh@gmail.com>
2011-07-25 15:04:15 -0400

    Change from module to class. Provide singleton.
    Because parsing yaml files shouldn't happen on every request, even in dev mode.

--- lib/local_names.rb
+++ lib/local_names.rb
@@ -1,7 +1,26 @@
-module LocalNames
+class LocalNames
 
   attr_accessor :local_names
   attr_accessor :yaml_file
+  attr_accessor :logger
+  attr_accessor :theme_name
+
+
+  # Lots of nay-saying about singleton patterns be-damned.
+  class << self
+    def instance(theme)
+      @instances = {} unless @instances
+      unless @instances[theme]
+        @instances[theme] = self.new(theme)
+      end
+      return @instances[theme]
+    end
+  end
+
+  def initialize(theme  = APP_CONFIG[:theme] || 'default')
+    self.theme_name = theme
+    self.logger = Rails.logger
+  end
 
   def load_names(path=File.join("config","local_names.yml"))
     self.yaml_file = File.join(RAILS_ROOT, path)
@@ -16,14 +35,14 @@ module LocalNames
   
   def parse_yaml(string)
     config = YAML::load(string)
-    names = config["names"]
+    names = config[theme_name]
     unless names
       logger.warn("No 'names' specified in #{self.yaml_file}")
       self.local_names = {}
       return
     end
     unless names.kind_of? Hash
-      logger.warn("'names' should be specified as Hash in #{self.yaml_file}")
+      logger.warn("no entry for #{self.theme_name} specified as Hash in #{self.yaml_file}")
       self.local_names = {}
       return
     end
@@ -45,4 +64,3 @@ module LocalNames
     return self.local_names[key] || self.local_names[key.downcase] || default
   end
 end
-
--- spec/libs/local_names_spec.rb
+++ spec/libs/local_names_spec.rb
@@ -1,15 +1,9 @@
 require File.expand_path('../../spec_helper', __FILE__)
 
-# class including modules under test
-class ClassWithLocalNames
-  attr_accessor :logger
-  include LocalNames
-end
-
 def valid_yaml
 return <<YAML
 ---
-names:
+my_theme:
   Hash : this is a Hash
   someString : a string replacement
 YAML
@@ -21,9 +15,9 @@ end
 
 describe LocalNames do
   before(:each) do
-    @class_with_local_names = ClassWithLocalNames.new
+    @instance = LocalNames.instance('my_theme')
     @logger = mock("Logger")
-    @class_with_local_names.logger=@logger
+    @instance.logger=@logger
     @mock_file = mock("File")
   end
   describe "Loading local names from a file config/local_names.yml" do
@@ -33,7 +27,7 @@ describe LocalNames do
       end
       it "should not cause an error, but should report a warning" do
         @logger.should_receive(:warn)
-        @class_with_local_names.load_names
+        @instance.load_names
       end
     end
 
@@ -44,7 +38,7 @@ describe LocalNames do
       it "should not cause an error, but should produce a warning message" do
         @mock_file.should_receive(:read).and_return(invalid_yaml)
         @logger.should_receive(:warn)
-        @class_with_local_names.load_names
+        @instance.load_names
       end
     end
   end 
@@ -53,27 +47,27 @@ describe LocalNames do
     before(:each) do
       File.stub!(:open).and_return(@mock_file)
       @mock_file.should_receive(:read).and_return(valid_yaml)
-      @class_with_local_names.load_names
+      @instance.load_names
     end
     it "should have local names" do
-      @class_with_local_names.local_names.should_not be_nil
+      @instance.local_names.should_not be_nil
     end
     describe "Asking for local names" do
       describe "When a local replacement has been defined" do
         it "should use the replacement" do
-          @class_with_local_names.local_name_for({}).should == "this is a Hash"
-          @class_with_local_names.local_name_for("someString").should == "a string replacement"
+          @instance.local_name_for({}).should == "this is a Hash"
+          @instance.local_name_for("someString").should == "a string replacement"
         end
       end
       describe "When no local replacement has been defined" do
         describe "When the caller specifies a default value" do  
           it "should use the default replacement value" do
-            @class_with_local_names.local_name_for(self,"default").should == "default"
+            @instance.local_name_for(self,"default").should == "default"
           end
         end
         describe "When the caller doesn't specify a default" do
           it "should use the titlecased human readable class name" do
-            @class_with_local_names.local_name_for(@class_with_local_names).should == "Class With Local Names"
+            @instance.local_name_for(@instance).should == "Local Names"
           end
         end
       end
