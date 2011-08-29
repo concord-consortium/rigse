d716c6c188b0c7c2fedbd22a7c2cf622f639645a
Stephen Bannasch <stephen.bannasch@gmail.com>
2011-08-29 01:33:38 -0400

    define constant JRUBY

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,6 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
+JRUBY = defined? RUBY_ENGINE && RUBY_ENGINE == 'jruby'
 # Initialize the rails application
 RailsPortal::Application.initialize!
