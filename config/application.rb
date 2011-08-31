022e177745d29db6187c9750e4ecc84b24ab2a23
Noah Paessel <knowuh@gmail.com>
2011-08-31 16:10:20 -0400

    Include the lib directory itself -not just subdirs

--- config/application.rb
+++ config/application.rb
@@ -53,7 +53,7 @@ module RailsPortal
     # Add additional load paths for your own custom dirs
     # config.load_paths += %W( #{::Rails.root.to_s}/extras )
     config.autoload_paths += Dir["#{config.root}/lib/**/"] # include all subdirectories
-    # config.autoload_paths += Dir["#{config.root}/lib/*"]  # dont include subdirectories yet..
+    config.autoload_paths += Dir["#{config.root}/lib/*"]   
     # Force all environments to use the same logger level
     # (by default production uses :info, the others :debug)
     # config.log_level = :debug
