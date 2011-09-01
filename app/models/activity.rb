12d206f782a9e34d7bd4d218bbb66ab0e2ecc0eb
Stephen Bannasch <stephen.bannasch@gmail.com>
2011-09-01 13:57:47 -0400

    remove invalid multi-byte char

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -161,53 +161,53 @@ class Activity < ActiveRecord::Base
     )
   end
 
-@@opening_xhtml= <<HEREDOC
-  <h3>Procedures</h3>
-  <p><em>What activities will you and your students do and how are they connected to the objectives?</em></p>
-  <p></p>
-  <h4>What will you be doing?</h4>
-  <p><em>How do you activate and assess students’ prior knowledge and connect it to this new learning?</em></li>
-  <p></p>
-  <p><em>How do you get students engaged in this lesson?</em></li>
-  <p></p>
-  <h4>What will the students be doing?</h4>
-  <p><em>Students will discuss the following driving question:</em></p>
-  <p></p>
-  <p><em>Key components:</p>
-  <p></p>
-  <p><em>Starting conditions:</p>
-  <p></p>
-  <p><em>Ability to change variables:</p>
-  <p></p>
-HEREDOC
+  @@opening_xhtml = <<-HEREDOC
+<h3>Procedures</h3>
+<p><em>What activities will you and your students do and how are they connected to the objectives?</em></p>
+<p></p>
+<h4>What will you be doing?</h4>
+<p><em>How do you activate and assess students' prior knowledge and connect it to this new learning?</em></li>
+<p></p>
+<p><em>How do you get students engaged in this lesson?</em></li>
+<p></p>
+<h4>What will the students be doing?</h4>
+<p><em>Students will discuss the following driving question:</em></p>
+<p></p>
+<p><em>Key components:</p>
+<p></p>
+<p><em>Starting conditions:</p>
+<p></p>
+<p><em>Ability to change variables:</p>
+<p></p>
+  HEREDOC
 
-@@engagement_xhtml= <<HEREDOC
-  <h3>Engagement</h3>
-  <h4>What will you be doing?</h4>
-  <p><em>What questions can you pose to encourage students to take risks and to deepen students’ understanding?</em></p>
-  <p></p>
-  <p><em>How do you facilitate student discourse?</em></p>
-  <p></p>
-  <p><em>How do you facilitate the lesson so that all students are active learners and reflective during this lesson?</em></p>
-  <p></p>
-  <p><em>How do you monitor students’ learning throughout this lesson?</em></p>
-  <p></p>
-  <p><em>What formative assessment is imbedded in the lesson?</em></p>
-  <p></p>
-  <h4>What will the students be doing?</h4>
-  <p></p>
-HEREDOC
+  @@engagement_xhtml = <<-HEREDOC
+<h3>Engagement</h3>
+<h4>What will you be doing?</h4>
+<p><em>What questions can you pose to encourage students to take risks and to deepen students' understanding?</em></p>
+<p></p>
+<p><em>How do you facilitate student discourse?</em></p>
+<p></p>
+<p><em>How do you facilitate the lesson so that all students are active learners and reflective during this lesson?</em></p>
+<p></p>
+<p><em>How do you monitor students' learning throughout this lesson?</em></p>
+<p></p>
+<p><em>What formative assessment is imbedded in the lesson?</em></p>
+<p></p>
+<h4>What will the students be doing?</h4>
+<p></p>
+  HEREDOC
 
-@@closure_xhtml= <<HEREDOC
-  <h3>Closure</h3>
-  <h4>What will you be doing?</h4>
-  <p><em>What kinds of questions do you ask to get meaningful student feedback?</em></p>
-  <p></p>
-  <p><em>What opportunities do you provide for students to share their understandings of the task(s)?</em></p>
-  <p></p>
-  <h4>What will the students be doing?</h4>
-  <p></p>
-HEREDOC
+  @@closure_xhtml = <<-HEREDOC
+<h3>Closure</h3>
+<h4>What will you be doing?</h4>
+<p><em>What kinds of questions do you ask to get meaningful student feedback?</em></p>
+<p></p>
+<p><em>What opportunities do you provide for students to share their understandings of the task(s)?</em></p>
+<p></p>
+<h4>What will the students be doing?</h4>
+<p></p>
+  HEREDOC
 
   # TODO: we have to make this container nuetral,
   # using parent / tree structure (children)
