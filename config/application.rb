6e7c5106165be90e3ac58f2feaceb1df425a3007
Stephen Bannasch <stephen.bannasch@gmail.com>
2011-09-01 13:57:20 -0400

    require 'paperclip' in application controller
    I'll bet there is some way we have changed the way
    this rails3 app starts up that is different than a 
    normal rails3 app. Requires like the following shouldn't
    be needed in the application controller:
    
    require 'acts-as-taggable-on'
    require 'paperclip'

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,7 @@
 require File.expand_path('../boot', __FILE__)
 require 'rails/all'
 require 'acts-as-taggable-on'
+require 'paperclip'
 
 module RailsPortal
   class Application < Rails::Application
