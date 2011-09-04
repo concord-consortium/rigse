91a58b324b43f45ff0517bc8b8ea832bc5abdbda
Stephen Bannasch <stephen.bannasch@gmail.com>
2011-09-03 22:47:39 -0400

    dperecation fix: use ::Rails.env

--- config/initializers/02_app.rb
+++ config/initializers/02_app.rb
@@ -31,7 +31,7 @@
 #   Mysql::Error.ancestors
 #   => [Mysql::Error, StandardError, ...
 #
-RUNNING_TESTS = RAILS_ENV == 'test' || ::Rails.env == 'cucumber'
+RUNNING_TESTS = (::Rails.env == 'test') || (::Rails.env == 'cucumber')
 
 if RUNNING_TESTS || $PROGRAM_NAME =~ /rake/
   puts "Executing rake task or running in test/cucumber env: skipping Admin::Project.create_or_update_default_project_from_settings_yml"
