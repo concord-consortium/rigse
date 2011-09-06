a02dbfecbf61b1737c9d1e16bfaea62bdca31e72
Stephen Bannasch <stephen.bannasch@gmail.com>
2011-09-06 13:54:26 -0400

    use parens around args to defined?()

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,7 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
-JRUBY = defined? RUBY_ENGINE && RUBY_ENGINE == 'jruby'
+JRUBY = defined?(RUBY_ENGINE) && RUBY_ENGINE == 'jruby'
 
 require 'yaml'
 YAML::ENGINE.yamler = "psych"
