7a3a2b5344d5eee4b0a9716838cabfa5c230770d
Stephen Bannasch <stephen.bannasch@gmail.com>
2011-09-06 19:51:50 -0400

    fix: have_tag => have_selector lost in rebase resolution

--- spec/views/admin/tags/edit.html.erb_spec.rb
+++ spec/views/admin/tags/edit.html.erb_spec.rb
@@ -15,8 +15,7 @@ describe "/admin/tags/edit.html.haml" do
 
   it "renders the edit tags form" do
     render
-
-    rendered.should have_tag("form[action=#{admin_tag_path(@admin_tag)}][method=post]") do
+    response.should have_selector("form[action=#{admin_tag_path(@admin_tag)}][method=post]") do
       with_tag('input#admin_tag_scope[name=?]', "admin_tag[scope]")
       with_tag('input#admin_tag_tag[name=?]', "admin_tag[tag]")
     end
