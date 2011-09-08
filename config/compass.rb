c2351754c105d02e3ea20b7306936cfa439834f8
Richard Klancer <rpk@pobox.com>
2011-09-07 20:47:22 -0400

    Update config/compass.rb
    This was generated using the command 
    bin/compass config config/compass.rb --sass-dir public/stylesheets/scss/ --css-dir public/stylesheets/ -s compact --force

--- config/compass.rb
+++ config/compass.rb
@@ -4,9 +4,10 @@ project_type = :rails
 
 # Set this to the root of your project when deployed:
 http_path = "/"
+css_dir = "public/stylesheets/"
+sass_dir = "public/stylesheets/scss/"
 
-# You can select your preferred output style here (can be overridden via the command line):
-# output_style = :expanded or :nested or :compact or :compressed
+output_style = :compact
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
 # relative_assets = true
@@ -19,4 +20,4 @@ http_path = "/"
 # project again passing --syntax sass, or you can uncomment this:
 # preferred_syntax = :sass
 # and then run:
-# sass-convert -R --from scss --to sass app/stylesheets scss && rm -rf sass && mv scss sass
+# sass-convert -R --from scss --to sass public/stylesheets/scss scss && rm -rf sass && mv scss sass
