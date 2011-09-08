9ac1efe33bc8f52f0503c1c53344f18a66330edb
Stephen Bannasch <stephen.bannasch@gmail.com>
2011-09-08 18:13:44 -0400

    yield and html_safe problems rendering otml
    This needs more work and then to be applied more generally.
    
    The html safe part of the problem could be helped
    if we could turn off Rails 3 SafeBuffer's when rendering otml.
    
    The yield problem is more mysterious.
    
    1) the yield fails, 
    2) if it worked it would return the empty string which is not false

--- app/helpers/otml_helper.rb
+++ app/helpers/otml_helper.rb
@@ -43,6 +43,30 @@ module OtmlHelper
     "#{prefix}#{class_name}_#{component.id}"
   end
   
+  def yield_object_or_reference(embeddable)
+    content = ''
+    #
+    # FIXME: the following test shouldn't be needed
+    # Rails 3 should be returning an empty string if the block being yielded to can't be found
+    # See: http://asciicasts.com/episodes/227-upgrading-to-rails-3-part-3
+    # But instead Rails returns:
+    #   ActionView::Template::Error (no block given (yield)):
+    # which caused caused by a more basic Ruby error:
+    #   LocalJumpError Exception: no block given (yield)
+    #
+    # In fact I'm not sure when this condition IS true??
+    #
+    if block_given?
+      content_generator = "element_#{ot_dom_id_for(embeddable)}".to_sym
+      Raise "*** FIXME: OtmlHelper#yield_object_or_reference ***"
+      content = yield(content_generator)
+    end
+    if content.length == 0
+      content = "<object refid=\"#{ot_refid_for(embeddable)}\"/>"
+    end
+    content.html_safe
+  end
+  
   def data_filter_inports
     Probe::DataFilter.find(:all).collect { |df| df.otrunk_object_class }
   end
