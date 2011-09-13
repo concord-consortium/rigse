c70efd3cea1b4e1c0930945850bdfb489adf0ee0
Scott Cytacki <scytacki@concord.org>
2011-09-13 14:26:01 -0400

    split out view tests from investigations_controller
    This way one set of tests can be super simple just focusing on the code in the controller itself.  
    The non otml view tests are moved to @wip cucumber tests of investigation duplication
    The otml view tests are moved to a new otml specific spec test.  It might make sense to even move that view spec test out into cucumber as well.

--- spec/controllers/investigations_controller_spec.rb
+++ spec/controllers/investigations_controller_spec.rb
@@ -1,80 +1,32 @@
 require File.expand_path('../../spec_helper', __FILE__)#include ApplicationHelper
 
 describe InvestigationsController do
-  render_views
-
   before(:each) do
-    @current_project = mock(
-      :name => "test project",
-      :using_custom_css? => false,
-      :use_bitmap_snapshots? => false,
-      :maven_jnlp_family => nil,
-      :snapshot_enabled => false)
-    Admin::Project.stub!(:default_project).and_return(@current_project)
-    
-    # this part is broken when the monkey patched application controller was removed
-    # spec/support/controller_helper.rb
-    controller.stub(:before_render) {
-      response.template.stub(:net_logo_package_name).and_return("blah")
-      response.template.stub_chain(:current_project).and_return(@current_project);
-    }
-
     @admin_user = Factory.create(:user, { :email => "test@test.com", :password => "password", :password_confirmation => "password" })
     @admin_user.add_role("admin")
 
     stub_current_user :admin_user
-
+    
     @investigation = Factory.create(:investigation, {
       :name => "test investigation",
-      :description => "new decription"
+      :description => "new decription",
+      :publication_status => "published"
     })
-
-    Investigation.stub!(:find).and_return(@investigation)
-    Investigation.stub!(:published).and_return([@investigation])
   end
 
-  it "should display a 'duplicate' link for authorized users" do
-    @investigation.should_receive(:duplicateable?).with(@logged_in_user).and_return(true)
-
+  it "should handle the show method without error" do
     get :show, :id => @investigation.id
-
-    #@response.body.should include(duplicate_link_for(@investigation))
-    assert_select("a[href=?]", duplicate_investigation_url(@investigation), { :text => "duplicate", :count => 1 })
-  end
-
-  it "should not display a 'duplicate' link for unauthorized users" do
-    @investigation.should_receive(:duplicateable?).with(@logged_in_user).and_return(false)
-
-    get :show, :id => @investigation.id
-
-    #@response.body.should_not include(duplicate_link_for(@investigation))
-    assert_select("a[href=?]", duplicate_investigation_url(@investigation), { :text => "duplicate", :count => 0 })
-  end
-
-  it "should render prievew warning in OTML" do
-    get :show, :id => @investigation.id, :format => 'otml'
-    assert_select "*.warning"
-  end
-
-  it "should render overlay removing warning in dynamic_otml" do
-    get :show, :id => @investigation.id, :format => 'dynamic_otml'
-    assert_select "overlays" do
-      assert_select "OTOverlay" do
-        assert_select "deltaObjectMap" do
-          assert_select "entry[key=?]", "#{@investigation.uuid}!/preview_warning"
-        end
-      end
-    end
   end
 
   describe "Researcher Reports" do
     before(:each) do
       controller.should_receive(:send_data) { | data, options | 
         options[:type].should == "application/vnd.ms.excel"
-        # this last bit is necessary because we are 'rendering views'
-        # without it the rending system will complain about a missing template
-        controller.render :text => "blah"
       }
+      # this is needed to prevent a missing template call, the real send_data method
+      # keeps rails from doing an implicit render, but since we are stubing send_data here
+      # the implicit render isn't stopped
+      controller.stub!(:render)
     end
     
     it 'should return an XLS file for the global Usage Report' do
