c7e251bfd4feb54becc7d8a7bdc636726d581ba3
Noah Paessel <knowuh@gmail.com>
2011-09-13 16:21:30 -0400

    More use of fake_drop for drag_and_drop testing.

--- features/step_definitions/assignment_steps.rb
+++ features/step_definitions/assignment_steps.rb
@@ -1,10 +1,15 @@
 def assign_runnable(runnable_element)
-  drop = find("#clazz_offerings")
-  scroll_into_view("#clazz_offerings")
-  runnable_element.drag_to(drop)
-  # TODO: Eliminate sleep() call.
-  # Problem is that selenium is not waiting for callback to finish
-  sleep(2)
+  drop_id = "#clazz_offerings"
+  drop = find(drop_id)
+  # NP 2011-09
+  #
+  # TODO: When Selenium issue ( http://bit.ly/q9LHR4 ) closes 
+  # simulate the drag event like this:
+  # 
+  # scroll_into_view(drop_id)
+  # find("##{runnable_element}").drag_to(drop)
+  # sleep(2) # TODO: eliminate sleep call
+  fake_drop(runnable_element,drop_id);
 end
 
 def polymorphic_assign(assignable_type, assignable_name, clazz_name)
@@ -33,6 +38,5 @@ end
 # this is the interactive version of the step above
 When /^I assign the ([^"]+) "([^"]*)" to the class "([^"]*)"$/ do |assignable_type, assignable_name, class_name|
   assignable = assignable_type.gsub(/\s/, "_").classify.constantize.find_by_name(assignable_name)
-  runnable_element = find("##{dom_id_for(assignable)}")
-  assign_runnable(runnable_element)
+  assign_runnable(dom_id_for(assignable))
 end
