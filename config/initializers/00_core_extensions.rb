b299295b109264ebdf9192ad3fb7bd24495840e0
Sam Fentress <sfentress@concord.org>
2011-09-13 10:30:28 -0400

    Move Enumerable ext out of application.rb and into core_extensions
    Tests such as features/researcher_reports.feature were failing because
    they couldn't find this method that was supposed to be extended in Enumerable
    
    Can't quite see how this ever worked, because like this Enumberable was being
    defined as being inside RailsPortal. Moving it out of RailsPortal fixed the
    issue, and core_extensions seemed like a better place for it.

--- config/initializers/00_core_extensions.rb
+++ config/initializers/00_core_extensions.rb
@@ -137,3 +137,23 @@ end
 # include #display_name everywhere
 Object.send(:include, ::DisplayNameMethod)
 
+
+module Enumerable
+  # An extended group_by which will group at multiple depths
+  # Ex:
+  # >> ["aab","abc","aba","abd","aac","ada"].extended_group_by([lambda {|e| e.first}, lambda {|e| e.first(2)}])
+  # => {"a"=>{"aa"=>["aab", "aac"], "ab"=>["abc", "aba", "abd"], "ad"=>["ada"]}}
+
+  def extended_group_by(lambdas)
+    lamb = lambdas.shift
+    result = lamb ? self.group_by{|e| lamb.call(e)} : self
+    if lambdas.size > 0
+      final = {}
+      temp = result.map{|k,v| {k => v.extended_group_by(lambdas.clone)}}
+      temp.each {|r| final.merge!(r) }
+      result = final
+    end
+    return result
+  end
+end
+
