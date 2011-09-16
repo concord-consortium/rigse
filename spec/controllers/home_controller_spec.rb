adf913ce1edf4b4fbe4f2ddbf9b7f761cae1162f
Scott Cytacki <scytacki@concord.org>
2011-09-16 13:18:43 -0400

    simplify view stubbing and remove useless test

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -4,19 +4,9 @@ describe HomeController do
 
   before(:each) do
     @test_project = mock("project")
-    controller.stub(:before_render) {
-      response.template.stub(:current_project).and_return(@test_project)
-    }
+    Admin::Project.stub(:default_project).and_return(@test_project)
   end
 
-  #Delete this example and add some real ones
-  it "should use HomeController" do
-    @test_project.stub(:home_page_content).and_return(nil)
-    @test_project.stub(:name).and_return("Test Project")
-    get :index
-    controller.should be_an_instance_of(HomeController)
-  end
-  
   it "should display home page content from the current admin project" do
     # it appears that some previous tests leave a user logged in somehow
     # so we explicitly log in anonymous here
@@ -31,9 +21,6 @@ describe HomeController do
   end
 
   describe "GET /stylesheets/project.css" do
-    before(:each) do
-      Admin::Project.stub(:default_project).and_return(@test_project)
-    end
     describe "when a project is configured to use custom styles" do
       it "should return the custom css" do
         css_text = ".some_class { font-height: 10px; }"
