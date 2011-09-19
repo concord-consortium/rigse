fc9f929a88f55fde294c6e866c054bb29a726b75
Scott Cytacki <scytacki@concord.org>
2011-09-19 13:36:16 -0400

    Remove cookie_only => false (for now)
    Removing this will fix somethings but break others.  It will break the .config requests made by Java.  Those requests are done with a query_parameter including the session_id.  Setting cookie_only => false meant that rails would handle that query_parameter for us.  But it also meant that anyone could access any route with a session_id in the query_parameter.
    
    Besides that lowered security, cookie_only is broken at least in rails 3.0.10.  Sometimes it causes the params array to be setup incorrectly.
    
    We are looking into a more specific way to handle the .config requests instead of a global cookie_only.

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,13 +1,9 @@
 # Be sure to restart your server when you modify this file.
 
+# the key used to be _bort_session in the rails2 version of this app
+
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
 # (create the session table with "rails generate session_migration")
-RailsPortal::Application.config.session_store :active_record_store, 
 
-  # this used to be _bort_session in the rails2 version of this app
-  :key => '_rails_portal_session', 
-  
-  # allow session to be loaded from params. This is used so java
-  # connections can use the same session, specifically the config file
-  :cookie_only => false
+RailsPortal::Application.config.session_store :active_record_store, :key => '_rails_portal_session'
