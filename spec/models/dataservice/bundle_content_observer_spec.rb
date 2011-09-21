64ce4abb48ecc26098b52c3714154ad9190f739d
Scott Cytacki <scytacki@concord.org>
2011-09-21 17:01:10 -0400

    fix observer spec

--- spec/models/dataservice/bundle_content_observer_spec.rb
+++ spec/models/dataservice/bundle_content_observer_spec.rb
@@ -4,6 +4,7 @@ describe Dataservice::BundleContentObserver do
   it "should fire off async methods" do 
       @bc = mock_model(Dataservice::BundleContent)
       @obs = Dataservice::BundleContentObserver.instance
+      @bc.should_receive(:otml_empty?).and_return(false)
       @bc.should_receive(:extract_saveables)
       @bc.should_receive(:copy_to_collaborators)
       @obs.after_save(@bc)
