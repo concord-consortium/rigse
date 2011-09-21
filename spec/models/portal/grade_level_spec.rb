80e0242c8f4811f42f0615d771b613c86df80932
Scott Cytacki <scytacki@concord.org>
2011-09-21 11:17:13 -0400

    make sure there is a valid grade to reference

--- spec/models/portal/grade_level_spec.rb
+++ spec/models/portal/grade_level_spec.rb
@@ -2,10 +2,11 @@ require File.expand_path('../../../spec_helper', __FILE__)
 
 describe Portal::GradeLevel do
   before(:each) do
+    grade = Factory(:portal_grade)
     @valid_attributes = {
       :has_grade_levels_id => 1,
       :has_grade_levels_type => "value for has_grade_levels_type",
-      :grade_id => 1,
+      :grade_id => grade.id,
       :uuid => "value for uuid"
     }
   end
