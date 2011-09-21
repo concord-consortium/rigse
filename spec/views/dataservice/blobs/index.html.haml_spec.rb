24ccecb3f671d54baabc10956902717079abddc5
Scott Cytacki <scytacki@concord.org>
2011-09-21 13:49:04 -0400

    fix view tests

--- spec/views/dataservice/blobs/index.html.haml_spec.rb
+++ spec/views/dataservice/blobs/index.html.haml_spec.rb
@@ -7,13 +7,16 @@ describe "/dataservice/blobs/index.html.haml" do
     # cut off the show_menu_for helper which traverses lots of other code
     view.stub!(:show_menu_for).and_return("show menu")
 
+    power_user = stub_model(User, :has_role? => true)
+    view.stub!(:current_user).and_return(power_user)
+
     # the changeable? => true prevents a current_user lookup, but will test if editing links correctly wrap the passed block
     collection = WillPaginate::Collection.create(1,10) do |coll|
       coll << stub_model(Dataservice::Blob, :id => 1, :token => "8ad04a50ba96463d80407cd119173b86", :content => "1", :changeable? => true)
       coll << stub_model(Dataservice::Blob, :id => 2, :token => "8ad04a50ba96463d80407cd119173b86", :content => "2", :changeable? => true)
       coll.total_entries = 2
     end
-    assigns[:dataservice_blobs] = collection
+    assign(:dataservice_blobs, collection)
   end
 
   it "renders a list of dataservice_blobs" do
--- spec/views/dataservice/blobs/show.html.haml_spec.rb
+++ spec/views/dataservice/blobs/show.html.haml_spec.rb
@@ -6,9 +6,13 @@ describe "/dataservice/blobs/show.html.haml" do
     # cut off the show_menu_for helper which traverses lots of other code
     view.stub!(:show_menu_for).and_return("show menu")
 
+    power_user = stub_model(User, :has_role? => true)
+    view.stub!(:current_user).and_return(power_user)
+
     # :changeable? => true prevents a current_user lookup, but will test if editing links correctly wrap the passed block
-    assigns[:dataservice_blob] = @dataservice_blob = stub_model(Dataservice::Blob, :id => 1, :token => "8ad04a50ba96463d80407cd119173b86", 
+    @dataservice_blob = stub_model(Dataservice::Blob, :id => 1, :token => "8ad04a50ba96463d80407cd119173b86", 
       :content => "", :changeable? => true)
+    assign(:dataservice_blob, @dataservice_blob)
   end
 
   it "renders attributes in <p>" do
