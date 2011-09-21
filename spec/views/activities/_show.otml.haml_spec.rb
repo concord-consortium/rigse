12472235adbc0cbda3fdf6860c095d9cedfb9824
Scott Cytacki <scytacki@concord.org>
2011-09-21 11:34:17 -0400

    explicitly set the format to work around rspec

--- spec/views/activities/_show.otml.haml_spec.rb
+++ spec/views/activities/_show.otml.haml_spec.rb
@@ -2,6 +2,10 @@ require File.expand_path('../../../spec_helper', __FILE__)
 
 describe "/activities/_show.otml.haml" do
   it "renders without error" do
-    render :locals => {:teacher_mode => false, :activity => Factory.build(:activity)}
+    # the following should work because rspec is supposed to pick up the partial from the describe block or file name
+    # however it fails set the format correctly in this case, whereas when explicity setting the partial file name rails
+    # figures the format from the file name
+    # render :locals => {:teacher_mode => false, :activity => Factory.build(:activity)}
+    render :partial => 'activities/show.otml', :locals => {:teacher_mode => false, :activity => Factory.build(:activity)}
   end
 end
