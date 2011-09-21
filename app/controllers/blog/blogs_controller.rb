543cfb5a0eac8acf7697a567c9b3ded5141f8d3f
Sam Fentress <sfentress@concord.org>
2011-09-21 16:24:41 -0400

    Return response.body after posting to WP, so we can get the post #

--- app/controllers/blog/blogs_controller.rb
+++ app/controllers/blog/blogs_controller.rb
@@ -14,7 +14,7 @@ class Blog::BlogsController < ApplicationController
 
       result = wp.post_blog(blog_name, current_user, post_title, post_content, post_tags)
 
-      render :text => "OK!\n#{result}"
+      render :text => "OK!\n#{result.body}"
     rescue => e
       render :text => "NOT OK!\n#{e}", :status => 404
     end
