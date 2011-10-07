966383bb931521c5782a9adfc33b83a9b2ba8f23
Noah Paessel <knowuh@gmail.com>
2011-10-07 14:43:52 -0400

    local transport for testing.

---
+++ lib/sis_importer/local_file_transport.rb
@@ -0,0 +1,38 @@
+
+module SisImporter
+  class LocalFileTransport < FileTransport
+
+    def defaults
+      super.merge({
+        :remote_root => "district"
+      })
+    end
+
+    def remote_root
+      @configuration.remote_root
+    end
+
+    def remote_path(file)
+      File.join(remote_root,file)
+    end
+
+    def remote_district_file(file)
+      File.join(remote_path(district),file)
+    end
+
+    def copy(remote, local)
+      begin
+        FileUtils.cp(remote, local)
+        logger.info("copyed: #{remote} ==>  #{local}")
+      rescue RuntimeError => e
+        raise Errors::TransportError.new("copy Failed: #{remote} ==> #{local}", e)
+      end
+    end
+
+    def get_csv_file(csv_filename)
+      copy(remote_district_file(csv_filename),local_current_district_file(csv_filename))
+    end
+
+  end
+end
+
