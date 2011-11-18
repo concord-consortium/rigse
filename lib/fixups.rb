b0966b9b3972dac9861975ddec5588ea438fb3bb
Noah Paessel <knowuh@gmail.com>
2011-11-18 15:06:03 -0500

    Rake task to remove teachers test student accounts
    Mostly this is an error condition, where a teacher also has a student account.
    Happened for RITES summer workshop 2011 from SIS feed with bad data.

--- lib/fixups.rb
+++ lib/fixups.rb
@@ -22,4 +22,35 @@ class Fixups
     end
   end
 
+  def self.destroy_teacher(teacher)
+    teacher.clazzes.each(&:destroy)
+    teacher.clazzes=[]
+    # destroy could damage learner data.
+    teacher.delete
+    teacher = nil
+  end
+
+  def self.destroy_student(student)
+    student.user.security_questions.each(&:destroy)
+    student.destroy
+    student = nil
+  end
+
+  def self.remove_teachers_test_students
+    teachers = Portal::Teacher.all
+    teachers.each do |teacher|
+      user = teacher.user
+      if user
+        if user.portal_student
+          puts "removing one student (#{user.portal_student.id}) (#{user.name} | #{user.id}) (#{user.portal_student.created_at})"
+          self.destroy_student(user.portal_student)
+          user.portal_student = nil
+        end
+      else
+        puts "removing one bad teacher (#{teacher.id}) (null user) (#{teacher.created_at})"
+        self.destroy_teacher(teacher)
+      end
+    end
+  end
+
 end
