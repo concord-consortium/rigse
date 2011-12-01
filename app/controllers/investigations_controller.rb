19410f7a23d7f04cf0a8aaffc9ab4640a4ec8e45
Aaron Unger <aaron.unger@gmail.com>
2011-12-01 15:03:30 -0800

    Support for logging a launch process event when the Investigation otml is requested.

--- app/controllers/investigations_controller.rb
+++ app/controllers/investigations_controller.rb
@@ -196,7 +196,17 @@ class InvestigationsController < AuthoringController
       }
 
       format.config { render :partial => 'shared/show', :locals => { :runnable => @investigation, :teacher_mode => @teacher_mode, :session_id => (params[:session] || request.env["rack.session.options"][:id]) } }
-      format.dynamic_otml { render :partial => 'shared/show', :locals => {:runnable => @investigation, :teacher_mode => @teacher_mode} }
+      format.dynamic_otml {
+        learner = (params[:learner_id] ? Portal::Learner.find(params[:learner_id]) : nil)
+        if learner && learner.bundle_logger.in_progress_bundle
+          launch_event = Dataservice::LaunchProcessEvent.create(
+            :event_type => Dataservice::LaunchProcessEvent::TYPES[:activity_otml_requested],
+            :event_details => "Activity content loaded. Activity should now be running...",
+            :bundle_content => learner.bundle_logger.in_progress_bundle
+          )
+        end
+        render :partial => 'shared/show', :locals => {:runnable => @investigation, :teacher_mode => @teacher_mode}
+      }
       format.otml   { render :layout => 'layouts/investigation' } # investigation.otml.haml
       format.xml    { render :xml => @investigation }
       format.pdf    { render :layout => false }
