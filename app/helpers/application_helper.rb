c32cf519a0f656bd95a5c097f3b941f7979de0b5
Noah Paessel <knowuh@gmail.com>
2011-12-19 12:11:02 -0500

    use AR properties to check if project allows teachers to author

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1274,7 +1274,7 @@ module ApplicationHelper
   # this appears to not be used in master right now
   def current_user_can_author
     return true if current_user.has_role? "author" 
-    if settings_for(:teachers_can_author)
+    if current_project.teachers_can_author?
       return true unless current_user.portal_teacher.nil?
     end
     # TODO add aditional can-author conditions
