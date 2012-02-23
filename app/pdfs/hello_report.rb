d2b82804caf362df6c41d6e6fb86adc20ab74223
Stephen Bannasch <stephen.bannasch@gmail.com>
2012-02-23 17:46:14 -0500

    extend simple pdf generation example at: /report
    Opening http://<portal.org>/report generates a simple pdf. 
    This update adds more text and embeds and image.

--- app/pdfs/hello_report.rb
+++ app/pdfs/hello_report.rb
@@ -1,7 +1,9 @@
 class HelloReport < Prawn::Document
   def to_pdf
+    image(Rails.root.join("public", "images", "new", "banners", "empty.png"), :width => 450)
     text("Generating a pdf report:")
     text("Using the Ruby Gem: <u><b><link href='http://github.com/sandal/prawn/wiki/'>Prawn</link></b></u>", :inline_format => true)
+    text(Admin::Project.summary_info)
     render
   end
 end
