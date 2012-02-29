817e89f28c3d7227e461079bb7c67dda1ab7f9d7
Aaron Unger <aaron.unger@gmail.com>
2012-02-29 10:11:34 -0800

    Bring console_content more in-line with bundle_content

--- app/metal/console_content.rb
+++ app/metal/console_content.rb
@@ -4,18 +4,19 @@ require(File.dirname(__FILE__) + "/../../config/environment") unless defined?(Ra
 # /dataservice/console_loggers/7/console_contents.console
 
 class ConsoleContent
+  REQUEST_METHOD = 'REQUEST_METHOD'.freeze unless defined?(REQUEST_METHOD)
+  POST = 'POST'.freeze unless defined?(POST)
   
+  PATH_INFO = 'PATH_INFO'.freeze unless defined?(PATH_INFO)
   POST_BODY = 'rack.input'.freeze unless defined?(POST_BODY)
   
   def self.call(env)
-    if console_logger_id = env["PATH_INFO"][/\/dataservice\/console_loggers\/(\d+)\/console_contents\.bundle/, 1]
-      if console_logger = ::Dataservice::ConsoleLogger.find(console_logger_id)
-        if console_content = console_logger.console_contents.create(:body => env[POST_BODY].read)
-          digest = Digest::MD5.hexdigest(console_content.body)
-        end
-      end
+    console_logger_id = env[PATH_INFO][/\/dataservice\/console_loggers\/(\d+)\/console_contents\.bundle/, 1]
+    if env[REQUEST_METHOD] == POST && console_logger_id && console_logger = ::Dataservice::ConsoleLogger.find(console_logger_id)
+      console_content = console_logger.console_contents.create(:body => env[POST_BODY].read)
     end
     if console_content
+      digest = Digest::MD5.hexdigest(console_content.body)
       [201, 
         { 'Content-Type' => 'text/xml', 
           'Last-Modified' => console_content.created_at.httpdate, 
