e584d6afe56fda770abc35f04ed2811ac1b66dab
Aaron Unger <aaron.unger@gmail.com>
2012-03-05 13:54:12 -0800

    Password reset: redirect_back_or had issues
    Use a more explicit alternative instead.
    
    Conflicts:
    
    	app/controllers/users_controller.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -264,6 +264,7 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     p = Password.new(:user_id => @user.id)
     p.save(false) # we don't need the user to have a valid email address...
+    session[:return_to] = request.referer
     redirect_to change_password_path(:reset_code => p.reset_code)
   end
 
