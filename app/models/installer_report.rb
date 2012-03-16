3f22111127abadd0de1c459eacf443bef424aa5b
Aaron Unger <aaron.unger@gmail.com>
2012-03-16 11:50:28 -0700

    Add model methods for installer report info

--- app/models/installer_report.rb
+++ app/models/installer_report.rb
@@ -1,2 +1,25 @@
 class InstallerReport < ActiveRecord::Base
+
+  # some helpers for extracting info out of the body text dump
+  def cache_dir
+    find /^Writeable cache dir: (.*)$/
+  end
+
+  def saved_jar
+    find /^Local jar url: (.*)$/
+  end
+
+  def local_address
+    find /^local_socket_address = (.*)$ /
+  end
+
+  def using_temp_dir?
+    find(/^Writing to a (temp directory)!$/) == "temp directory"
+  end
+
+  # expects a regular expression with at least one capture group.
+  # if more than one capture group is present, only the first group value will be reported
+  def find(regexp)
+    self.body[regexp] ? $1 : "not found"
+  end
 end
