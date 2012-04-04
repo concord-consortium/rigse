fdc544eb588f50ad0916a562702223d4acfd22fd
Scott Cytacki <scytacki@concord.org>
2012-04-04 10:21:25 -0400

    Explicit check for completion of assignment
    Without making sure that ajax calls have completed then threading
    issues can occur because the browser might still be making a request
    while the testing thread moves on to the next test.

--- features/step_definitions/assignment_steps.rb
+++ features/step_definitions/assignment_steps.rb
@@ -38,5 +38,10 @@ end
 # this is the interactive version of the step above
 When /^I assign the ([^"]+) "([^"]*)" to the class "([^"]*)"$/ do |assignable_type, assignable_name, class_name|
   assignable = assignable_type.gsub(/\s/, "_").classify.constantize.find_by_name(assignable_name)
-  assign_runnable(dom_id_for(assignable))
+  assignable_id = dom_id_for(assignable)
+  assign_runnable(assignable_id)
+  with_scope("#clazz_offerings") do
+    # this isn't the best approach but it might be good enough for now
+    page.should have_content(assignable_name)
+  end
 end
