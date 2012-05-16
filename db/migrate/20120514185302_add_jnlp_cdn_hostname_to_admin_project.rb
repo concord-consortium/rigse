620338dd1121f37e3010527365ff9fbe33144cf0
Scott Cytacki <scytacki@concord.org>
2012-05-16 10:38:59 -0400

    update migration to be rails 3.0 compatible

--- db/migrate/20120514185302_add_jnlp_cdn_hostname_to_admin_project.rb
+++ db/migrate/20120514185302_add_jnlp_cdn_hostname_to_admin_project.rb
@@ -1,5 +1,9 @@
 class AddJnlpCdnHostnameToAdminProject < ActiveRecord::Migration
-  def change
+  def self.up
     add_column :admin_projects, :jnlp_cdn_hostname, :string
+  end
+
+  def self.down
+    remove_column :admin_projects, :jnlp_cdn_hostname
   end
 end
