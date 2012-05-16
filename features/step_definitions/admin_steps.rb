65d76ff81cd286b9aafa692a9b8062dc547129aa
Scott Cytacki <scytacki@concord.org>
2012-05-15 20:58:14 -0400

    MIGRATION: add jnlp_cdn_hostname to admin_project
    if this is set the codebase of the jnlp files is modified to point to this
    hostname. This enabled the installer to pull in files from a CDN like
    Amazon CloudFront instead of our jnlp server. This can also be used
    to serve the files from a simple static website instead of a jnlp server.

--- features/step_definitions/admin_steps.rb
+++ features/step_definitions/admin_steps.rb
@@ -14,3 +14,54 @@ Given /^member registration is (.+)$/ do |member_registration|
   project.enable_member_registration = state
   project.save
 end
+
+When /^an admin sets the jnlp CDN hostname to "([^"]*)"$/ do |cdn_hostname|
+  admin = Factory.next(:admin_user)
+  login_as(admin.login)
+  visit admin_projects_path
+  click_link "edit project"
+  fill_in "admin_project[jnlp_cdn_hostname]", :with => cdn_hostname
+  # we turn on the opportunisitc installer inorder to test the most functionality
+  check "Opportunistic Installer"
+  click_button "Save"
+  page.should have_no_button("Save")
+end
+
+Then /^the installer jnlp codebase and wrapped_jnlp should start with "([^"]*)"$/ do |codebase_start|
+  inv = Factory.create(:investigation)
+  # switch the driver to rack_test so we can inspect the content
+  original_driver = Capybara.current_driver
+  Capybara.current_driver = :rack_test
+  # request some simple jnlp perhaps we need to actually make an investigation to request before we can do this
+  visit "/investigations/#{inv.id}.jnlp"
+  jnlp_xml = Nokogiri::XML(page.driver.response.body)
+  codebase = jnlp_xml.xpath("/jnlp/@codebase")
+  codebase.text.should match /^#{codebase_start}.*/
+
+  wrapped_jnlp_attr = jnlp_xml.xpath("/jnlp/resources/property[@name='wrapped_jnlp']/@value")
+  wrapped_jnlp_attr.should_not be_nil
+  wrapped_jnlp_attr.text.should match /^#{codebase_start}.*/
+
+  Capybara.current_driver = original_driver
+end
+
+Then /^the non installer jnlp codebase should not start with "([^"]*)"$/ do |codebase_start|
+  inv = Factory.create(:investigation)
+  # switch the driver to rack_test so we can inspect the content
+  original_driver = Capybara.current_driver
+  Capybara.current_driver = :rack_test
+
+  visit "/investigations/#{inv.id}.jnlp?skip_installer=true"
+  jnlp_xml = Nokogiri::XML(page.driver.response.body)
+
+  # make sure we really are not using the installer
+  main_class_attr = jnlp_xml.xpath("/jnlp/application-desc/@main-class")
+  main_class_attr.should_not be_nil
+  main_class_attr.text.should == 'net.sf.sail.emf.launch.EMFLauncher2'
+
+  codebase = jnlp_xml.xpath("/jnlp/@codebase")
+  codebase.text.should_not match /^#{codebase_start}.*/
+
+
+  Capybara.current_driver = original_driver
+end
