be370758966b18cb702b07fba613e68f926757f4
Aaron Unger <aaron.unger@gmail.com>
2012-05-18 07:44:15 -0700

    Fix some bad references in session_end_notification

--- app/controllers/dataservice/periodic_bundle_loggers_metal_controller.rb
+++ app/controllers/dataservice/periodic_bundle_loggers_metal_controller.rb
@@ -2,13 +2,13 @@ class Dataservice::PeriodicBundleLoggersMetalController < ActionController::Meta
 
   def session_end_notification
     if pbl = Dataservice::PeriodicBundleLogger.find(params[:id])
-      if bundle_logger = pbl.learner.bundle_logger.in_progress_bundle
+      if ipb = pbl.learner.bundle_logger.in_progress_bundle
         launch_event = ::Dataservice::LaunchProcessEvent.create(
           :event_type => ::Dataservice::LaunchProcessEvent::TYPES[:bundle_saved],
           :event_details => "Learner session data saved. Activity should now be closed.",
-          :bundle_content => bundle_logger.in_progress_bundle
+          :bundle_content => ipb
         )
-        bundle_logger.end_bundle( { :body => "" } )
+        pbl.learner.bundle_logger.end_bundle( { :body => "" } )
       end
       self.status = 201
       self.content_type = 'text/xml'
