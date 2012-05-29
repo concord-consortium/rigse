7488bd6b3b9efced1fc76ae08b8e360c4132ed1d
Suhas Sanmukh <suhas.sanmukh@zeuslearning.com>
2012-05-29 14:56:14 +0530

    Creating branch for left pane changes.
    - Creating branch for left pane changes
    
    - Submenu links will be activated as and when
      development of pages progresses.

---
+++ public/javascripts/clazzes_nav.js
@@ -0,0 +1,30 @@
+/**
+ * @author Tanzeel R.A. Kazi
+ */
+
+	
+function createNewClass(portalClazzPath)
+{
+	window.location.href = portalClazzPath;
+}
+
+function manageClassList()
+{
+	alert('Manage Classes Stub');
+	/* indow.location.href = 'manage'; */
+}
+
+function showFullStatus()
+{
+	alert('Full Status Link Stub');
+}
+
+function showMaterials()
+{
+	alert('Materials Link Stub');
+}
+
+function studentRoster()
+{
+	alert('Student Roster Stub');
+}
