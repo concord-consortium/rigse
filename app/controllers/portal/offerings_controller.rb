e35f116477a9bcf2b8758a21acd29a2b70a6c587
Aaron Unger <aaron.unger@gmail.com>
2012-06-01 14:14:36 -0700

    Pre- and Post- tests are now run as lightweight browser forms
    Conflicts:
    
    	app/controllers/portal/offerings_controller.rb
    	app/helpers/application_helper.rb
    	app/views/embeddable/diy/sensors/_show.otml.haml
    	app/views/embeddable/multiple_choices/_show_choice.html.haml
    	app/views/layouts/run.run_html.haml
    	config/routes.rb
    	public/stylesheets/sass/app.sass
    	themes/itsisu/views/activities/_list_item.html.haml
    	themes/itsisu/views/portal/offerings/_portal_edit.html.haml
    	themes/itsisu/views/shared/_offering_for_student.html.haml

--- app/controllers/portal/offerings_controller.rb
+++ app/controllers/portal/offerings_controller.rb
@@ -2,6 +2,7 @@ class Portal::OfferingsController < ApplicationController
 
   include RestrictedPortalController
   before_filter :teacher_admin_or_config, :only => [:report, :open_response_report, :multiple_choice_report, :separated_report, :report_embeddable_filter]
+  before_filter :student_teacher_admin_or_config, :only => [:answers]
 
   def current_clazz
     Portal::Offering.find(params[:id]).clazz
@@ -29,6 +30,10 @@ class Portal::OfferingsController < ApplicationController
       format.html # show.html.erb
       format.xml  { render :xml => @offering }
 
+      format.run_html   {
+        @learner = setup_portal_student
+        render :show, :layout => "layouts/run"
+      }
       format.run_sparks_html   {
         if learner = setup_portal_student
           session[:put_path] = saveable_sparks_measuring_resistance_url(:format => :json)
@@ -324,4 +329,63 @@ class Portal::OfferingsController < ApplicationController
       render :status => 500, :text => "problem loading offering"
     end
   end
+
+  def answers
+    @offering = Portal::Offering.find(params[:id])
+    if @offering
+      learner = setup_portal_student
+      if learner && params[:questions]
+        # create saveables
+        params[:questions].each do |dom_id, value|
+          # translate the dom id into an actual Embeddable
+          embeddable = parse_embeddable(dom_id)
+          # create saveable
+          create_saveable(embeddable, @offering, learner, value) if embeddable
+        end
+      end
+      flash[:notice] = "Your answers have been saved."
+      redirect_to :home
+    else
+      render :text => 'problem loading offering', :status => 500
+    end
+  end
+
+  private
+
+  def parse_embeddable(dom_id)
+    # make sure to support at least Embeddable::OpenResponse, Embeddable::MultipleChoice, and Embeddable::MultipleChoiceChoice
+    if dom_id =~ /embeddable__([^\d]+)_(\d+)$/
+      klass = "Embeddable::#{$1.classify}".constantize
+      return klass.find($2.to_i) if klass
+    end
+    nil
+  end
+
+  def create_saveable(embeddable, offering, learner, answer)
+    case embeddable
+    when Embeddable::OpenResponse
+      saveable_open_response = Saveable::OpenResponse.find_or_create_by_learner_id_and_offering_id_and_open_response_id(learner.id, offering.id, embeddable.id)
+      if saveable_open_response.response_count == 0 || saveable_open_response.answers.last.answer != answer
+        saveable_open_response.answers.create(:bundle_content_id => nil, :answer => answer)
+      end
+    when Embeddable::MultipleChoice
+      choice = parse_embeddable(answer)
+      answer = choice ? choice.choice : ""
+      if embeddable && choice
+        saveable = Saveable::MultipleChoice.find_or_create_by_learner_id_and_offering_id_and_multiple_choice_id(learner.id, offering.id, embeddable.id)
+        if saveable.answers.empty? || saveable.answers.last.answer != answer
+          saveable.answers.create(:bundle_content_id => nil, :choice_id => choice.id)
+        end
+      else
+        if ! choice
+          logger.error("Missing Embeddable::MultipleChoiceChoice id: #{choice_id}")
+        elsif ! embeddable
+          logger.error("Missing Embeddable::MultipleChoice id: #{choice.multiple_choice_id}")
+        end
+      end
+    else
+      nil
+    end
+  end
+
 end
