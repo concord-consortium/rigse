a42e42cc172891506b3a2cf3c8a5cc81b579bd88
Aaron Unger <aaron.unger@gmail.com>
2012-06-04 11:01:30 -0700

    Fix Image spec tests

--- spec/controllers/images_controller_spec.rb
+++ spec/controllers/images_controller_spec.rb
@@ -1,10 +1,18 @@
 require File.expand_path('../../spec_helper', __FILE__)
+SAMPLE_IMAGE = "#{RAILS_ROOT}/public/images/cc-footer.png" unless defined?(SAMPLE_IMAGE)
+
 describe ImagesController do
 
+  def mock_image_image(stubs={})
+    stubs = {:path => SAMPLE_IMAGE}.merge(stubs)
+    @mock_image_image ||= mock_model(Object, stubs)
+  end
+
   def mock_image(stubs={})
+    stubs = {:name => "Image", :attribution => "Image subtext", :image => mock_image_image, :changeable? => true, :reload => true, :user= => true}.merge(stubs)
     @mock_image ||= mock_model(Image, stubs)
   end
-  
+
   before(:each) do
     login_admin
   end
@@ -13,23 +21,11 @@ describe ImagesController do
   describe "responding to GET index" do
 
     it "should expose an array of all the @images" do
-      Image.should_receive(:find).with(:all).and_return([mock_image])
+      Image.should_receive(:find).with(:all, {:limit=>36, :offset=>0}).and_return([mock_image])
       get :index
       assigns[:images].should == [mock_image]
     end
 
-    describe "with mime type of xml" do
-  
-      it "should render all images as xml" do
-        request.env["HTTP_ACCEPT"] = "application/xml"
-        Image.should_receive(:find).with(:all).and_return(images = mock("Array of Images"))
-        images.should_receive(:to_xml).and_return("generated XML")
-        get :index
-        response.body.should == "generated XML"
-      end
-    
-    end
-  
   end
   
   describe "responding to GET show" do
@@ -122,13 +118,13 @@ describe ImagesController do
   
       it "should expose the requested image as @image" do
         Image.stub!(:find).and_return(mock_image(:update_attributes => true))
-        put :update, :id => "1"
+        put :update, :id => "1", :image => {:these => 'params'}
         assigns(:image).should equal(mock_image)
       end
   
       it "should redirect to the image" do
         Image.stub!(:find).and_return(mock_image(:update_attributes => true))
-        put :update, :id => "1"
+        put :update, :id => "1", :image => {:these => 'params'}
         response.should redirect_to(image_url(mock_image))
       end
   
@@ -144,13 +140,13 @@ describe ImagesController do
   
       it "should expose the image as @image" do
         Image.stub!(:find).and_return(mock_image(:update_attributes => false))
-        put :update, :id => "1"
+        put :update, :id => "1", :image => {:these => 'params'}
         assigns(:image).should equal(mock_image)
       end
   
       it "should re-render the 'edit' template" do
         Image.stub!(:find).and_return(mock_image(:update_attributes => false))
-        put :update, :id => "1"
+        put :update, :id => "1", :image => {:these => 'params'}
         response.should render_template('edit')
       end
   
