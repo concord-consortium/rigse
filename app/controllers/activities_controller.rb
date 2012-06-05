7d996ff89f01a405f484f313f7938074436b7cee
Aaron Unger <aaron.unger@gmail.com>
2012-06-05 14:46:37 -0700

    Enable using the installer when running at the activity level

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -158,7 +158,13 @@ class ActivitiesController < ApplicationController
         end
       }
       format.run_html   { render :show, :layout => "layouts/run" }
-      format.jnlp   { render :partial => 'shared/show', :locals => { :runnable => @activity, :teacher_mode => @teacher_mode } }
+      format.jnlp   {
+        if params.delete(:skip_installer)
+          render :partial => 'shared/show', :locals => { :runnable => @activity, :teacher_mode => @teacher_mode }
+        else
+          render :partial => 'shared/installer', :locals => { :runnable => @activity, :teacher_mode => @teacher_mode }
+        end
+      }
       format.config { render :partial => 'shared/show', :locals => { :runnable => @activity, :teacher_mode => @teacher_mode, :session_id => (params[:session] || request.env["rack.session.options"][:id]) } }
       format.dynamic_otml {
         learner = (params[:learner_id] ? Portal::Learner.find(params[:learner_id]) : nil)
