02fd69396c989ada87a25084677653c8b3c1d6ea
Sagar Ghadge <sagar.ghadge@zeuslearning.com>
2012-06-13 19:39:30 +0530

    Fixing checkbox bug posted by Dan
    - Fixing checkbox bug in sortable list of classes
      posted by Dan

--- public/javascripts/manage_classes.js
+++ public/javascripts/manage_classes.js
@@ -125,3 +125,32 @@ function copyClass(btnSave)
 	oSubmitText.style.display = "inline";
 	return;
 }
+
+var strDraggedElementCheckBoxID="";
+var bDraggedElementChecked;
+
+function ChangeOrder(elementDragged)
+{
+	var oDraggedElementCheckBox = $$('#'+elementDragged.element.id+' input:[type="checkbox"]')[0];
+	strDraggedElementCheckBoxID = oDraggedElementCheckBox.id;
+	bDraggedElementChecked = oDraggedElementCheckBox.checked;
+}
+
+function UpdateOrder()
+{
+	var oDraggedElementCheckBox;
+	if(strDraggedElementCheckBoxID.length > 0)
+	{
+		setTimeout (function(){
+			document.getElementById(strDraggedElementCheckBoxID).checked = bDraggedElementChecked;
+			strDraggedElementCheckBoxID = "";
+			},20);
+	}
+}
+
+document.observe("dom:loaded", function() {
+	Sortable.sortables.sortable.draggables.each(function(oDraggable){
+		oDraggable.options.change = ChangeOrder;
+		oDraggable.options.onEnd = UpdateOrder;
+	});
+});
