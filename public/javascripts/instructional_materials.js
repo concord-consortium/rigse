002e3002750753324c1be507377a489058880fdd
Sagar Ghadge <sagar.ghadge@zeuslearning.com>
2012-06-13 14:42:01 +0530

    Implementing changes as suggested by Noah
    - Implementing investigation tabs by extracting css, javascript
      and implementing javascript using prototype
    
    - Moving method 'user_report_data ' from offerings controller
      to report learner model

--- public/javascripts/instructional_materials.js
+++ public/javascripts/instructional_materials.js
@@ -48,3 +48,25 @@ function showHideActivityButtons(investigation_id, oLink){
 	$('oExpandCollapseText_'+investigation_id).update(strExpandCollapseText);
 	oLink.update(strLinkText);
 }
+
+function setSelectedTab(strTabID){
+	$(strTabID).simulate('click');
+	$$('.scrollertab').each(function(oTab){
+		var strDirection = oTab.hasClassName('tableft')? "l" : "r";
+		oTab.observe('mouseover',function(){
+			startScroll(strDirection,5);
+			});
+		oTab.observe('mouseout',stopScroll);
+	});
+}
+
+document.observe("dom:loaded", function() {
+	var arrTabs = $$('#oTabcontainer .tab');
+	arrTabs.each(function(oTab){
+		oTab.observe('click',function(){
+			showInstructionalMaterial(oTab);
+		});
+	});
+	var strTabID = arrTabs[0].id;
+	setSelectedTab(strTabID);
+});
