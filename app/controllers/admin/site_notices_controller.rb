f364dd164d2a4b13ca732361f8aee7e5ca411834
niteshkp <nitesh.patil@zeuslearning.com>
2012-07-12 14:13:02 +0530

    Fix merge conflict for site_notices_controller
    - Fix merge conflict for site_notices_controller

--- app/controllers/admin/site_notices_controller.rb
+++ app/controllers/admin/site_notices_controller.rb
@@ -29,4 +29,62 @@ class Admin::SiteNoticesController < ApplicationController
     @all_notices = SiteNotice.all
     redirect_to admin_site_notices_path
   end
+  
+  def update
+      #update notice
+      site_notice = SiteNotice.find(params[:id])
+      site_notice.notice_html= params[:notice_html]
+      site_notice.creator_id = params[:creator_id]
+      site_notice.save!
+      
+      #destroying all roles associated with current id
+      roles=params[:roles]
+      site_notice_id = SiteNoticeRole.find(params[:id])
+      site_notice_id.each do |id|
+         id.destroy
+      end
+      
+      #Storing new roles
+      roles = params[:roles]
+      roles.each do |role_id|
+      site_notice_roles = SiteNoticeRole.new
+      site_notice_roles.notice_id = params[:id]
+      site_notice_roles.role.id = role_id  
+    end
+      respond_to do |format|
+      respond.html # update.html.erb
+    end
+  end
+   
+   def edit
+      site_notice = SiteNotice.new
+      site_notice.notice_html = params[:notice_html]
+      site_notice.creator_id = current_user.id
+      site_notice.save!
+    
+    #storing all roles that should see this notice
+      roles = params[:role]
+      roles.each do |role_id|
+        site_notice_roles.notice_id = site_notice.id
+        site_notice_roles = SiteNoticeRole.new
+        site_notice_roles.role_id = role_id  
+      end
+      redirect_to admin_site_notices_path
+   end
+    
+   def delete
+      #delete notice
+      SiteNotice.find(params[:id]).destroy
+      SiteNoticeRole.find(params[:id]).destroy
+      
+      render :update do |page|
+          page << "var element = $(#'temp');"
+          page << "element.remove();"
+      end
+      
+      #redirect_to :action => 'index';
+      #redirect_to admin_site_notices_path
+   end
+  
+    
 end
