b7c168c243ed94fdeea818a5e5630307993f22fd
Scott Cytacki <scytacki@concord.org>
2012-07-13 17:12:06 -0400

    eager load more models to speed up the page

--- app/controllers/portal/clazzes_controller.rb
+++ app/controllers/portal/clazzes_controller.rb
@@ -623,8 +623,7 @@ class Portal::ClazzesController < ApplicationController
       redirect_to home_url
       return
     end
-    
-    @portal_clazz = Portal::Clazz.find(params[:id]);
+    @portal_clazz = Portal::Clazz.includes(:offerings => :learners, :students => :user).find(params[:id])
     
     # Save the left pane sub-menu item
     Portal::Teacher.save_left_pane_submenu_item(current_user, Portal::Teacher.LEFT_PANE_ITEM['MATERIALS'])
