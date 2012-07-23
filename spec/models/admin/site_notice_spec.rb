c176040cce52e12608115c4b4290836a6bdeb9a9
Sunny Singh <sunny.singh@zeuslearning.com>
2012-07-24 02:30:54 +0530

    Fix whitespace issues in files
    - Fix whitespace issues in files

--- spec/models/admin/site_notice_spec.rb
+++ spec/models/admin/site_notice_spec.rb
@@ -4,17 +4,17 @@ describe Admin::SiteNotice do
   describe "Display notices for different users" do
     before(:each) do
       @admin_user = Factory.next(:admin_user)
-      
+
       role = Role.find_by_title('admin')
       @first_notice = Factory.create(:site_notice, :created_by => @admin_user.id)
       Factory.create(:site_notice_role, :notice_id => @first_notice.id,:role_id => role.id)
-      
+
       @second_notice = Factory.create(:site_notice, :created_by => @admin_user.id)
       Factory.create(:site_notice_role, :notice_id => @second_notice.id,:role_id => role.id)
-      
+
       @third_notice = Factory.create(:site_notice, :created_by => @admin_user.id)
       Factory.create(:site_notice_role, :notice_id => @third_notice.id,:role_id => role.id)
-      
+
     end
     it"should show no notice if there is no notice" do
       Admin::SiteNoticeRole.destroy_all
@@ -26,7 +26,7 @@ describe Admin::SiteNotice do
     it"should show uncollapsed notice container if there are recent notices" do
       notices_hash = Admin::SiteNotice.get_notices_for_user(@admin_user)
       assert_equal(notices_hash[:notice_display_type] , Admin::SiteNotice.NOTICE_DISPLAY_TYPES[:new_notices])
-      
+
       notice_ids = notices_hash[:notices].map {|n| n.id}
       assert_equal(notice_ids.length, 3)
       assert(notice_ids.include?(@first_notice.id))
@@ -37,18 +37,18 @@ describe Admin::SiteNotice do
       Factory.create(:site_notice_user, :notice_id => @first_notice.id,:user_id => @admin_user.id, :notice_dismissed => true)
       notices_hash = Admin::SiteNotice.get_notices_for_user(@admin_user)
       assert_equal(notices_hash[:notice_display_type] , Admin::SiteNotice.NOTICE_DISPLAY_TYPES[:new_notices])
-      
+
       notice_ids = notices_hash[:notices].map {|n| n.id}
       assert_equal(notice_ids.length, 2)
       assert_equal(notice_ids.include?(@first_notice.id), false)
       assert(notice_ids.include?(@second_notice.id))
       assert(notice_ids.include?(@third_notice.id))
     end
-    it"should show collapsed notice container if there are no recent notices and user had collapsed the notice container" do  
+    it"should show collapsed notice container if there are no recent notices and user had collapsed the notice container" do
       Factory.create(:notice_user_display_status,:user_id => @admin_user.id,:last_collapsed_at_time => DateTime.now, :collapsed_status => true)
       notices_hash = Admin::SiteNotice.get_notices_for_user(@admin_user)
       assert_equal(notices_hash[:notice_display_type] , Admin::SiteNotice.NOTICE_DISPLAY_TYPES[:collapsed_notices])
-      
+
       notice_ids = notices_hash[:notices].map {|n| n.id}
       assert_equal(notice_ids.length, 3)
       assert(notice_ids.include?(@first_notice.id))
