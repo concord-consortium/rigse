db74394c8e85f9ebeb5b8bb7dbc7f6d53932b938
Sunny Singh <sunny.singh@zeuslearning.com>
2012-07-24 16:21:22 +0530

    Make offerings sortable
    - Make offerings sortable on the class summary page

--- app/controllers/portal/clazzes_controller.rb
+++ app/controllers/portal/clazzes_controller.rb
@@ -597,4 +597,8 @@ class Portal::ClazzesController < ApplicationController
     @portal_clazz = Portal::Clazz.find(params[:id]);
   end
   
+  def sort_offerings
+    params[:clazz_offerings].each_with_index{|id,idx| Portal::Offering.update(id, :position => (idx + 1))}
+  end
+  
 end
