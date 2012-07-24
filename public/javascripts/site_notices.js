7f473743861c8645a5c3eae0dd68a2873d2078ea
Tanzeel Kazi <tanzeel.kazi@zeuslearning.com>
2012-07-24 17:38:53 +0530

    Fix whitespace issues in files
    - Fix whitespace issues in files

--- public/javascripts/site_notices.js
+++ public/javascripts/site_notices.js
@@ -1,5 +1,5 @@
 function toggle_Notice_Display(oLink){
-	$('user_notice_container_div').toggle();
-	var strLinkText = ($('user_notice_container_div').getStyle('display') == "none")? "Show Notices" : "Hide Notices";
-	oLink.update(strLinkText);
+    $('user_notice_container_div').toggle();
+    var strLinkText = ($('user_notice_container_div').getStyle('display') == "none")? "Show Notices" : "Hide Notices";
+    oLink.update(strLinkText);
 }
