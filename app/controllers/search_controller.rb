33e694879bc789e8f48364d7e0ad7731f661d5f6
Vikash Sharma <vikash.sharma@zeuslearning.com>
2012-07-31 19:47:52 +0530

    Optimize activity and investigation search logic
    - Apply material type filter to search results.
    
    - Display search result summary.
    
    - Group search results by material type.
    
    - Add search preloader.

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -3,6 +3,7 @@ class SearchController < ApplicationController
   in_place_edit_for :investigation, :name
   
   def index
+     @b_check = false;
      @name = params[:name]
      @sort_order = param_find(:sort_order, (params[:method] == :get))
      search_options = {
@@ -13,17 +14,27 @@ class SearchController < ApplicationController
     }
     
     unless params[:investigation].nil?
-      @investigations = Investigation.search_list(search_options) 
+      @investigations = Investigation.search_list(search_options)
+      if @investigations.length > 0
+        @b_check = @b_check || true;
+      else
+        @b_check = @b_check || false;
+      end
     end
     unless params[:activity].nil?  
       @activities = Activity.search_list(search_options)
+      if @activities.length > 0
+        @b_check = @b_check || true;
+      else
+        @b_check = @b_check || false;
+      end
     end
     
     if request.xhr?
       #render :partial => 'search/filters'
      
     render :update do |page|
-        page.replace_html 'results', :partial => 'search/search_results' 
+        page.replace_html 'offering_list', :partial => 'search/search_results' 
         page<<"$('suggestions').remove()"
       end  
     else
@@ -48,16 +59,26 @@ class SearchController < ApplicationController
     
     unless params[:investigation].nil?
       @investigations = Investigation.search_list(search_options) 
+      if @investigations.length > 0
+        @b_check = @b_check || true;
+      else
+        @b_check = @b_check || false;
+      end
     end
     unless params[:activity].nil?  
       @activities = Activity.search_list(search_options)
+      if @activities.length > 0
+        @b_check = @b_check || true;
+      else
+        @b_check = @b_check || false;
+      end
     end
     
     if request.xhr?
       render :update do |page|
         
         page.replace_html 'search_suggestions', {:partial => 'search/search_suggestions',:locals=>{:textlength=>@name.length}}
-        page.replace_html 'results', :partial => 'search/search_results' 
+        page.replace_html 'offering_list', :partial => 'search/search_results' 
       end   
     else
       respond_to do |format|
