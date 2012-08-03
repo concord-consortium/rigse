099b826c2b9e1c4848d0ae6cc868b7cb5bb0cd00
Sagar Ghadge <sagar.ghadge@zeuslearning.com>
2012-08-03 15:49:26 +0530

    Fix Full Status UI as per feedback from Concord
    - Fix UI for Full Status page as per feedback from Concord
    
    - Truncate table header text based on available width

--- public/javascripts/full_status.js
+++ public/javascripts/full_status.js
@@ -1,5 +1,6 @@
 function showHideDetailedSummary(investigation_id, bShowDetails, strURL)
 {
+    setTableHeaders(1);
     var arrSummaryRows = $$(".investigation_summary_row_" + investigation_id);
     var arrDetailRows = $$(".investigation_detail_row_" + investigation_id);
     var strSummaryRowDisplay = "";
@@ -22,5 +23,33 @@ function showHideDetailedSummary(investigation_id, bShowDetails, strURL)
         oDetailRow.setStyle({'display':strDetailRowDisplay});
     })
     
+    setTableHeaders();
+    
     new Ajax.Request(strURL, {asynchronous:true, evalScripts:true, method:'post'});
+}
+
+document.observe("dom:loaded", function() {
+    setTableHeaders()
+});
+
+
+function setTableHeaders(iDefaultWidth)
+{
+    var iWidth;
+    $$("th.expand_collapse_text").each(function(oTitle){
+        var oChild = oTitle.childElements()[0]
+        if(oChild)
+        {
+            if(iDefaultWidth)
+                iWidth = iDefaultWidth
+            else
+                iWidth = (oTitle.getStyle('display') == "none")? 1 : oTitle.offsetWidth*0.9
+            oChild.setStyle({'display':'none'}) 
+            oChild.setStyle({'width':iWidth+'px'})
+        }
+    })
+    
+    $$("th.expand_collapse_text > div.progressbar_container").each(function(oTitle){
+        oTitle.setStyle({'display':''})
+    })
 }
