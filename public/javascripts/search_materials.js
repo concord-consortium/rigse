b7ac7acf8ac82ccd2de36caa68530f777ad9463b
Vikash Sharma <vikash.sharma@zeuslearning.com>
2012-08-03 15:55:21 +0530

    Modify Search Materials UI as per wireframes
    - Toggle Show/hide filters display
    
    - Modify Search results listing UI as per wireframes
    
    - Conflicts:
        app/views/search/_result_activity.html.haml
        db/schema.rb

--- public/javascripts/search_materials.js
+++ public/javascripts/search_materials.js
@@ -13,3 +13,42 @@ function highlightlabel(e){
 function showsuggestion(){
 	$('show_suggestion').writeAttribute('name','show_suggestion')
 }
+
+function showHideProbes(activity){
+	
+	var bVisible = false;
+    $$('.probes_container,.probes_header').each(function(oButtonContainer){
+        oButtonContainer.toggle();
+        bVisible = bVisible || oButtonContainer.getStyle('display')=='none';
+    });
+    	
+}
+
+function showHideFilters(oLink){
+    var bVisible = false;
+    $$('.collapse_filters').each(function(oButtonContainer){
+        oButtonContainer.toggle();
+        bVisible = bVisible || oButtonContainer.getStyle('display')=='none';
+    });
+    
+    var strLinkText = "";
+    var strExpandCollapseText = "";
+    if(bVisible){
+        strLinkText = "Show Filters";
+        strExpandCollapseText = "+";
+    }
+    else{
+        strLinkText = "Hide Filters";
+        strExpandCollapseText = "-";
+    }
+    
+    $('oExpandCollapseText').update(strExpandCollapseText);
+    oLink.update(strLinkText);
+}
+function uncheckedallprobes()
+{
+	$$(".probe_items").each(function(e){
+		e.checked= false;
+		
+	})
+}
