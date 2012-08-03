252959ab49d58ee8bab46d78f35927bcc14c9a60
Nitesh Patil <nitesh.patil@zeuslearning.com>
2012-08-03 14:00:08 +0530

    Modify Search Materials suggestions logic
    - Modify Search Instructional Materials suggestions logic
    
    - Modify Sort By filter UI

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -3,48 +3,11 @@ class SearchController < ApplicationController
   in_place_edit_for :investigation, :name
   
   def index
-     @b_check = false;
-     @name = params[:name]
-     @sort_order = param_find(:sort_order, (params[:method] == :get))
-     search_options = {
-      :name => @name,
-      :sort_order => @sort_order || 'name ASC',
-      :paginate => true,
-    #  :page => pagination
-    }
-    
-    unless params[:investigation].nil?
-      @investigations = Investigation.search_list(search_options)
-      if @investigations.length > 0
-        @b_check = @b_check || true;
-      else
-        @b_check = @b_check || false;
-      end
-    end
-    unless params[:activity].nil?  
-      @activities = Activity.search_list(search_options)
-      if @activities.length > 0
-        @b_check = @b_check || true;
-      else
-        @b_check = @b_check || false;
-      end
-    end
-    
-    if request.xhr?
-      #render :partial => 'search/filters'
-     
-    render :update do |page|
-        page.replace_html 'offering_list', :partial => 'search/search_results' 
+   if request.xhr?
+     render :update do |page| 
         page<<"$('suggestions').remove()"
       end  
-    else
-      respond_to do |format|
-          format.html do
-            render 'index'
-          end
-          format.js
-        end
-    end
+   end   
   end
   
   def show
@@ -76,9 +39,12 @@ class SearchController < ApplicationController
     
     if request.xhr?
       render :update do |page|
+        @bshow = params[:show_suggestion]
+        if @bshow=='true'
+          page.replace_html 'search_suggestions', {:partial => 'search/search_suggestions',:locals=>{:textlength=>@name.length}}
+        end
+        page.replace_html 'offering_list', :partial => 'search/search_results'
         
-        page.replace_html 'search_suggestions', {:partial => 'search/search_suggestions',:locals=>{:textlength=>@name.length}}
-        page.replace_html 'offering_list', :partial => 'search/search_results' 
       end   
     else
       respond_to do |format|
--- public/javascripts/search_materials.js
+++ public/javascripts/search_materials.js
@@ -1,5 +1,15 @@
-function select_material(){
- var strSuggestiontext=event.toElement.textContent.trim();
+function select_material(e){
+ var strSuggestiontext=e.textContent.trim();
  $('name').value=strSuggestiontext;
  $('suggestions').remove();
+ $('show_suggestion').writeAttribute('name','no_suggestion')
+}
+
+function highlightlabel(e){
+	$$('.highlightoption')[0].removeClassName('highlightoption');
+	e.addClassName('highlightoption');
+}
+ 
+function showsuggestion(){
+	$('show_suggestion').writeAttribute('name','show_suggestion')
 }
