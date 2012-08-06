d6db8110ac7c5879bb2d6c3becc5405e04698e97
Sagar Ghadge <sagar.ghadge@zeuslearning.com>
2012-08-06 22:42:18 +0530

    Remove Search Materials javascript code lints
    - Remove lints from javascript code for Search Materials page

--- public/javascripts/search_materials.js
+++ public/javascripts/search_materials.js
@@ -1,69 +1,66 @@
 var suggestioncount=-1;
 
 function select_material(e){
- var strSuggestiontext=e.textContent.trim();
- $('name').value=strSuggestiontext;
- $('suggestions').remove();
- $('show_suggestion').writeAttribute('name','no_suggestion');
- suggestioncount=-1;
+    var strSuggestiontext = e.textContent.trim();
+    $('name').value = strSuggestiontext;
+    $('suggestions').remove();
+    $('show_suggestion').writeAttribute('name','no_suggestion');
+    suggestioncount =- 1;
 }
 
 function highlightlabel(e){
-	$$('.highlightoption')[0].removeClassName('highlightoption');
-	e.addClassName('highlightoption');
+    $$('.highlightoption')[0].removeClassName('highlightoption');
+    e.addClassName('highlightoption');
 }
 
-function showsuggestion(event,oelem){	
-	$('show_suggestion').writeAttribute('name','show_suggestion');
-	var osuggestions=$$('.suggestion');
-	var ohoverelements=$$('.suggestionhover');
-	if(osuggestions.length==0)
-	{
-		return;
-	}
-	if(ohoverelements.length>0)
-	{
-	 ohoverelements[0].removeClassName('suggestionhover');
-	}
-	switch (event.keyCode)
-	{
-		case 40:
-			suggestioncount++;
-			if(suggestioncount>=osuggestions.length)
-			{
-				suggestioncount=0;
-			}
-			osuggestions[suggestioncount].addClassName('suggestionhover');
-			break;
-		
-		case 38:
-			suggestioncount--;
-			if(suggestioncount<=-1)
-			{
-				suggestioncount=osuggestions.length-1;
-			}
-			osuggestions[suggestioncount].addClassName('suggestionhover');
-			break;
-		
-		case 13:
-			select_material(osuggestions[suggestioncount]);
-			suggestioncount=-1;
-			break;
-		
-		default:
-			suggestioncount=-1;
-	}
-	
+function showsuggestion(event,oelem){
+    $('show_suggestion').writeAttribute('name','show_suggestion');
+    var osuggestions=$$('.suggestion');
+    var ohoverelements=$$('.suggestionhover');
+    if(osuggestions.length === 0)
+    {
+        return;
+    }
+    if(ohoverelements.length > 0)
+    {
+        ohoverelements[0].removeClassName('suggestionhover');
+    }
+    switch (event.keyCode)
+    {
+        case 40:
+            suggestioncount++;
+            if(suggestioncount>=osuggestions.length)
+            {
+                suggestioncount=0;
+            }
+            osuggestions[suggestioncount].addClassName('suggestionhover');
+            break;
+        
+        case 38:
+            suggestioncount--;
+            if(suggestioncount<=-1)
+            {
+                suggestioncount=osuggestions.length-1;
+            }
+            osuggestions[suggestioncount].addClassName('suggestionhover');
+            break;
+        
+        case 13:
+            select_material(osuggestions[suggestioncount]);
+            suggestioncount=-1;
+            break;
+        
+        default:
+            suggestioncount=-1;
+    }
 }
 
 function showHideProbes(activity){
-	
-	var bVisible = false;
+    var bVisible = false;
     $$('.probes_container,.probes_header').each(function(oButtonContainer){
         oButtonContainer.toggle();
         bVisible = bVisible || oButtonContainer.getStyle('display')=='none';
     });
-    	
 }
 
 function showHideFilters(oLink){
@@ -89,12 +86,11 @@ function showHideFilters(oLink){
 }
 function uncheckedallprobes()
 {
-	$$(".probe_items").each(function(e){
-		e.checked= false;
-		
-	})
+    $$(".probe_items").each(function(e){
+        e.checked= false;
+    });
 }
 
 function removesuggestions(){
-	$('suggestions').remove();
+    $('suggestions').remove();
 }
