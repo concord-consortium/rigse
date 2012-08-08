3af9803a19a7fdf9e42e6c2ce5c07f2037b00c98
Sagar Ghadge <sagar.ghadge@zeuslearning.com>
2012-08-08 20:04:31 +0530

    Modify In Progress students table UI
    - Modify In Progress students table UI in
      Recent Activity page

--- public/javascripts/instructional_materials.js
+++ public/javascripts/instructional_materials.js
@@ -93,9 +93,34 @@ document.observe("dom:loaded", function() {
             showInstructionalMaterial(oTab);
         });
     });
+    setTableHeaders(1);
     if (arrTabs.length > 0)
     {
         var strTabID = arrTabs[0].id;
         setSelectedTab(strTabID);
     }
+
 });
+
+function setTableHeaders(iDefaultWidth)
+{
+    var iWidth;
+    var iContainerWidth;
+    $$("th.expand_collapse_text").each(function(oTitle){
+        var oChild = oTitle.childElements()[0];
+        if(oChild)
+        {
+            iContainerWidth = (oTitle.offsetWidth > 0)? oTitle.offsetWidth-10 : 1;
+            if(iDefaultWidth)
+                iWidth = iDefaultWidth;
+            else
+                iWidth = (oTitle.getStyle('display') == "none")? 1 : iContainerWidth;
+            oChild.setStyle({'display':'none'});
+            oChild.setStyle({'width':iWidth+'px'});
+        }
+    });
+    
+    $$("th.expand_collapse_text > div.progressbar_container").each(function(oTitle){
+        oTitle.setStyle({'display':''});
+    });
+}
