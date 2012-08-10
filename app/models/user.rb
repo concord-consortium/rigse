ebdda898cd15e1d868b1559d75fe6ecb14dafe86
Scott Cytacki <scytacki@concord.org>
2012-08-10 16:00:20 -0400

    fix issue with case sensitivity in usernames

--- app/models/user.rb
+++ app/models/user.rb
@@ -63,7 +63,7 @@ class User < ActiveRecord::Base
 
   validates_presence_of     :login
   validates_length_of       :login,    :within => 1..40
-  validates_uniqueness_of   :login
+  validates_uniqueness_of   :login, :case_sensitive => false
   validates_format_of       :login,    :with => Authentication.login_regex, :message => Authentication.bad_login_message
 
   validates_format_of       :first_name,     :with => Authentication.name_regex,  :message => Authentication.bad_name_message, :allow_nil => true
@@ -74,7 +74,7 @@ class User < ActiveRecord::Base
 
   validates_presence_of     :email
   validates_length_of       :email,    :within => 6..100 #r@a.wk
-  validates_uniqueness_of   :email
+  validates_uniqueness_of   :email, :case_sensitive => false
   validates_format_of       :email,    :with => Authentication.email_regex, :message => Authentication.bad_email_message
 
   validates_presence_of     :vendor_interface_id
