549f030bb43c8362deb5be7f64dbd49d0a09256b
Nitesh Patil <nitesh.patil@zeuslearning.com>
2012-08-10 19:37:30 +0530

    Update Search functionality
    - Limit Search suggestions to 10
    
    - Remove suggestion box on 'GO' button click
    
    - Remove redundant code from search controller

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -7,33 +7,14 @@ class SearchController < ApplicationController
       redirect_to root_path
       return
     end
-    
-    @name = params[:search_term]
-    @sort_order = param_find(:sort_order, (params[:method] == :get))
-    
-    search_options = {
-      :name => @name|| "",
-      :sort_order => @sort_order || 'created_at DESC',
-      :paginate => false
-      #:page => params[:investigation_page]? params[:investigation_page] : 1,
-      #:per_page => 10
-    }
+    search_options=get_investigation_searchoptions()
     @investigations = Investigation.search_list(search_options)
     @investigations_count = @investigations.length
-    @investigations = @investigations.paginate(:page => params[:investigation_page]? params[:activity_page] : 1, :per_page => 10) 
-    activity_search_options = {
-      :name => @name || "",
-      :sort_order => @sort_order || 'created_at DESC',
-      :paginate => false
-      #:page => params[:activity_page]? params[:activity_page] : 1,
-      #:per_page => 10
-    }
+    @investigations = @investigations.paginate(:page => params[:investigation_page]? params[:investigation_page] : 1, :per_page => 2) 
+    activity_search_options=get_activity_searchoptions()
     @activities = Activity.search_list(activity_search_options)
     @activities_count = @activities.length
-    unless @investigations_count || @investigations_count
-      @b_check=true
-    end
-    @activities = @activities.paginate(:page => params[:activity_page]? params[:activity_page] : 1, :per_page => 10)
+    @activities = @activities.paginate(:page => params[:activity_page]? params[:activity_page] : 1, :per_page => 2)
   end
   
   def show
@@ -41,50 +22,20 @@ class SearchController < ApplicationController
       redirect_to root_path
       return
     end
-    @name = params[:search_term]
-    @sort_order = param_find(:sort_order, (params[:method] == :get))
-    search_options = {
-      :name => @name || '',
-      :sort_order => @sort_order || 'created_at DESC',
-      :paginate => false,
-      #:page => params[:investigation_page] ? params[:investigation_page] : 1,
-      #:per_page => 10
-    }
     
-    @investigations = Investigation.search_list(search_options)
-    @investigations_count = @investigations.length
-    if @investigations_count > 0
-      @b_check = @b_check || true;
-      @suggestions += @investigations
-    else
-      @b_check = @b_check || false;
-    end
-    @investigations = @investigations.paginate(:page => params[:activity_page]? params[:activity_page] : 1, :per_page => 10)
-    
-    activity_search_options = {
-      :name => @name || '',
-      :sort_order => @sort_order || 'created_at DESC',
-      :paginate => false,
-      #:page => params[:activity_page] ? params[:activity_page] : 1,
-      #:per_page => 10
-    }
-    
-      @activities = Activity.search_list(activity_search_options)
-      @activities_count = @activities.length
-      if @activities_count > 0
-        @suggestions += @activities
-        @b_check = @b_check || true;
-      else
-        @b_check = @b_check || false;
-      end
-      @activities = @activities.paginate(:page => params[:activity_page]? params[:activity_page] : 1, :per_page => 10)
-    
+    search_options=get_investigation_searchoptions()
+    investigations = Investigation.search_list(search_options)
+    @investigations_count = investigations.length
+    investigations = investigations.paginate(:page => params[:activity_page]? params[:activity_page] : 1, :per_page => 2)
+    activity_search_options=get_activity_searchoptions()
+    activities = Activity.search_list(activity_search_options)
+    @activities_count = activities.length
+    activities = activities.paginate(:page => params[:activity_page]? params[:activity_page] : 1, :per_page => 2)
     
     if request.xhr?
       render :update do |page| 
-        page.replace_html 'offering_list', :partial => 'search/search_results'
+        page.replace_html 'offering_list', :partial => 'search/search_results',:locals=>{:investigations=>investigations,:activities=>activities}
         page << "$('suggestions').remove();"
-        
       end
     else
       respond_to do |format|
@@ -96,20 +47,46 @@ class SearchController < ApplicationController
     end
   end
   
-  def get_search_suggestions
-    @name = params[:search_term]
+  def get_investigation_searchoptions
+    @search_term = params[:search_term]
+    @sort_order = param_find(:sort_order, (params[:method] == :get))
     search_options = {
-      :name => @name,
-      :sort_order => 'created_at DESC'
+      :name => @search_term || '',
+      :sort_order => @sort_order || 'name ASC',
+      :paginate => false,
+      #:page => params[:investigation_page] ? params[:investigation_page] : 1,
+      #:per_page => 10
+    }
+    return search_options
+  end
+
+  def get_activity_searchoptions
+    @search_term = params[:search_term]
+    @sort_order = param_find(:sort_order, (params[:method] == :get))
+    activity_search_options = {
+      :name => @search_term || '',
+      :sort_order => @sort_order || 'name ASC',
+      :paginate => false,
+      #:page => params[:activity_page] ? params[:activity_page] : 1,
+      #:per_page => 10
+    }
+    return activity_search_options
+  end
+  
+  def get_search_suggestions
+    @search_term = params[:search_term]
+    search_options = {
+      :name => @search_term,
+      :sort_order => 'name ASC'
     }
     
-    @investigations = Investigation.search_list(search_options)
-    @activities = Activity.search_list(search_options)
+    investigations = Investigation.search_list(search_options)
+    activities = Activity.search_list(search_options)
     @suggestions= [];
-    @suggestions = @investigations + @activities
+    @suggestions = investigations + activities
     if request.xhr?
        render :update do |page|
-         page.replace_html 'search_suggestions', {:partial => 'search/search_suggestions',:locals=>{:textlength=>@name.length}}
+         page.replace_html 'search_suggestions', {:partial => 'search/search_suggestions',:locals=>{:textlength=>@search_term.length}}
        end
     end
   end
--- public/javascripts/search_materials.js
+++ public/javascripts/search_materials.js
@@ -1,139 +1,148 @@
-var suggestioncount=-1;
-
-function select_suggestion(e){
+var suggestioncount = -1;
+var ajaxRequest = "";
+var ajaxRequestSend = 0;
+function select_suggestion(e) {
     var strSuggestiontext = e.textContent.trim();
     $('search_term').value = strSuggestiontext;
     $('suggestions').remove();
     //$('show_suggestion').writeAttribute('name','no_suggestion');
-    suggestioncount =- 1;
+    suggestioncount = -1;
 }
 
-function highlightlabel(e){
+function highlightlabel(e) {
     $$('.highlightoption')[0].removeClassName('highlightoption');
     e.addClassName('highlightoption');
 }
 
-function searchsuggestions(e,oElement){
-    if(e.keyCode == 13 || e.keyCode == 40 || e.keyCode == 38 ){
-       // if(e.keyCode == 13)
-        return false;    
-        }
-    new Ajax.Request('search/get_search_suggestions', {parameters:{search_term:oElement.value},method: 'get'} );
+function searchsuggestions(e, oElement) {
+    if(e.keyCode == 13 || e.keyCode == 40 || e.keyCode == 38) {
+        // if(e.keyCode == 13)
+        return false;
+    }
+    ajaxRequest = new Ajax.Request('get_search_suggestions', {
+        parameters : {
+            search_term : oElement.value
+        },
+        method : 'get'
+    });
+    ajaxRequestSend = 1;
 }
 
-function showsuggestion(event,oelem){
-   // $('show_suggestion').writeAttribute('name','show_suggestion');
-    var osuggestions=$$('.suggestion');
-    var ohoverelements=$$('.suggestionhover');
-    if(osuggestions.length === 0)
-    {   
-        if(event.keyCode==13)
-        {   
+function showsuggestion(event, oelem) {
+    // $('show_suggestion').writeAttribute('name','show_suggestion');
+    var osuggestions = $$('.suggestion');
+    var ohoverelements = $$('.suggestionhover');
+    $('search_suggestions').show();
+    if(osuggestions.length === 0) {
+        if(event.keyCode == 13) {
             submitsuggestion();
         }
         return;
     }
-    if(ohoverelements.length > 0)
-    {
+    if(ohoverelements.length > 0) {
         ohoverelements[0].removeClassName('suggestionhover');
     }
-    switch (event.keyCode)
-    {
+    switch (event.keyCode) {
         case 40:
             suggestioncount++;
-            if(suggestioncount>=osuggestions.length)
-            {
-                suggestioncount=0;
+            if(suggestioncount >= osuggestions.length) {
+                suggestioncount = 0;
             }
             osuggestions[suggestioncount].addClassName('suggestionhover');
             break;
-        
+
         case 38:
             suggestioncount--;
-            if(suggestioncount<=-1)
-            {
-                suggestioncount=osuggestions.length-1;
+            if(suggestioncount <= -1) {
+                suggestioncount = osuggestions.length - 1;
             }
             osuggestions[suggestioncount].addClassName('suggestionhover');
             break;
-        
+
         case 13:
-            if (suggestioncount!=-1)
-            {
-              select_suggestion(osuggestions[suggestioncount]);
-              suggestioncount=-1;
+            if(suggestioncount != -1) {
+                select_suggestion(osuggestions[suggestioncount]);
+                suggestioncount = -1;
             }
-           
+
             submitsuggestion();
             break;
-        
+
         default:
-            suggestioncount=-1;
+            suggestioncount = -1;
     }
 }
 
-function showHideProbes(activity){
+function showHideProbes(activity) {
     var bVisible = false;
-    $$('.probes_container,.probes_header').each(function(oButtonContainer){
+    $$('.probes_container,.probes_header').each(function(oButtonContainer) {
         oButtonContainer.toggle();
-        bVisible = bVisible || oButtonContainer.getStyle('display')=='none';
+        bVisible = bVisible || oButtonContainer.getStyle('display') == 'none';
     });
 }
 
-function showHideFilters(oLink){
+function showHideFilters(oLink) {
     var bVisible = false;
-    $$('.collapse_filters').each(function(oButtonContainer){
+    $$('.collapse_filters').each(function(oButtonContainer) {
         oButtonContainer.toggle();
-        bVisible = bVisible || oButtonContainer.getStyle('display')=='none';
+        bVisible = bVisible || oButtonContainer.getStyle('display') == 'none';
     });
-    
+
     var strLinkText = "";
     var strExpandCollapseText = "";
-    if(bVisible){
+    if(bVisible) {
         strLinkText = "Show Filters";
         strExpandCollapseText = "+";
-    }
-    else{
+    } else {
         strLinkText = "Hide Filters";
         strExpandCollapseText = "-";
     }
-    
+
     $('oExpandCollapseText').update(strExpandCollapseText);
     oLink.update(strLinkText);
 }
-function uncheckedallprobes()
-{
-    $$(".probe_items").each(function(e){
-        e.checked= false;
+
+function uncheckedallprobes() {
+    $$(".probe_items").each(function(e) {
+        e.checked = false;
     });
 }
 
-function removesuggestions(){
+function removesuggestions() {
     $('suggestions').remove();
 }
 
-function preventsubmit(){
+function preventsubmit() {
     $('prevent_submit').setValue(1);
 }
 
-function allowsubmit(){
+function allowsubmit() {
     $('prevent_submit').setValue(0);
 }
 
-function submitsuggestion(){
+function submitsuggestion() {
     $('prevent_submit').setValue(0);
     document.getElementsByName('GO')[0].click();
 }
 
-function CheckSubmitStatus(){
-    var oVal=$('prevent_submit');
-    if (oVal.value==1)
-    {  
+function CheckSubmitStatus() {
+    var oVal = $('prevent_submit');
+    if(oVal.value == 1) {
         oVal.setValue(0);
         return false;
-    }
-    else
-    {  
+    } else {
         return true;
+    }
+}
+
+function abortAjaxRequest() {
+    if(ajaxRequestSend) {
+        ajaxRequest.transport.abort();
+        ajaxRequestSend = 0;
+        $('search_suggestions').hide();
+        if($('suggestions')) {
+            $('suggestions').remove();
+        }
+
     }
 }
