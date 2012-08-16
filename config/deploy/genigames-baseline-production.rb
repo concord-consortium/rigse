e4fb3ec265a892172bf21f08d37ca6c3a30d15de
Aaron Unger <aaron.unger@gmail.com>
2012-08-16 10:02:38 -0700

    Add genigames deploy configs

---
+++ config/deploy/genigames-baseline-production.rb
@@ -0,0 +1,7 @@
+# genigames baseline
+set :user, "deploy"
+set :domain, "baseline.genigames.concord.org"
+set :deploy_to, "/web/portal"
+server domain, :app, :web
+role :db, domain, :primary => true
+set :branch, "gv-r32"
