9e52769579f87d2beda956cb28da302cd64b7183
Vikash Sharma <vikash.sharma@zeuslearning.com>
2012-08-22 15:36:19 +0530

    Fix Issues and Modify probes filter UI
    - Fix Grade filter selection issue
    
    - Modify probes filter UI

--- public/javascripts/search_materials.js
+++ public/javascripts/search_materials.js
@@ -117,10 +117,11 @@ function showHideFilters(oLink) {
     oLink.update(strLinkText);
 }
 
-function uncheckedallprobes() {
+function uncheckedallprobes(allChecked) {
     $$(".probe_items").each(function(e) {
-        e.checked = false;
+        e.checked = allChecked;
     });
+    $('probe_0').checked= !allChecked;
 }
 
 function removesuggestions() {
@@ -251,17 +252,42 @@ function materialCheckOnClick(ObjId)
 
 }
 
-function selectAllGreades(grades)
+function selectAllGreades(gradeObj,grades)
 {
-    if($('allgrades').checked){
-        grades.each(function(obj){
-            $(obj).checked= "checked";
-        });
+    
+    if(gradeObj.id == 'allgrades'){
+        if($(gradeObj).checked){
+            grades.each(function(obj){
+                $(obj).checked= "checked";
+            });
+        }
+        else if(!$(gradeObj).checked){
+            grades.each(function(obj){
+                $(obj).checked= false;
+            });
+        }
     }
-    else{
-        grades.each(function(obj){
-            $(obj).checked= false;
-        });
+    else {
+        if(!$(gradeObj).checked){
+            $('allgrades').checked= false;
+        }
+        else{
+            allgrades_selected = true;
+            grades.each(function(obj){
+                allgrades_selected = allgrades_selected && $(obj).checked;
+                
+            });
+            if(allgrades_selected){
+                $('allgrades').checked= "checked";
+            }
+        }
+    }
+}
+
+function uncheckednoprobe(probeObj)
+{
+    if($(probeObj).checked){
+        $('probe_0').checked= false;
     }
 }
 
