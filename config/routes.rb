be1c9011b705dba84ba389e86adc8df983d879d9
Scott Cytacki <scytacki@concord.org>
2012-09-13 22:16:37 -0400

    link in login box to browse materials too
    this also adds a named route for the search page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 RailsPortal::Application.routes.draw do
+  match "search" => 'search#index', :as => :search
   get "search/index"
   match '/search/list/filter' => 'search#show', :as => :list_filter_search, :method => :get
   post '/search/get_current_material_unassigned_clazzes'
