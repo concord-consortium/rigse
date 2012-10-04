0c8d2da181544a535634bf400e95259ebd01d99d
Parker Morse <flashesofpanic@gmail.com>
2012-10-04 14:31:41 -0400

    Changing branch to deploy to lightweight-mw

--- config/deploy/lightweight-mw.rb
+++ config/deploy/lightweight-mw.rb
@@ -4,4 +4,4 @@ set :domain, "lightweight-mw.concord.org"
 set :deploy_to, "/web/portal"
 server domain, :app, :web
 role :db, domain, :primary => true
-set :branch, "lightweight-mw"
+set :branch, "lightweight-mw-authoring"
