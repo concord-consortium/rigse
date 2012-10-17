72e860f0a2dc7ea4a0d3635e1296982b17ccfad3
Noah Paessel <knowuh@gmail.com>
2012-10-17 14:31:43 -0400

    [#36875831] don't show teacher sections to students in activities.
    (formatting cleanup)
    https://www.pivotaltracker.com/story/show/36875831

--- lib/controller_param_utils.rb
+++ lib/controller_param_utils.rb
@@ -1,12 +1,15 @@
 module ControllerParamUtils
-  # restrict access to sensitive routes to manager 
+  
   def self.included(clazz)
     clazz.class_eval do
+  
       protected  
+  
+      # ugly. but we need to undo our madness of 
+      # explicitly seeting boolean params to 'false' (strings)
       def boolean_param(symbol)
         boolvalue = params[symbol]
-        # ugly. but we need to undo our madness of 
-        # explicitly seeting boolean params to 'false' (strings)
+
         if boolvalue.class == String
           boolvalue = false if (boolvalue =~ /false/i)
           boolvalue = true if (boolvalue  =~ /true/i)
