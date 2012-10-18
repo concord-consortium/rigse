79398af93cf0eaeacd3a6e453726f421de6d2bc2
Parker Morse <flashesofpanic@gmail.com>
2012-10-18 14:32:27 -0400

    Moves lists of Embeddables into a constant.
    Several models define arrays of Embeddable models (actually, the constants that name them) and iterate over those arrays to define their has_many_x relationships with those elements. I've defined two constants which contain strings representing those Embeddables in an initializer, then iterated over that constant in the model definitions. Modification of the list of embeddables can therefore be done in just one place.

---
+++ config/initializers/embeddables.rb
@@ -0,0 +1,29 @@
+BASE_EMBEDDABLES = [ 
+"Embeddable::Xhtml",
+"Embeddable::OpenResponse",
+"Embeddable::MultipleChoice",
+"Embeddable::DataTable",
+"Embeddable::DrawingTool",
+"Embeddable::DataCollector",
+"Embeddable::LabBookSnapshot",
+"Embeddable::InnerPage",
+"Embeddable::MwModelerPage",
+"Embeddable::NLogoModel",
+"Embeddable::RawOtml",
+"Embeddable::Biologica::World",
+"Embeddable::Biologica::Organism",
+"Embeddable::Biologica::StaticOrganism",
+"Embeddable::Biologica::Chromosome",
+"Embeddable::Biologica::ChromosomeZoom",
+"Embeddable::Biologica::BreedOffspring",
+"Embeddable::Biologica::Pedigree",
+"Embeddable::Biologica::MultipleOrganism",
+"Embeddable::Biologica::MeiosisView",
+"Embeddable::Smartgraph::RangeQuestion"
+]
+
+ALL_EMBEDDABLES = BASE_EMBEDDABLES + [
+"Embeddable::ImageQuestion",
+"Embeddable::SoundGrapher",
+"Embeddable::VideoPlayer"
+]
