6a81536c4bf4e2beb3327702d230a997e0c3c7e7
Nitesh Patil <nitesh.patil@zeuslearning.com>
2012-10-18 18:26:22 +0530

    Refine "Edit Help" functionality
    - Display popup when no data is entered in textbox

--- app/assets/javascripts/project_form.js
+++ app/assets/javascripts/project_form.js
@@ -1,8 +1,5 @@
 function validate_project_form_help_page_settings(oForm){
-    debugger;
-    alert('1111111111111111111111111111111111111111');
-    
-    var helpPageOptions = oForm.getElementByName('admin_project[help_type]');
+    var helpPageOptions = $(oForm).select('input[name=admin_project[help_type]]');
     var selectedOption = 'none selected';
     for (var i = 0; i < helpPageOptions.length; i++){
         if(helpPageOptions[i].checked){
@@ -12,29 +9,28 @@ function validate_project_form_help_page_settings(oForm){
     }
     if(selectedOption == 'none selected'){
         list_modal = new UI.Window({ theme:"lightbox", width:500});
-        list_modal.setContent("<div style='padding:10px'>Select atleast one option for help page</div>").show(true).focus().center();
+        list_modal.setContent("<div style='padding:10px'>Select atleast one option for help page.</div>").show(true).focus().center();
         list_modal.setHeader("Error");
         return false;
     }
-    else if(selectedOption.value = 'external url'){
-        var externalUrl = oForm.getElementByName('admin_project[external_url]')[0].value
+    else if(selectedOption.value == 'external url'){
+        var externalUrl = $(oForm).select('input[name=admin_project[external_url]]')[0].value;
         if (!(/\S/.test(externalUrl))){
             list_modal = new UI.Window({ theme:"lightbox", width:500});
-            list_modal.setContent("<div style='padding:10px'>External url cannot be blank if selected for help page</div>").show(true).focus().center();
+            list_modal.setContent("<div style='padding:10px'>External url cannot be blank if selected for help page.</div>").show(true).focus().center();
             list_modal.setHeader("Error");
             return false;
         }
         
     }
-    else if(selectedOption.value = 'help custom html'){
-        var customHelpPageHtml = oForm.getElementByName('admin_project[custom_help_page_html]')[0].value
+    else if(selectedOption.value == 'help custom html'){
+        var customHelpPageHtml = $(oForm).select('textarea[name=admin_project[custom_help_page_html]]')[0].value
         if (!(/\S/.test(customHelpPageHtml))){
             list_modal = new UI.Window({ theme:"lightbox", width:500});
-            list_modal.setContent("<div style='padding:10px'>Custom HTML cannot be blank if selected for help page</div>").show(true).focus().center();
+            list_modal.setContent("<div style='padding:10px'>Custom HTML cannot be blank if selected for help page.</div>").show(true).focus().center();
             list_modal.setHeader("Error");
             return false;
         }
     }
-    alert('222222222222222222222222222222222222222222222222222222222');
     return true;
 }
