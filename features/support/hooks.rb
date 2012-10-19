f7a45b6ae42e779dbcfa355333f201debcc7b509
Noah Paessel <knowuh@gmail.com>
2012-10-19 15:06:11 -0400

    introduce @lightweight hooks to configure feature for non-jnlp runnables

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -1,4 +1,5 @@
 Around('@lightweight') do |scenario, block|
+  @default_use_jnlps = APP_CONFIG[:use_jnlps]
   APP_CONFIG[:use_jnlps] = false  
   block.call
   APP_CONFIG[:use_jnlps] = @default_use_jnlps
