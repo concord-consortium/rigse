3e98bdcefc5c9603fcbea2da7142d0fd45d1aa2a
Sunny Singh <sunny.singh@zeuslearning.com>
2012-10-19 18:25:23 +0530

    Refine home page preview
    - Add height and width for preview window.

--- app/assets/javascripts/preview_home_page.js
+++ app/assets/javascripts/preview_home_page.js
@@ -5,8 +5,8 @@ function preview_home_page(homePageContentId, homePageContent, isDomId){
     else{
         var previewContent = homePageContent
     }
-    var previewWindow = window.open('','preview_window','fullscreen=yes');
-    var formString = '<form id="preview_project_form" action="/home/preview_home_page" method="post" style="display: none"><textarea class="mceNoEditor" cols="40" id="preview_content_container" name="home_page_preview_content" rows="20">'+previewContent+'</textarea></form>';
+    var previewWindow = window.open('','preview_window','height = 700 width = 800');
+    var formString = '<form id="preview_project_form" action="/home/preview_home_page" method="post" style="display: none"><textarea class="mceNoEditor" cols="40" id="preview_content_container" name="home_page_preview_content" rows="20" style="opacity:0;">'+previewContent+'</textarea></form>';
     previewWindow.document.write(formString);
     previewWindow.document.getElementById('preview_project_form').submit();
 }
