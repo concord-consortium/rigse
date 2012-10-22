0365a43f7a342aac20045635340a1d733741fbe0
Sunny Singh <sunny.singh@zeuslearning.com>
2012-10-22 17:18:54 +0530

    Refine home page preview
    - Handle cross browser issues for popup window.
    
    - Remove class 'pie' from preview button.

--- app/assets/javascripts/preview_home_page.js
+++ app/assets/javascripts/preview_home_page.js
@@ -5,7 +5,7 @@ function preview_home_page(homePageContentId, homePageContent, isDomId){
     else{
         var previewContent = homePageContent
     }
-    var previewWindow = window.open('','preview_window','height = 700 width = 800');
+    var previewWindow = window.open('','preview_window','height = 700 width = 800, resizable = 1, scrollbars = 1');
     var formString = '<form id="preview_project_form" action="/home/preview_home_page" method="post" style="display: none"><textarea class="mceNoEditor" cols="40" id="preview_content_container" name="home_page_preview_content" rows="20" style="opacity:0;">'+previewContent+'</textarea></form>';
     previewWindow.document.write(formString);
     previewWindow.document.getElementById('preview_project_form').submit();
