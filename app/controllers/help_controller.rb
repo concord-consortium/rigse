8f98bd35052afbba62159b6eb211f9021603b861
Sunny Singh <sunny.singh@zeuslearning.com>
2012-10-25 15:04:33 +0530

    Refine help page preview
    - Open help link in a popup window when help type is
      custom html
    
    - Open help window with current help type on refresh.

--- app/controllers/help_controller.rb
+++ app/controllers/help_controller.rb
@@ -5,13 +5,21 @@ class HelpController < ApplicationController
   theme "rites"
   
   def index
-    @help_page_content = current_project.custom_help_page_html
-    # Turn untrusted string to UTF-8. We need to do this
-    # because for some reason the code being taken is an ascii
-    # string being treated as UTF-8.
-    # Code taken from http://stackoverflow.com/a/968618
-    ic = Iconv.new('UTF-8//IGNORE', 'UTF-8')
-    @help_page_content = ic.iconv(@help_page_content + ' ')[0..-2]
+    case current_project.help_type
+    when 'no help'
+      redirect_to :root
+    when 'external url'
+      external_url = current_project.external_url
+      redirect_to "#{external_url}"
+    when 'help custom html'
+      @help_page_content = current_project.custom_help_page_html
+      # Turn untrusted string to UTF-8. We need to do this
+      # because for some reason the code being taken is an ascii
+      # string being treated as UTF-8.
+      # Code taken from http://stackoverflow.com/a/968618
+      ic = Iconv.new('UTF-8//IGNORE', 'UTF-8')
+      @help_page_content = ic.iconv(@help_page_content + ' ')[0..-2]
+    end
   end
   
   def preview_help_page
