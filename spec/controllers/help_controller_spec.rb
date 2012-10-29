072165c54ed9576e5d3ccbb3dd6757ec7566050d
Sunny Singh <sunny.singh@zeuslearning.com>
2012-10-29 12:57:37 +0530

    Modify spec test for help controller
    - Modify spec help_controller_spec to accommodate changes
      in help controller.

--- spec/controllers/help_controller_spec.rb
+++ spec/controllers/help_controller_spec.rb
@@ -4,16 +4,16 @@ describe HelpController do
   
   before(:each) do
       @admin_user = Factory.next(:admin_user)
-      @test_project = Factory.create(:admin_project_no_jnlps, :user => @admin_user)
+      @test_project = Factory.create(:admin_project_no_jnlps, :user => @admin_user, :id=> 1)
       stub_current_user :admin_user
       Admin::Project.stub(:default_project).and_return(@test_project)
   end
   
   describe "GET index" do
-    it "should redirect to root when help type is no help" do
+    it "should render no help page template when help type is no help" do
       @test_project.help_type = 'no help'
       get :index
-      response.should redirect_to :root
+      assert_template 'help/no_help_page'
     end
     it "should redirect to external url when help type is external url" do
       @test_project.external_url = 'www.concord.org'
@@ -21,7 +21,7 @@ describe HelpController do
       get :index
       response.should redirect_to 'www.concord.org'
     end
-    it "should redirect to external url when help type is external url" do
+    it "should render index template when help type is help custom html" do
       @test_project.custom_help_page_html = '<b>Help page</b>'
       @test_project.help_type = 'help custom html'
       get :index
@@ -29,7 +29,7 @@ describe HelpController do
       assert_template 'index'
     end
   end
-  
+
   describe "POST preview_help_page" do
     it "should set variables with data coming from front end when preview is from edit page" do
       @post_params = {
@@ -40,25 +40,41 @@ describe HelpController do
       assert_template 'preview_help_page'
     end
     
-    it "should redirect to root when help type is no help and preview is from summary page." do
+    it "should render no help page template when help type is no help and preview is from summary page." do
       @test_project.help_type = 'no help'
-      get :preview_help_page
-      response.should redirect_to :root
+      @test_project.save!
+      @test_project.reload
+      @post_params = {
+          :preview_help_page_from_summary_page => "#{@test_project.id}"
+        }
+      get :preview_help_page, @post_params
+      assert_template 'help/no_help_page'
     end
     
     it "should redirect to external url when help type is external url and preview is from summary page." do
       @test_project.external_url = 'www.concord.org'
       @test_project.help_type = 'external url'
-      get :preview_help_page
+      @test_project.save!
+      @test_project.reload
+      @post_params = {
+          :preview_help_page_from_summary_page => "#{@test_project.id}"
+        }
+      get :preview_help_page, @post_params
       response.should redirect_to 'www.concord.org'
     end
     
-    it "should redirect to external url when help type is external url and preview is from summary page." do
+    it "should render preview_help_page template when help type is help custom html and preview is from summary page." do
       @test_project.custom_help_page_html = '<b>Help page</b>'
       @test_project.help_type = 'help custom html'
-      get :preview_help_page
+      @test_project.save!
+      @test_project.reload
+      @post_params = {
+          :preview_help_page_from_summary_page => "#{@test_project.id}"
+        }
+      get :preview_help_page, @post_params
       assert_equal assigns[:help_page_preview_content], '<b>Help page</b>'
       assert_template 'preview_help_page'
     end
+    
   end
 end
