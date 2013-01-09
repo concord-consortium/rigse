db7926acbe238e41cdc0058e71dd94286a9d1fb8
Scott Cytacki <scytacki@concord.org>
2013-01-09 13:19:41 -0500

    conditionalize external db connections
    this removes extraneous output when they aren't needed

--- app/models/itsi/itsi.rb
+++ app/models/itsi/itsi.rb
@@ -10,9 +10,11 @@ class Itsi::Itsi < ActiveRecord::Base
   end
 
 #  self.table_name_prefix = ""
-  begin
-    establish_connection :itsi
-  rescue 
-    puts "unable to establish connection for itsi (models/itsi/itsi)"
+  if configurations.include? 'itsi'
+    begin
+      establish_connection :itsi
+    rescue 
+      puts "unable to establish connection for itsi (models/itsi/itsi)"
+    end
   end
 end
