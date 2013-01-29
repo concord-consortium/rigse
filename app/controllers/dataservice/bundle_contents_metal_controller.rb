c1d0dae80351fe88ea177152756f5fa2b58dfac4
Scott Cytacki <scytacki@concord.org>
2013-01-28 22:45:33 -0500

    record upload time in bundle_content

--- app/controllers/dataservice/bundle_contents_metal_controller.rb
+++ app/controllers/dataservice/bundle_contents_metal_controller.rb
@@ -1,6 +1,7 @@
 class Dataservice::BundleContentsMetalController < ActionController::Metal
 
   def create
+    controller_start_time = Time.now
     if bundle_logger = Dataservice::BundleLogger.find(params[:id])
       body = request.body.read
       if bundle_logger.in_progress_bundle
@@ -10,7 +11,15 @@ class Dataservice::BundleContentsMetalController < ActionController::Metal
           :bundle_content => bundle_logger.in_progress_bundle
         )
       end
-      bundle_logger.end_bundle( { :body => body } )
+
+      # make a time object out of X-Queue-Start header if it exists
+      upload_time = nil
+      x_queue_start = request.headers['X-Queue-Start']
+      if x_queue_start
+        usecs_since_1970 = x_queue_start.match(/t=(\d+)/)[1].to_i
+        upload_time = controller_start_time - Time.at(usecs_since_1970/1000000)
+      end
+      bundle_logger.end_bundle( { :body => body, :upload_time => upload_time } )
       bundle_content = bundle_logger.bundle_contents.last
       digest = Digest::MD5.hexdigest(body)
 
