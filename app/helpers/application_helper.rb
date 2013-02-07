1d71b20160f485613a8a63f217628539ea80c495
Parker Morse <pmorse@concord.org>
2013-02-07 13:53:18 -0500

    Google Analytics setup [#43732555]

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1230,4 +1230,17 @@ module ApplicationHelper
     return false
   end
 
+  def google_analytics_config
+    <<CONFIG
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', '#{GOOGLE_ANALYTICS_ACCOUNT}']);
+_gaq.push(['_trackPageview']);
+
+(function() {
+  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+  ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
+CONFIG
+  end
 end
