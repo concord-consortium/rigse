767be82aecfffbdcb1db7c1b6b3b130b012ef0dc
Noah Paessel <knowuh@gmail.com>
2013-02-14 15:01:40 -0500

    Temporarily switch interactions-staging to this branch, master soon?

--- config/deploy/interactions-staging.rb
+++ config/deploy/interactions-staging.rb
@@ -4,4 +4,4 @@ set :domain, "interactions.staging.concord.org"
 set :deploy_to, "/web/portal"
 server domain, :app, :web
 role :db, domain, :primary => true
-set :branch, "master"
+set :branch, "topic_np_image_gallery_from_itsisu"
