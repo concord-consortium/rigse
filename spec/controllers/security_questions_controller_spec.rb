29ab9ff4881a86d2f40329f40ed55d319fa7f84c
Noah Paessel <knowuh@gmail.com>
2013-03-01 12:19:24 -0500

    whitespace cleanup.

--- spec/controllers/security_questions_controller_spec.rb
+++ spec/controllers/security_questions_controller_spec.rb
@@ -2,7 +2,7 @@ require File.expand_path('../../spec_helper', __FILE__)
 
 describe SecurityQuestionsController do
   render_views
-  
+
   before(:each) do
     @student = Factory.create(:portal_student, :user => Factory.create(:user))
     stub_current_user @student.user
@@ -13,14 +13,14 @@ describe SecurityQuestionsController do
     @test_project.stub!(:enabled_bookmark_types).and_return([])
     Admin::Project.stub(:default_project).and_return(@test_project)
   end
-  
+
   describe "GET edit" do
     it "fills in the form with the student's current security questions, if any" do
       @student.user.security_questions.create({ :question => "Test question 1", :answer => "test answer 1" })
       @student.user.security_questions.create({ :question => "Test question 2", :answer => "test answer 2" })
-      
+
       get :edit
-      
+
       @student.user.security_questions.each_with_index do |q, i|
         assert_select("select[name=?]", "security_questions[question#{i}][question_idx]") do
           assert_select("option[value='current']", :text => q.question_idx)
@@ -29,7 +29,7 @@ describe SecurityQuestionsController do
       end
     end
   end
-  
+
   describe "PUT update" do
     before(:each) do
       @params_for_update = {
@@ -49,26 +49,26 @@ describe SecurityQuestionsController do
         }
       }
     end
-    
+
     it "updates the user's security questions" do
       SecurityQuestion.should_receive(:make_questions_from_hash_and_user)
       SecurityQuestion.should_receive(:errors_for_questions_list!)
       @student.user.should_receive(:update_security_questions!)
-      
+
       put :update, @params_for_update
     end
-    
+
     it "redirects the user to their home page once they set their security questions" do
       put :update, @params_for_update
-      
+
       @response.should redirect_to(root_path)
     end
-    
+
     it "does not accept invalid question values" do
       SecurityQuestion.should_receive(:errors_for_questions_list!).and_return(["Wicked bad errors!"])
-      
+
       put :update, @params_for_update
-      
+
       flash[:error].should include("Wicked bad errors!")
       assert_template "edit"
     end
