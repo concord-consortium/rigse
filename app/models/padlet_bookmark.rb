c695fc6e0a10a85513515c8ecc222d5908971be4
Noah Paessel <knowuh@gmail.com>
2013-03-01 05:42:19 -0500

    Authenticate with the users portal username and password.

--- app/models/padlet_bookmark.rb
+++ app/models/padlet_bookmark.rb
@@ -4,7 +4,8 @@ class PadletBookmark < Bookmark
   def self.create_for_user(user)
     found = self.for_user(user)
     return found unless found.blank?
-    padlet = PadletWrapper.make_bookmark
+    email  = user.email || "#{user.login}@concord.org"
+    padlet = PadletWrapper.make_bookmark(email,'password')
     url    = padlet.padlet_url
     name   = "#{user.name}'s Padlet"
     made   = self.create(:user => user, :name => name, :url => url)
