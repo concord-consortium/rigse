f91ab1ec055beba8a09b2a11055d80c2ed281cd6
Noah Paessel <knowuh@gmail.com>
2013-03-01 12:18:06 -0500

    Bookmark : rm test case; Bookmark Types are manually registered 4now.

--- spec/models/bookmark_spec.rb
+++ spec/models/bookmark_spec.rb
@@ -6,11 +6,6 @@ describe Bookmark do
       it "should return an array of subclasses" do
         Bookmark.available_types.should be_kind_of Array
       end
-      it "should include all subclasses of Bookmark" do
-        class Frog < Bookmark
-        end
-        Bookmark.available_types.should include Frog
-      end
     end
   end
 end
