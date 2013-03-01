c4388ee257abd2a8d9eeb20a7459fedfb6b16044
Aaron Unger <aaron.unger@gmail.com>
2013-03-01 13:23:02 -0800

    Really fix the time calculation

--- app/controllers/misc_metal_controller.rb
+++ app/controllers/misc_metal_controller.rb
@@ -2,6 +2,6 @@ class MiscMetalController < ActionController::Metal
   def time
     self.status = 200
     self.content_type = 'text/plain'
-    self.response_body = "#{((Time.now.to_f + Time.now.gmt_offset) * 1000).to_i}"
+    self.response_body = "#{((Time.now.to_f - Time.now.gmt_offset) * 1000).to_i}"
   end
 end
