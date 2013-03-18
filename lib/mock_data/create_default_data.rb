0e19f855a507d36cc2de1ddc6b3be3d119a2f8e9
Sunny Singh <sunny.singh@zeuslearning.com>
2013-03-18 17:22:34 +0530

    Refine default data creation method.
    - Refine default data creation method.

--- lib/mock_data/create_default_data.rb
+++ lib/mock_data/create_default_data.rb
@@ -5,8 +5,9 @@ module MockData
   #load all the factories
   Dir[File.dirname(__FILE__) + '/../../factories/*.rb'].each {|file| require file }
   
-  
-  
+  @default_users = nil
+  @default_teachers = nil
+  @default_students = nil
   
   def self.convert_hash_keys_to_symbols(hash_to_convert)
     new_hash = {}
@@ -20,23 +21,23 @@ module MockData
     
     default_password = APP_CONFIG[:password_for_default_users]
     user = nil
-    roles = user_info.delete('roles')
+    roles = user_info.delete(:roles)
     roles = roles ? roles.split(/,\s*/) : []
     
     #TODO: if YAML provides a user password don't override it with the default
-    user_info.merge!('password' => default_password)
+    user_info.merge!(:password => default_password)
     
-    user_by_uuid = User.find_by_uuid(user_info["uuid"])
-    user_by_login = User.find_by_login(user_info["login"])
+    user_by_uuid = User.find_by_uuid(user_info[:uuid])
+    user_by_login = User.find_by_login(user_info[:login])
     
     if user_by_uuid
       user = user_by_uuid
-      user.password = user_info['password']
-      user.password_confirmation = user_info['password']
+      user.password = user_info[:password]
+      user.password_confirmation = user_info[:password]
       
-      user.first_name = user_info['first_name'] if user_info['first_name']
-      user.last_name = user_info['last_name'] if user_info['last_name']
-      user.email = user_info['email'] if user_info['email']
+      user.first_name = user_info[:first_name] if user_info[:first_name]
+      user.last_name = user_info[:last_name] if user_info[:last_name]
+      user.email = user_info[:email] if user_info[:email]
       
       user.save!
     elsif user_by_login.nil?
@@ -68,14 +69,14 @@ module MockData
     
     #create a district
     default_district = nil
-    district_info = DEFAULT_DATA['district']
-    district_by_uuid = Portal::District.find_by_uuid(district_info["uuid"])
-    district_by_name = Portal::District.find_by_name(district_info["name"])
+    district_info = DEFAULT_DATA[:district]
+    district_by_uuid = Portal::District.find_by_uuid(district_info[:uuid])
+    district_by_name = Portal::District.find_by_name(district_info[:name])
     
     if district_by_uuid
       default_district = district_by_uuid
-      default_district.name = district_info["name"]
-      default_district.description = district_info['description']
+      default_district.name = district_info[:name]
+      default_district.description = district_info[:description]
       default_district.save!
     elsif district_by_name.nil?
        default_district = Portal::District.create!(district_info)
@@ -85,29 +86,28 @@ module MockData
     #create schools if default district is present
     default_schools = []
     if default_district
-      DEFAULT_DATA['schools'].each do |school, school_info|
-        semester_info = school_info.delete("semesters")
-        school_by_uuid = Portal::School.find_by_uuid(school_info["uuid"])
-        school_by_name_and_district = Portal::School.find_by_name_and_district_id(school_info["name"], default_district.id)
+      DEFAULT_DATA[:schools].each do |school, school_info|
+        semester_info = school_info.delete(:semesters)
+        school_by_uuid = Portal::School.find_by_uuid(school_info[:uuid])
+        school_by_name_and_district = Portal::School.find_by_name_and_district_id(school_info[:name], default_district.id)
         school = nil
         if school_by_uuid
           school = school_by_uuid
-          school.name = school_info['name']
-          school.description = school_info['description']
+          school.name = school_info[:name]
+          school.description = school_info[:description]
           school.district_id = default_district.id
           school.save!
           default_schools << school
         elsif school_by_name_and_district.nil?
-          school_info['district_id'] = default_district.id
-          school_info = self.convert_hash_keys_to_symbols(school_info)
+          school_info[:district_id] = default_district.id
           school = Portal::School.create!(school_info)
           default_schools << school
         end
         
         if school
           semester_info.each do |semester, sem_info|
-            sem = Portal::Semester.find_or_create_by_uuid(sem_info['uuid'])
-            sem.name = sem_info['name']
+            sem = Portal::Semester.find_or_create_by_uuid(sem_info[:uuid])
+            sem.name = sem_info[:name]
             sem.school_id = school.id
             sem.save!
           end
@@ -119,7 +119,7 @@ module MockData
     #following users exist
     default_users = []
     
-    DEFAULT_DATA['users'].each do |user, user_info|
+    DEFAULT_DATA[:users].each do |user, user_info|
       
       user = add_default_user(user_info)
       
@@ -131,10 +131,10 @@ module MockData
     
     
     #following teachers exist
-    
-    DEFAULT_DATA['teachers'].each do |teacher, teacher_info|
+    default_teachers = []
+    DEFAULT_DATA[:teachers].each do |teacher, teacher_info|
       
-      teacher_school_name = teacher_info.delete('school')
+      teacher_school_name = teacher_info.delete(:school)
       teacher_school = default_schools.select { |school| school.name == teacher_school_name }
       if teacher_school.length == 0
         next
@@ -143,15 +143,15 @@ module MockData
       end
       
       
-      cohorts = teacher_info.delete("cohort_list")
+      cohorts = teacher_info.delete(:cohort_list)
       
-      roles = teacher_info['roles']
+      roles = teacher_info[:roles]
       if roles
         roles << 'member'
       else
         roles = ['member']
       end
-      teacher_info['roles'] = roles
+      teacher_info[:roles] = roles
       
       user = add_default_user(teacher_info)
       
@@ -167,35 +167,40 @@ module MockData
         portal_teacher.save!
         
         default_users << user
-        
+        default_teachers << portal_teacher
       end
     end
+    @default_teachers = default_teachers
     
     
-    
-    DEFAULT_DATA['students'].each do |student, student_info|
+    default_students = []
+    DEFAULT_DATA[:students].each do |student, student_info|
       
-      roles = student_info['roles']
+      roles = student_info[:roles]
       if roles
         roles << 'member'
       else
         roles = ['member']
       end
       
-      student_info['roles'] = roles
+      student_info[:roles] = roles
       
       user = add_default_user(student_info)
       
       if user
         portal_student = user.portal_student
         unless portal_student
-          Portal::Student.create!(:user_id => user.id)
+          portal_student = Portal::Student.create!(:user_id => user.id)
         end
         
         default_users << user
+        default_students << portal_student
       end
     end
     
+    @default_students = default_students
+    
+    @default_users = default_users
     
   end #end of method create_default_users
   
@@ -206,46 +211,56 @@ module MockData
     # student class mapping
     
     #following classes exist:
-    DEFAULT_DATA['classes'].each do |claz, clazz_info|
-      user = User.find_by_login clazz_info['teacher']
-      teacher = user.portal_teacher
-      clazz_info.merge!('teacher' => teacher)
+    default_classes = []
+    DEFAULT_DATA[:classes].each do |clazz, clazz_info|
+      default_clazz = nil
+      default_clazz_by_uuid = Portal::Clazz.find_by_uuid(clazz_info[:uuid])
+      default_clazz_by_clazz_word = Portal::Clazz.find_by_class_word(clazz_info[:class_word])
+      teacher = @default_teachers.find{|t| t.user.login == clazz_info[:teacher]}
       
-      clazz = Portal::Clazz.find_by_class_word(clazz_info['class_word'])
-      if clazz
-        clazz.name = clazz_info['name']
-        clazz.add_teacher(teacher)
-      else
-        Factory.create(:portal_clazz, clazz_info)
+      if default_clazz_by_uuid
+        unless default_clazz_by_clazz_word
+          default_clazz = default_clazz_by_uuid
+          default_clazz.clazz_word = clazz_info[:class_word]
+          default_clazz.teacher_id = teacher.id
+        end
+      elsif teacher and default_clazz_by_clazz_word.nil?
+        info = {
+          :name => clazz_info[:name],
+          :class_word => clazz_info[:class_word],
+          :teacher_id => teacher.id,
+          :uuid => clazz_info[:uuid]
+        }
+        default_clazz = Portal::Clazz.create!(info)
       end
+      
+      default_classes << default_clazz if default_clazz
     end
     
     #following teacher and class mapping exists:
-    DEFAULT_DATA['teacher_clazzes'].each do |teacher, clazzes_name|
-      user = User.find_by_login(teacher)
-      portal_teacher = Portal::Teacher.find_by_user_id(user.id)
-      clazzes_name = clazzes_name.split(",").map{|c| c.strip }
-      clazzes = clazzes_name.map!{|c| Portal::Clazz.find_by_name(c)}
-      clazzes.each do |clazz|
-        teacher_clazz = Portal::TeacherClazz.find_by_clazz_id_and_teacher_id(clazz.id, portal_teacher.id)
-        unless teacher_clazz
-          teacher_clazz = Portal::TeacherClazz.new()
-          teacher_clazz.clazz_id = clazz.id
-          teacher_clazz.teacher_id = portal_teacher.id
-          save_result = teacher_clazz.save!
+    DEFAULT_DATA[:teacher_clazzes].each do |teacher_clazz, teacher_clazz_info|
+      map_teacher = @default_teachers.find{|t| t.user.login == teacher_clazz_info[:teacher]}
+      if map_teacher
+        clazz_names = teacher_clazz_info[:clazz_names].split(",").map{|c| c.strip }
+        clazz_names.each do |clazz_name|
+          map_clazz = default_classes.find{|c| c.name == clazz_name}
+          if map_clazz
+            teacher_clazz = Portal::TeacherClazz.find_or_create_by_teacher_id_and_clazz_id(map_teacher.id, map_clazz.id)
+          end
         end
       end
     end
     
-    #And following student clazz mapping exist
-    DEFAULT_DATA['student_clazzes'].each do |student_name, clazzes|
-      student = User.find_by_login(student_name).portal_student
-      clazzes = clazzes.split(",").map{|c| c.strip }
-      clazzes.map!{|c| Portal::Clazz.find_by_name(c)}
-      clazzes.each do |each_clazz|
-        student_clazz = Portal::StudentClazz.find_by_student_id_and_clazz_id(student.id, each_clazz.id)
-        unless student_clazz
-          Factory.create :portal_student_clazz, :student => student, :clazz => each_clazz
+    #And following student class mapping exist
+    DEFAULT_DATA[:student_clazzes].each do |student_clazz, student_clazz_info|
+      map_student = @default_students.find{|s| s.user.login == student_clazz_info[:student]}
+      if map_student
+        clazz_names = student_clazz_info[:clazz_names].split(",").map{|c| c.strip }
+        clazz_names.each do |clazz_name|
+          map_clazz = default_classes.find{|c| c.name == clazz_name}
+          if map_clazz
+            student_clazz = Portal::StudentClazz.find_or_create_by_student_id_and_clazz_id(map_student.id, map_clazz.id)
+          end
         end
       end
     end
