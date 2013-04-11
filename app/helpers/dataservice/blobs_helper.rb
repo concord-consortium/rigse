6655208b0c880245fa489a4d339607e1432c88c6
Noah Paessel <knowuh@gmail.com>
2013-04-11 17:01:23 -0400

    blobs_helper.rb : A student can get the URL of their snapshot image.
    [#44420837]
    https://www.pivotaltracker.com/story/show/44420837
    
    A student can get the URL of their snapshot image and use it in the drawing tool or Wallwisher.
    Need a publicly available URL so that can get to it without logging in.

--- app/helpers/dataservice/blobs_helper.rb
+++ app/helpers/dataservice/blobs_helper.rb
@@ -1,2 +1,5 @@
 module Dataservice::BlobsHelper
+  def blob_url_for(answer)
+    dataservice_blob_raw_url(:id => answer[:blob].id, :token => answer[:blob].token)
+  end
 end
--- spec/helpers/dataservice/blobs_helper_spec.rb
+++ spec/helpers/dataservice/blobs_helper_spec.rb
@@ -8,4 +8,16 @@ describe Dataservice::BlobsHelper do
     included_modules.should include(Dataservice::BlobsHelper)
   end
 
+  describe "blob_url_for(answer)" do
+    it "should return a url for an answer" do
+      blob = mock_model(Dataservice::Blob,
+        :id => 10,
+        :token => '9db05e66b61d11e08a75000c29231be6')
+      answer = {}
+      answer[:blob] = blob
+      url = blob_url_for(answer)
+      url.should match %r[dataservice/blobs/10.blob/9db05e66b61d11e08a75000c29231be6]
+    end
+  end
+
 end
