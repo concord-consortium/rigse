40bd654f75105bbaffe9a1b0a35e4f3bee149551
Vikash Sharma <vikash.sharma@zeuslearning.com>
2013-04-12 17:49:47 +0530

    Delete cookies with domain
    - Delete cookies with domain for sso integration.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,6 @@
 class SessionsController < Devise::SessionsController
   def destroy
-    cookies.delete :current_user_single_sign_on_id
+    cookies.delete :current_user_single_sign_on_id, :domain => '.rites.concord.zeuslearning.com'
     redirect_path = request.referer
     sign_out current_user
     redirect_to redirect_path
