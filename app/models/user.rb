a1f679e917e71e644dc24b9b1acf8d79ddafe1b4
Aaron Unger <aaron.unger@gmail.com>
2013-04-26 14:28:09 -0700

    Custom devise strategy for supporting bearer token authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ActiveRecord::Base
   has_many :access_grants, :dependent => :delete_all
 
 
-  devise :database_authenticatable, :registerable,:token_authenticatable, :confirmable,
+  devise :database_authenticatable, :registerable,:token_authenticatable, :confirmable, :bearer_token_authenticatable,
          :recoverable,:timeoutable, :rememberable, :trackable, :validatable,:encryptable, :encryptor => :restful_authentication_sha1
   self.token_authentication_key = "oauth_token"
   users = User.arel_table
