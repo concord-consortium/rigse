a089ed674472ecc48370b42560077f949c14c29b
Parker Morse <pmorse@concord.org>
2013-06-12 14:22:28 -0400

    Deploy nextgen from master

--- config/deploy/nextgen-production.rb
+++ config/deploy/nextgen-production.rb
@@ -1,7 +1,7 @@
-# interactions staging branch
+# nextgen production branch
 set :user, "deploy"
 set :domain, "nextgen.concord.org"
 set :deploy_to, "/web/portal"
 server domain, :app, :web
 role :db, domain, :primary => true
-set :branch, "wip-activity-runtime-connection"
+set :branch, "master"
