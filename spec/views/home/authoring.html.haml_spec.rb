477f5d0608d69aeed5c173826a9a804b227c6d97
Parker Morse <pmorse@concord.org>
2013-07-16 16:29:00 -0400

    Making this mock work

--- spec/views/home/authoring.html.haml_spec.rb
+++ spec/views/home/authoring.html.haml_spec.rb
@@ -3,8 +3,8 @@ require File.expand_path('../../../spec_helper', __FILE__)
 describe "home/authoring.html.haml" do
   before (:each) do
     @investigations  = [mock_model(Investigation, :name => "I1"),mock_model(Investigation, :name => "I2")]
-    @resources_pages = [mock_model(ResourcePage, :name => "RP1", :display_name => 'RP1'),mock_model(ResourcePage, :name => "RP2", :display_name => 'RP1')]
-    @resource_pages  = [mock_model(ResourcePage, :name => "RP1", :display_name => 'RP1'),mock_model(ResourcePage, :name => "RP2", :display_name => 'RP1')]
+    @resources_pages = [mock_model(ResourcePage, :name => "RP1", :display_name => 'RP1', :model_name => 'Resource Page'),mock_model(ResourcePage, :name => "RP2", :display_name => 'RP1', :model_name => 'Resource Page')]
+    @resource_pages  = [mock_model(ResourcePage, :name => "RP1", :display_name => 'RP1', :model_name => 'Resource Page'),mock_model(ResourcePage, :name => "RP2", :display_name => 'RP1', :model_name => 'Resource Page')]
     @external_activities = [mock_model(ExternalActivity, :name => "RP1"),mock_model(ExternalActivity, :name => "RP2")]
     @user = mock(
       :investigations  => @investigations,
