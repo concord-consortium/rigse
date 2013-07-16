d4d21d09c8ca33042b9838aa81f1552e94a06e7e
Noah Paessel <knowuh@gmail.com>
2013-07-16 17:29:47 -0400

    blob.rb: Add mimetype,  file_extension, checksum
    Learner.rb: Add assocation to lightweight_blobs <=> blobs
    Also: add methods to pull content from URL, generate checksum
    &related tests.

--- app/models/dataservice/blob.rb
+++ app/models/dataservice/blob.rb
@@ -3,7 +3,8 @@ class Dataservice::Blob < ActiveRecord::Base
 
   belongs_to :bundle_content, :class_name => "Dataservice::BundleContent", :foreign_key => "bundle_content_id"
   belongs_to :periodic_bundle_content, :class_name => "Dataservice::PeriodicBundleContent", :foreign_key => "periodic_bundle_content_id"
-
+  # lightweight learners create blobs directly (without a bundle content...)
+  belongs_to :leightweight_learner, :class_name => "Portal::Learner", :foreign_key => "learner_id"
   before_create :create_token
 
   def create_token
@@ -35,11 +36,13 @@ class Dataservice::Blob < ActiveRecord::Base
   # IF you add more auto-detectable mime-types, be sure to add an html representation in the html_content method,
   # and add support in the Dataservice::BlobsController#show action.
   def mimetype
+    return attributes['mimetype'] if attributes['mimetype']
     return "image/png" if content =~ /^.PNG/
     return "application/octet-stream"
   end
 
   def file_extension
+    return self.attributes['file_extension'] if self.attributes['file_extension']
     case self.mimetype
     when "image/png"
       return "png"
@@ -50,11 +53,49 @@ class Dataservice::Blob < ActiveRecord::Base
 
   def html_content(path_to_self)
     case self.mimetype
-    when "image/png"
+    when /image/
       return "<img src='#{path_to_self}' />"
     when "application/octet-stream"
       return "<div>Unknown binary content</div>"
     end
     return "<div>Unknown binary content</div>"
   end
+
+  def content=(new_content)
+    if new_content != self.content
+      write_attribute(:content,new_content)
+      compute_checksum
+    end
+  end
+
+  def checksum
+    return attributes['checksum'] if attributes['checksum']
+    compute_checksum
+  end
+
+  def compute_checksum
+    digest = Digest::SHA1.new
+    digest << (self.content         || "")
+    digest << (self.learner_id.to_s || "")
+    self.checksum = digest.to_s
+  end
+
+  def load_content_from(url)
+    web_client = HTTPClient.new
+    response = web_client.get(url)
+    if HTTP::Status.successful?(response.status)
+      self.mimetype = response.contenttype
+      self.content  = response.content
+    end
+  end
+
+  def self.for_learner_and_url(learner, url)
+    new_blob = self.new(:leightweight_learner => learner)
+    new_blob.load_content_from(url)
+    found = self.find_by_checksum(new_blob.checksum)
+    return found if found;
+    new_blob.save!
+    return new_blob
+  end
+
 end
--- spec/models/dataservice/blob_spec.rb
+++ spec/models/dataservice/blob_spec.rb
@@ -1,13 +1,170 @@
 require File.expand_path('../../../spec_helper', __FILE__)
+require 'webmock/rspec'
 
 describe Dataservice::Blob do
-  before(:each) do
-    @valid_attributes = {
-      
-    }
+  let(:attributes)  { {} }
+  subject           { Dataservice::Blob.create!(attributes) }
+
+  describe "a bare instance" do
+    %w|content mimetype file_extension checksum learner_id|.each do |attr|
+      it "should have a #{attr} attribute" do
+        subject.attributes.should include attr
+      end
+    end
   end
 
-  it "should create a new instance given valid attributes" do
-    Dataservice::Blob.create!(@valid_attributes)
+  describe "mimetype" do
+    describe "with a mimetype attrbiute value set" do
+      let(:attributes)  { {'mimetype' => "application/json"} }
+      its(:mimetype)    {      should == "application/json" }
+    end
+    describe "backwards compatibility, guessing mimetype" do
+      describe "matching png by content" do
+        let(:attributes) { {'content' => ".PNG blah blah"} }
+        its(:mimetype)   { should == "image/png" }
+      end
+      describe "default guess should be application/octet-stream" do
+        its(:mimetype)   { should == "application/octet-stream"}
+      end
+    end
+  end
+
+  describe "file_extension" do
+    describe "with a file_extension attrbiute value set" do
+      let(:attributes)    { {'file_extension' => "gif"} }
+      its(:file_extension)      {      should == "gif" }
+    end
+    describe "backwards compatibility, guess file_extension" do
+      describe "by using mimetype info" do
+        describe "guessing file_extension for image/png" do
+          let(:attributes)     { {'mimetype' => "image/png"} }
+          its(:file_extension) { should == "png" }
+        end
+        describe "guessing file_extension for application/octet-stream" do
+          let(:attributes)     { {'mimetype' => "application/octet-stream"} }
+          its(:file_extension) { should == "blob" }
+        end
+      end
+      describe "guessing file_extension blindly" do
+        its(:file_extension)  { should == "blob"}
+      end
+    end
+  end
+
+  describe "html_content" do
+    describe "for image mimetypes" do
+
+      it "should render an image tag for jpegs" do
+        subject.mimetype = "image/jpg"
+        subject.html_content("path").should match /<img src/
+      end
+
+      it "should render an image tag for gifs" do
+        subject.mimetype = "image/gif"
+        subject.html_content("path").should match /<img src/
+      end
+
+      it "should render a div tag for others" do
+        subject.mimetype = "application/octet-stream"
+        subject.html_content("path").should match /<div/
+      end
+    end
+  end
+
+  describe "compute_checksum" do
+    let(:attriutes) do
+      {
+        :content    => "simple content value here",
+        :learner_id => 4
+      }
+    end
+
+    describe "without hashable attrbiutes" do
+      let(:attributes){}
+      its(:checksum) { should_not be_nil }
+    end
+
+    it "should change when content changes" do
+      first_checksum  = subject.checksum
+      subject.content  = "updated content value here"
+      subject.checksum.should_not == first_checksum
+    end
+  end
+
+  describe "load_content_from(url)" do
+    let (:url)         {"www.example.com/srpr/logo4w.png" }
+    let (:mimetype)    { 'image/png'}
+    let (:url_content) { 'this is the url content'}
+    let (:status)      { 200 }
+    let (:learner)     { mock_model(Portal::Learner, :id => '234234') }
+    let (:atributes)   { { :learner => learner }}
+
+    before(:each) do
+      stub_request(:get, url).
+      to_return(:body => url_content, :status => status, :headers => { 'Content-Type' => mimetype})
+    end
+
+    describe "when there is good content" do
+      let(:status) { 200 }
+      it "should update its content with the content" do
+        subject.content.should be_nil
+        subject.load_content_from(url)
+        subject.content.should == url_content
+      end
+    end
+
+    describe "when there is an http error" do
+      let(:status) { 500 }
+      it "should update its content with the content" do
+        subject.content = "booga booga"
+        subject.load_content_from(url)
+        subject.content.should_not == url_content
+      end
+    end
+
+  end
+
+  describe "class methods" do
+    subject { Dataservice::Blob }
+    describe "for_learner_and_content" do
+      let(:url)              {"www.example.com/srpr/logo4w.png" }
+      let(:mimetype)         { 'image/png'}
+      let(:url_content)      { 'this is the url content'}
+      let(:status)           { 200 }
+      let(:learner)          { mock_model(Portal::Learner, :id => '234234') }
+      let(:existing_content) { "prexisting content"}
+      subject { Dataservice::Blob.for_learner_and_url(learner,url) }
+
+      before(:each) do
+        stub_request(:get, url).
+        to_return(:body => url_content, :status => status, :headers => { 'Content-Type' => mimetype})
+        @existing = Dataservice::Blob.create(:learner_id => learner.id, :content => existing_content )
+      end
+
+      describe "with new content and new learner" do
+        it "should have correct values from loading content" do
+          subject.mimetype.should == mimetype
+          subject.file_extension.should == "png"
+          subject.content.should == url_content
+          subject.id.should_not be_nil
+          subject.should be_valid
+        end
+
+        it "should not use an existing instance" do
+          subject.should_not == @existing
+          subject.id.should_not == @existing.id
+        end
+      end
+
+      describe "with existing content and learner" do
+        let(:existing_content)    { url_content }
+        it "should reuse the existing instance" do
+          subject.should == @existing
+          subject.content.should == existing_content
+          subject.id.should == @existing.id
+        end
+      end
+
+    end
   end
 end
