7fc558cb5a54f6b9b4aa5c4cbadf838e6726c10d
Aaron Unger <aaron.unger@gmail.com>
2013-08-09 10:05:16 -0700

    Move labbook helper methods into a library

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1307,19 +1307,4 @@ _gaq.push(['_trackPageview']);
 CONFIG
   end
 
-  def labbook_enabled?
-    Labbook.enabled?
-  end
-
-  def labbook_url(learner_or_user, model=nil)
-    if ENV['LABBOOK_PROVIDER_URL']
-      id = "#{learner_or_user.id}"
-      id += "-user" if learner_or_user.is_a?(User)
-      id += "-#{model.class.to_s}-#{model.id}" if model
-      "#{ENV['LABBOOK_PROVIDER_URL']}/albums?source=#{request.host}&user_id=#{Digest::MD5.hexdigest(id)}".gsub('//albums', '/albums')
-    else
-      nil
-    end
-  end
-
 end
--- lib/labbook.rb
+++ lib/labbook.rb
@@ -1,5 +1,16 @@
-class Labbook
+module Labbook
   def self.enabled?
     (ENV['LABBOOK_PROVIDER_URL'] && !ENV['LABBOOK_PROVIDER_URL'].empty?) ? true : false
+  end
+
+  def self.url(learner_or_user, request, model=nil)
+    if ENV['LABBOOK_PROVIDER_URL']
+      id = "#{learner_or_user.id}"
+      id += "-user" if learner_or_user.is_a?(User)
+      id += "-#{model.class.to_s}-#{model.id}" if model
+      "#{ENV['LABBOOK_PROVIDER_URL']}/albums?source=#{request.host}&user_id=#{Digest::MD5.hexdigest(id)}".gsub('//albums', '/albums')
+    else
+      nil
+    end
   end
 end
