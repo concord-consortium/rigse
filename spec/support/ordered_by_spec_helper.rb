f8d09ff0ae4eedb43a187a5d1911852ec4c3ea9e
Noah Paessel <knowuh@gmail.com>
2013-09-27 09:37:03 -0400

    Add Spec Helpers for unit testing Search models.

---
+++ spec/support/ordered_by_spec_helper.rb
@@ -0,0 +1,41 @@
+require 'delorean'
+RSpec::Matchers.define :be_ordered_by do |attribute|
+  match do |actual|
+    result = true
+    reverse_indicator = "_desc"
+    attribute = attribute.to_s
+    if attribute =~ /#{reverse_indicator}/
+      symbol = attribute.gsub(/#{reverse_indicator}/,'').to_sym
+      sorted = actual.sort{ |a,b| b.send(symbol) <=> a.send(symbol)}
+    else
+      sorted = actual.sort{ |a,b| a.updated_at <=> b.updated_at}
+    end
+    sorted.each_with_index do |a,i|
+      result = false unless actual[i] == a
+    end
+    result # return true or false for this matcher.
+  end
+
+  failure_message_for_should do |actual|
+    "expected that #{actual.map {|i| i.send attribute}} would be sorted by #{attribute}"
+  end
+
+  failure_message_for_should_not do |actual|
+    "expected that #{actual.map {|i| i.send attribute}} would not be sorted by #{attribute}"
+  end
+
+  description do
+    "be a sorted by #{attribute}"
+  end
+end
+
+def collection_with_rand_mod_time(factory,count=10,opts={})
+  count.times do
+    Delorean.time_travel_to(rand(Date.parse('2011-01-01')..Date.parse('2012-12-01')))
+    FactoryGirl.create(factory.to_sym, opts)
+  end
+  Delorean.back_to_the_present
+end
+
+# force the eval of a let expression for readability
+def make(let_expression); end
---
+++ spec/support/solar_spec_helper.rb
@@ -0,0 +1,24 @@
+$original_sunspot_session = Sunspot.session
+Sunspot.session = Sunspot::Rails::StubSessionProxy.new($original_sunspot_session)
+
+module SolrSpecHelper
+
+  def solr_setup
+    unless $sunspot
+      $sunspot = Sunspot::Rails::Server.new
+
+      pid = fork do
+        STDERR.reopen('/dev/null')
+        STDOUT.reopen('/dev/null')
+        $sunspot.run
+      end
+      # shut down the Solr server
+      at_exit { Process.kill('TERM', pid) }
+      # wait for solr to start
+      sleep 5
+      ::WebMock.disable_net_connect!(:allow => ["localhost:8981"])
+    end
+
+    Sunspot.session = $original_sunspot_session
+  end
+end
