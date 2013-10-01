b845cd8c8f696cd7c62ff4214afe3dd5d73b8fcc
Noah Paessel <knowuh@gmail.com>
2013-10-01 13:50:36 -0400

    Clean up sort parameters that come in.

--- app/models/search.rb
+++ app/models/search.rb
@@ -8,6 +8,7 @@ class Search
   attr_accessor :private
   attr_accessor :probe_type
   attr_accessor :grade_span
+  attr_accessor :domain_id
 
   AllMaterials = [Investigation, Activity, ResourcePage, ExternalActivity]
   Newest       = [:updated_at, :desc]
@@ -21,24 +22,30 @@ class Search
   def initialize(opts={})
     @results        = []
     @hits           = []
-
     @material_types = opts[:material_types] || AllMaterials
+    @domain_id      = opts[:domain_id]
     @text           = opts[:search_term]    || NoSearchTerm
     @engine         = opts[:engine]         || Sunspot
-    @sort_order     = opts[:sort_order]     || Newest
     @grade_span     = opts[:grade_span]     || NoGradeSpan
     @probe_type     = opts[:probe_type]     || NoProbeType
     @private        = opts[:private]
+    @sort_order     = parse_sort_order(opts[:sort_order])
     self.search()
+  end
+
+  def parse_sort_order(sort_order)
+    return Newest if sort_order.blank?
+    return sort_order.split().map {|i| i.to_sym}
   end
 
   def search
     _results = @engine.search(AllMaterials) do |s|
       s.fulltext(@text)
       s.with(:published, true) unless @private
-      s.with(:material_type, material_types)
+      s.with(:material_type, @material_types)
+      s.with(:domain_id, @domain_id) if @domain_id
       s.facet :material_type
-      s.order_by(*@sort_order)
+      # s.order_by(*@sort_order)
     end
     self.results        = _results.results
     self.hits           = _results.hits
