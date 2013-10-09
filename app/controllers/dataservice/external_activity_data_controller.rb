aff62e327e1ab85cd34e708183f38fb3ca412333
Noah Paessel <knowuh@gmail.com>
2013-10-09 15:27:48 -0400

    ★ ★ ★ cherry pick this. Allow admins to repost learner data from LARA. (cherry picked from commit 5ce14bbf3d6c8d2312067e70b3bf7944339ec490)

--- app/controllers/dataservice/external_activity_data_controller.rb
+++ app/controllers/dataservice/external_activity_data_controller.rb
@@ -1,9 +1,20 @@
 class Dataservice::ExternalActivityDataController < ApplicationController
 
+  private
+  def can_create(learner)
+    # allow admins and managers to re-post learner data
+    # from LARA
+    return true if (current_visitor.has_role? "admin")
+    return true if (current_visitor.has_role? "manager")
+    return true if (current_visitor == learner.user )
+    return false
+  end
+
+  public
   def create
     learner_id = params[:id]
     if learner = Portal::Learner.find(learner_id)
-      if learner.user == current_visitor
+      if can_create(learner)
         Delayed::Job.enqueue Dataservice::ProcessExternalActivityDataJob.new(learner_id, request.body.read)
         render :status => 201, :nothing => true
         return
