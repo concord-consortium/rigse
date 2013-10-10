022f0eb915dbc351b7a97890fd2f1277fc05c41d
Noah Paessel <knowuh@gmail.com>
2013-10-10 17:46:02 -0400

    whitespace cleanup.

--- spec/controllers/search_controller_spec.rb
+++ spec/controllers/search_controller_spec.rb
@@ -2,35 +2,35 @@ require File.expand_path('../../spec_helper', __FILE__)
 
 describe SearchController do
   include SolrSpecHelper
-  let (:admin_project)   { Factory.create(:admin_project_no_jnlps, :include_external_activities => false) }
+  let(:admin_project)   { Factory.create(:admin_project_no_jnlps, :include_external_activities => false) }
 
-  let (:mock_semester)   { Factory.create(:portal_semester, :name => "Fall") }
-  let (:mock_school)     { Factory.create(:portal_school, :semesters => [mock_semester]) }
+  let(:mock_semester)   { Factory.create(:portal_semester, :name => "Fall") }
+  let(:mock_school)     { Factory.create(:portal_school, :semesters => [mock_semester]) }
 
-  let (:teacher_user)    { Factory.create(:confirmed_user, :login => "teacher_user") }
-  let (:teacher)         { Factory.create(:portal_teacher, :user => teacher_user, :schools => [mock_school]) }
-  let (:admin_user)      { Factory.next(:admin_user) }
-  let (:author_user)     { Factory.next(:author_user) }
-  let (:manager_user)    { Factory.next(:manager_user) }
-  let (:researcher_user) { Factory.next(:researcher_user) }
+  let(:teacher_user)    { Factory.create(:confirmed_user, :login => "teacher_user") }
+  let(:teacher)         { Factory.create(:portal_teacher, :user => teacher_user, :schools => [mock_school]) }
+  let(:admin_user)      { Factory.next(:admin_user) }
+  let(:author_user)     { Factory.next(:author_user) }
+  let(:manager_user)    { Factory.next(:manager_user) }
+  let(:researcher_user) { Factory.next(:researcher_user) }
 
-  let (:student_user)    { Factory.create(:confirmed_user, :login => "authorized_student") }
-  let (:student)         { Factory.create(:portal_student, :user_id => student_user.id) }
+  let(:student_user)    { Factory.create(:confirmed_user, :login => "authorized_student") }
+  let(:student)         { Factory.create(:portal_student, :user_id => student_user.id) }
 
-  let (:physics_investigation)     { Factory.create(:investigation, :name => 'physics_inv', :user => author_user, :publication_status => 'published') }
-  let (:chemistry_investigation)   { Factory.create(:investigation, :name => 'chemistry_inv', :user => author_user, :publication_status => 'published') }
-  let (:biology_investigation)     { Factory.create(:investigation, :name => 'mathematics_inv', :user => author_user, :publication_status => 'published') }
-  let (:mathematics_investigation) { Factory.create(:investigation, :name => 'biology_inv', :user => author_user, :publication_status => 'published') }
-  let (:lines)                     { Factory.create(:investigation, :name => 'lines_inv', :user => author_user, :publication_status => 'published') }
+  let(:physics_investigation)     { Factory.create(:investigation, :name => 'physics_inv', :user => author_user, :publication_status => 'published') }
+  let(:chemistry_investigation)   { Factory.create(:investigation, :name => 'chemistry_inv', :user => author_user, :publication_status => 'published') }
+  let(:biology_investigation)     { Factory.create(:investigation, :name => 'mathematics_inv', :user => author_user, :publication_status => 'published') }
+  let(:mathematics_investigation) { Factory.create(:investigation, :name => 'biology_inv', :user => author_user, :publication_status => 'published') }
+  let(:lines)                     { Factory.create(:investigation, :name => 'lines_inv', :user => author_user, :publication_status => 'published') }
 
-  let (:laws_of_motion_activity)  { Factory.create(:activity, :name => 'laws_of_motion_activity' ,:investigation_id => physics_investigation.id, :user => author_user) }
-  let (:fluid_mechanics_activity) { Factory.create(:activity, :name => 'fluid_mechanics_activity' , :investigation_id => physics_investigation.id, :user => author_user) }
-  let (:thermodynamics_activity)  { Factory.create(:activity, :name => 'thermodynamics_activity' , :investigation_id => physics_investigation.id, :user => author_user) }
-  let (:parallel_lines)           { Factory.create(:activity, :name => 'parallel_lines' , :investigation_id => lines.id, :user => author_user) }
+  let(:laws_of_motion_activity)  { Factory.create(:activity, :name => 'laws_of_motion_activity' ,:investigation_id => physics_investigation.id, :user => author_user) }
+  let(:fluid_mechanics_activity) { Factory.create(:activity, :name => 'fluid_mechanics_activity' , :investigation_id => physics_investigation.id, :user => author_user) }
+  let(:thermodynamics_activity)  { Factory.create(:activity, :name => 'thermodynamics_activity' , :investigation_id => physics_investigation.id, :user => author_user) }
+  let(:parallel_lines)           { Factory.create(:activity, :name => 'parallel_lines' , :investigation_id => lines.id, :user => author_user) }
 
-  let (:external_activity1) { Factory.create(:external_activity, :name => 'external_1', :url => "http://concord.org", :publication_status => 'published', :is_official => true) }
-  let (:external_activity2) { Factory.create(:external_activity, :name => 'a_study_in_lines_and_curves', :url => "http://github.com", :publication_status => 'published', :is_official => true) }
-  let (:external_activity3) { Factory.create(:external_activity, :name => "Copy of external_1", :url => "http://concord.org", :publication_status => 'published', :is_official => false) }
+  let(:external_activity1) { Factory.create(:external_activity, :name => 'external_1', :url => "http://concord.org", :publication_status => 'published', :is_official => true) }
+  let(:external_activity2) { Factory.create(:external_activity, :name => 'a_study_in_lines_and_curves', :url => "http://github.com", :publication_status => 'published', :is_official => true) }
+  let(:external_activity3) { Factory.create(:external_activity, :name => "Copy of external_1", :url => "http://concord.org", :publication_status => 'published', :is_official => false) }
 
   before(:each) do
     admin_project
@@ -263,18 +263,18 @@ describe SearchController do
 
   describe "Post get_current_material_unassigned_clazzes" do
 
-    let (:physics_clazz)     { Factory.create(:portal_clazz, :name => 'Physics Clazz', :course => @mock_course,:teachers => [teacher]) }
-    let (:chemistry_clazz)   { Factory.create(:portal_clazz, :name => 'Chemistry Clazz', :course => @mock_course,:teachers => [teacher]) }
-    let (:mathematics_clazz) { Factory.create(:portal_clazz, :name => 'Mathematics Clazz', :course => @mock_course,:teachers => [teacher]) }
+    let(:physics_clazz)     { Factory.create(:portal_clazz, :name => 'Physics Clazz', :course => @mock_course,:teachers => [teacher]) }
+    let(:chemistry_clazz)   { Factory.create(:portal_clazz, :name => 'Chemistry Clazz', :course => @mock_course,:teachers => [teacher]) }
+    let(:mathematics_clazz) { Factory.create(:portal_clazz, :name => 'Mathematics Clazz', :course => @mock_course,:teachers => [teacher]) }
 
-    let (:investigations_for_all_clazz) do
+    let(:investigations_for_all_clazz) do
       inv = Factory.create(:investigation, :name => 'investigations_for_all_clazz', :user => author_user, :publication_status => 'published')
       #assign investigations_for_all_clazz to physics class
       Portal::Offering.find_or_create_by_clazz_id_and_runnable_type_and_runnable_id(physics_clazz.id,'Investigation',inv.id)
       inv
     end
 
-    let (:activity_for_all_clazz) do
+    let(:activity_for_all_clazz) do
       act = Factory.create(:activity, :name => 'activity_for_all_clazz' ,:investigation_id => physics_investigation.id, :user => author_user)
       #assign activity_for_all_clazz to physics class
       Portal::Offering.find_or_create_by_clazz_id_and_runnable_type_and_runnable_id(physics_clazz.id,'Activity',act.id)
@@ -326,11 +326,11 @@ describe SearchController do
   end
   describe "POST add_material_to_clazzes" do
 
-    let (:clazz)         { Factory.create(:portal_clazz,:course => @mock_course,:teachers => [teacher]) }
-    let (:another_clazz) { Factory.create(:portal_clazz,:course => @mock_course,:teachers => [teacher]) }
+    let(:clazz)         { Factory.create(:portal_clazz,:course => @mock_course,:teachers => [teacher]) }
+    let(:another_clazz) { Factory.create(:portal_clazz,:course => @mock_course,:teachers => [teacher]) }
 
-    let (:already_assigned_offering) { Factory.create(:portal_offering, :clazz_id=> clazz.id, :runnable_id=> chemistry_investigation.id, :runnable_type => 'Investigation'.classify) }
-    let (:another_assigned_offering) { Factory.create(:portal_offering, :clazz_id=> clazz.id, :runnable_id=> laws_of_motion_activity.id, :runnable_type => 'Investigation'.classify) }
+    let(:already_assigned_offering) { Factory.create(:portal_offering, :clazz_id=> clazz.id, :runnable_id=> chemistry_investigation.id, :runnable_type => 'Investigation'.classify) }
+    let(:another_assigned_offering) { Factory.create(:portal_offering, :clazz_id=> clazz.id, :runnable_id=> laws_of_motion_activity.id, :runnable_type => 'Investigation'.classify) }
 
     it "should assign only unassigned investigations to the classes" do
       already_assigned_offering
