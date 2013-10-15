8ea19e98d698ae12e2ff3fab751bc4f3dd5786cc
Noah Paessel <knowuh@gmail.com>
2013-10-15 14:54:03 -0400

    search.rb Fix pagination.

--- app/models/search.rb
+++ app/models/search.rb
@@ -11,7 +11,8 @@ class Search
   attr_accessor :grade_span
   attr_accessor :domain_id
   attr_accessor :without_teacher_only
-  attr_accessor :page
+  attr_accessor :activity_page
+  attr_accessor :investigation_page
   attr_accessor :per_page
   attr_accessor :user_id
   attr_accessor :include_contributed
@@ -50,54 +51,73 @@ class Search
   end
 
   def initialize(opts={})
-    @text           = Search.clean_search_terms(opts[:search_term])
-    @domain_id      = Search.clean_domain_id(opts[:domain_id])
-    @results        = {}
-    @hits           = {}
-    @no_probes      = false
-    @material_types = opts[:material_types] || AllMaterials
+    self.text           = Search.clean_search_terms(opts[:search_term])
+    self.domain_id      = Search.clean_domain_id(opts[:domain_id])
+    self.results        = {}
+    self.hits           = {}
+    self.no_probes      = false
 
-    @engine         = opts[:engine]         || Sunspot
-    @grade_span     = opts[:grade_span]     || NoGradeSpan
-    @probe          = opts[:probe]          || AnyProbeType
-    @no_probes      = opts[:no_probe]       || false
-    @private        = opts[:private]        || false
-    @sort_order     = opts[:sort_order]     || Newest
-    @page           = opts[:page]           || 1
-    @per_page       = opts[:per_page]       || 10
-    @user_id        = opts[:user_id]
-    @include_contributed = opts[:include_contributed] || false
-    @without_teacher_only = opts[:without_teacher_only] || true
+    self.user_id        = opts[:user_id]
+    self.material_types = opts[:material_types] || AllMaterials
+    self.engine         = opts[:engine]         || Sunspot
+    self.grade_span     = opts[:grade_span]     || NoGradeSpan
+    self.probe          = opts[:probe]          || AnyProbeType
+    self.no_probes      = opts[:no_probe]       || false
+    self.private        = opts[:private]        || false
+    self.sort_order     = opts[:sort_order]     || Newest
+    self.per_page       = opts[:per_page]       || 10
+
+    self.activity_page        = opts[:activity_page]       || 1
+    self.investigation_page   = opts[:investigation_page]  || 1
+    self.include_contributed  = opts[:include_contributed] || false
+    self.without_teacher_only = opts[:without_teacher_only]|| true
     self.search()
   end
 
   def search
     self.results[:all] = []
     self.hits[:all] = []
-    @material_types.each do |type|
-      _results = @engine.search(SearchableModels) do |s|
-        s.fulltext(@text)
+    self.material_types.each do |type|
+      _results = self.engine.search(SearchableModels) do |s|
+        s.fulltext(self.text)
         s.any_of do |c|
           c.with(:published, true)
-          c.with(:published, [true, false]) if @private
-          c.with(:user_id, @user_id)
+          c.with(:published, [true, false]) if self.private
+          c.with(:user_id, self.user_id)
         end
         s.with(:material_type, type)
-        s.with(:domain_id, @domain_id) unless @domain_id.empty?
-        s.with(:grade_span, @grade_span) unless @grade_span.empty?
-        s.with(:probe_type_ids, @probe) unless (@probe.empty? || @no_probes)
-        s.with(:no_probes, true) if @no_probes
-        s.with(:is_official, true) unless @include_contributed
+        s.with(:domain_id, self.domain_id) unless self.domain_id.empty?
+        s.with(:grade_span, self.grade_span) unless self.grade_span.empty?
+        s.with(:probe_type_ids, self.probe) unless (self.probe.empty? || self.no_probes)
+        s.with(:no_probes, true) if self.no_probes
+        s.with(:is_official, true) unless self.include_contributed
         s.facet :material_type
-        s.order_by(*SortOptions[@sort_order])
-        s.paginate(:page => @page, :per_page => @per_page)
-
+        s.order_by(*SortOptions[self.sort_order])
+        if (type==Search::ActivityMaterial)
+          s.paginate(:page => self.activity_page, :per_page => self.per_page)
+        elsif (type==Search::InvestigationMaterial)
+          s.paginate(:page => self.investigation_page, :per_page => self.per_page)
+        end
       end
       self.results[:all] += _results.results
       self.hits[:all]    += _results.hits
       self.results[type] = _results.results
       self.hits[type]    = _results.hits
     end
+  end
+
+  def params
+    params = {}
+    keys = [:user_id, :material_types, :grade_span, :probe, :private, :sort_order,
+      :per_page, :include_contributed, :investigation_page, :activity_page]
+    keys.each do |key|
+      value = self.send key
+      if value
+        params[key] = value
+      end
+    end
+    # TODO: remove coupled controller concerns from this:
+    params.merge({:controller => 'search', :action => 'index'})
   end
 
   def types(*types)
@@ -111,9 +131,9 @@ class Search
 
   alias_method :probe_type, :probe
 
-  def investigation_checkedstatus; @search.material_types.include? ::Investigation ; end
-  def activity_checkedstatus; @search.material_types.include? ::Activity ; end
-  def external_activity_checkedstatus; @search.material_types.include? ::Activity ; end
+  def investigation_checkedstatus; self.search.material_types.include? ::Investigation ; end
+  def activity_checkedstatus; self.search.material_types.include? ::Activity ; end
+  def external_activity_checkedstatus; self.search.material_types.include? ::Activity ; end
   def include_external_activities?; false; end
 
 end
