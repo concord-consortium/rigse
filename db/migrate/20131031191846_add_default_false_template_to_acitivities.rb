42c589a475acd7f033aee3073203703b36c4eaa9
Noah Paessel <knowuh@gmail.com>
2013-10-31 16:13:48 -0400

    Provide reverse / down migration for is_template on activities.

--- db/migrate/20131031191846_add_default_false_template_to_acitivities.rb
+++ db/migrate/20131031191846_add_default_false_template_to_acitivities.rb
@@ -1,5 +1,8 @@
 class AddDefaultFalseTemplateToAcitivities < ActiveRecord::Migration
-  def change
+  def up
     change_column :activities, :is_template, :boolean, :default => false
+  end
+  def down
+    change_column :activities, :is_template, :boolean
   end
 end
