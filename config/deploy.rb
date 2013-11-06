5f746f12e59a76cf887be4e3cf5d6aa6ba1b5ffb
Noah Paessel <knowuh@gmail.com>
2013-11-06 11:34:12 -0500

    deploy.rb : refine solr capistrano task to honor solr/config/schema.xml

--- config/deploy.rb
+++ config/deploy.rb
@@ -224,6 +224,7 @@ namespace :deploy do
       mkdir -p #{shared_path}/config/initializers &&
       mkdir -p #{shared_path}/system/attachments &&
       mkdir -p #{shared_path}/solr/data &&
+      mkdir -p #{shared_path}/solr/pids &&
       touch #{shared_path}/config/database.yml &&
       touch #{shared_path}/config/settings.yml &&
       touch #{shared_path}/config/installer.yml &&
@@ -259,7 +260,9 @@ namespace :deploy do
       ln -nfs #{shared_path}/public/installers #{release_path}/public/installers &&
       ln -nfs #{shared_path}/config/nces_data #{release_path}/config/nces_data &&
       ln -nfs #{shared_path}/sis_import_data #{release_path}/sis_import_data &&
-      ln -nfs #{shared_path}/system #{release_path}/public/system
+      ln -nfs #{shared_path}/system #{release_path}/public/system &&
+      ln -nfs #{shared_path}/solr/data #{release_path}/solr/data &&
+      ln -nfs #{shared_path}/solr/pids #{release_path}/solr/pids
     CMD
     # This is part of the setup necessary for using newrelics reporting gem
     # run "ln -nfs #{shared_path}/config/newrelic.yml #{release_path}/config/newrelic.yml"
@@ -678,25 +681,30 @@ namespace 'jnlp' do
 end
 
 
-
+# Tasks to interact with Solr and SunSpot
 namespace :solr do
   desc "start solr"
   task :start, :roles => :app, :except => { :no_release => true } do
-    run "cd #{current_path} && RAILS_ENV=#{rails_env} bundle exec sunspot-solr start --port=8983 --data-directory=#{shared_path}/solr/data --pid-dir=#{shared_path}/pids"
+    run "cd #{current_path} && RAILS_ENV=#{rails_env} bundle exec rake sunspot:solr:start"
   end
   desc "stop solr"
   task :stop, :roles => :app, :except => { :no_release => true } do
-    run "cd #{current_path} && RAILS_ENV=#{rails_env} bundle exec sunspot-solr stop --port=8983 --data-directory=#{shared_path}/solr/data --pid-dir=#{shared_path}/pids"
+    run "cd #{current_path} && RAILS_ENV=#{rails_env} bundle exec rake sunspot:solr:stop ;true"
   end
-  desc "reindex the whole database"
-  task :reindex, :roles => :app do
+
+  desc "stop solr, remove data, start solr, reindex all records"
+  task :hard_reindex, :roles => :app do
     stop
-    run "rm -rf #{shared_path}/solr/data"
+    run "rm -rf #{shared_path}/solr/data/*"
     start
+    reindex
+  end
+
+  desc "simple reindex" #note the yes | reindex to avoid the nil.chomp error
+  task :reindex, roles: :app do
     run "cd #{current_path} && yes | RAILS_ENV=#{rails_env} bundle exec rake sunspot:solr:reindex"
   end
 end
-
 
 before 'deploy:restart', 'deploy:set_permissions'
 before 'deploy:update_code', 'deploy:make_directory_structure'
