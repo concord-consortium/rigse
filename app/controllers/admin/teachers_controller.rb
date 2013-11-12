96377fc83949db204193ecbdc848a832466cf31f
Noah Paessel <knowuh@gmail.com>
2013-11-12 17:19:49 -0500

    Views and Styling for “Admin can manage student permissions”
    [#59376134] [#50555503]
    
    https://www.pivotaltracker.com/story/show/50555503
    https://www.pivotaltracker.com/story/show/59376134

--- app/controllers/admin/teachers_controller.rb
+++ app/controllers/admin/teachers_controller.rb
@@ -10,27 +10,65 @@ class Admin::TeachersController < ApplicationController
   end
 
   public
-  class TeacherSearchForm < Struct.new(:name, :cohort_name)
+  class TeacherSearchForm < Struct.new(:name, :order)
+
     def initialize(params)
-      self.name = params[:name] || "noah"
-      self.cohort_name = params[:cohort_name]
+      params ||= {}
+      self.name  = params[:name]
+      self.order = params[:order] || "last_name"
     end
     def search
+      return [] unless self.name
       value  = "%#{self.name}%"
-      where  = "users.login like ? or users.first_name like ? or users.last_name like ?"
-      Portal::Teacher.joins(:user).where(where, value, value, value)
+      where  = "users.login like ? " +
+               "or users.first_name like ? " +
+               "or users.last_name like ?" +
+               "or users.email like ?"
+      order  = "users.last_name"
+      group  = "users.login"
+      teachers = Portal::Teacher.joins(:user,:clazzes).
+        where(where, value, value, value, value).
+        order(order).
+        group(group).
+        limit(30)
+      teachers.map { |t| TeacherView.new(t)}
     end
   end
 
-  class TeacherView < Struct.new(:display_name, :class_names, :students)
-    def intialize(teacher)
+  class StudentView < Struct.new(:name, :id, :login, :perms)
+    def initialize(portal_student)
+      user          = portal_student.user
+      self.name     = user.name
+      self.login    = user.login
+      self.id       = portal_student.id
+      self.perms    = portal_student.permission_forms
+    end
+  end
 
+  class ClazzView < Struct.new(:id, :name, :students, :word)
+    def initialize(portal_clazz)
+      self.name     = portal_clazz.name
+      self.word     = portal_clazz.class_word
+      self.id       = portal_clazz.id
+      self.students = portal_clazz.students.joins(:user).order('users.last_name').map{ |s| StudentView.new(s)}
+    end
+  end
+
+  class TeacherView < Struct.new(:name, :email, :login, :clazzes, :id)
+    def initialize(teacher)
+      user         = teacher.user
+      self.name    = user.name
+      self.login   = user.login
+      self.email   = user.email
+      self.id      = user.id
+      self.clazzes = teacher.clazzes.map { |c| ClazzView.new(c) }
+      self.clazzes = clazzes.reject { |c| c.students.size < 1}
     end
   end
 
 
   def index
-    form = TeacherSearchForm.new(params)
+    form = TeacherSearchForm.new(params[:form])
     @teachers = form.search
   end
 
