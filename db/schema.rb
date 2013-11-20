f3a8003c41b3b0f8e9c0cc48251dffe37d1e4fc9
Noah Paessel <knowuh@gmail.com>
2013-11-20 17:37:52 -0500

    Bump schema info.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131108201323) do
+ActiveRecord::Schema.define(:version => 20131114142629) do
 
   create_table "access_grants", :force => true do |t|
     t.string   "code"
