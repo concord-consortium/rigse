5753f2cdefda4127a84731d40346be5674a0a03a
Parker Morse <pmorse@concord.org>
2013-12-30 15:07:29 -0500

    Some comments on learner data receiving via API

--- app/controllers/dataservice/external_activity_data_controller.rb
+++ app/controllers/dataservice/external_activity_data_controller.rb
@@ -14,10 +14,10 @@ class Dataservice::ExternalActivityDataController < ApplicationController
   def create
     learner_id = params[:id]
     if learner = Portal::Learner.find(learner_id)
-      if can_create(learner)
+      if can_create(learner) # If this fails we raise RoutingError('Not Allowed') (see above)
+        # TODO: wrap this in a begin/rescue/end and return a real-ish error
         Delayed::Job.enqueue Dataservice::ProcessExternalActivityDataJob.new(learner_id, request.body.read)
-        render :status => 201, :nothing => true
-        return
+        render :status => 201, :nothing => true and return
       end
     end
     raise ActionController::RoutingError.new('Not Found')
