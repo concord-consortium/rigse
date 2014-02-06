bca222d8e82e11a00d2b75a47ae3ff7974839313
Aaron Unger <aaron.unger@gmail.com>
2014-02-06 15:26:58 -0800

    Updated activity count script

--- script/counts_of_activities.rb
+++ script/counts_of_activities.rb
@@ -26,37 +26,62 @@ def published?(act)
   end
 end
 
-@counts = {
-  :exemplar_copy => [],
-  :exemplar_copy_published => [],
-  :other_teacher => [],
-  :other_teacher_published => [],
-  :original => [],
-  :original_published => [],
-  :unknown => [],
-  :unknown_published => []
-}
+def used?(a)
+  if a.respond_to?('offerings')
+    return a.offerings.map {|o| o.learners.select{|l| l.bundle_logger.bundle_contents.size > 0 }.size }.sum > 5
+  elsif a.respond_to?('learners')
+    return a.learners.select {|l| l.learner_sessions.count > 0 }.size > 5
+  end
+  return false
+end
+
+def itsi_cohort?(a)
+  return a.user.portal_teacher.cohort_list.include?('itsisu') rescue false
+end
+
+def timestamp(a)
+  return a.created_at if a.respond_to?('created_at')
+  return a.versions.first.updated_at if a.respond_to?('versions')
+  return nil
+end
+
+@counts = {}
+
+def record_activity(a, type, published, used, itsisu)
+  @counts[type] ||= {:earliest => Time.now, :latest => Time.parse("2000-01-01"), :total => 0, :subs => {}}
+
+  @counts[type][:total] += 1
+
+  sub_key = published ? "published" : "unpublished"
+  sub_key += used ? "_used" : "_unused"
+  sub_key += itsisu ? "_itsisu" : "_general"
+
+  @counts[type][:subs][sub_key] ||= 0
+  @counts[type][:subs][sub_key] += 1
+
+  t = timestamp(a)
+  unless t.nil?
+    @counts[type][:earliest] = t if t < @counts[type][:earliest]
+    @counts[type][:latest]   = t if t > @counts[type][:latest]
+  end
+end
 
 @c = 0
 def process_activities(acts)
   acts.each do |a|
     next if is_exemplar?(a)
+    type = :unknown
     if a.respond_to?('original') || a.respond_to?('parent')
       orig = a.respond_to?('original') ? a.original : a.parent
       if orig.nil?
-        @counts[:original].push a
-        @counts[:original_published].push(a) if published?(a)
+        type = :original
       elsif is_exemplar?(orig)
-        @counts[:exemplar_copy].push a
-        @counts[:exemplar_copy_published].push(a) if published?(a)
+        type = :exemplar_copy
       else
-        @counts[:other_teacher].push a
-        @counts[:other_teacher_published].push(a) if published?(a)
+        type = :other_teacher_copy
       end
-    else
-      @counts[:unknown].push a
-      @counts[:unknown_published].push(a) if published?(a)
     end
+    record_activity(a, type, published?(a), used?(a), itsi_cohort?(a))
     print "\n#{@c}: " if @c % 60 == 0
     print "."
     @c += 1
@@ -75,25 +100,9 @@ puts "\n"
 
 @counts.keys.sort_by{|k| k.to_s }.each do |k|
   v = @counts[k]
-  puts "#{k}: #{v.size}"
-  if v.size > 0
-    if v[0].respond_to?('created_at')
-      v = v.sort_by{|a| a.created_at }
-    elsif v[0].respond_to?('versions')
-      v = v.sort_by{|a| a.versions.first.updated_at }
-    else
-      v = v.sort_by{|a| a.id }
-    end
-
-    earliest = v.first
-    latest = v.last
-    puts "first: #{earliest.created_at}" if earliest && earliest.respond_to?('created_at')
-    puts "last: #{latest.created_at}" if latest && latest.respond_to?('created_at')
-    if earliest and earliest.respond_to?('created_at')
-      puts "num since Feb 2013: #{v.select{|a| a.created_at > Time.parse('2013-02-01')}.size}"
-    end
-    puts "first: #{earliest.versions.first.updated_at}" if earliest && earliest.respond_to?('versions')
-    puts "last: #{latest.versions.first.updated_at}" if latest && latest.respond_to?('versions')
+  puts "#{k}:           #{v[:total]}"
+  v[:subs].keys.sort.each do |sk|
+    sv = v[:subs][sk]
+    puts "  #{sk}:      #{sv}"
   end
-  puts ""
 end
