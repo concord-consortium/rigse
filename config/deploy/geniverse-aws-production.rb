39ba18b1815c3b55cc55eba77788a076b0024bf6
Sam Fentress <sfentress@concord.org>
2014-04-07 15:57:53 -0400

    Update roles and servers for geniverse deploy

--- config/deploy/geniverse-aws-production.rb
+++ config/deploy/geniverse-aws-production.rb
@@ -1,9 +1,9 @@
 # geniverse aws production
 set :user, "deploy"
-set :domain, "gvnew.concord.org"
 set :deploy_to, "/web/portal"
-server domain, :app, :web
-role :db, domain, :primary => true
+role :app, "geniverse1.concord.org", "geniverse2.concord.org", "geniverse3.concord.org", "geniverse4.concord.org", "geniverse5.concord.org"
+role :web, "geniverse1.concord.org", "geniverse2.concord.org", "geniverse3.concord.org", "geniverse4.concord.org", "geniverse5.concord.org"
+role :db, "geniverse1.concord.org", :primary => true
 set :branch, "master"
 
 set :asset_env, "RAILS_GROUPS=assets RAILS_RELATIVE_URL_ROOT=/portal"
