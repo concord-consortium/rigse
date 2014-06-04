2567d3f21d5f24f6b4a984e0e1ae4401952817aa
Piotr Janik <janikpiotrek@gmail.com>
2014-06-04 17:50:08 +0200

    Add /views/browse specs, test whether descriptions have HTML stripped
    [#49156339]

---
+++ spec/views/browse/external_activities/show.html.haml_spec.rb
@@ -0,0 +1,23 @@
+require 'spec_helper'
+
+describe "browse/external_activities/show" do
+  let (:user) { Factory.create(:user) }
+  let (:ext_act) { Factory.create(:external_activity, :url => 'http://activities.com', :description => '<p>desc foo bar</p>', :user => user) }
+  let (:search_material) { Search::SearchMaterial.new(ext_act, user) }
+
+  before(:each) do
+    view.stub!(:current_visitor).and_return(user)
+    assigns[:search_material] = @search_material = search_material
+  end
+
+  it 'should present an external activity description' do
+    render
+    rendered.should match /desc foo bar/
+  end
+
+  it 'should remove all HTML tags from the description' do
+    render
+    rendered.should_not match /<p>desc foo bar<\/p>/
+    rendered.should_not match /&lt;p&gt;desc foo bar&lt;\/p&gt;/
+  end
+end
