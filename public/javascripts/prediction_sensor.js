a67d88905e5f74f1c4c6814f6224b61852a9874c
Aaron Unger <aaron.unger@gmail.com>
2014-07-28 13:11:25 -0700

    Re-sync prediction data to sensor graph periodically and whenever the sensor graph is reset

--- public/javascripts/prediction_sensor.js
+++ public/javascripts/prediction_sensor.js
@@ -1,10 +1,12 @@
 (function() {
   window.connectPredictionToSensor = function (pred_dom, sensor_dom) {
     var predictionPhone = phones[pred_dom],
-        sensorPhone     = phones[sensor_dom];
+        sensorPhone     = phones[sensor_dom],
+        haveSeenEvents  = false;
 
     var _registerRelay = function(eventName) {
         predictionPhone.addListener("prediction-dataset-"+eventName, function(evt) {
+          haveSeenEvents = true;
           sensorPhone.post('sendDatasetEvent', {eventName: eventName, datasetName: 'sensor-dataset', data: evt.data });
         });
         predictionPhone.post('listenForDatasetEvent', {eventName: eventName, datasetName: 'prediction-dataset'});
@@ -18,6 +20,25 @@
       }
     };
 
+    var setupPeriodicSync = function() {
+      // periodically send the complete prediction dataset over
+      predictionPhone.addListener("dataset", function(evt) {
+        sensorPhone.post('sendDatasetEvent', {eventName: 'dataReset', datasetName: 'sensor-dataset', data: evt.value.initialData });
+      });
+      setInterval(function() {
+        if (haveSeenEvents) {
+          predictionPhone.post('getDataset', 'prediction-dataset');
+          haveSeenEvents = false;
+        }
+      }, 10000);
+
+      // Also send the complete prediction dataset if the sensor interactive is loaded/reset
+      sensorPhone.addListener("modelLoaded", function(evt) {
+        predictionPhone.post('getDataset', 'prediction-dataset');
+      });
+    };
+
     setupCoordination();
+    setupPeriodicSync();
   };
 })();
