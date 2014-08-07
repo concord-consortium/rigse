f52f443ea0b397da6487e86eadfec8e1e71be3c0
Aaron Unger <aaron.unger@gmail.com>
2014-08-07 14:22:47 -0700

    Add script for gathering info on ITSI cohort teachers

---
+++ script/cohort_teachers.rb
@@ -0,0 +1,25 @@
+#!/usr/bin/env ruby
+
+STDOUT.sync = true
+
+require File.expand_path(File.join(File.dirname(__FILE__), '..', 'config', 'environment'))
+
+require 'csv'
+
+puts "\nGathering info on cohort teachers..."
+
+CSV.open("cohort_teachers_#{Time.now.strftime('%Y%m%d')}.csv", "wb") do |csv|
+  csv << ['User ID', 'Name', 'Username', 'Email', 'District', 'School']
+  Portal::Teacher.tagged_with('itsisu').each do |t|
+    csv << [
+      t.id,
+      t.name,
+      (t.login rescue "??"),
+      (t.email rescue "??"),
+      (t.school.district.name rescue "??"),
+      (t.school.name rescue "??")
+    ]
+  end
+end
+
+puts "Done."
