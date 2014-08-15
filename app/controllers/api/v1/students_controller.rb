83704991c7a2f832c5a7b7de076b29193a6ff719
Noah Paessel <knowuh@gmail.com>
2014-08-15 17:36:18 -0400

    Form model tests for new registration API  [#75226372]
    https://www.pivotaltracker.com/story/show/75226372

--- app/controllers/api/v1/students_controller.rb
+++ app/controllers/api/v1/students_controller.rb
@@ -1,26 +1,12 @@
 class API::V1::StudentsController < API::APIController
 
 	def create
-		errors     = {}
-		student    = {}
-		attributes = ['first_name', 'last_name', 'class_word', 'password', 'questions', 'answers','over_18']
-		
-		attributes.each do |field|
-			if params[field].blank?
-				errors[field] = "required field"
-			else
-				student[field] = params[field]
-			end
-		end
-		
-		if errors.size > 0
-			error(errors)
+		registration = API::V1::StudentRegistration.new(params)
+
+		if registration.valid?
+			render :json => registration.attributes
 		else
-			# TODO: actually do the model creation.
-			# this is just a BOGUS response to expedite client testing
-			student['username']  = 'bogus_username'
-			student['id']        = '007'
-			render :json => student
+			error(registration.errors)
 		end
 	end
 end
