887e771a38e9c6b917035dab08c056da20602a78
Piotr Janik <janikpiotrek@gmail.com>
2014-08-19 18:51:41 +0200

    Don't expand material details when user tries to select text
    [#76892670]

--- app/assets/javascripts/search_materials_expand.js
+++ app/assets/javascripts/search_materials_expand.js
@@ -3,10 +3,15 @@
 
   $(function() {
     $('.material_list_item').on('click', function (e) {
+      // Make sure that we don't expand material details when:
+      // - user clicks a link
+      // - user tries to select some text fragment (e.g. description)
       if (e.target.tagName.toLowerCase() === 'a') return;
+      if (getSelection().toString()) return;
+
       var $this = $(this);
       $this.find('.material-details').slideToggle(250, function () {
-      	$this.find('.toggle-details').toggle();
+        $this.find('.toggle-details').toggle();
       });
     });
   });
