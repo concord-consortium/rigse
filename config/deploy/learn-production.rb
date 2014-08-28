c056e16d086a85aeddb821f48830985435f0660c
sfentress <sfentress@concord.org>
2014-08-28 16:11:36 -0400

    Add geniverse_remote_auth feature to learn portal
    This allows us to log in by posting to /remote_login

--- config/deploy/learn-production.rb
+++ config/deploy/learn-production.rb
@@ -5,3 +5,5 @@ set :deploy_to, "/web/portal"
 server domain, :app, :web
 role :db, domain, :primary => true
 set :branch, "master"
+
+default_environment['PORTAL_FEATURES'] = "geniverse_remote_auth"
