adcce4759cc95021da05ed314239b56fae211b9a
Noah Paessel <knowuh@gmail.com>
2014-10-14 17:17:13 -0400

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20141006195946) do
+ActiveRecord::Schema.define(:version => 20141014105134) do
 
   create_table "access_grants", :force => true do |t|
     t.string   "code"
