f2cfdcbeb022c7b4c088ea89e174b52700d5e5c1
Noah Paessel <knowuh@gmail.com>
2014-10-15 10:55:04 -0400

    Stop with the annoying JS alerts for N+1 queries.  (keep page footer though)

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -44,9 +44,7 @@ RailsPortal::Application.configure do
   config.assets.debug = true
   config.after_initialize do
     Bullet.enable = true
-    Bullet.alert = true
     Bullet.bullet_logger = true
-    Bullet.console = true
     Bullet.rails_logger = true
     Bullet.add_footer = true
   end
