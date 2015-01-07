e5b4340d5e95113449147b2902a703cee9101a2a
Noah Paessel <knowuh@gmail.com>
2015-01-07 14:56:30 -0500

    Revert "Fix finder SQL when looking up users by their authentication tokens.  See more of this msg."
    This reverts commit 4979e21fb56e2172555e676663c6f6dcd3c588ad.

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,7 +18,7 @@ class User < ActiveRecord::Base
   end
 
   def self.find_for_token_authentication(conditions)
-    where(["access_grants.access_token = ? AND (access_grants.access_token_expires_at IS NULL OR access_grants.access_token_expires_at > time(?))", conditions[token_authentication_key], Time.now]).joins(:access_grants).select("users.*").first
+    where(["access_grants.access_token = ? AND (access_grants.access_token_expires_at IS NULL OR access_grants.access_token_expires_at > ?)", conditions[token_authentication_key], Time.now]).joins(:access_grants).select("users.*").first
   end
 
   NO_EMAIL_STRING='no-email-'
