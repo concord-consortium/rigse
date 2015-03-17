66405589bcb614fd806b3f1e2d9f36516f1e4dfe
Aaron Unger <aaron.unger@gmail.com>
2015-03-17 11:54:42 -0700

    Disable timeout redirects in devise
    [#90326702]

--- lib/custom_failure.rb
+++ lib/custom_failure.rb
@@ -1,4 +1,17 @@
 class CustomFailure < Devise::FailureApp
+
+  # Disable redirects (by redirecting to the current url) because of session timeouts.
+  # Let the normal authentication rules force a redirect.
+  def redirect
+    store_location!
+    message = warden.message || warden_options[:message]
+    if message == :timeout
+      redirect_to attempted_path
+    else
+      super
+    end
+  end
+
   def redirect_url
     #return super unless [:worker, :employer, :user].include?(scope) #make it specific to a scope
      #new_user_session_url(:subdomain => 'secure')
