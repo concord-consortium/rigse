8cc4fe30486e1d5a6f7387337531efc64ab529e8
Sagar Talla <sagar.talla@zeuslearning.com>
2015-03-17 14:56:45 +0530

    Add the new Labbook LARA question type after every model/sensor/prediction.[#90191674]

--- app/models/page.rb
+++ app/models/page.rb
@@ -307,7 +307,11 @@ class Page < ActiveRecord::Base
     }
 
     page_description = self.description
-
+    labbook_export = {
+      :action_type => 1, #snapshot mode
+      :name => "Labbook album",
+      :type => "Embeddable::Labbook"
+    }
     self.page_elements.each do |page_element|
       if page_element.is_enabled
         case page_element.embeddable_type
@@ -324,14 +328,16 @@ class Page < ActiveRecord::Base
           page_json[:embeddables] << page_element.embeddable.export_as_lara_activity
 
         when "Embeddable::Diy::Sensor"
-          page_json[:show_interactive] = true
-          page_json[:interactives] << page_element.embeddable.export_as_lara_activity
-
         when "Embeddable::Diy::EmbeddedModel"
           page_json[:show_interactive] = true
           page_json[:interactives] << page_element.embeddable.export_as_lara_activity
+          if page_element.embeddable.diy_model.model_type.name == "Digital Microscope"
+            labbook_export[:custom_action_label] = "Upload Image"
+            labbook_export[:action_type] = 0 # upload mode
+          end
+          page_json[:embeddables] << labbook_export
         else
-          puts "hi"
+          puts "Type not supported"
         end
       end
     end
