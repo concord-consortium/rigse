d402e15fa3a49f3276e12d46438dedc519a2b00a
Piotr Janik <janikpiotrek@gmail.com>
2015-05-07 13:40:33 +0200

    Materials Collection API: support fake collections returning own materials
    [#91659452] [#92150260]

--- app/controllers/api/v1/materials_collections_controller.rb
+++ app/controllers/api/v1/materials_collections_controller.rb
@@ -1,23 +1,55 @@
 class API::V1::MaterialsCollectionsController < API::APIController
   include React::DataHelpers
 
-  # GET /api/v1/materials_collections/data?id=:id OR GET /api/v1/materials_collections/data?id[]=:id1&id[]=:id2
-  # Supports multiple IDs provided. Always returns ARRAY of collections (even if only one ID is provided).
-  # The main difference from regular MaterialsCollectionsController#show is that it also includes materials data.
-  # It's intended to be used by client-side rendering code, so the ID is extracted out of path (more convenient).
+  # GET /api/v1/materials_collections/data?<params>id=:id OR GET /api/v1/materials_collections/data?id[]=:id1&id[]=:id2
+  # Always returns ARRAY of collections (even if single collection is returned).
+  # Supported params:
+  #   - ?id=:id or ?id[]=:id1&id[]=:id2 - returns collections with given IDs
+  #   - ?own_external_activities=true - returns 'fake' collection with own external activities
+  #   - ?own_activities=true - returns 'fake' collection with own activities
+  #   - ?own_investigations=true - returns 'fake' collection with own investigations
+  #   - ?own_materials=true - returns 'fake' collection with own materials (sum of all categories above)
   def data
-    # Preserver order of collections provided by client!
-    collection_by_id = MaterialsCollection.where(id: params[:id]).index_by { |mc| mc.id.to_s }
-    ids = Array(params[:id])
-    render json: ids.map { |id| materials_collection_data(collection_by_id[id]) }
+    collections = collections_by_ids(Array(params[:id]))
+    collections += [own_materials]           if params[:own_materials]
+    collections += [own_external_activities] if params[:own_external_activities]
+    collections += [own_activities]          if params[:own_activities]
+    collections += [own_investigations]      if params[:own_investigations]
+    render json: collections
   end
 
   private
 
-  def materials_collection_data(materials_collection)
+  def collections_by_ids(ids)
+    return [] if ids.empty?
+    # Preserver order of collections provided by client!
+    collection_by_id = MaterialsCollection.where(id: params[:id]).index_by { |mc| mc.id.to_s }
+    ids.map do |id|
+      col = collection_by_id[id]
+      materials_collection_data(col.name, col.materials)
+    end
+  end
+
+  def own_materials
+    materials_collection_data('My materials', current_visitor.materials)
+  end
+
+  def own_external_activities
+    materials_collection_data('My materials', current_visitor.external_activities)
+  end
+
+  def own_activities
+    materials_collection_data('My materials', current_visitor.activities)
+  end
+
+  def own_investigations
+    materials_collection_data('My materials', current_visitor.investigations)
+  end
+
+  def materials_collection_data(name, materials)
     {
-        name: materials_collection.name,
-        materials: materials_data(materials_collection.materials)
+        name: name,
+        materials: materials_data(materials)
     }
   end
 end
