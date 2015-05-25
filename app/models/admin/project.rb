9136859a5bfb3a7679386edfcf2d70b187905bc2
Sagar Talla <sagar.talla@zeuslearning.com>
2015-05-25 18:02:58 +0530

    Associate interactives with projects.[#94148910]

--- app/models/admin/project.rb
+++ app/models/admin/project.rb
@@ -14,6 +14,7 @@ class Admin::Project < ActiveRecord::Base
   has_many :activities, through: :project_materials, source: :material, source_type: 'Activity'
   has_many :investigations, through: :project_materials, source: :material, source_type: 'Investigation'
   has_many :external_activities, through: :project_materials, source: :material, source_type: 'ExternalActivity'
+  has_many :interactives, through: :project_materials, source: :material, source_type: 'Interactive'
   has_many :materials_collections
 
   validates :name, presence: true
