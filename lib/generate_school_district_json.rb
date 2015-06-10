41c15d25449178a403f997c839142d4b8620b701
Sagar Talla <sagar.talla@zeuslearning.com>
2015-06-10 18:20:48 +0530

    Some optimizations[#95062186] If it is an NCES school, it doesn't need to include the extra NCES data.

--- lib/generate_school_district_json.rb
+++ lib/generate_school_district_json.rb
@@ -2,17 +2,49 @@ class GenerateSchoolDistrictJSON < Struct.new(:export_id,:user_id)
   def perform
     export = Export.find(export_id)
     user = User.find(user_id)
-    sql = "SELECT portal_schools.name, portal_schools.description, portal_schools.state, portal_schools.zipcode, portal_schools.ncessch, portal_schools.nces_school_id, portal_districts.uuid
+    sql = "SELECT portal_schools.name, portal_schools.description, portal_schools.state, portal_schools.zipcode, portal_schools.ncessch, portal_districts.uuid
            FROM portal_schools
            INNER JOIN portal_districts
            ON portal_schools.district_id = portal_districts.id;"
-    records_array = ActiveRecord::Base.connection.select_all(sql)
-    export_data = {:schools => records_array }
+    records_array = ActiveRecord::Base.connection.select_all(sql)    
+    records_array.map! { |record|
+      if record["ncessch"]
+        {
+          "ncessch" => record["ncessch"],
+          "uuid" => record["uuid"]
+        }
+      else
+        {
+          "state" => record["state"],
+          "description" => record["description"],
+          "name" => record["name"],
+          "zipcode" => record["zipcode"],
+          "uuid" => record["uuid"]
+        }
+      end
+    }
+    export_data = {"schools" => records_array }
 
-    sql = "SELECT name, description, state, zipcode, leaid, uuid, nces_district_id
+    sql = "SELECT name, description, state, zipcode, leaid, uuid
            FROM portal_districts;"
     records_array = ActiveRecord::Base.connection.select_all(sql)
-    export_data[:districts] = records_array
+    records_array.map! { |record|             
+      if record["leaid"]
+        {
+          "leaid" => record["leaid"],
+          "uuid" => record["uuid"]
+        }
+      else
+        {
+          "name" => record["name"],
+          "description" => record["description"],
+          "state" => record["state"],
+          "zipcode" => record["zipcode"],
+          "uuid" => record["uuid"]
+        }
+      end
+    }
+    export_data["districts"] = records_array
     name = "portal_schools_#{UUIDTools::UUID.timestamp_create.hexdigest}.json"
     directory = "public/json"
     path = File.join(directory, name)
