9c6610c54ef2602e2c783e4c445a324660babe8d
Sagar Ghadge <sagar.ghadge@zeuslearning.com>
2015-07-02 19:28:16 +0530

    [#95062328][#95625522]
    Modify migration process to import batch of 500 records in a single ActiveRecord transaction instead of individual transaction for each record

--- app/models/import_schools_and_districts.rb
+++ app/models/import_schools_and_districts.rb
@@ -7,58 +7,78 @@ class ImportSchoolsAndDistricts < Struct.new(:import_id)
     total_imports_count = total_schools_count + total_districts_count
     us_country = Portal::Country.find(:first, :conditions =>{:two_letter => "US"})
     district_index = 0
+    batch_size = 500
+    total_batches = (total_districts_count/batch_size).ceil
+    start_index = 0
+    end_index = batch_size - 1
 
     import.update_attribute(:total_imports, total_imports_count)
+    
+    0.upto(total_batches-1){|batch_index|
+      start_index = batch_index * batch_size
+      end_index = (batch_index == total_batches - 1)? (total_districts_count - 1) : (start_index + batch_size - 1)
+      ActiveRecord::Base.transaction do
+        start_index.upto(end_index){|index|
+            district = content_hash[:districts][index]
+            nces_district = nil
+            if district[:leaid]
+              nces_district = Portal::Nces06District.find(:first, :conditions => {:LEAID => district[:leaid]})
+            end
+            if nces_district
+              new_district = Portal::District.find_or_create_by_nces_district(nces_district)
+            else
+              district_name = district[:name].titlecase.strip
+              district_params = {}
+              district_params[:name] = district_name
+              district_params[:state] = district[:state] if district[:state] 
+              district_params[:leaid] = district[:leaid] if district[:leaid]
+              existing_district = Portal::District.find(:first, :conditions => district_params)
+              new_district = existing_district || Portal::District.create(district_params)
+              new_district.description = district[:description]
+              new_district.zipcode = district[:zipcode]
+            end
+            new_map = ImportSchoolDistrictMapping.find(:first, :conditions => {:district_id => new_district.id, :import_district_uuid => district[:uuid]})
+            new_map = new_map || ImportSchoolDistrictMapping.create({:district_id => new_district.id, :import_district_uuid => district[:uuid]})
+            import.update_attribute(:progress, (index + 1))
+            district_index = index + 1
+        }
+      end
+    }
 
-    content_hash[:districts].each_with_index do |district, index|
-      nces_district = nil
-      if district[:leaid]
-        nces_district = Portal::Nces06District.find(:first, :conditions => {:LEAID => district[:leaid]})
+    total_batches = (total_schools_count/batch_size).ceil
+    
+    0.upto(total_batches-1){|batch_index|
+      start_index = batch_index * batch_size
+      end_index = (batch_index == total_batches - 1)? (total_schools_count - 1) : (start_index + batch_size - 1)
+      ActiveRecord::Base.transaction do
+        start_index.upto(end_index){|index|
+          school = content_hash[:schools][index]
+          nces_school= nil
+          if school[:ncessch]
+            nces_school = Portal::Nces06School.find(:first, :conditions => {:NCESSCH => school[:ncessch]})
+          end
+          if nces_school  
+            new_school = Portal::School.find_or_create_by_nces_school(nces_school)
+          else
+            school_name = school[:name].titlecase.strip
+            school_district = ImportSchoolDistrictMapping.find(:first, :conditions => {:import_district_uuid => school[:district_uuid]})
+            school_params = {}
+            school_params[:name] = school_name
+            school_params[:state] = school[:state] if school[:state]
+            school_params[:district_id] = school_district.id if school_district
+            school_params[:ncessch] = school[:ncessch] if school[:ncessch]
+            existing_school = Portal::School.find(:first ,:conditions => school_params)        
+            new_school = existing_school || Portal::School.create(school_params)
+            new_school.description = school[:description]
+            new_school.zipcode = school[:zipcode]
+            new_school.country = us_country if school[:state] != "XX"
+          end
+          new_map = ImportUserSchoolMapping.find(:first, :conditions => {:school_id => new_school.id, :import_school_url => school[:school_url]})
+          new_map = new_map || ImportUserSchoolMapping.create({:school_id => new_school.id, :import_school_url => school[:school_url]})
+          import.update_attribute(:progress, (index + district_index + 1))
+        }
       end
-      if nces_district
-        new_district = Portal::District.find_or_create_by_nces_district(nces_district)
-      else
-        district_name = district[:name].titlecase.strip
-        district_params = {}
-        district_params[:name] = district_name
-        district_params[:state] = district[:state] if district[:state] 
-        district_params[:leaid] = district[:leaid] if district[:leaid]
-        existing_district = Portal::District.find(:first, :conditions => district_params)
-        new_district = existing_district || Portal::District.create(district_params)
-        new_district.description = district[:description]
-        new_district.zipcode = district[:zipcode]
-      end
-      new_map = ImportSchoolDistrictMapping.find(:first, :conditions => {:district_id => new_district.id, :import_district_uuid => district[:uuid]})
-      new_map = new_map || ImportSchoolDistrictMapping.create({:district_id => new_district.id, :import_district_uuid => district[:uuid]})
-      import.update_attribute(:progress, (index + 1))
-      district_index = index + 1
-    end
-
-    content_hash[:schools].each_with_index do |school, index|
-      nces_school= nil
-      if school[:ncessch]
-        nces_school = Portal::Nces06School.find(:first, :conditions => {:NCESSCH => school[:ncessch]})
-      end
-      if nces_school  
-        new_school = Portal::School.find_or_create_by_nces_school(nces_school)
-      else
-        school_name = school[:name].titlecase.strip
-        school_district = ImportSchoolDistrictMapping.find(:first, :conditions => {:import_district_uuid => school[:district_uuid]})
-        school_params = {}
-        school_params[:name] = school_name
-        school_params[:state] = school[:state] if school[:state]
-        school_params[:district_id] = school_district.id if school_district
-        school_params[:ncessch] = school[:ncessch] if school[:ncessch]
-        existing_school = Portal::School.find(:first ,:conditions => school_params)        
-        new_school = existing_school || Portal::School.create(school_params)
-        new_school.description = school[:description]
-        new_school.zipcode = school[:zipcode]
-        new_school.country = us_country if school[:state] != "XX"
-      end
-      new_map = ImportUserSchoolMapping.find(:first, :conditions => {:school_id => new_school.id, :import_school_url => school[:school_url]})
-      new_map = new_map || ImportUserSchoolMapping.create({:school_id => new_school.id, :import_school_url => school[:school_url]})
-      import.update_attribute(:progress, (index + district_index + 1))
-    end
+    }
     ImportSchoolDistrictMapping.delete_all
     import.update_attribute(:job_finished_at, Time.current)
   end
--- app/models/import_users.rb
+++ app/models/import_users.rb
@@ -3,75 +3,87 @@ class ImportUsers < Struct.new(:import_id)
     import = Import.find(import_id)
     content_hash = JSON.parse(import.upload_data, :symbolize_names => true)
     total_users_count = content_hash[:users].size
+    batch_size = 500
+    total_batches = (total_users_count/batch_size).ceil
+    start_index = 0
+    end_index = batch_size - 1
     importing_portal = ImportingPortal.find(:first, :conditions => {:portal_url => content_hash[:portal_name]})
     importing_portal = importing_portal || ImportingPortal.create({:portal_url => content_hash[:portal_name]})
     import.update_attribute(:total_imports, total_users_count)
-    content_hash[:users].each_with_index do |user, index|
-      new_user = User.find_by_email(user[:email])
-      unless new_user
-        new_user = User.find_by_login(user[:login])
-        duplicate_user = nil
-        if new_user
-          new_index = ImportDuplicateUser.find_all_by_login(user[:login]).count + 1
-          user[:login] = "#{user[:login]}-#{new_index}"
-          duplicate_user = ImportDuplicateUser.create({
-            :login => user[:login],
-            :email => user[:email],
-            :duplicate_by => ImportDuplicateUser::DUPLICATE_BY_LOGIN_AND_EMAIL,
-            :data => user,
-            :import_id => import.id
-          })
-        end
-        new_user = User.create({
-          :email => user[:email],
-          :login => user[:login],
-          :last_name => user[:last_name],
-          :first_name => user[:first_name],
-          :password => "password", #default password
-          :password_confirmation => "password",
-          :require_password_reset => true
-        }){|u| u.skip_notifications = true}
-        new_user.confirm!
-        user[:roles].each do |role|
-          new_user.add_role(role)
-        end
-        new_user.imported_user = ImportedUser.create({
-          :user_url => user[:user_page_url],
-          :importing_portal_id => importing_portal.id
-        })
-        if user[:teacher]
-          if user[:school]
-            if user[:school][:url]
-              user_school_map = ImportUserSchoolMapping.find(:first, :conditions => {:import_school_url => user[:school][:url]})
-              if user_school_map
-                school = Portal::School.find(:first, :conditions => {:id => user_school_map.school_id})
-              end
+    
+    0.upto(total_batches-1){|batch_index|
+      start_index = batch_index * batch_size
+      end_index = (batch_index == total_batches - 1)? (total_users_count - 1) : (start_index + batch_size - 1)
+      ActiveRecord::Base.transaction do
+        start_index.upto(end_index){|index|
+          user = content_hash[:users][index]
+          new_user = User.find_by_email(user[:email])
+          unless new_user
+            new_user = User.find_by_login(user[:login])
+            duplicate_user = nil
+            if new_user
+              new_index = ImportDuplicateUser.find_all_by_login(user[:login]).count + 1
+              user[:login] = "#{user[:login]}-#{new_index}"
+              duplicate_user = ImportDuplicateUser.create({
+                :login => user[:login],
+                :email => user[:email],
+                :duplicate_by => ImportDuplicateUser::DUPLICATE_BY_LOGIN_AND_EMAIL,
+                :data => user,
+                :import_id => import.id
+              })
             end
+            new_user = User.create({
+              :email => user[:email],
+              :login => user[:login],
+              :last_name => user[:last_name],
+              :first_name => user[:first_name],
+              :password => "password", #default password
+              :password_confirmation => "password",
+              :require_password_reset => true
+            }){|u| u.skip_notifications = true}
+            new_user.confirm!
+            user[:roles].each do |role|
+              new_user.add_role(role)
+            end
+            new_user.imported_user = ImportedUser.create({
+              :user_url => user[:user_page_url],
+              :importing_portal_id => importing_portal.id
+            })
+            if user[:teacher]
+              if user[:school]
+                if user[:school][:url]
+                  user_school_map = ImportUserSchoolMapping.find(:first, :conditions => {:import_school_url => user[:school][:url]})
+                  if user_school_map
+                    school = Portal::School.find(:first, :conditions => {:id => user_school_map.school_id})
+                  end
+                end
+              end
+              portal_teacher = Portal::Teacher.new 
+              portal_teacher.user = new_user
+              portal_teacher.schools << school if school
+              user[:cohorts].each do |cohort|
+                portal_teacher.cohort_list.add(cohort)
+              end
+              portal_teacher.save!
+            end
+            if duplicate_user
+              duplicate_user.user_id = new_user.id
+              duplicate_user.save!
+            end
+          else
+            duplicate_by = new_user.login == user[:login] ? ImportDuplicateUser::DUPLICATE_BY_LOGIN_AND_EMAIL : ImportDuplicateUser::DUPLICATE_BY_EMAIL
+            ImportDuplicateUser.create({
+              :login => user[:login], 
+              :email => user[:email], 
+              :duplicate_by => duplicate_by, 
+              :data => user.to_json,
+              :import_id => import.id
+            })
           end
-          portal_teacher = Portal::Teacher.new 
-          portal_teacher.user = new_user
-          portal_teacher.schools << school if school
-          user[:cohorts].each do |cohort|
-            portal_teacher.cohort_list.add(cohort)
-          end
-          portal_teacher.save!
-        end
-        if duplicate_user
-          duplicate_user.user_id = new_user.id
-          duplicate_user.save!
-        end
-      else
-        duplicate_by = new_user.login == user[:login] ? ImportDuplicateUser::DUPLICATE_BY_LOGIN_AND_EMAIL : ImportDuplicateUser::DUPLICATE_BY_EMAIL
-        ImportDuplicateUser.create({
-          :login => user[:login], 
-          :email => user[:email], 
-          :duplicate_by => duplicate_by, 
-          :data => user.to_json,
-          :import_id => import.id
-        })
+          import.update_attribute(:progress, (index + 1))
+        }
       end
-      import.update_attribute(:progress, (index + 1))
-    end
+    }
     import.update_attribute(:job_finished_at, Time.current)
   end
 
