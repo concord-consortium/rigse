fb9231676de2acdc2c8b441d7a9fa9286fa71909
Aaron Unger <aunger@concord.org>
2015-07-08 09:38:46 -0700

    Fix typo in itsi-master-staging deploy config

--- config/deploy/itsi-master-staging.rb
+++ config/deploy/itsi-master-staging.rb
@@ -1,5 +1,5 @@
 set :user, "deploy"
-set :domain, "itsi-master.staging.concord.org"
+set :domain, "itsi-master-staging.concord.org"
 set :deploy_to, "/web/portal"
 server domain, :app, :web
 role :db, domain, :primary => true
