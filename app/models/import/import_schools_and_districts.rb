f3892ca6bee579c634efb716d6e6bb109bbab96c
Scott Cytacki <scytacki@concord.org>
2015-07-16 16:18:43 -0400

    let jobs go for a very long time
    the import job was taking over 4 hours
    also don’t delete the failed jobs, let delay_job store the error
    [#95062328]

--- app/models/import/import_schools_and_districts.rb
+++ app/models/import/import_schools_and_districts.rb
@@ -85,9 +85,11 @@ class Import::ImportSchoolsAndDistricts < Struct.new(:import_id)
     import.update_attribute(:job_finished_at, Time.current)
   end
 
+  def max_attempts
+    1
+  end
+
   def error(job, exception)
-    p exception
-    job.destroy
     import = Import::Import.find(import_id)
     import.update_attribute(:progress, -1)
   end
