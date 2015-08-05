b1b17682cfc27b471a03a906d48bb8fc2282044c
Aaron Unger <aunger@concord.org>
2015-08-05 12:15:26 -0700

    Fix the hello report

--- app/pdfs/hello_report.rb
+++ app/pdfs/hello_report.rb
@@ -1,6 +1,6 @@
 class HelloReport < Prawn::Document
   def to_pdf
-    image(Rails.root.join("public", "images", "new", "banners", "empty.png"), :width => 450)
+    image(Rails.root.join("app", "assets", "new", "banners", "empty.png"), :width => 450)
     text("Generating a pdf report:")
     text("Using the Ruby Gem: <u><b><link href='http://github.com/sandal/prawn/wiki/'>Prawn</link></b></u>", :inline_format => true)
     text(Admin::Settings.summary_info)
