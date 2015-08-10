079b3772f005f8084d289295be5691747afd59a4
Sagar Ghadge <sagar.ghadge@zeuslearning.com>
2015-08-10 19:12:34 +0530

    Include pages marked private in batch export[#97849194]
    - Export urls of pages marked as private during batch export.

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -384,7 +384,8 @@ class ActivitiesController < ApplicationController
         }
       end
       # all tests
-      Page.published.each do |page|
+      pages = Page.find(:all, :conditions => {:publication_status => ["published","private"]})
+      pages.each do |page|
         exportable = page.activity
         if exportable
           data << {
