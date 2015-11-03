88a713321f74c3f8a015018baf79f89846804947
Doug Martin <doug@dougmart.in>
2015-11-03 17:01:37 -0500

    Updated pundit auth in security_questions_controller

--- app/controllers/security_questions_controller.rb
+++ app/controllers/security_questions_controller.rb
@@ -1,22 +1,26 @@
 class SecurityQuestionsController < ApplicationController
-  # PUNDIT_CHECK_FILTERS
-  before_filter :user_has_security_questions
-  
+
+  rescue_from Pundit::NotAuthorizedError, with: :pundit_user_not_authorized
+
+  private
+
+  def pundit_user_not_authorized(exception)
+    redirect_to(root_path)
+  end
+
+  public
+
   # GET
   def edit
-    # PUNDIT_REVIEW_AUTHORIZE
-    # PUNDIT_CHECK_AUTHORIZE (did not find instance)
-    # authorize @security_question
+    authorize SecurityQuestion
     @security_questions = SecurityQuestion.fill_array(current_visitor.security_questions)
   end
 
   # PUT
   def update
-    # PUNDIT_REVIEW_AUTHORIZE
-    # PUNDIT_CHECK_AUTHORIZE (did not find instance)
-    # authorize @security_question
+    authorize SecurityQuestion
     @security_questions = SecurityQuestion.make_questions_from_hash_and_user(params[:security_questions], current_visitor)
-    errors = SecurityQuestion.errors_for_questions_list!(@security_questions)  
+    errors = SecurityQuestion.errors_for_questions_list!(@security_questions)
     if (!errors) || errors.empty?
       current_visitor.update_security_questions!(@security_questions)
       flash[:notice] = "Your security questions have been successfully updated."
@@ -27,30 +31,23 @@ class SecurityQuestionsController < ApplicationController
       render :action => "edit"
     end
   end
-  
+
   protected
-  
-  def user_has_security_questions
-    unless current_visitor && !current_visitor.portal_student.nil?
-      redirect_to(root_path)
-      return
-    end
-  end
-  
+
   # def make_questions_from_params
   #   (0..2).to_a.collect do |i|
   #     data = params["question#{i}"]
   #     next if data.nil?
-  #     
+  #
   #     existing_object = current_visitor.security_questions.find_by_id(data[:id]) if data[:id] && data[:question_idx] == "current"
-  #     
+  #
   #     if existing_object.nil?
   #       new_question = SecurityQuestion::QUESTIONS[data[:question_idx].to_i] if data[:question_idx].to_i.to_s == data[:question_idx].to_s
   #       next if new_question.nil?
   #     else
   #       new_question = existing_object.question
   #     end
-  #     
+  #
   #     SecurityQuestion.new({ :question => new_question, :answer => data[:answer] })
   #   end.compact
   # end
--- app/policies/security_question_policy.rb
+++ app/policies/security_question_policy.rb
@@ -1,2 +1,16 @@
 class SecurityQuestionPolicy < ApplicationPolicy
+
+  def edit?
+    has_security_questions?
+  end
+
+  def update?
+    has_security_questions?
+  end
+
+  private
+
+  def has_security_questions?
+    user && !user.portal_student.nil?
+  end
 end
