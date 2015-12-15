b7fb7210aadc8e4f0c82b305e4586ccf47d6b6da
Doug Martin <doug@dougmart.in>
2015-12-15 15:03:36 -0500

    Removed use of verify_request_is_peer in external activity data controller

--- app/controllers/dataservice/external_activity_data_controller.rb
+++ app/controllers/dataservice/external_activity_data_controller.rb
@@ -1,17 +1,11 @@
 class Dataservice::ExternalActivityDataController < ApplicationController
-  
-  private
-  def can_create(learner)
-    # allow admins and managers to re-post learner data
-    # from LARA
-    return true if (current_visitor.has_role? "admin")
-    return true if (current_visitor.has_role? "manager")
-    return true if (current_visitor == learner.user )
-    return true if verify_request_is_peer
-    raise_auth_error(learner)
-  end
 
-  def raise_auth_error(learner)
+  rescue_from Pundit::NotAuthorizedError, with: :pundit_user_not_authorized
+
+  private
+
+  def pundit_user_not_authorized(exception)
+    learner_id = params[:id]
     learner_deets = LearnerDetail.new(learner)
     visitor = current_visitor ? current_visitor.name : 'anonymous'
     error_string = "Auth error for #{visitor} - #{learner_deets}"
@@ -19,17 +13,14 @@ class Dataservice::ExternalActivityDataController < ApplicationController
   end
 
   public
+
   def create
-    # PUNDIT_REVIEW_AUTHORIZE
-    # PUNDIT_CHECK_AUTHORIZE
-    # authorize Dataservice::ExternalActivityDatum
     learner_id = params[:id]
     if learner = Portal::Learner.find(learner_id)
-      if can_create(learner) # If this fails we raise RoutingError('Not Allowed') (see above)
-        # TODO: wrap this in a begin/rescue/end and return a real-ish error
-        Delayed::Job.enqueue Dataservice::ProcessExternalActivityDataJob.new(learner_id, request.body.read)
-        render :status => 201, :nothing => true and return
-      end
+      authorize Dataservice::ProcessExternalActivityDataJob
+      # TODO: wrap this in a begin/rescue/end and return a real-ish error
+      Delayed::Job.enqueue Dataservice::ProcessExternalActivityDataJob.new(learner_id, request.body.read)
+      render :status => 201, :nothing => true and return
     end
     raise ActionController::RoutingError.new('Not Found')
   end
