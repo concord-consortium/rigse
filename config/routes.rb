fcd96a3033e547276dcb72367804db39e5a19938
Piotr Janik <janikpiotrek@gmail.com>
2016-01-18 16:17:43 -0800

    Add offering info endpoint
    [#111245000]

--- config/routes.rb
+++ config/routes.rb
@@ -684,9 +684,7 @@ RailsPortal::Application.routes.draw do
         namespace :answers do
           get :student_answers
         end
-        namespace :dashboard_reports do
-          get :report
-        end
+        resources :offerings, only: [:show]
       end
     end
 
