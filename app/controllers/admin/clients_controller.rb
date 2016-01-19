ebdadb05e4b078b4637536c8586bfb9a53cf178e
Noah Paessel <knowuh@gmail.com>
2016-01-19 15:10:17 -0500

    CRUD for Authentication Clients.
    [#111247362]
    
    https://www.pivotaltracker.com/story/show/111247362

---
+++ app/controllers/admin/clients_controller.rb
@@ -0,0 +1,82 @@
+class Admin::ClientsController < ApplicationController
+
+  rescue_from Pundit::NotAuthorizedError, with: :pundit_user_not_authorized
+
+  private
+  def pundit_user_not_authorized(exception)
+    flash[:notice] = "Please log in as an administrator"
+    redirect_to(:home)
+  end
+
+  public
+
+  # GET /admin/clients
+  def index
+    authorize Client
+    @clients = Client.find(:all).paginate(:per_page => 20, :page => params[:page])
+  end
+
+  # GET /admin/client/1
+  def show
+    authorize Client
+    @client = Client.find(params[:id])
+  end
+
+  # GET /admin/client/new
+  def new
+    authorize Client
+    @client = Client.new
+  end
+
+  # GET /admin/client/1/edit
+  def edit
+    authorize Client
+    @client = Client.find(params[:id])
+    if request.xhr?
+      render :partial => 'remote_form', :locals => { :project => @client }
+    end
+  end
+
+  # POST /admin/client
+  def create
+    authorize Client
+    @client = Client.new(params[:client])
+
+    if @client.save
+      flash[:notice]='Client was successfully created.'
+      redirect_to action: :index
+    else
+      render :action => 'new'
+    end
+  end
+
+  # PUT /admin/client/1
+  def update
+    authorize Client
+    @client = Client.find(params[:id])
+    if request.xhr?
+      if @client.update_attributes(params[:client])
+        render :partial => 'show', :locals => { :project => @client }
+      else
+        render :partial => 'remote_form', :locals => { :project => @client }, :status => 400
+      end
+    else
+      if @client.update_attributes(params[:client])
+        flash[:notice]= 'Client was successfully updated.'
+        redirect_to action: :index
+      else
+        render :action => 'edit'
+      end
+    end
+  end
+
+  # DELETE /admin/client/1
+  def destroy
+    authorize Client
+    @client = Client.find(params[:id])
+    @client.destroy
+    flash[:notice]= 'Client was successfully deleted.'
+    redirect_to action: :index
+  end
+
+end
--- app/policies/client_policy.rb
+++ app/policies/client_policy.rb
@@ -1,2 +1,42 @@
 class ClientPolicy < ApplicationPolicy
+
+  class Scope < Scope
+    def resolve
+      if user.has_role?('admin')
+        all
+      else
+        none
+      end
+    end
+  end
+
+  def index?
+    is_admin
+  end
+
+  def show?
+    is_admin
+  end
+
+  def create?
+    is_admin
+  end
+
+  def edit?
+    is_admin
+  end
+
+  def update?
+    is_admin
+  end
+
+  def changeable?
+    is_admin
+  end
+
+  private
+  def is_admin
+    has_roles?('admin')
+  end
+
 end
