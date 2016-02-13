3dab12115cc6e1226fefbfa3e44f8688aef3616e
Scott Cytacki <scytacki@concord.org>
2016-02-12 23:24:20 -0500

    require a branch or tag when deploying code

--- config/deploy/learn-staging.rb
+++ config/deploy/learn-staging.rb
@@ -4,6 +4,5 @@ set :domain, "learn.staging.concord.org"
 set :deploy_to, "/web/portal"
 server domain, :app, :web
 role :db, domain, :primary => true
-set :branch, "master"
 
 default_environment['PORTAL_FEATURES'] = "geniverse_remote_auth genigames_data geniverse_backend allow_cors"
