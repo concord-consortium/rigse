b230974821fb4b0ac9acc0ffa421cd1f82e6641a
Scott Cytacki <scytacki@concord.org>
2016-02-17 22:25:45 -0500

    clean up deploy targets
    Some old targets have been left because the server is being kept around
    for archive purposes for a little while.

--- config/deploy/itsi-master-staging.rb
+++
@@ -1,6 +0,0 @@
-set :user, "deploy"
-set :domain, "itsi-master-staging.concord.org"
-set :deploy_to, "/web/portal"
-server domain, :app, :web
-role :db, domain, :primary => true
-set :branch, "master"
--- config/deploy/ngss-assessment-production.rb
+++ config/deploy/ngss-assessment-production.rb
@@ -4,4 +4,3 @@ set :domain, "ngss-assessment.portal.concord.org"
 set :deploy_to, "/web/portal"
 server domain, :app, :web
 role :db, domain, :primary => true
-set :branch, "master"
