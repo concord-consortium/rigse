df7127a28e81639b8b6db3dd96deeac94a2a0540
Piotr Janik <janikpiotrek@gmail.com>
2016-02-21 20:51:45 -0800

    Refactor offering controller and offering Pundit policy
    [#113874781]
    
    + remove dead code (index, show, edit, new actions and forms).

--- app/policies/portal/offering_policy.rb
+++ app/policies/portal/offering_policy.rb
@@ -1,17 +1,73 @@
 class Portal::OfferingPolicy < ApplicationPolicy
 
-  def show?
+  def destroy?
     class_teacher_or_admin?
   end
 
+  def activate?
+    class_teacher_or_admin?
+  end
+
+  def deactivate?
+    class_teacher_or_admin?
+  end
+
+  def update?
+    class_teacher_or_admin?
+  end
+
+  def show?
+    class_teacher_or_admin? || class_student?
+  end
+
+  def answers?
+    class_teacher_or_admin? || class_student?
+  end
+
+  def student_report?
+    class_student?
+  end
+
+  def get_recent_student_report?
+    class_teacher_or_admin? || class_student?
+  end
+
+  def report?
+    class_teacher_or_admin?
+  end
+
+  def open_response_report?
+    class_teacher_or_admin?
+  end
+
+  def multiple_choice_report?
+    class_teacher_or_admin?
+  end
+
+  def separated_report?
+    class_teacher_or_admin?
+  end
+
+  def report_embeddable_filter?
+    class_teacher_or_admin?
+  end
+
+  def offering_collapsed_status?
+    teacher?
+  end
+
   private
+
   def class_teacher?
-    return false unless user && user.portal_teacher
-    record && record.clazz.teachers.include?(user.portal_teacher)
+    user && record && record.clazz.is_teacher?(user)
+  end
+
+  def class_student?
+    user && record && record.clazz.is_student?(user)
   end
 
   def class_teacher_or_admin?
-    return class_teacher? || admin?
+    class_teacher? || admin?
   end
 
 end
