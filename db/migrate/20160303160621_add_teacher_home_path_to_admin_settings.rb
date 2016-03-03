6e77744113c42c2927014d233cf9708a08c7e011
Noah Paessel <knowuh@gmail.com>
2016-03-03 17:21:13 -0500

    Change the default teacher_home_path to "/getting_started"

--- db/migrate/20160303160621_add_teacher_home_path_to_admin_settings.rb
+++ db/migrate/20160303160621_add_teacher_home_path_to_admin_settings.rb
@@ -1,5 +1,5 @@
 class AddTeacherHomePathToAdminSettings < ActiveRecord::Migration
   def change
-    add_column :admin_settings, :teacher_home_path, :string
+    add_column :admin_settings, :teacher_home_path, :string, default: "/getting_started"
   end
 end
