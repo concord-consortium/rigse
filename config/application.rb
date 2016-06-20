4fc0b2882c08bf04624e3c406a0fefa1a21ecdc7
Doug Martin <doug@zoopdoop.com>
2016-06-20 08:31:56 -0400

    Comment out precompile line for travis

--- config/application.rb
+++ config/application.rb
@@ -179,7 +179,7 @@ module RailsPortal
     end
 
     # do not initialize on precompile so that the Dockerfile can run the precompile
-    config.assets.initialize_on_precompile = false
+    # config.assets.initialize_on_precompile = false
 
   end
 
