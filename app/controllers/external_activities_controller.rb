634d3591377d6f172d549d4f639d01276b7d5351
Doug Martin <doug@zoopdoop.com>
2017-01-17 04:45:07 -0500

    Changed autologin test from teacher to all non-anonymous users and changed query parser/builder to use Rack::Utils

--- app/controllers/external_activities_controller.rb
+++ app/controllers/external_activities_controller.rb
@@ -75,10 +75,13 @@ class ExternalActivitiesController < ApplicationController
         redirect_to(browse_external_activity_path(@external_activity))
       }
       format.run_resource_html {
-        # automatically log in teachers
-        if current_visitor.portal_teacher
+        # ensure that a logged in user is the same user on LARA
+        if !current_visitor.anonymous?
            uri = URI.parse(@external_activity.url)
-           uri.query = URI.encode_www_form(URI.decode_www_form(uri.query || '') << ["domain", root_url] << ["domain_uid", current_visitor.id])
+           query = Rack::Utils.parse_query(uri.query)
+           query["domain"] = root_url
+           query["domain_uid"] = current_visitor.id
+           uri.query = Rack::Utils.build_query(query)
            redirect_to(uri.to_s)
         else
           redirect_to(@external_activity.url)
