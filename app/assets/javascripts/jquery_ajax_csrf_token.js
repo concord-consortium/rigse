8a38839001e05c013f476c298bb2af2121f93fc6
Piotr Janik <janikpiotrek@gmail.com>
2017-02-13 12:08:06 +0100

    Fix search (send CSRF token as header, not data field)
    [#59376656]

--- app/assets/javascripts/jquery_ajax_csrf_token.js
+++ app/assets/javascripts/jquery_ajax_csrf_token.js
@@ -1,7 +1,9 @@
 // Automatically adds CSRF token to all Ajax calls.
 // AUTH_TOKEN is provided by the page page (e.g. application.html.haml).
+// It's also possible to set token in POST data (`authenticity_token`) but it can
+// break some features (e.g. search which is comparing query string of the request and response).
 
 jQuery(function() {
   var authToken = jQuery('meta[name="csrf-token"]').attr('content');
-  jQuery.ajaxSetup({data: {authenticity_token: authToken}});
+  jQuery.ajaxSetup({headers: {'X-CSRF-Token': authToken}});
 });
