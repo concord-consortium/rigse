56ec6296bf527b6fa500218d4df4ca71176f510e
Scott Cytacki <scytacki@concord.org>
2017-03-13 14:48:03 -0400

    Fix failing spec
    [#136965555]

--- spec/integration/sign_in_redirect_spec.rb
+++ spec/integration/sign_in_redirect_spec.rb
@@ -4,9 +4,9 @@ describe "when user signs in and 'after_sign_in_path' parameter is provided" do
   let(:user) { Factory.create(:confirmed_user) }
   let(:custom_url) { "/some-foo-bar-path" }
 
-  it "user is redirected back to this page" do
+  it "user is redirected back to this page with extra param" do
     post "/users/sign_in", user: {login: user.login, password: user.password}, after_sign_in_path: custom_url
-    expect(response).to redirect_to(custom_url)
+    expect(response).to redirect_to("#{custom_url}?redirecting_after_sign_in=1")
   end
 
   describe "and user is student" do
