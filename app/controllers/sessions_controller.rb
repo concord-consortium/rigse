3507ac4d4e36a7bedc596188b877db4688b987bc
Scott Cytacki <scytacki@concord.org>
2017-04-09 22:23:43 -0400

    Disable CSRF token check on log in
    With the CSRF check enabled on log in, a failed CSRF token causes strange behavior.
    The session gets cleared by the failed check, but then the login continues as normal.
    The result is that the user sees a flash message saying the login was successful, and they are redirected
    to the page they would normally see. But the user isn’t actually logged in.
    So the page is typically blank and there is a login form in the top right.
    [#143357307]

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,13 @@
+class SessionsController < Devise::SessionsController
+  # With Rails 3 and Devise there doesn't seem to be a good way to handle an invalid
+  # token. The session is cleared and then the login actually continues, but with
+  # mostly empty session.  In more recent Rails the authenticity token can be setup to
+  # throw an exception. If it through that exception then the login should stop and the
+  # the user could see an error message.
+  # In the meantime we'll disable the authenticity token check on login.
+  skip_before_filter :verify_authenticity_token, :only => [:create]
+
+  def new
+    super
+  end
+end
