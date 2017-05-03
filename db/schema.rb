408d06f74ae0252ab314c0b7232969f1f88db2d3
Sam Fentress <sfentress@concord.org>
2017-05-03 16:28:08 -0400

    Update db/schema.rb
    Added as a separate commit due to rebasing.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20170312203545) do
+ActiveRecord::Schema.define(:version => 20170410201106) do
 
   create_table "access_grants", :force => true do |t|
     t.string   "code"
@@ -2402,6 +2402,8 @@ ActiveRecord::Schema.define(:version => 20170312203545) do
     t.string   "teachers_district"
     t.string   "teachers_state"
     t.string   "teachers_email"
+    t.string   "permission_form_ids"
+    t.string   "teacher_ids"
   end
 
   add_index "report_learners", ["class_id"], :name => "index_report_learners_on_class_id"
