7dc00a578434b8e8928ae50837eb77724bc9c3a4
Jason Simpson <jsimpson@concord.org>
2017-06-02 20:00:57 -0700

    Fix typo setting environment in spec runner script.

--- docker/dev/run-rspec.sh
+++ docker/dev/run-rspec.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 #
 # Run rspec tests in docker environment:
-#   – Execute like this: `docker-compose run --rm app ./docker/dev/run-rspec.sh`
-#   – Or make an alias `alias dspec='docker-compose run --rm app ./docker/dev/run-rspec.sh'`
-#        then type `dspec` to start Continuous Integration Testing.
-#   – Or run from shell in docker (`docker-compose run --rm bash` … ./docker/dev/run-rpsec.sh`)
+#   - Execute like this: `docker-compose run --rm app ./docker/dev/run-rspec.sh`
+#   - Or make an alias `alias dspec='docker-compose run --rm app ./docker/dev/run-rspec.sh'`
+# 	    then type `dspec` to start Continuous Integration Testing.
+#   - Or run from shell in docker (`docker-compose run --rm bash` ./docker/dev/run-rpsec.sh`)
+#   - Or run from a shell within the container instance:
+#       docker-compose exec app bash
+#       ./docker/dev/run-rpsec.sh
+#
 
 #
 # Prepare spec tests
 #
-export RTAILS_ENV=test
+export RAILS_ENV=test
 bundle exec rake db:create
 bundle exec rake db:schema:load
 bundle exec rake db:test:prepare
@@ -17,5 +21,5 @@ bundle exec rake db:test:prepare
 #
 # Run spec tests
 #
-RAILS_ENV=test bundle exec rspec spec/
+bundle exec rspec spec/
 
