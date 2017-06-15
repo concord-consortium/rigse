31fd3539c3ec0133ddc9760f7a3fcd0f82e4bd87
Scott Cytacki <scytacki@concord.org>
2017-06-14 20:16:07 -0400

    Fix more specs for switch to AXLS
    [#145550201]

--- spec/libs/reports/column_definition_spec.rb
+++ spec/libs/reports/column_definition_spec.rb
@@ -6,24 +6,22 @@ describe Reports::ColumnDefinition do
     it 'defines default values' do
       column_defs = Reports::ColumnDefinition.new
       column_defs.title.should == 'Title'
-      column_defs.width.should == 12
-      column_defs.left_border.should == :none
-      column_defs.top_border.should == :none
-      column_defs.right_border.should == :none
-      column_defs.bottom_border.should == :none
       column_defs.col_index.should be_nil
     end
 
     it 'accepts arguments' do
-      column_defs = Reports::ColumnDefinition.new({ :title => 'Here is a title', :width => 20 })
+      column_defs = Reports::ColumnDefinition.new({ :title => 'Here is a title' })
       column_defs.title.should == 'Here is a title'
-      column_defs.width.should == 20
     end
 
     it 'accepts nil arguments' do
-      column_defs = Reports::ColumnDefinition.new({ :title => nil, :width => 13 })
+      column_defs = Reports::ColumnDefinition.new({ :title => nil })
       column_defs.title.should == 'Title'
-      column_defs.width.should == 13
+    end
+
+    it 'ignores width argument (so we can re-add width support later)' do
+      column_defs = Reports::ColumnDefinition.new({ :title => 'Here is a title', :width => 13 })
+      column_defs.title.should == 'Here is a title'
     end
   end
 
