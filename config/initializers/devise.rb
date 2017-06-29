0a9c8cd7fdd9ee4ac4f644d5bd68a028b29c4416
Jason Simpson <jsimpson@concord.org>
2017-06-29 16:35:57 -0700

    Add gem and devise stub for google_oauth2
    [#148097177]

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -217,6 +217,13 @@ Devise.setup do |config|
     config.omniauth 'schoology', ENV['SCHOOLOGY_CONSUMER_KEY'], ENV['SCHOOLOGY_CONSUMER_SECRET'], scope: 'user', strategy_class: 'OmniAuth::Strategies::Schoology', setup: SETUP_PROC
   end
 
+
+  if ENV['GOOGLE_CLIENT_ID'] && ENV['GOOGLE_CLIENT_SECRET']
+
+    config.omniauth :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET'], { :name=> 'Google', :scope => 'email,profile' }
+
+  end
+
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
   # change the failure app, you can configure them inside the config.warden block.
