a2515ba2a52381b07f218512887244a21b5a5aa5
Scott Cytacki <scytacki@concord.org>
2017-08-18 08:43:29 -0400

    server, controlled redirects after sign in, with hints from client
    This keeps the previous behavior of allowing pages to ask that they be redirected back to themselves.
    And it allows the server to decide where the page should be redirected to.
    [#150306681]

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -10,6 +10,12 @@ class API::V1::SessionsController < Devise::SessionsController
   # The #create method (through the /api/v1/users/sign_in route) receives:
   # {"user"=>{"login"=>"login", "password"=>"password"}}
   #
+  # It return a json object with a success message, and a path which the user should
+  # be redirected to
+  #
+  # this method also accepts an additional top level json paramter "after_sign_in_path"
+  # if this is set, in most cases it will override the default path
+  #
   def create
 
     username = params[:user][:login]
@@ -20,7 +26,10 @@ class API::V1::SessionsController < Devise::SessionsController
     if user && user.valid_password?(password)
 
       sign_in(resource_name, user)
-      render status: 200, :json => { :message => "Login success." }
+      render status: 200, :json => {
+        :message => "Login success.",
+        :redirect_path => after_sign_in_path_for(current_user)
+      }
 
     else
       render status: 401, :json => { :message => "Login failed." }
