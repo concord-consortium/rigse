45738443698c31d5731a88f1e5b9d8c93e17e3c2
Jason Simpson <jsimpson@concord.org>
2017-08-29 15:40:24 -0700

    Add admin setting to enable/disable social media sharing.
    [#150607021]

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20170622210725) do
+ActiveRecord::Schema.define(:version => 20170829220408) do
 
   create_table "access_grants", :force => true do |t|
     t.string   "code"
@@ -152,6 +152,7 @@ ActiveRecord::Schema.define(:version => 20170622210725) do
     t.boolean  "wrap_home_page_content",                             :default => true
     t.string   "custom_search_path",                                 :default => "/search"
     t.string   "teacher_home_path",                                  :default => "/getting_started"
+    t.boolean  "enable_social_media",                                :default => true
   end
 
   create_table "admin_settings_vendor_interfaces", :force => true do |t|
