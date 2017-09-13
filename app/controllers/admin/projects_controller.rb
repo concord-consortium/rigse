25b550a694e49053616373e786b4663e90d95442
Ethan McElroy <emcelroy@concord.org>
2017-09-13 16:12:55 -0400

    Added OG meta tags for various pages

--- app/controllers/admin/projects_controller.rb
+++ app/controllers/admin/projects_controller.rb
@@ -12,9 +12,30 @@ class Admin::ProjectsController < ApplicationController
   def landing_page
     # no authorization needed ...
     @project = Admin::Project.where(landing_page_slug: params[:landing_page_slug]).first!
+
     @landing_page_content = @project.landing_page_content
     # Redirect back to project landing page after user signs in.
     @after_sign_in_path = request.path
+    # set page title tag and meta tag values
+    page_meta = {
+      :meta_tags => {},
+      :open_graph => {}
+    }
+    @page_title = @project.name
+    meta_tags = page_meta[:meta_tags]
+    meta_tags[:description] = @project.project_card_description
+    if meta_tags[:description].blank?
+      meta_tags[:description] = "Check out this collection of educational resources from the Concord Consortium."
+    end
+    open_graph = page_meta[:open_graph]
+    open_graph[:title] = @page_title
+    open_graph[:description] = meta_tags[:description]
+    open_graph[:image] = @project.project_card_image_url
+    if open_graph[:image].blank?
+      open_graph[:image] = "https://learn-resources.concord.org/images/stem-resources/stem-resource-finder.jpg"
+    end
+    @open_graph = page_meta[:open_graph]
+
   end
 
   # GET /admin/projects
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -15,6 +15,20 @@ class HomeController < ApplicationController
   def index
     homePage = HomePage.new(current_visitor, current_settings)
     flash.keep
+    page_meta = {
+      :open_graph => {}
+    }
+    open_graph = page_meta[:open_graph]
+    unless ENV['OG_TITLE'].blank?
+      open_graph[:title] = ENV['OG_TITLE']
+    end
+    unless ENV['OG_DESCRIPTION'].blank?
+      open_graph[:description] = ENV['OG_DESCRIPTION']
+    end
+    unless ENV['OG_IMAGE_URL'].blank?
+      open_graph[:image] = ENV['OG_IMAGE_URL']
+    end
+    @open_graph = page_meta[:open_graph]
     case homePage.redirect
       when HomePage::MyClasses
         redirect_to :my_classes
@@ -68,11 +82,35 @@ class HomeController < ApplicationController
   end
 
   def about
+    @page_title = 'About'
+    page_meta = {
+      :open_graph => {}
+    }
+    open_graph = page_meta[:open_graph]
+    open_graph[:title] = 'About the STEM Resource Finder'
+    unless ENV['OG_DESCRIPTION'].blank?
+      open_graph[:description] = ENV['OG_DESCRIPTION']
+    end
+    unless ENV['OG_IMAGE_URL'].blank?
+      open_graph[:image] = ENV['OG_IMAGE_URL']
+    end
+    @open_graph = page_meta[:open_graph]
     render layout: 'minimal'
   end
 
   def collections
-    render layout: 'minimal'
+    @page_title = 'Collections'
+    page_meta = {
+      :open_graph => {}
+    }
+    open_graph = page_meta[:open_graph]
+    open_graph[:title] = @page_title
+    open_graph[:description] = 'Many of the Concord Consortium\'s educational STEM resources are part of collections created by our various research projects. Each collection has specific learning goals within the context of a larger subject area.'
+    unless ENV['OG_IMAGE_URL'].blank?
+      open_graph[:image] = ENV['OG_IMAGE_URL']
+    end
+    @open_graph = page_meta[:open_graph]
+  render layout: 'minimal'
   end
 
   def requirements
@@ -239,7 +277,7 @@ class HomeController < ApplicationController
             # Block anonymous user.
             #
             @lightbox_resource = nil
-        else 
+        else
             #
             # For logged in user, block if user is not either resource owner
             # or admin.
@@ -253,6 +291,27 @@ class HomeController < ApplicationController
     if @lightbox_resource
       @lightbox_resource = materials_data([@lightbox_resource], nil, 4).shift()
       @page_title = @lightbox_resource[:name]
+      page_meta = {
+        :title => nil,
+        :meta_tags => {},
+        :open_graph => {}
+      }
+      page_meta[:title] = @page_title
+      meta_tags = page_meta[:meta_tags]
+      meta_tags[:title] = page_meta[:title]
+      meta_tags[:description] = @lightbox_resource[:description]
+      if meta_tags[:description].blank?
+        meta_tags[:description] = "Check out this educational resource from the Concord Consortium."
+      end
+      open_graph = page_meta[:open_graph]
+      open_graph[:title] = @page_title
+      open_graph[:description] = meta_tags[:description]
+      @resource_icon = @lightbox_resource[:icon]
+      open_graph[:image] = @resource_icon[:url]
+      if open_graph[:image].blank?
+        open_graph[:image] = "https://learn-resources.concord.org/images/stem-resources/stem-resource-finder.jpg"
+      end
+      @open_graph = page_meta[:open_graph]
     else
       @page_title = "Resource not found"
     end
