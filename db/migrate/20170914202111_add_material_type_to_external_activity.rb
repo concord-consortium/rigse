635e16bd6c27b594b3c44a4fa111c9ffe9447d5e
Jason Simpson <jsimpson@concord.org>
2017-09-19 05:54:41 -0700

    Rename field material_type
    [#150903768]

--- db/migrate/20170914202111_add_material_type_to_external_activity.rb
+++ db/migrate/20170914202111_add_material_type_to_external_activity.rb
@@ -1,5 +1,5 @@
 class AddExternalTypeToExternalActivity < ActiveRecord::Migration
   def change
-    add_column :external_activities, :external_type, :string
+    add_column :external_activities, :material_type, :string
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20170914202111) do
+ActiveRecord::Schema.define(:version => 20170830191912) do
 
   create_table "access_grants", :force => true do |t|
     t.string   "code"
@@ -865,7 +865,6 @@ ActiveRecord::Schema.define(:version => 20170914202111) do
     t.string   "license_code"
     t.boolean  "enable_sharing",                               :default => true
     t.boolean  "append_auth_token"
-    t.string   "external_type"
   end
 
   add_index "external_activities", ["is_featured", "publication_status"], :name => "featured_public", :length => {"is_featured"=>nil, "publication_status"=>191}
