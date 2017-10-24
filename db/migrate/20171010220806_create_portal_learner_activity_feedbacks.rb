7b5de546f4828954e8a5d08e7ae5ed9fc5c2f024
Sam Fentress <sfentress@concord.org>
2017-10-24 14:06:05 -0400

    Remove default from text column
    Fixes error on some systems where running migration throws
    
    `Mysql2::Error: BLOB/TEXT column 'text_feedback' can't have a default value`

--- db/migrate/20171010220806_create_portal_learner_activity_feedbacks.rb
+++ db/migrate/20171010220806_create_portal_learner_activity_feedbacks.rb
@@ -1,7 +1,7 @@
 class CreatePortalLearnerActivityFeedbacks < ActiveRecord::Migration
   def change
     create_table :portal_learner_activity_feedbacks do |t|
-      t.text :text_feedback, default: ""
+      t.text :text_feedback
       t.integer :score, default: 10
       t.boolean :has_been_reviewed, default: false
       t.references :portal_learner
