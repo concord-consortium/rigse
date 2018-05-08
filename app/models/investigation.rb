c704666019c222742f8b73600a26ef4c701c5af9
Noah Paessel <knowuh@gmail.com>
2018-05-08 17:13:09 -0400

    Removes scopes that reference deleted embeddables.

--- app/models/investigation.rb
+++ app/models/investigation.rb
@@ -158,21 +158,6 @@ class Investigation < ActiveRecord::Base
     }
   }
 
-  scope :probe_type, {
-    :joins => "INNER JOIN activities ON activities.investigation_id = investigations.id INNER JOIN sections ON sections.activity_id = activities.id INNER JOIN pages ON pages.section_id = sections.id INNER JOIN page_elements ON page_elements.page_id = pages.id INNER JOIN embeddable_data_collectors ON embeddable_data_collectors.id = page_elements.embeddable_id AND page_elements.embeddable_type = 'Embeddable::GraphyThing' INNER JOIN probe_probe_types ON probe_probe_types.id = embeddable_data_collectors.probe_type_id"
-    }
-
-  scope :probe, lambda { |pt|
-    pt = pt.size > 0 ? pt.map{|i| i.to_i} : []
-    {
-      :conditions => ['probe_probe_types.id in (?)', pt ]
-    }
-  }
-
-  scope :no_probe,{
-    :select => "investigations.id",
-    :joins => "INNER JOIN activities ON activities.investigation_id = investigations.id INNER JOIN sections ON sections.activity_id = activities.id INNER JOIN pages ON pages.section_id = sections.id INNER JOIN page_elements ON page_elements.page_id = pages.id INNER JOIN embeddable_data_collectors ON embeddable_data_collectors.id = page_elements.embeddable_id AND page_elements.embeddable_type = 'Embeddable::GraphyThing' INNER JOIN probe_probe_types ON probe_probe_types.id = embeddable_data_collectors.probe_type_id"
-  }
 
   scope :like, lambda { |name|
     name = "%#{name}%"
