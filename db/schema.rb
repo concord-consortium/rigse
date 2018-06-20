b6c11abef2d4e3f3469ccacbc3b9848a4144cb2b
Noah Paessel <knowuh@gmail.com>
2018-06-20 16:40:53 -0400

    Add `link_id` and `pop_out` to admin_project_links
    Migrations and model changes for adding links to projects
    
    [#158011724]
    [#157698255]
    
    https://www.pivotaltracker.com/story/show/158011724
    https://www.pivotaltracker.com/story/show/157698255
    
    REBASE:  This DB migration needed to happen 2 commits ago.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20180612151521) do
+ActiveRecord::Schema.define(:version => 20180620194214) do
 
   create_table "access_grants", :force => true do |t|
     t.string   "code"
@@ -85,6 +85,8 @@ ActiveRecord::Schema.define(:version => 20180612151521) do
     t.text     "href",       :limit => 16777215
     t.datetime "created_at",                     :null => false
     t.datetime "updated_at",                     :null => false
+    t.string   "link_id"
+    t.boolean  "pop_out"
   end
 
   create_table "admin_project_materials", :force => true do |t|
