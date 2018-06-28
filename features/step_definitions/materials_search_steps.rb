1f294985a62fb5375255f48f2fc0c34b68226f80
Scott Cytacki <scytacki@concord.org>
2018-06-27 21:26:16 -0400

    fix error in test

--- features/step_definitions/materials_search_steps.rb
+++ features/step_definitions/materials_search_steps.rb
@@ -5,5 +5,5 @@ end
 
 When /^I wait for the search to be ready$/ do
   search_result_delay = ENV['SECONDS_BEFORE_TESTING_SEARCH_RESULTS'].presence || 3
-  sleep(seconds.to_i)
+  sleep(search_result_delay.to_i)
 end
