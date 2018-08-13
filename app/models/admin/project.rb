552aadc84dc8b68468397ba6a18eb8ce31832cf9
Unknown <emcelroy@concord.org>
2018-08-13 14:20:14 -0400

    Added method for getting list of project admins.

--- app/models/admin/project.rb
+++ app/models/admin/project.rb
@@ -27,6 +27,21 @@ class Admin::Project < ActiveRecord::Base
     self.where("landing_page_slug <> ''").order("name ASC")
   end
 
+  def self.get_project_admins(project_id)
+    # get a list of project admins for specified project
+    project_admin_ids = []
+    project_admins = []
+    project_admin_query = "SELECT user_id FROM admin_project_users WHERE project_id='#{project_id}' AND is_admin='1'"
+    result = ActiveRecord::Base.connection.exec_query(project_admin_query)
+    result.each do |row|
+      project_admin_ids.push(row['user_id'])
+    end
+    project_admin_ids.each do |project_admin_id|
+      project_admins.push(User.find(project_admin_id))
+    end
+    project_admins
+  end
+
   has_many :project_materials, dependent: :destroy
   has_many :activities, through: :project_materials, source: :material, source_type: 'Activity'
   has_many :investigations, through: :project_materials, source: :material, source_type: 'Investigation'
