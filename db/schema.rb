4157e04ca097d0b3359b16c8b6279d452e8575a2
Unknown <emcelroy@concord.org>
2018-08-14 12:18:30 -0400

    Added email_notifications_enabled field to admin_cohorts table.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20180807215058) do
+ActiveRecord::Schema.define(:version => 20180814144731) do
 
   create_table "access_grants", :force => true do |t|
     t.string   "code"
@@ -66,6 +66,7 @@ ActiveRecord::Schema.define(:version => 20180807215058) do
   create_table "admin_cohorts", :force => true do |t|
     t.integer "project_id"
     t.string  "name"
+    t.boolean "email_notifications_enabled", :default => false
   end
 
   add_index "admin_cohorts", ["project_id", "name"], :name => "index_admin_cohorts_on_project_id_and_name", :unique => true
