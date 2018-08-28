a5a1857ef68137e3595ac654d0135667d94880b1
Emily Watkins <emily.e.dolliver@gmail.com>
2018-08-28 15:37:28 -0700

    Fix capitalization error and escape double quotes

--- features/step_definitions/materials_search_steps.rb
+++ features/step_definitions/materials_search_steps.rb
@@ -1,6 +1,6 @@
 Then /I search for my own materials/ do
-  search_mine_string = I18n.translate("Search.only_mine")
-  step "I should see #{search_mine_string}"
+  search_mine_string = I18n.translate("search.only_mine")
+  step "I should see \"#{search_mine_string}\""
 end
 
 When /^I wait for the search to be ready$/ do
