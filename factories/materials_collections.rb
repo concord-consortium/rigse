8d5f9ae96b1c03b136baf8573339dcfb23c417f2
Tim Chambers <tim@hint.io>
2018-09-17 13:20:21 -0700

    Missed 2 deprecations

--- factories/materials_collections.rb
+++ factories/materials_collections.rb
@@ -11,8 +11,8 @@ FactoryBot.define do
     # association :project, :factory => :admin_project
 
     factory :materials_collection_with_items do
-      ignore do
-        items_count 5
+      transient do
+        items_count {5}
       end
 
       after(:create) do |collection, evaluator|
