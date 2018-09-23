2cab4c366320f1071fd8675e065b3e89fb1dc93e
Scott Cytacki <scytacki@concord.org>
2018-09-22 22:28:01 -0400

    remove tables and references

---
+++ db/migrate/20180923020414_remove_ri_gse.rb
@@ -0,0 +1,27 @@
+class RemoveRiGse < ActiveRecord::Migration
+  def remove_table(name)
+    drop_table name.to_sym if ActiveRecord::Base.connection.tables.include?(name)
+  end
+
+  def up
+    [
+      "ri_gse_assessment_target_unifying_themes",
+      "ri_gse_assessment_targets",
+      "ri_gse_big_ideas",
+      "ri_gse_domains",
+      "ri_gse_expectation_indicators",
+      "ri_gse_expectation_stems",
+      "ri_gse_expectations",
+      "ri_gse_grade_span_expectations",
+      "ri_gse_knowledge_statements",
+      "ri_gse_unifying_themes"
+    ].each{|t| remove_table(t)}
+
+    remove_column :portal_teachers, :domain_id
+    remove_column :investigations, :grade_span_expectation_id
+  end
+
+  def down
+    raise ActiveRecord::IrreversibleMigration
+  end
+end
