a798ed7cba96902e4c96e3a512c2e333935e539d
Unknown <emcelroy@concord.org>
2018-09-25 16:46:56 -0400

    Correctly send notification emails to multiple email addresses

--- app/models/portal/clazz_mailer.rb
+++ app/models/portal/clazz_mailer.rb
@@ -3,6 +3,7 @@ class Portal::ClazzMailer < Devise::Mailer
 
   def clazz_creation_notification(user, clazz)
     if user.present?
+      cohort_admin_emails = []
       user.cohorts.each do |uc|
         if uc.email_notifications_enabled
           @cohort = uc
@@ -11,15 +12,19 @@ class Portal::ClazzMailer < Devise::Mailer
           cohort_admins = @cohort_project.project_admins
           @clazz_name = clazz.name
           @user = user
-          subject = "#{@cohort_project.name} cohort #{@cohort_name} Update: New class created by #{@user.name}"
-          finish_email(cohort_admins, subject)
+          @subject = "#{@cohort_project.name} cohort #{@cohort_name} Update: New class created by #{@user.name}"
+          cohort_admins.each do |cohort_admin|
+            cohort_admin_emails.push("#{cohort_admin.name} <#{cohort_admin.email}>")
+          end
         end
       end
+      finish_email(cohort_admin_emails, @subject)
     end
   end
 
   def clazz_assignment_notification(user, clazz, offering_name)
     if user.present?
+      cohort_admin_emails = []
       user.cohorts.each do |uc|
         if uc.email_notifications_enabled
           @cohort = uc
@@ -29,22 +34,23 @@ class Portal::ClazzMailer < Devise::Mailer
           @clazz_name = clazz.name
           @offering_name = offering_name
           @user = user
-          subject = "#{@cohort_project.name} cohort #{@cohort_name} Update: New assignment added by #{@user.name}"
-          finish_email(cohort_admins, subject)
+          @subject = "#{@cohort_project.name} cohort #{@cohort_name} Update: New assignment added by #{@user.name}"
+          cohort_admins.each do |cohort_admin|
+            cohort_admin_emails.push("#{cohort_admin.name} <#{cohort_admin.email}>")
+          end
         end
       end
+      finish_email(cohort_admin_emails, @subject)
     end
   end
 
   protected
 
-  def finish_email(cohort_admins, subject)
+  def finish_email(cohort_admin_emails, subject)
     # Need to set the theme because normally it gets set in a controller before_filter...
     set_theme(APP_CONFIG[:theme]||'default')
-    cohort_admins.each do |cohort_admin|
-      mail(:to => "#{cohort_admin.name} <#{cohort_admin.email}>",
-           :subject => subject,
-           :date => Time.now)
-    end
+    mail(:to => cohort_admin_emails,
+         :subject => subject,
+         :date => Time.now)
   end
 end
