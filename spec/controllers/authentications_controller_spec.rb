6aa072f6ddf8ea7f5ad9c163d273ccd8c344b5bb
Unknown <emcelroy@concord.org>
2018-10-10 11:26:10 -0400

    Added test for one step of SSO registration process

--- spec/controllers/authentications_controller_spec.rb
+++ spec/controllers/authentications_controller_spec.rb
@@ -4,5 +4,27 @@ require 'spec_helper'
 
 RSpec.describe AuthenticationsController, type: :controller do
 
+  describe '#google' do
+    it 'GET google' do
+      @request.env['devise.mapping'] = Devise.mappings[:user]
+      @request.env['omniauth.auth'] = double(
+        extra: double(
+          username: nil,
+          first_name: 'Fake',
+          last_name: 'Fakerson'
+        ),
+        info: double(
+          email: 'fakename@fakedomain.org',
+          uid: '12345'
+        ),
+        provider: 'google',
+        uid: '12345'
+      )
+      @request.env['omniauth.origin'] = double()
+      get :google
+
+      expect(response).to have_http_status(302)
+    end
+  end
 
 end
