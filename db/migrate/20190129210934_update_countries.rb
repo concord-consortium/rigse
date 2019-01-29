7d62445c996961061b0dad7febb6bbe311af985f
Noah Paessel <knowuh@gmail.com>
2019-01-29 16:23:09 -0500

    Add Migration to import Georgia to countries list
    Add migration update_countries.rb
    
    [#163407719]
    https://www.pivotaltracker.com/story/show/163407719

---
+++ db/migrate/20190129210934_update_countries.rb
@@ -0,0 +1,52 @@
+
+class UpdateCountries < ActiveRecord::Migration
+  class PortalCountry < ActiveRecord::Base
+    self.table_name = :portal_countries
+
+    def self.csv_filemame
+        File.join(Rails.root,"resources/country-codes_csv.csv")
+    end
+
+    def self.from_csv_file
+        body   = File.open(self.csv_filemame).read
+        csv    = CSV.new(body, :headers => true, :header_converters => :symbol, :converters => [:all])
+        hashes = csv.to_a.map {|row| row.to_hash }
+        hashes.map! { |h| from_hash(remap_keys(h)) }
+    end
+
+    def self.remap_keys(in_hash)
+        result = Hash[in_hash.map {|k, v| [field_name_map[k] || :xxxX, v] }]
+        result.delete(:xxxX)
+        result
+    end
+
+    def self.field_name_map
+        {
+            :cldr_display_name      => :name,
+            :official_name_en       => :formal_name,
+            :capital                => :capital,
+            :iso31661alpha2         => :two_letter,
+            :iso31661alpha3         => :three_letter,
+            :iso31661numeric        => :iso_id,
+            :tld                    => :tld
+        }
+    end
+
+    def self.from_hash(in_hash)
+       if in_hash[:name]
+         in_hash[:name] = in_hash[:name].strip.gsub("&","and")
+         existing = self.where("lower(name) like ?", in_hash[:name].downcase).first || self.new()
+         existing.update_attributes(in_hash)
+       end
+    end
+  end
+
+  def up
+    PortalCountry.from_csv_file
+  end
+
+  def down
+    # Nothing to do.
+  end
+
+end
