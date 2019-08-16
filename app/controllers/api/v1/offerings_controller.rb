734ec61c2274d3d4b6deaa1a01737a1cdcbd08ff
pjanik <janikpiotrek@gmail.com>
2019-08-16 16:36:18 +0200

    Add support of "add_external_report" URL param
    [#167761386]
    
    On following pages:
    - /recent_activity
    - /portal/classes/[class_id]/materials (teacher page)
    - /my_classes (student page)
    - /portal/classes/[class_id] (student page)

--- app/controllers/api/v1/offerings_controller.rb
+++ app/controllers/api/v1/offerings_controller.rb
@@ -13,7 +13,7 @@ class API::V1::OfferingsController < API::APIController
       return error('offering not found', 404)
     end
     authorize offering, :api_show?
-    offering_api = API::V1::Offering.new(offering, request.protocol, request.host_with_port, current_user)
+    offering_api = API::V1::Offering.new(offering, request.protocol, request.host_with_port, current_user, params[:add_external_report])
     render :json => offering_api.to_json, :callback => params[:callback]
   end
 
