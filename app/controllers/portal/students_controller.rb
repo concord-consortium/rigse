83c3b75d3445872a75f388bd24bff75068532cdb
Unknown <emcelroy@concord.org>
2019-10-18 10:33:46 -0400

    Send API call to report service.

--- app/controllers/portal/students_controller.rb
+++ app/controllers/portal/students_controller.rb
@@ -1,3 +1,4 @@
+require 'uri'
 class Portal::StudentsController < ApplicationController
   include RestrictedPortalController
 
@@ -283,23 +284,35 @@ class Portal::StudentsController < ApplicationController
     @portal_student.add_clazz(@new_class)
 
     # initialize JSON for report API call
-    @report_json = JSON['{"class_info_url": "' + @new_class.id.to_s + '", "context_id": "' + @new_class.hash.to_s + '", "platform_id": "' + APP_CONFIG[:site_url].to_s + '", "platform_user_id": "' + @portal_student.user_id.to_s + '"}']
+    @site_protocol = URI.parse(APP_CONFIG[:site_url]).scheme
+    @site_domain = URI.parse(APP_CONFIG[:site_url]).host
+    @report_json = JSON['{"class_info_url": "' + @new_class.class_info_url(@site_protocol, @site_domain) + '", "context_id": "' + @new_class.class_hash.to_s + '", "current_context_id": "' + @current_class.class_hash.to_s + '", "platform_id": "' + APP_CONFIG[:site_url].to_s + '", "platform_user_id": "' + @portal_student.user_id.to_s + '"}']
     @assignments = []
     # find matches between student learners and new class's offerings. Update offering_id values to match those in new class (student work on assignments that aren't assigned to new class becomes orphaned)
     @portal_student.learners.each do |sa|
+      puts "testing..."
+      puts sa.inspect
       @new_class.offerings.each do |nca|
         if sa.offering.runnable == nca.runnable
           @learner_to_update = Portal::Learner.find(sa.id)
           @learner_to_update.update_attribute('offering_id', nca.id)
-          @learner_to_update.report_learner.update_fields
+          @learner_to_update.report_learner.update_fields # does this still need to be here?
           # add assignment to JSON for report API call
-          @assignments << JSON['{"new_resource_link_id": "' + nca.id.to_s + '", "old_resource_link_id": "' + sa.id.to_s + '"}']
+          @assignments << JSON['{"new_resource_link_id": "' + nca.id.to_s + '", "current_resource_link_id": "' + sa.offering_id.to_s + '", "tool_id": "' + ENV['TEMP_TOOL_ID'] + '"}']
         end
       end
     end
 
     # add learner IDs to JSON for report API
-    @report_json['new_assignments'] = @assignments
+    @report_json['assignments'] = @assignments
+    puts @report_json.to_json
+
+    # post data to report service, include bearer token in request ENV['REPORT_SERVICE_BEARER_TOKEN']
+    req = Net::HTTP::Post.new('/api/move_student_work', {'Authorization' => 'Bearer ' + ENV['REPORT_SERVICE_BEARER_TOKEN'], 'Content-Type' => 'application/json'})
+    req.body = @report_json.to_json
+    http = Net::HTTP.new('us-central1-report-service-dev.cloudfunctions.net', '443')
+    http.use_ssl = true
+    http.request(req)
 
     flash[:notice] = 'Successfully moved student to new class.' # + JSON[@report_json]
     redirect_to(@portal_student)
