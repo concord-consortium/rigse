dbcb15b38be212b9c29193121f79c01795a8ae9b
Noah Paessel <knowuh@gmail.com>
2019-10-31 16:22:13 -0400

    ✔  New teacher_policy fixes failing tests.
    After lengthy consult with @dougmartin we discovered we needed to be able to execute raw sql for our selection of portal_teacher.ids.
    We generate the set of Ids in one large UNION query, and then modify our scope to select on those portal_teacher.id values.
    
    [#169465198]
    http://www.pivotaltracker.com/slack_links/story/169465198

--- app/policies/portal/teacher_policy.rb
+++ app/policies/portal/teacher_policy.rb
@@ -5,16 +5,43 @@ class Portal::TeacherPolicy < ApplicationPolicy
       if user && user.has_role?('manager','admin','researcher')
         all
       elsif user && (user.is_project_admin? || user.is_project_researcher?)
-        # prevents a bunch of unnecessary model loads by not using the user#admin_for_project_teachers and user#researcher_for_project_teachers methods
+        # prevents a bunch of unnecessary model loads by not using the
+        # user#admin_for_project_teachers and user#researcher_for_project_teachers methods.
+        # We have to look through project cohort items, and also check if the
+        # user is a teacher (edge case) in a cohort they are not admin for ...
+        # See this bug: https://www.pivotaltracker.com/story/show/169465198
+        sql = "
+          SELECT DISTINCT `portal_teachers`.* FROM
+          (
+              SELECT portal_teachers.id FROM portal_teachers
+                  INNER JOIN
+                      admin_cohort_items __aci_scope ON (__aci_scope.item_id = portal_teachers.id)
+                  INNER JOIN
+                      admin_cohorts __ac_scope ON (__ac_scope.id = __aci_scope.admin_cohort_id)
+                  INNER JOIN
+                      admin_project_users __apu_scope ON (__apu_scope.project_id = __ac_scope.project_id)
+                  WHERE
+                      __aci_scope.item_type = 'Portal::Teacher'
+                  AND
+                      __apu_scope.user_id = #{user.id}
+                  AND
+                      (__apu_scope.is_admin = 1 OR __apu_scope.is_researcher = 1)
+              UNION
+                  SELECT id
+                  FROM portal_teachers
+                  WHERE user_id = #{user.id}
+          ) as all_teachers
+          JOIN portal_teachers ON portal_teachers.id = all_teachers.id
+        "
+
+        ids = Portal::Teacher.connection.select_all(sql).map { |i| i['id'] }
+        # Return a new scope selecting those records:
         scope
-          .joins("INNER JOIN admin_cohort_items __aci_scope ON __aci_scope.item_id = portal_teachers.id")
-          .joins("INNER JOIN admin_cohorts __ac_scope ON __ac_scope.id = __aci_scope.admin_cohort_id")
-          .joins("INNER JOIN admin_project_users __apu_scope ON __apu_scope.project_id = __ac_scope.project_id")
-          .where("__aci_scope.item_type = 'Portal::Teacher'")
-          .where("__apu_scope.user_id = ? AND (__apu_scope.is_admin = 1 OR __apu_scope.is_researcher = 1)", user.id)
+          .where('portal_teachers.id IN (?)', ids)
           .uniq
-      else
-        none
+
+      elsif (user.portal_teacher)
+        scope.where('user_id = (?)', user.id)
       end
     end
   end
