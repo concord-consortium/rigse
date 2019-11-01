1757fce5f1bd82d9c93213b37df5df5341e7d619
Noah Paessel <knowuh@gmail.com>
2019-11-01 09:28:53 -0400

    Code Climate adjustment: reduce size of method.
    [#169465198]
    http://www.pivotaltracker.com/slack_links/story/169465198

--- app/policies/portal/teacher_policy.rb
+++ app/policies/portal/teacher_policy.rb
@@ -1,5 +1,27 @@
 class Portal::TeacherPolicy < ApplicationPolicy
 
+  def self.teacher_query(user)
+    return "
+      SELECT portal_teachers.id FROM portal_teachers
+      INNER JOIN
+        admin_cohort_items __aci_scope ON (__aci_scope.item_id = portal_teachers.id)
+      INNER JOIN
+          admin_cohorts __ac_scope ON (__ac_scope.id = __aci_scope.admin_cohort_id)
+      INNER JOIN
+          admin_project_users __apu_scope ON (__apu_scope.project_id = __ac_scope.project_id)
+      WHERE
+          __aci_scope.item_type = 'Portal::Teacher'
+      AND
+          __apu_scope.user_id = #{user.id}
+      AND
+          (__apu_scope.is_admin = 1 OR __apu_scope.is_researcher = 1)
+      UNION
+        SELECT id
+        FROM portal_teachers
+        WHERE user_id = #{user.id}
+    "
+  end
+  
   class Scope < Scope
     def resolve
       if user && user.has_role?('manager','admin','researcher')
@@ -8,26 +30,7 @@ class Portal::TeacherPolicy < ApplicationPolicy
         # prevents a bunch of unnecessary model loads by not using the model scopes
         # Also covers some tricky edge cases.
         # See this bug: https://www.pivotaltracker.com/story/show/169465198
-        sql = "
-          SELECT portal_teachers.id FROM portal_teachers
-            INNER JOIN
-              admin_cohort_items __aci_scope ON (__aci_scope.item_id = portal_teachers.id)
-            INNER JOIN
-                admin_cohorts __ac_scope ON (__ac_scope.id = __aci_scope.admin_cohort_id)
-            INNER JOIN
-                admin_project_users __apu_scope ON (__apu_scope.project_id = __ac_scope.project_id)
-            WHERE
-                __aci_scope.item_type = 'Portal::Teacher'
-            AND
-                __apu_scope.user_id = #{user.id}
-            AND
-                (__apu_scope.is_admin = 1 OR __apu_scope.is_researcher = 1)
-          UNION
-            SELECT id
-            FROM portal_teachers
-            WHERE user_id = #{user.id}
-        "
-
+        sql = Portal::TeacherPolicy::teacher_query(user)
         ids = Portal::Teacher.connection.select_all(sql).map { |i| i['id'] }
         # Return a new scope selecting those records:
         scope
