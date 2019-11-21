2c93af0c9483205dac8a4b8a39afca8cfbaa410b
Unknown <emcelroy@concord.org>
2019-11-20 21:19:48 -0500

    Update users_controller_spec tests.

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -5,132 +5,168 @@ describe UsersController do
 
   render_views
 
-  before(:each) do
-    generate_default_settings_and_jnlps_with_mocks
-    logout_user
+  context "when logged in" do
+    before(:each) do
+      generate_default_settings_and_jnlps_with_mocks
+    end
+
+    describe "as admin" do
+      it "lets user view users index page" do
+        login_admin
+        get :index
+        expect(response.status).to eq(200)
+      end
+    end
+
+    describe "as manager" do
+      it "lets user view users index page" do
+        login_manager
+        get :index
+        expect(response.status).to eq(200)
+      end
+    end
+
+    describe "as project admin" do
+      it "lets user view users index page" do
+        login_manager
+
+        get :index
+        expect(response.status).to eq(200)
+      end
+    end
+
+    describe "as researcher" do
+      it "does not let user view users index page" do
+        login_researcher
+        get :index
+        expect(response.status).to eq(302)
+      end
+    end
+
   end
 
-  it "lets admin user view users index page" do
-    login_admin
-    get :index
-    expect(response.status).to eq(200)
-  end
+  context "when logged out" do
+    before(:each) do
+      generate_default_settings_and_jnlps_with_mocks
+      logout_user
+    end
 
-  it 'allows signup' do
-    skip "Broken example"
-    expect do
+    it 'allows signup' do
+      skip "Broken example"
+      expect do
+        create_user
+        expect(response).to be_redirect
+      end.to change(User, :count).by(1)
+    end
+
+    it 'signs up user in pending state' do
+      skip "Broken example"
       create_user
-      expect(response).to be_redirect
-    end.to change(User, :count).by(1)
-  end
+      assigns(:user).reload
+      expect(assigns(:user)).to be_pending
+    end
 
-  it 'signs up user in pending state' do
-    skip "Broken example"
-    create_user
-    assigns(:user).reload
-    expect(assigns(:user)).to be_pending
-  end
+    it 'signs up user with activation code' do
+      skip "Broken example"
+      create_user
+      assigns(:user).reload
+      expect(assigns(:user).activation_code).not_to be_nil
+    end
+    it 'requires login on signup' do
+      skip "Broken example"
+      expect do
+        create_user(:login => nil)
+        expect(assigns[:user].errors[:login]).not_to be_nil
+        expect(response).to be_success
+      end.not_to change(User, :count)
+    end
 
-  it 'signs up user with activation code' do
-    skip "Broken example"
-    create_user
-    assigns(:user).reload
-    expect(assigns(:user).activation_code).not_to be_nil
-  end
-  it 'requires login on signup' do
-    skip "Broken example"
-    expect do
-      create_user(:login => nil)
-      expect(assigns[:user].errors[:login]).not_to be_nil
-      expect(response).to be_success
-    end.not_to change(User, :count)
-  end
+    it 'requires password on signup' do
+      skip "Broken example"
+      expect do
+        create_user(:password => nil)
+        expect(assigns[:user].errors[:password]).not_to be_nil
+        expect(response).to be_success
+      end.not_to change(User, :count)
+    end
 
-  it 'requires password on signup' do
-    skip "Broken example"
-    expect do
-      create_user(:password => nil)
-      expect(assigns[:user].errors[:password]).not_to be_nil
-      expect(response).to be_success
-    end.not_to change(User, :count)
-  end
+    it 'requires password confirmation on signup' do
+      skip "Broken example"
+      expect do
+        create_user(:password_confirmation => nil)
+        expect(assigns[:user].errors[:password_confirmation]).not_to be_nil
+        expect(response).to be_success
+      end.not_to change(User, :count)
+    end
 
-  it 'requires password confirmation on signup' do
-    skip "Broken example"
-    expect do
-      create_user(:password_confirmation => nil)
-      expect(assigns[:user].errors[:password_confirmation]).not_to be_nil
-      expect(response).to be_success
-    end.not_to change(User, :count)
-  end
+    it 'requires email on signup' do
+      skip "Broken example"
+      expect do
+        create_user(:email => nil)
+        expect(assigns[:user].errors[:email]).not_to be_nil
+        expect(response).to be_success
+      end.not_to change(User, :count)
+    end
 
-  it 'requires email on signup' do
-    skip "Broken example"
-    expect do
-      create_user(:email => nil)
-      expect(assigns[:user].errors[:email]).not_to be_nil
-      expect(response).to be_success
-    end.not_to change(User, :count)
-  end
+    it 'activates user' do
+      skip "Broken example"
+      expect(User.authenticate('aaron', 'monkey')).to be_nil
+      get :activate, :activation_code => users(:aaron).activation_code
+      expect(response).to redirect_to('/login')
+      expect(flash[:notice]).not_to be_nil
+      expect(flash[:error ]).to     be_nil
+      expect(User.authenticate('aaron', 'monkey')).to eq(users(:aaron))
+    end
 
-  it 'activates user' do
-    skip "Broken example"
-    expect(User.authenticate('aaron', 'monkey')).to be_nil
-    get :activate, :activation_code => users(:aaron).activation_code
-    expect(response).to redirect_to('/login')
-    expect(flash[:notice]).not_to be_nil
-    expect(flash[:error ]).to     be_nil
-    expect(User.authenticate('aaron', 'monkey')).to eq(users(:aaron))
-  end
+    it 'does not activate user without key' do
+      skip "Broken example"
+      get :activate
+      expect(flash[:notice]).to     be_nil
+      expect(flash[:error ]).not_to be_nil
+    end
 
-  it 'does not activate user without key' do
-    skip "Broken example"
-    get :activate
-    expect(flash[:notice]).to     be_nil
-    expect(flash[:error ]).not_to be_nil
-  end
+    it 'does not activate user with blank key' do
+      skip "Broken example"
+      get :activate, :activation_code => ''
+      expect(flash[:notice]).to     be_nil
+      expect(flash[:error ]).not_to be_nil
+    end
 
-  it 'does not activate user with blank key' do
-    skip "Broken example"
-    get :activate, :activation_code => ''
-    expect(flash[:notice]).to     be_nil
-    expect(flash[:error ]).not_to be_nil
-  end
+    it 'does not activate user with bogus key' do
+      skip "Broken example"
+      get :activate, :activation_code => 'i_haxxor_joo'
+      expect(flash[:notice]).to     be_nil
+      expect(flash[:error ]).not_to be_nil
+    end
 
-  it 'does not activate user with bogus key' do
-    skip "Broken example"
-    get :activate, :activation_code => 'i_haxxor_joo'
-    expect(flash[:notice]).to     be_nil
-    expect(flash[:error ]).not_to be_nil
-  end
+    it 'shows thank you page to teacher on successful registration' do
 
-  it 'shows thank you page to teacher on successful registration' do
+      get :registration_successful, {:type => 'teacher'}
 
-    get :registration_successful, {:type => 'teacher'}
+      expect(@response).to render_template("users/thanks")
 
-    expect(@response).to render_template("users/thanks")
+      assert_select 'h2', /thanks/i
+      assert_select 'p', /activation code/i
 
-    assert_select 'h2', /thanks/i
-    assert_select 'p', /activation code/i
+    end
 
-  end
+    it 'shows thank you page to the student with login name on successful registration' do
 
-  it 'shows thank you page to the student with login name on successful registration' do
+      get :registration_successful, {:type => 'student'}
 
-    get :registration_successful, {:type => 'student'}
+      expect(@response).to render_template("portal/students/signup_success")
 
-    expect(@response).to render_template("portal/students/signup_success")
+      # should show text "your username is"
+      assert_select "p", /username\s+is/i
 
-    # should show text "your username is"
-    assert_select "p", /username\s+is/i
+      # should show directions to login:
+      assert_select 'p', /login/i
 
-    # should show directions to login:
-    assert_select 'p', /login/i
+      assert_select "*#clazzes_nav", false
 
-    assert_select "*#clazzes_nav", false
-
-    expect(flash[:error]).to be_nil
-    expect(flash[:notice]).to be_nil
+      expect(flash[:error]).to be_nil
+      expect(flash[:notice]).to be_nil
+    end
   end
 
   def create_user(options = {})
