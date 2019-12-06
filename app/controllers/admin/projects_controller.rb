41b1df686721393ab49d84db50aa1d4ee12f0c7f
Unknown <emcelroy@concord.org>
2019-12-06 12:48:01 -0500

    Update teachers' recent collections when they visit a collections page.

--- app/controllers/admin/projects_controller.rb
+++ app/controllers/admin/projects_controller.rb
@@ -36,6 +36,11 @@ class Admin::ProjectsController < ApplicationController
         "https://learn-resources.concord.org/images/stem-resources/stem-resource-finder.jpg"
     }
     render layout: 'minimal'
+
+    # if portal user is a teacher, update their recently visited collections pages
+    if (@portal_teacher.present?)
+      @portal_teacher.add_recent_collection_page(@project)
+    end
   end
 
   # GET /admin/projects
