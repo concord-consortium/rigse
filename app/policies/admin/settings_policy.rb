13a11090f2f1f6a245c5f6e8bf45b53b9895b345
Unknown <emcelroy@concord.org>
2020-01-31 18:00:02 -0500

    Rename admin settings policy file, add scope and methods.

---
+++ app/policies/admin/settings_policy.rb
@@ -0,0 +1,19 @@
+class Admin::SettingsPolicy < ApplicationPolicy
+
+  class Scope < Scope
+    def resolve
+      if user.has_role?('admin')
+        all
+      else
+        none
+      end
+    end
+  end
+
+  def new_or_create?
+    admin?
+  end
+  def update_edit_or_destroy?
+    admin?
+  end
+end
