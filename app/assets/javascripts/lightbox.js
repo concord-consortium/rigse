0a714d9da72f16d70e5f679e192cfbf7e03dc2f2
Unknown <emcelroy@concord.org>
2020-04-09 17:21:57 -0400

    Update/fix Lightbox.js to include ID specified in config options.

--- app/assets/javascripts/lightbox.js
+++ app/assets/javascripts/lightbox.js
@@ -9,18 +9,18 @@
     {
         return;
     }
-    
+
     var Lightbox = function (options) {
         this._constructor(options);
         return;
     };
-    
+
     Lightbox.type = {
         NONE: 1,
         ALERT: 2,
         CONFIRM: 3
     };
-    
+
     Lightbox.defaultOptions = {
         id: null,
         theme: "lightbox",
@@ -37,14 +37,14 @@
         callbackContext: null,
         closeOnNextPopup: false
     };
-    
+
     Lightbox.iNextAutoId = 1;
-    
+
     Lightbox.list = [];
     Lightbox.listIndex = {};
-    
+
     Lightbox.popupToCloseOnNextPopup = null;
-    
+
     Lightbox.add = function (lightbox) {
         if (!(lightbox instanceof Lightbox)) {
             return;
@@ -52,23 +52,23 @@
         this.listIndex[lightbox.id] = this.list.length;
         this.list.push(lightbox);
     };
-    
-    
+
+
     Lightbox.remove = function (lightbox) {
-        
+
         if (lightbox instanceof String) {
             lightbox = this.findById(lightbox);
         }
-        
+
         if (!(lightbox instanceof Lightbox)) {
             return;
         }
-        
+
         this.list[this.listIndex[lightbox.id]] = null;
         delete this.listIndex[lightbox.id];
-        
+
     };
-    
+
     Lightbox.findById = function (lightboxId) {
         var lightbox = null;
         if (lightboxId in this.listIndex) {
@@ -76,7 +76,7 @@
         }
         return lightbox;
     };
-    
+
     Lightbox.close = function (lightboxId, callbackData) {
         callbackData = callbackData || null;
         var lightbox = this.findById(lightboxId);
@@ -86,13 +86,13 @@
         lightbox.close(callbackData);
         return;
     };
-    
+
     Lightbox.flash = function (key, message) {
         var title = '<span style="text-transform: capitalize;">' + key + '</span>';
         var content = '<div style="padding: 5px 15px;">' +
                       message +
                       '</div>';
-        
+
         var lightboxConfig = {
             type: Lightbox.type.ALERT,
             title: title,
@@ -103,26 +103,26 @@
         var lightbox = new Lightbox(lightboxConfig);
         return lightbox;
     };
-    
-    
+
+
     Lightbox.prototype.id = null;
     Lightbox.prototype.config = null;
     Lightbox.prototype.handle = null;
-    
+
     Lightbox.prototype._constructor = function (options) {
-        
+
         var config = Object.extend({}, Lightbox.defaultOptions);
         Object.extend(config, options);
         this.config = config;
-        
+
         if (config.id === null) {
             config.id = "lightbox" + (Lightbox.iNextAutoId++);
         }
-        
+
         this.id = config.id;
-        
+
         var btnHtml = '';
-        
+
         switch (config.type) {
             case Lightbox.type.ALERT:
                 config.title = (typeof config.title === "string") ? config.title : 'Message';
@@ -131,7 +131,7 @@
                           '</div>';
                 config.content += btnHtml;
                 break;
-                
+
             case Lightbox.type.CONFIRM:
                 config.title = (typeof config.title === "string") ? config.title : 'Confirmation';
                 btnHtml = '<div class="buttons_container">' +
@@ -141,35 +141,36 @@
                           '</div>';
                 config.content += btnHtml;
                 break;
-            
+
             default:
                 break;
         }
-        
+
         var popupToCloseOnNextPopup = Lightbox.popupToCloseOnNextPopup;
-        
+
         if (popupToCloseOnNextPopup !== null) {
             popupToCloseOnNextPopup.close();
             popupToCloseOnNextPopup = null;
         }
-        
+
         if (config.closeOnNextPopup) {
             popupToCloseOnNextPopup = this;
         }
-        
+
         Lightbox.popupToCloseOnNextPopup = popupToCloseOnNextPopup;
-        
+
         var lightboxConfig = {
+            id: config.id,
             theme: config.theme,
             resizable: config.resizable,
             width: config.width,
             height: config.height,
             close: false
         };
-        
+
         var lightbox = new UI.Window(lightboxConfig);
         this.handle = lightbox;
-        
+
         Lightbox.add(this);
         // 2018-08-20 NP: To drop a modal shadow over items such as #navigation
         // this value must be at least:
@@ -179,40 +180,40 @@
         if (typeof config.title === "string") {
             lightbox.setHeader(config.title);
         }
-        
+
         lightbox.show(config.show);
-        
+
         if (config.show) {
             if (config.focus) {
                 lightbox.focus();
             }
-            
+
             if (config.center) {
                 lightbox.center();
             }
         }
-        
+
         return;
     };
-    
+
     Lightbox.prototype.close = function (callbackData) {
-        
+
         callbackData = callbackData || null;
         if (!(callbackData instanceof Array)) {
             callbackData = [callbackData];
         }
-        
+
         this.handle.destroy();
         Lightbox.remove(this);
-        
+
         if (typeof(this.config.callback) === "function") {
             var callbackContext = this.config.callbackContext || window;
             this.config.callback.apply(callbackContext, callbackData);
         }
         return;
     };
-    
+
     window[moduleName] = Lightbox;
-    
+
     return;
 })();
