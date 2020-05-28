d0837d81ad4fca6da9d0823e509ec460c1b67e83
Unknown <emcelroy@concord.org>
2020-05-28 16:01:03 -0400

    Remove no longer needed site notices JS file.

--- app/assets/javascripts/site_notices.js
+++
@@ -1,5 +0,0 @@
-function toggle_Notice_Display(oLink){
-    $('user_notice_container_div').toggle();
-    var strLinkText = ($('user_notice_container_div').getStyle('display') == "none")? "Show Notices" : "Hide Notices";
-    oLink.update(strLinkText);
-}
