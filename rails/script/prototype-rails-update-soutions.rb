43770ded0457ea8e875a62377d01f472527d6b9f
Scott Cytacki <scytacki@concord.org>
2020-08-19 15:04:41 -0400

    whitespace and sorting updates to prototype-rails-solutions after reprocessing
    this also updates the script to include a summary after it processes the file

--- rails/script/prototype-rails-update-soutions.rb
+++ rails/script/prototype-rails-update-soutions.rb
@@ -19,8 +19,12 @@ end
 
 sorted_solutions = solutions.sort.to_h
 puts "Writing: #{solutions_file_name}"
-
 File.open(solutions_file_name, 'w') do |f|
   f.write "# This was generated using: cd rails/script; ruby prototype-railsupdate-solutions.rb\n"
   f.write sorted_solutions.to_yaml
 end
+
+unsolved_files = sorted_solutions.filter{|name, solution| solution["solution"].nil?}
+puts unsolved_files.map{|name, solutions| name}.to_yaml
+puts "Unsolved files: #{unsolved_files.length}"
+puts "Total files #{sorted_solutions.length}"
