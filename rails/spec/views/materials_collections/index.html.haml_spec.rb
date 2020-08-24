561dc072203bf1d8013b64f020679d062c5c6585
Noah Paessel <knowuh@gmail.com>
2020-08-24 15:56:11 -0400

    update site and middleman

--- rails/spec/views/materials_collections/index.html.haml_spec.rb
+++ rails/spec/views/materials_collections/index.html.haml_spec.rb
@@ -2,7 +2,6 @@ require 'spec_helper'
 
 describe "materials_collections/index" do
   before(:each) do
-
     collection1 = FactoryBot.create(:materials_collection)
     collection2 = FactoryBot.create(:materials_collection_with_items)
 
@@ -15,9 +14,7 @@ describe "materials_collections/index" do
   it "renders a list of materials_collections" do
     render
     # Run the generator again with the --webrat flag if you want to use webrat matchers
-    assert_select ".list-item-wrapper .accordion_content .editable_block h4", :text => "Some name".to_s, :count => 2
-    assert_select ".list-item-wrapper .accordion_content .editable_block p", :text => "Some description".to_s, :count => 2
-    assert_select ".list-item-wrapper .accordion_content .editable_block .material_items p", :text => "No materials have been added to this collection.".to_s, :count => 1
-    assert_select ".list-item-wrapper .accordion_content .editable_block .material_items .material_item", :count => 5
+    assert_select ".list-item-wrapper h3", :text => "Some name".to_s, :count => 2
+    assert_select ".list-item-wrapper p", :text => "Some description".to_s, :count => 2
   end
 end
