9fe430e81042455994fcb5c62b47f4e06eab683d
Scott Cytacki <scytacki@concord.org>
2020-08-28 13:18:25 -0400

    fix schema version, I choose the wrong one during last merge

--- rails/db/schema.rb
+++ rails/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20200826205527) do
+ActiveRecord::Schema.define(:version => 20200827012553) do
 
   create_table "access_grants", :force => true do |t|
     t.string   "code"
