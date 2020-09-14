39a75ca713a49f5ae2bd2993d7575b49929badd6
Scott Cytacki <scytacki@concord.org>
2020-09-14 10:48:14 -0400

    fix standards authoring in 1.53.x
    [##174775754]

--- react-components/src/library/library.js
+++ react-components/src/library/library.js
@@ -37,6 +37,7 @@ import EditBookmarks from './components/bookmarks/edit'
 import ManageClasses from './components/portal-classes/manage-classes'
 import EditMaterialsCollectionList from './components/materials-collection/edit-list'
 import JoinClass from './components/portal-students/join-class'
+import StandardsTable from './components/standards/standards-table'
 
 // previously React and ReactDOM were set by the react-rails gem
 window.React = React
@@ -261,5 +262,10 @@ window.PortalComponents = {
   JoinClass: JoinClass,
   renderJoinClass: function (options, id) {
     render(React.createElement(JoinClass, options), id)
+  },
+
+  StandardsTable: StandardsTable,
+  renderStandardsTable: function (options, id) {
+    render(React.createElement(StandardsTable, options), id)
   }
 }
