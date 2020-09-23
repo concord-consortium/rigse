b5c22fc09022cd3ec07e2cd18ded653d700523c5
Noah Paessel <knowuh@gmail.com>
2020-09-23 16:40:34 -0400

    174945853 — rm AASM gem, update Publishable module
    [#174945853]
    https://www.pivotaltracker.com/story/show/174945853

--- rails/lib/publishable.rb
+++ rails/lib/publishable.rb
@@ -1,8 +1,4 @@
-require 'aasm'
-#
-#  Methods for containers that can be made public through some workflow
-#  (challenge will be defining this in a project agnostic way...)
-#
+
 module Publishable
   ##
   ## Called when a class extends this module:
@@ -10,28 +6,19 @@ module Publishable
   def self.included(clazz)
     ## add before_save hooks
     clazz.class_eval do
-      # use rubyist-aasm gem (acts_as_state_machine)
-      # for publication status:
-      # see: http://www.practicalecommerce.com/blogs/post/440-Acts-As-State-Machine-Is-Now-A-Gem
-      # and http://www.ruby-forum.com/topic/179721
-      # for a discussion on how the new aasm gem differs from the old plugin...
-      include AASM
 
-      aasm_initial_state :draft
-      aasm_column :publication_status
       @@protected_publication_states=[:published]
       @@publication_states = [:draft,:published,:private]
-      @@publication_states.each { |s| aasm_state s}
 
       # this needs to come after the class variable definition...
       cattr_accessor :publication_states
 
-      aasm_event :publish do
-        transitions :to => :published, :from => [:draft]
+      def publish!
+        self.publication_status = "published"
       end
 
-      aasm_event :un_publish do
-        transitions :to => :draft, :from => [:published]
+      def un_publish!
+        self.publication_status = "draft"
       end
 
       scope :published, -> {
@@ -51,6 +38,10 @@ module Publishable
 
       def public?
         return publication_status == 'published'
+      end
+
+      def published?
+        public?
       end
     end
   end
--- rails/spec/libs/publishable_spec.rb
+++ rails/spec/libs/publishable_spec.rb
@@ -2,26 +2,85 @@
 
 require 'spec_helper'
 
+class DummyClass
+  def self.aasm_column(symbol) end
+  def self.scope(name, lambda) end
+  include Publishable
+  attr_accessor :publication_status
+  def initialize
+    self.publication_status = :draft
+  end
+end
+
 RSpec.describe Publishable do
+  let(:user_has_roles) { false }
+  let(:user) { mock_model(User, {has_role?: user_has_roles}) }
+  let(:instance) { DummyClass.new }
 
-  # TODO: auto-generated
   describe '#available_states' do
-    xit 'available_states' do
-      publishable = described_class
-      who_wants_to_know = User.new
-      result = publishable.available_states(who_wants_to_know)
+    describe 'when a non-admin is asking:' do
+      let(:user_has_roles) { false }
+      it 'should display all states besides public' do
+        result = instance.available_states(user)
 
-      expect(result).not_to be_nil
+        expect(result).to include(:draft, :private)
+        expect(result).not_to include(:published)
+      end
+    end
+    describe 'when aa admin is asking:' do
+      let(:user_has_roles) { true }
+      it 'should display all states' do
+        result = instance.available_states(user)
+        expect(result).to include(:draft, :private, :published)
+      end
     end
   end
 
-  # TODO: auto-generated
   describe '#public?' do
-    xit 'public?' do
-      publishable = described_class
-      result = publishable.public?
+    describe 'intial state' do
+      it 'should be false' do
+        expect(instance.public?).to eql false
+      end
+    end
 
-      expect(result).not_to be_nil
+    describe 'when published' do
+      it 'should be true' do
+        instance.publication_status = 'published'
+        expect(instance.public?).to eql true
+      end
+    end
+
+    describe 'when unpublished' do
+      it 'should be published' do
+        instance.publication_status = 'private'
+        expect(instance.public?).to eql false
+      end
+    end
+  end
+
+  describe '#publish!' do
+    it 'should update publication_status' do
+      instance.publication_status = 'private'
+      instance.publish!
+      expect(instance.publication_status).to eql 'published'
+    end
+    it 'should change result of #public? to true' do
+      instance.publication_status = 'private'
+      instance.publish!
+      expect(instance.public?).to eql true
+    end
+  end
+
+  describe '#un_publish!' do
+    it 'should set publication_status to draft' do
+      instance.publication_status = 'published'
+      instance.un_publish!
+      expect(instance.publication_status).to eql 'draft'
+    end
+    it 'should change result of #public? to false' do
+      instance.publication_status = 'published'
+      instance.un_publish!
+      expect(instance.public?).to eql false
     end
   end
 
