241a47c04656c9de1f95afef9535b790c1b3aca7
Doug Martin <dmartin@concord.org>
2020-10-07 08:42:47 -0400

    Fixed publication tests

--- rails/features/step_definitions/global_steps.rb
+++ rails/features/step_definitions/global_steps.rb
@@ -50,7 +50,8 @@ def post_with_bearer_token(path, post_data, user = 'admin')
                                       :access_token_expires_at => Time.now + 10.minutes
                                     }, :without_protection => true)
   token = grant.access_token
-  page.driver.post(path, post_data, {"Authorization" => "Bearer #{token}"})
+  page.driver.header "Authorization", "Bearer #{token}"
+  page.driver.post(path, post_data)
 end
 
 def login_with_auth_login_page_as(username, password)
