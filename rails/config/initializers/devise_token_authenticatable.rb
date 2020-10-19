9ecfb484ef06ff69719f4b39b81d29d613b33654
Doug Martin <dmartin@concord.org>
2020-10-19 10:16:47 -0400

    Fixed devise token authentication
    Instead of using the indrect variable to hold the token attribute name the real attribute name is used.

--- rails/config/initializers/devise_token_authenticatable.rb
+++ rails/config/initializers/devise_token_authenticatable.rb
@@ -1,7 +1,7 @@
 Devise::TokenAuthenticatable.setup do |config|
   # set the authentication key name used by this module,
   # defaults to :auth_token
-  config.token_authentication_key = "access_token"
+  config.token_authentication_key = :access_token
 
   # POSSIBLE OPTIONS - using defaults for now
 
