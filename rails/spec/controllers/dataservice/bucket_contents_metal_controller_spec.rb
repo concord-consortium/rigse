0e879fc84cb80a8f2536b907920e7692b4d94b50
Doug Martin <dmartin@concord.org>
2020-10-23 15:20:50 -0400

    Converted bucket contents metal spec to request spec

--- rails/spec/controllers/dataservice/bucket_contents_metal_controller_spec.rb
+++ rails/spec/controllers/dataservice/bucket_contents_metal_controller_spec.rb
@@ -2,11 +2,12 @@
 
 require 'spec_helper'
 
-RSpec.describe Dataservice::BucketContentsMetalController, type: :controller do
+RSpec.describe Dataservice::BucketContentsMetalController, type: :request do
 
   # TODO: auto-generated
   describe '#create' do
     xit 'POST create' do
+      # when converting from xit to it change these to url paths - too lazy to do it now :)
       post :create, {}, {}
 
       expect(response).to have_http_status(:ok)
@@ -16,6 +17,7 @@ RSpec.describe Dataservice::BucketContentsMetalController, type: :controller do
   # TODO: auto-generated
   describe '#create_by_learner' do
     xit 'GET create_by_learner' do
+      # when converting from xit to it change these to url paths - too lazy to do it now :)
       get :create_by_learner, id: FactoryBot.create(:full_portal_learner).to_param
 
       expect(response).to have_http_status(:not_found)
@@ -25,7 +27,7 @@ RSpec.describe Dataservice::BucketContentsMetalController, type: :controller do
   # TODO: auto-generated
   describe '#create_by_name' do
     it 'GET create_by_name' do
-      get :create_by_name, name: 'name'
+      post "/dataservice/bucket_loggers/name/name/bucket_contents.bundle"
 
       expect(response).to have_http_status(:created)
     end
