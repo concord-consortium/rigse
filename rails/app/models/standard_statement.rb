8e835a15817f7d62f14358e05325a71982501672
Doug Martin <dmartin@concord.org>
2020-10-25 08:33:19 -0400

    Put serialize calls inlined when attr_accessible was updated into 1 line each

--- rails/app/models/standard_statement.rb
+++ rails/app/models/standard_statement.rb
@@ -5,8 +5,9 @@ class StandardStatement < ActiveRecord::Base
   #
   attr_accessible :uri, :doc, :material_type, :material_id, :material, :description, :statement_label, :statement_notation, :parents, :education_level, :is_leaf
 
-  serialize :description, :parents, :education_level
-
+  serialize :description
+  serialize :parents
+  serialize :education_level
 
   def duplicate_and_assign_to(material_type, material_id)
     statement = StandardStatement.new(attributes.except('id', 'created_at', 'updated_at', 'material_type', 'material_id'))
