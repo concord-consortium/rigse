7ba0259c7123447ceb10583b9ea97bf03cb2c4f5
Noah Paessel <knowuh@gmail.com>
2021-01-25 17:26:45 -0500

    NP WIP LARA Url ref update script
    Initial pass at rewriting DB references to LARA ⟷ Portal
    
    [#176382622], [#176382636]

---
+++ rails/script/update-lara-db-references.rb
@@ -0,0 +1,74 @@
+PORTAL_COLUMNS_TO_SEARCH = {
+  'ExternalActivity' => %w[url launch_url author_url print_url teacher_guide_url],
+  'Admin::AuthoringSite' => ['url'],
+  'Client' => ['site_url'],
+  'LearnerProcessingEvent' =>  ['url']
+}.freeze
+
+
+# CollaborationRuns: collaborators_data_url
+# InteractiveRunStates: learner_url
+# PortalPublications: portal_url
+# Runs: remote_endpoint, class_info_url
+# SequenceRuns: remote_endpoint
+
+LARA_COLUMNS_TO_SEARCH = {
+  'CollaborationRun' => ['collaborators_data_url'],
+  'InteractiveRunState' => ['learner_url'],
+  'PortalPublication' => ['portal_url'],
+  'Run' => %w[remote_endpoint class_info_url],
+  'SequenceRun' =>  %w[remote_endpoint class_info_url]
+}.freeze
+
+# Other LARA columns that might be important: imported_activity_url in sequences
+
+
+def replace_server_in_string(old_value, old_name, new_name)
+  new_value = old_value.gsub(old_name,new_name)
+  puts new_value
+end
+
+def replace_server_in_table_column(clazz, column, old_name, new_name)
+  table = clazz.table_name
+  sql = "UPDATE #{table} SET #{column} = REPLACE(#{column}, '#{old_name}','#{new_name}')"
+  puts sql
+  # ActiveRecord::Base.connection.execute(sql)
+end
+
+def print_values_in_table(clazz, column)
+  puts clazz.pluck(column)
+end
+
+def update_server_name(column_hash, old_name, new_name)
+  column_hash.each_key do |clazz_name|
+    columns = column_hash[clazz_name]
+    clazz = clazz_name.classify.constantize
+    columns.each do |column|
+      replace_server_in_table_column(clazz, column, old_name, new_name)
+      # print_values_in_table(clazz, column)
+    end
+  end
+end
+
+
+def delete_publications_from_other_portals(portal)
+
+end
+# In the Portal: Update references to LARA
+# update_server_name("authoring.concord.org", "lara-qa.concord-qa.org")
+# https://lara-npaessel-qa.concordqa.org
+# update_server_name(
+#   PORTAL_COLUMNS_TO_SEARCH,
+#   "lara-qa.concord-qa.org",
+#   "lara-npaessel-qa.concordqa.org"
+# )
+
+# In the Portal: Update references to PORTAL from LARA
+update_server_name(
+  LARA_COLUMNS_TO_SEARCH,
+  "ngss-assessment.portal.concord.org",
+  "ngsa-npaessel.concordqa.org"
+)
+
+# One other thing we should do ** just to be on the safe side ** is delete portal
+# publications where the the portal_url isn't the one we are paired with.
