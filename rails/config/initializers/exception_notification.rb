882649a8538f5d6ad312ce2137587526cdebcf47
Noah Paessel <knowuh@gmail.com>
2021-05-11 17:37:01 -0400

    Rails 5.1: Update Gemfile and exception notification config.
    At this point, we can bundle install, and start the app.
    
    We see active record errors when we log in.
    
    #177672617

--- rails/config/initializers/exception_notification.rb
+++ rails/config/initializers/exception_notification.rb
@@ -1,4 +1,7 @@
-RailsPortal::Application.config.middleware.use ExceptionNotifier,
-  :email_prefix => "[#{APP_CONFIG[:site_name]} Exception] ",
-  :sender_address => %("Application Error" <#{APP_CONFIG[:help_email]}>),
-  :exception_recipients => [%("Admin" <#{APP_CONFIG[:admin_email]}>)]
+Rails.application.config.middleware.use ExceptionNotification::Rack,
+  email: {
+    deliver_with: :deliver, # Rails >= 4.2.1 do not need this option since it defaults to :deliver_now
+    email_prefix: "[#{APP_CONFIG[:site_name]} Exception] ",
+    sender_address: %("Application Error" <#{APP_CONFIG[:help_email]}>),
+    exception_recipients: [%("Admin" <#{APP_CONFIG[:admin_email]}>)]
+  }
