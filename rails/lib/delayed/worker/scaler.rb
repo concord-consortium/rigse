30b302d44981fc937e08a196d3c0b706d5b8d21a
Noah Paessel <knowuh@gmail.com>
2021-06-14 18:00:34 -0400

    Cleanup Scaler delayed_job starter
    #178186921
    https://www.pivotaltracker.com/story/show/178186921

--- rails/lib/delayed/worker/scaler.rb
+++ rails/lib/delayed/worker/scaler.rb
@@ -1,15 +1,7 @@
 class JobManager
-  def self.instance
-    @@instance = self.new if @@instance.nil?
-    return @@instance
-  end
 
   def ps_tag
     Digest::MD5.hexdigest(Rails.root.to_s)[0..8]
-  end
-
-  def initialize
-    @jobs = []
   end
 
   def exec_env
@@ -18,39 +10,33 @@ class JobManager
 
   def workers
     # Synchronous read for jobs
-    running = `ps`.split("\n").select do |line|
-      line =~ /delayed_job start -i #{ps_tag}/ &&
-      line =~ /#{ps_tag}/
+    all_jobs = `ps aux`.split("\n")
+    running = all_jobs.select do |line|
+      line =~ /delayed_job/ && line =~ /#{ps_tag}/
     end
     running.size
   end
 
   def start
     cmd_args = "script/delayed_job start -i #{ps_tag}".split
-    @jobs << IO.popen([exec_env] + cmd_args)
+    IO.popen([exec_env] + cmd_args)
   end
 
   def stop
     cmd_args = "script/delayed_job stop -i #{ps_tag}".split
-    @jobs << IO.popen([exec_env] + cmd_args)
+    IO.popen([exec_env] + cmd_args)
   end
-
 end
 
 # Controls for spawning a background processor if one doesn't already exist
 # whenever delayed jobs are created.
-# 2021-06-14 NP: If you have any failed jobs, the worker will continue running.
+# 2021-06-14 NP: This worker will continue running. When we switch to
+# ActiveJob, we can use the built-in in-memory job queuing implementation
 
 module Delayed::Worker::Scaler
   def self.included(base)
     base.send :extend, ClassMethods
     base.class_eval do
-      after_commit(on: :update, if: proc { |r| !r.failed_at.nil? }) do
-        self.class.down
-      end
-      after_commit(on: :destroy, if: proc { |r| r.destroyed? || !r.failed_at.nil? }) do
-        self.class.down
-      end
       after_commit(on: :create) do
         self.class.up
       end
@@ -65,13 +51,6 @@ module Delayed::Worker::Scaler
       if workers == 0 && ((Time.now - @@last_start) > 30)
         @@last_start = Time.now
         @@job_manager.start
-      end
-      true
-    end
-
-    def down
-      if jobs.count == 0 and workers > 0
-        @@job_manager.stop
       end
       true
     end
