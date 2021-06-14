ea59c95a326d41986f64d240f8b4b389e8c974e2
Noah Paessel <knowuh@gmail.com>
2021-06-14 16:04:02 -0400

    Remove Rush and Sextant gems
    Sextant is no longer needed because `/rails/routes` works out of the box now.
    
    Remove some Fixnum deprecations
    Remove unused sextant
    
    #178186921
    https://www.pivotaltracker.com/story/show/178186921

--- rails/lib/delayed/worker/scaler.rb
+++ rails/lib/delayed/worker/scaler.rb
@@ -1,16 +1,51 @@
-require 'rush'
+class JobManager
+  def self.instance
+    @@instance = self.new if @@instance.nil?
+    return @@instance
+  end
+
+  def ps_tag
+    Digest::MD5.hexdigest(Rails.root.to_s)[0..8]
+  end
+
+  def initialize
+    @jobs = []
+  end
+
+  def exec_env
+    { "RAILS_ENV" => Rails.env }
+  end
+
+  def workers
+    # Synchronous read for jobs
+    running = `ps`.split("\n").select do |line|
+      line =~ /delayed_job start -i #{ps_tag}/ &&
+      line =~ /#{ps_tag}/
+    end
+    running.size
+  end
+
+  def start
+    cmd_args = "script/delayed_job start -i #{ps_tag}".split
+    @jobs << IO.popen([exec_env] + cmd_args)
+  end
+
+  def stop
+    cmd_args = "script/delayed_job stop -i #{ps_tag}".split
+    @jobs << IO.popen([exec_env] + cmd_args)
+  end
+
+end
 
 # Controls for spawning a background processor if one doesn't already exist
-# whenever delayed jobs are created. It will also tear the processor down
-# after all the jobs in the queue have been taken care of.
-
-# Based off the 'workless' gem: https://github.com/lostboy/workless
+# whenever delayed jobs are created.
+# 2021-06-14 NP: If you have any failed jobs, the worker will continue running.
 
 module Delayed::Worker::Scaler
   def self.included(base)
     base.send :extend, ClassMethods
     base.class_eval do
-      after_commit(on: :update, if: proc { |r| !r.failed_at.nil? }) do 
+      after_commit(on: :update, if: proc { |r| !r.failed_at.nil? }) do
         self.class.down
       end
       after_commit(on: :destroy, if: proc { |r| r.destroyed? || !r.failed_at.nil? }) do
@@ -23,36 +58,31 @@ module Delayed::Worker::Scaler
   end
 
   module ClassMethods
+    @@job_manager = JobManager.new
     @@ps_tag = nil
     @@last_start = 1.day.ago
     def up
       if workers == 0 && ((Time.now - @@last_start) > 30)
         @@last_start = Time.now
-        Rush::Box.new[Rails.root].bash("script/delayed_job start -i #{process_tag}", :background => true, :env => {:RAILS_ENV => Rails.env})
+        @@job_manager.start
       end
       true
     end
 
     def down
       if jobs.count == 0 and workers > 0
-        Rush::Box.new[Rails.root].bash("script/delayed_job stop -i #{process_tag}", :background => true, :env => {:RAILS_ENV => Rails.env})
+        @@job_manager.stop
       end
       true
     end
 
     def workers
-      Rush::Box.new.processes.filter(:cmdline => /delayed_job start -i #{process_tag}|delayed_job.#{process_tag}/).size
+      @@job_manager.workers
     end
 
     def jobs
       Delayed::Job.where(failed_at: nil)
     end
 
-    def process_tag
-      unless @@ps_tag
-        @@ps_tag = Digest::MD5.hexdigest(Rails.root.to_s)[0..8]
-      end
-      @@ps_tag
-    end
   end
 end
