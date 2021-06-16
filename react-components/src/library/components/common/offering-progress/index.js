fd05f83bc4aa329dd678e5f38d86219ccdbf8570
Noah Paessel <knowuh@gmail.com>
2021-06-16 14:51:42 -0400

    Don't display individual student progress bars.
    First Part of: Student Progress Text Only
    
    #173835131
    https://www.pivotaltracker.com/story/show/173835131

--- react-components/src/library/components/common/offering-progress/index.js
+++ react-components/src/library/components/common/offering-progress/index.js
@@ -1,12 +1,8 @@
 import React from 'react'
-import ProgressBar from './progress-bar'
 
 import css from './style.scss'
 
 const formatDate = date => `${date.getMonth() + 1}/${date.getDate()}`
-
-const notLaunchedDetailedProgress = activities => activities.map(a => ({ activityId: a.id, progress: 0, reportUrl: null }))
-const launchedDetailedProgress = activities => activities.map(a => ({ activityId: a.id, progress: 100, info: 'launched', reportUrl: null }))
 
 export default class ProgressTable extends React.Component {
   getFeedbackOptions (activityId) {
@@ -28,29 +24,8 @@ export default class ProgressTable extends React.Component {
       : name
   }
 
-  renderStudentProgressBars (student) {
-    const { activities } = this.props
-    let detailedProgress = []
-    if (student.detailedProgress) {
-      // Detailed progress available. Render it.
-      detailedProgress = student.detailedProgress
-    } else if (!student.startedActivity) {
-      // Otherwise, there are two options. Student has run offering or not. In this case we need to render
-      // empty bar or full progress bar with "launched" label.
-      detailedProgress = notLaunchedDetailedProgress(activities)
-    } else if (student.startedActivity) {
-      detailedProgress = launchedDetailedProgress(activities)
-    }
-
-    return detailedProgress.map((details, idx) => (
-      <td key={idx}>
-        <ProgressBar student={student} detailedProgress={details} feedbackOptions={this.getFeedbackOptions(details.activityId)} />
-      </td>
-    ))
-  }
-
   render () {
-    const { students, activities } = this.props
+    const { students } = this.props
     if (students.length === 0) {
       return null
     }
@@ -62,6 +37,7 @@ export default class ProgressTable extends React.Component {
               <tr>
                 <th>Student</th>
                 <th className={css.dateHeader}>Last Run</th>
+                <th>Status</th>
               </tr>
               {
                 students.map(student =>
@@ -70,22 +46,9 @@ export default class ProgressTable extends React.Component {
                     <td className={css.date} title={student.lastRun && student.lastRun.toLocaleDateString()}>
                       { student.lastRun ? formatDate(student.lastRun) : 'n/a' }
                     </td>
-                  </tr>
-                )
-              }
-            </tbody>
-          </table>
-        </div>
-        <div className={css.progressTableContainer}>
-          <table className={css.progressTable}>
-            <tbody>
-              <tr>
-                { activities.map((a, idx) => <th key={idx}>{ this.renderActivityHeader(a) }</th>) }
-              </tr>
-              {
-                students.map(student =>
-                  <tr key={student.id}>
-                    { this.renderStudentProgressBars(student) }
+                    <td className={css.status}>
+                      { student.startedActivity ? 'started' : 'not started' }
+                    </td>
                   </tr>
                 )
               }
