0a76afa5846d925b804562837e25deddcb320859
Sam Fentress <sfentress@concord.org>
2021-06-24 09:39:02 -0400

    Fix failing dataservice tests

--- rails/app/models/dataservice/process_external_activity_data_job.rb
+++ rails/app/models/dataservice/process_external_activity_data_job.rb
@@ -67,7 +67,7 @@ class Dataservice::ProcessExternalActivityDataJob
       end
     end
     learner.report_learner.last_run = Time.now
-    learner.update_report_model_cache if learner && learner.respond_to?(:update_report_model_cache)
+    learner.update_report_model_cache if learner
   end
 
   def internal_process_open_response(data, embeddable)
