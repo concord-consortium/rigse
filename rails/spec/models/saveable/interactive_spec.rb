1ec8103fb7b608204cfe227721bf8cfbfb2004b8
Piotr Janik <janikpiotrek@gmail.com>
2023-02-21 12:05:10 +0000

    chore: fix interactive_spec.rb
    [#184372422]

--- rails/spec/models/saveable/interactive_spec.rb
+++ rails/spec/models/saveable/interactive_spec.rb
@@ -14,25 +14,4 @@ RSpec.describe Saveable::Interactive, type: :model do
       expect(result).to be_nil
     end
   end
-
-  # TODO: auto-generated
-  describe '#submitted_answer' do
-    xit 'submitted_answer' do
-      interactive = described_class.new
-      result = interactive.submitted_answer
-
-      expect(result).not_to be_nil
-    end
-  end
-
-  # TODO: auto-generated
-  describe '#submitted?' do
-    it 'submitted?' do
-      interactive = described_class.new
-      result = interactive.submitted?
-
-      expect(result).not_to be_nil
-    end
-  end
-
 end
