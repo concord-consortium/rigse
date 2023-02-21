e8f310fe2d2f548749367ab1e29bf33459302236
Piotr Janik <janikpiotrek@gmail.com>
2023-02-21 11:07:08 +0000

    chore: remove OpenResponseAnswer + all related code, update create_default_data.rb
    [#184372422]

--- rails/app/models/saveable/open_response_answer.rb
+++
@@ -1,8 +0,0 @@
-class Saveable::OpenResponseAnswer < ApplicationRecord
-  self.table_name = "saveable_open_response_answers"
-
-  belongs_to :open_response,  :class_name => 'Saveable::OpenResponse', :counter_cache => :response_count
-
-  acts_as_list :scope => :open_response_id
-  delegate :learner, to: :open_response, allow_nil: :true
-end
--- rails/spec/models/saveable/open_response_spec.rb
+++ rails/spec/models/saveable/open_response_spec.rb
@@ -2,16 +2,6 @@ require File.expand_path('../../../spec_helper', __FILE__)
 
 describe Saveable::OpenResponse do
   # TODO: auto-generated
-  describe '#answered?' do
-    it 'answered?' do
-      open_response = described_class.new
-      result = open_response.answered?
-
-      expect(result).not_to be_nil
-    end
-  end
-
-  # TODO: auto-generated
   describe '#embeddable' do
     it 'embeddable' do
       open_response = described_class.new
@@ -20,16 +10,4 @@ describe Saveable::OpenResponse do
       expect(result).to be_nil
     end
   end
-
-  # TODO: auto-generated
-  describe '#submitted_answer' do
-    it 'submitted_answer' do
-      open_response = described_class.new
-      result = open_response.submitted_answer
-
-      expect(result).not_to be_nil
-    end
-  end
-
-
 end
