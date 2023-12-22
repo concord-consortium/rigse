8213098d93887a88c303946db8d767195acd7811
Aden <115961721+433eros@users.noreply.github.com>
2023-12-22 18:30:22 +0000

    chore(devspace): Add DevSpace start script (GH-1235)

--- rails/config/initializers/session_store.rb
+++ rails/config/initializers/session_store.rb
@@ -7,5 +7,5 @@
 # (create the session table with "rails generate session_migration")
 
 # For testing environments, we don't use SSL and can't set cookie to secure.
-secure = !(Rails.env.cucumber? || Rails.env.test? || Rails.env.feature_test?)
+secure = !(Rails.env.development? || Rails.env.cucumber? || Rails.env.test? || Rails.env.feature_test?)
 RailsPortal::Application.config.session_store :active_record_store, :key => '_rails_portal_session', secure: secure
