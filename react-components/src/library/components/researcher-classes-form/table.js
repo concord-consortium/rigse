9c203155ecbedf41d50beee57965923dea6b20e3
Piotr Janik <janikpiotrek@gmail.com>
2024-03-22 19:51:40 +0000

    feat: add Researcher Classes filters and table [PT-187185134] [PT-187185190]

---
+++ react-components/src/library/components/researcher-classes-form/table.js
@@ -0,0 +1,45 @@
+import React from 'react'
+import 'react-day-picker/lib/style.css'
+import css from './style.scss'
+
+export default class ResearcherClassesTable extends React.Component {
+  render () {
+    const { classes } = this.props
+    if (classes.length === 0) {
+      return null
+    }
+    return (
+      <div className={css.researcherClassesTable}>
+        <hr />
+        <div>Results</div>
+
+        <table>
+          <thead>
+            <tr>
+              <th>Cohort</th>
+              <th>Teacher</th>
+              <th>Class</th>
+              <th />
+            </tr>
+          </thead>
+          <tbody>
+            {
+              classes.map((c, i) => (
+                <tr key={i}>
+                  <td>{c.cohort_names}</td>
+                  <td>{c.teacher_names}</td>
+                  <td>{c.name}</td>
+                  <td><a href={c.class_url} target='_blank'>View Class</a></td>
+                </tr>
+              ))
+            }
+          </tbody>
+        </table>
+      </div>
+    )
+  }
+}
+
+ResearcherClassesTable.defaultProps = {
+  classes: []
+}
