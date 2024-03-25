5a3ec59721804ed8bf1883999c2e55bb51d444a1
Piotr Janik <janikpiotrek@gmail.com>
2024-03-25 17:27:01 +0000

    chore: refactor policies, add specs [PT-187185134]

--- rails/app/policies/portal/clazz_policy.rb
+++ rails/app/policies/portal/clazz_policy.rb
@@ -1,23 +1,19 @@
 class Portal::ClazzPolicy < ApplicationPolicy
   class Scope < Scope
     def resolve
-      if user && user.has_role?('admin')
+      return none unless user
+
+      if user.has_role?('admin')
         all
-      elsif user && (user.is_project_admin? || user.is_project_researcher?)
+      elsif user.is_project_admin? || user.is_project_researcher?
         # prevents a bunch of unnecessary model loads by not using the user#admin_for_project_teachers and user#researcher_for_project_teachers methods
-        teacher_scope = Pundit.policy_scope(user, Portal::Teacher)
-        teacher_clazz_ids = teacher_scope
-          .joins("INNER JOIN portal_teacher_clazzes __ptc_scope ON __ptc_scope.teacher_id = portal_teachers.id")
+        teacher_ids_subquery = Pundit.policy_scope(user, Portal::Teacher).select(:id)
+        scope
+          .joins("INNER JOIN portal_teacher_clazzes ON portal_teacher_clazzes.clazz_id = portal_clazzes.id")
+          .where(portal_teacher_clazzes: { teacher_id: teacher_ids_subquery })
           .distinct
-          .pluck("__ptc_scope.clazz_id")
-        if teacher_clazz_ids.length > 0
-          scope.where(id: teacher_clazz_ids)
-        else
-          none
-        end
-      elsif user && user.portal_teacher
-        clazz_ids = user.portal_teacher.clazz_ids
-        scope.where(id: clazz_ids)
+      elsif user.portal_teacher
+        scope.where(id: user.portal_teacher.clazz_ids)
       else
         none
       end
