34206c2be61eb43fbf03b2fe1e475af4b08a6988
Piotr Janik <janikpiotrek@gmail.com>
2024-05-21 18:28:41 +0000

    fix: fix admin notice edit and new forms [PT-187644327]

--- react-components/src/library/components/site-notices/edit.js
+++ react-components/src/library/components/site-notices/edit.js
@@ -1,47 +1,43 @@
-import React from 'react'
-
+import React, { useEffect } from 'react'
 import css from './style.scss'
 
-export default class SiteNoticesEditForm extends React.Component {
-  constructor (props) {
-    super(props)
-    this.state = {
-      notice: this.props.notice
-    }
-  }
+const SiteNoticesEditForm = ({ notice }) => {
+  useEffect(() => {
+    // See: app/helpers/tiny_mce_helper.rb
+    window.initTinyMCE()
+  }, [notice])
 
-  render () {
-    const { notice } = this.state
-    const formAction = '/api/v1/site_notices/' + notice.id
-    const formId = 'edit_admin_site_notice_' + notice.id
-    const authToken = jQuery('meta[name="csrf-token"]').attr('content')
-
-    if (!notice) {
-      return (
-        <div>
-          Loading...
-        </div>
-      )
-    }
-
+  if (!notice) {
     return (
-      <div className={css.adminSiteNoticesEdit}>
-        <h1>Edit Notice</h1>
-        <form acceptCharset='UTF-8' action={formAction} method='post' id={formId}>
-          <div id='editor' className={css.adminSiteNoticesEditEditor}>
-            <textarea className='tinymce_textarea' rows='40' name='notice_html' defaultValue={notice.notice_html} />
-          </div>
-          <div className={css.adminSiteNoticesEditBackLink}>
-            <a href='/admin/site_notices'>Cancel</a>
-          </div>
-          <div className={css.adminSiteNoticesEditSubmit}>
-            <input name='utf8' type='hidden' value='✓' />
-            <input name='_method' type='hidden' value='put' />
-            <input name='authenticity_token' type='hidden' value={authToken} />
-            <input className='pie' name='commit' type='submit' value='Update Notice' />
-          </div>
-        </form>
+      <div>
+        Loading...
       </div>
     )
   }
+
+  const formAction = '/api/v1/site_notices/' + notice.id
+  const formId = 'edit_admin_site_notice_' + notice.id
+  const authToken = jQuery('meta[name="csrf-token"]').attr('content')
+
+  return (
+    <div className={css.adminSiteNoticesEdit}>
+      <h1>Edit Notice</h1>
+      <form acceptCharset='UTF-8' action={formAction} method='post' id={formId}>
+        <div id='editor' className={css.adminSiteNoticesEditEditor}>
+          <textarea className='tinymce_textarea' rows='40' name='notice_html' defaultValue={notice.notice_html} />
+        </div>
+        <div className={css.adminSiteNoticesEditBackLink}>
+          <a href='/admin/site_notices'>Cancel</a>
+        </div>
+        <div className={css.adminSiteNoticesEditSubmit}>
+          <input name='utf8' type='hidden' value='✓' />
+          <input name='_method' type='hidden' value='put' />
+          <input name='authenticity_token' type='hidden' value={authToken} />
+          <input className='pie' name='commit' type='submit' value='Update Notice' />
+        </div>
+      </form>
+    </div>
+  )
 }
+
+export default SiteNoticesEditForm
--- react-components/src/library/components/site-notices/new.js
+++ react-components/src/library/components/site-notices/new.js
@@ -1,27 +1,32 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 import css from './style.scss'
 
-export default class SiteNoticesNewForm extends React.Component {
-  render () {
-    const authToken = jQuery('meta[name="csrf-token"]').attr('content')
-    return (
-      <div className={css.adminSiteNoticesEdit}>
-        <h1>Create Notice</h1>
-        <form acceptCharset='UTF-8' action={Portal.API_V1.SITE_NOTICES_CREATE} method='post'>
-          <div id='editor' className={css.adminSiteNoticesEditEditor}>
-            <textarea className={css.tinymce_textarea} rows='40' name='notice_html' defaultValue={''} />
-          </div>
-          <div className={css.adminSiteNoticesEditBackLink}>
-            <a href='/admin/site_notices'>Cancel</a>
-          </div>
-          <div className={css.adminSiteNoticesEditSubmit}>
-            <input name='utf8' type='hidden' value='✓' />
-            <input name='authenticity_token' type='hidden' value={authToken} />
-            <input className='pie' name='commit' type='submit' value='Publish Notice' />
-          </div>
-        </form>
-      </div>
-    )
-  }
+const SiteNoticesNewForm = () => {
+  useEffect(() => {
+    // See: app/helpers/tiny_mce_helper.rb
+    window.initTinyMCE()
+  }, [])
+
+  const authToken = jQuery('meta[name="csrf-token"]').attr('content')
+  return (
+    <div className={css.adminSiteNoticesEdit}>
+      <h1>Create Notice</h1>
+      <form acceptCharset='UTF-8' action={Portal.API_V1.SITE_NOTICES_CREATE} method='post'>
+        <div id='editor' className={css.adminSiteNoticesEditEditor}>
+          <textarea className={css.tinymce_textarea} rows='40' name='notice_html' defaultValue={''} />
+        </div>
+        <div className={css.adminSiteNoticesEditBackLink}>
+          <a href='/admin/site_notices'>Cancel</a>
+        </div>
+        <div className={css.adminSiteNoticesEditSubmit}>
+          <input name='utf8' type='hidden' value='✓' />
+          <input name='authenticity_token' type='hidden' value={authToken} />
+          <input className='pie' name='commit' type='submit' value='Publish Notice' />
+        </div>
+      </form>
+    </div>
+  )
 }
+
+export default SiteNoticesNewForm
