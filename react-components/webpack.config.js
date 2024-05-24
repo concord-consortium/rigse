d35a16182fab9fae5e470da766ae64a3c0035f23
Piotr Janik <janikpiotrek@gmail.com>
2024-05-24 12:35:57 +0000

    chore(TS-support): instal @types packages, update webpack config, add tsconfig and global.d.ts [PT-187665852]

--- react-components/webpack.config.js
+++ react-components/webpack.config.js
@@ -21,14 +21,8 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env', '@babel/preset-react']
-          }
-        }
+        test: /\.tsx?$/,
+        loader: 'ts-loader',
       },
       {
         test: [/node_modules[\\/].*\.(css|scss)$/, /library.scss$/],
