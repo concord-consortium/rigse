6a862c43270e143902effd3976b660d4df70042d
Piotr Janik <janikpiotrek@gmail.com>
2024-06-21 18:19:26 +0000

    feat(PermissionForms): add missing features to students table and add bulk edit UI [PT-187612778] [PT-187613422]

--- rails/react-components/src/library/components/permission-forms-v2/students-tab/students-table.tsx
+++ rails/react-components/src/library/components/permission-forms-v2/students-tab/students-table.tsx
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
+import Select from "react-select";
 import { useFetch } from "../../../hooks/use-fetch";
-import { CurrentSelectedProject, IStudent } from "./types";
+import { CurrentSelectedProject, IPermissionForm, IStudent } from "./types";
 
 import css from "./students-table.scss";
 
@@ -9,27 +10,90 @@ interface IProps {
   currentSelectedProject: CurrentSelectedProject;
 }
 
-export const StudentsTable = ({ classId }: IProps) => {
-  const { data: permissionFormsData, isLoading } = useFetch<IStudent[]>(Portal.API_V1.permissionFormsClassPermissionForms(classId), []);
+type PermissionFormOption = {
+  value: string;
+  label: string;
+};
 
-  if (isLoading) {
+export const StudentsTable = ({ classId }: IProps) => {
+  const { data: studentsData, isLoading: studentsLoading } = useFetch<IStudent[]>(Portal.API_V1.permissionFormsClassPermissionForms(classId), []);
+  const { data: permissionForms, isLoading: permissionFormsLoading } = useFetch<IPermissionForm[]>(Portal.API_V1.PERMISSION_FORMS, []);
+  const [isStudentSelected, setIsStudentSelected] = useState<Record<string, boolean>>({});
+  const [permissionFormsToAdd, setPermissionFormsToAdd] = useState<readonly PermissionFormOption[]>([]);
+  const [permissionFormsToRemove, setPermissionFormsToRemove] = useState<readonly PermissionFormOption[]>([]);
+
+  // When preparing the options for the Select component, we need to filter out the permission forms that are already
+  // selected to add or remove in the opposite dropdown. Both permissionFormsToAdd and permissionFormsToRemove need
+  // to be mutually exclusive.
+  const permissionFormToAddOptions = Object.freeze(
+    permissionForms.filter(pf => !permissionFormsToRemove.find(pfr => pfr.value === pf.id)).map(pf => ({ value: pf.id, label: pf.name }))
+  );
+
+  const permissionFormToRemoveOptions = Object.freeze(
+    permissionForms.filter(pf => !permissionFormsToAdd.find(pfr => pfr.value === pf.id)).map(pf => ({ value: pf.id, label: pf.name }))
+  );
+
+  if (studentsLoading) {
     return (<div>Loading...</div>);
   }
-  if (!permissionFormsData.length) {
+  if (!studentsData.length) {
     return (<div>No students found</div>);
   }
+
+  const handleStudentSelectedToggle = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const studentId = e.target.name;
+    if (e.target.checked) {
+      setIsStudentSelected(prevIsStudentSelected => ({ ...prevIsStudentSelected, [studentId]: true }));
+    } else {
+      setIsStudentSelected(prevIsStudentSelected => {
+        const { [studentId]: _, ...rest } = prevIsStudentSelected;
+        return rest;
+      });
+    }
+  };
+
+  const handleSelectAllChange = () => {
+    setIsStudentSelected(prevIsStudentSelected => {
+      const _allStudentsSelected = Object.keys(prevIsStudentSelected).length === studentsData.length;
+      if (_allStudentsSelected) {
+        return {};
+      }
+      const result: Record<string, boolean> = {};
+      studentsData.forEach(student => {
+        result[student.id] = true;
+      });
+      return result;
+    });
+  };
+
+  const handlePermissionFormToAddSelectChange = (selectedOptions: readonly PermissionFormOption[]) => {
+    setPermissionFormsToAdd(selectedOptions);
+  };
+
+  const handlePermissionFormToRemoveSelectChange = (selectedOptions: readonly PermissionFormOption[]) => {
+    setPermissionFormsToRemove(selectedOptions);
+  };
+
+  const selectedStudentsCount = Object.keys(isStudentSelected).length;
+  const allStudentsSelected = Object.keys(isStudentSelected).length === studentsData.length;
 
   return (
     <table className={css.studentsTable}>
       <thead>
-        <tr><th><input type="checkbox" /></th><th>Student Name</th><th>Username</th><th>Permission Forms</th><th></th></tr>
+        <tr>
+          <th><input type="checkbox" checked={allStudentsSelected} onChange={handleSelectAllChange} /></th>
+          <th>Student Name</th>
+          <th>Username</th>
+          <th>Permission Forms</th>
+          <th></th>
+        </tr>
       </thead>
       <tbody>
         {
-          permissionFormsData.map((studentInfo) => {
+          studentsData.map((studentInfo) => {
             return (
               <tr key={studentInfo.id}>
-                <td><input type="checkbox" /></td>
+                <td><input type="checkbox" name={studentInfo.id} checked={isStudentSelected[studentInfo.id] ?? false} onChange={handleStudentSelectedToggle} /></td>
                 <td>{ studentInfo.name }</td>
                 <td>{ studentInfo.login }</td>
                 <td>{ studentInfo.permission_forms.map(pf => pf.name).join(", ") }</td>
@@ -39,6 +103,48 @@ export const StudentsTable = ({ classId }: IProps) => {
           })
         }
       </tbody>
+      <tfoot>
+        <tr>
+          <td colSpan={5}>
+            <div className={css.tableFooter}>
+              <div className={css.summary}>
+                { selectedStudentsCount } selected { selectedStudentsCount === 1 ? "student" : "students" }
+              </div>
+              <div className={css.permissionFormSelects}>
+                <div className={css.selectContainer}>
+                  Add:
+                  <Select<PermissionFormOption, true>
+                    className={css.permissionFormSelect}
+                    // TODO: temporarily ignoring typing issue that I cannot understand
+                    options={permissionFormToAddOptions}
+                    isMulti={true}
+                    placeholder="Select permission form(s)..."
+                    isLoading={permissionFormsLoading}
+                    value={permissionFormsToAdd}
+                    onChange={handlePermissionFormToAddSelectChange}
+                  />
+                </div>
+                <div className={css.selectContainer}>
+                  Remove:
+                  <Select<PermissionFormOption, true>
+                    className={css.permissionFormSelect}
+                    // TODO: temporarily ignoring typing issue that I cannot understand
+                    options={permissionFormToRemoveOptions}
+                    isMulti={true}
+                    placeholder="Select permission form(s)..."
+                    isLoading={permissionFormsLoading}
+                    value={permissionFormsToRemove}
+                    onChange={handlePermissionFormToRemoveSelectChange}
+                  />
+                </div>
+              </div>
+              <div>
+                <button className={css.saveChangesButton}>Save Changes</button>
+              </div>
+            </div>
+          </td>
+        </tr>
+      </tfoot>
     </table>
   );
 };
