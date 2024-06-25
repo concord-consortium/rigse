11ca8fbe58bcfea07089a03ac6f41b65f750cac5
Piotr Janik <janikpiotrek@gmail.com>
2024-06-25 16:28:52 +0000

    fix(PermissionForm): escape special characters in search_teachers name param [PT-187595842]

--- rails/spec/controllers/api/v1/permission_forms_controller_spec.rb
+++ rails/spec/controllers/api/v1/permission_forms_controller_spec.rb
@@ -45,6 +45,58 @@ RSpec.describe API::V1::PermissionFormsController, type: :controller do
     expect(Portal::PermissionForm.count).to eq(0)
   end
 
+  describe 'GET search_teachers' do
+    it 'returns an empty array if the name is blank' do
+      get :search_teachers, params: { name: '' }
+      expect(response).to have_http_status(:ok)
+      expect(JSON.parse(response.body)).to eq([])
+    end
+
+    it 'returns a list of teachers with login matching the name' do
+      teacher = FactoryBot.create(:teacher, user: FactoryBot.create(:user, login: 'test_teacher'))
+      get :search_teachers, params: { name: 'test_teacher' }
+      expect(response).to have_http_status(:ok)
+      expect(JSON.parse(response.body)).to match([
+        hash_including('id' => teacher.id, 'name' => teacher.user.name, 'email' => teacher.user.email, 'login' => teacher.user.login)
+      ])
+    end
+
+    it 'returns a list of teachers with email matching the name' do
+      teacher = FactoryBot.create(:teacher, user: FactoryBot.create(:user, email: 'test_teacher@mail.com'))
+      get :search_teachers, params: { name: 'test_teacher' }
+      expect(response).to have_http_status(:ok)
+      expect(JSON.parse(response.body)).to match([
+        hash_including('id' => teacher.id, 'name' => teacher.user.name, 'email' => teacher.user.email, 'login' => teacher.user.login)
+      ])
+    end
+
+    it 'returns a list of teachers with first or last name matching the name' do
+      teacher = FactoryBot.create(:teacher, user: FactoryBot.create(:user, first_name: 'John', last_name: 'Doe'))
+      get :search_teachers, params: { name: 'John' }
+      expect(response).to have_http_status(:ok)
+      expect(JSON.parse(response.body)).to match([
+        hash_including('id' => teacher.id, 'name' => teacher.user.name, 'email' => teacher.user.email, 'login' => teacher.user.login)
+      ])
+
+      get :search_teachers, params: { name: 'Doe' }
+      expect(response).to have_http_status(:ok)
+      expect(JSON.parse(response.body)).to match([
+        hash_including('id' => teacher.id, 'name' => teacher.user.name, 'email' => teacher.user.email, 'login' => teacher.user.login)
+      ])
+    end
+
+    it 'escapes provided name parameter' do
+      teacher = FactoryBot.create(:teacher, user: FactoryBot.create(:user, login: 'test_teacher'))
+      get :search_teachers, params: { name: 't__t_teacher' }
+      expect(response).to have_http_status(:ok)
+      expect(JSON.parse(response.body)).to eq([])
+
+      get :search_teachers, params: { name: 't%t_teacher' }
+      expect(response).to have_http_status(:ok)
+      expect(JSON.parse(response.body)).to eq([])
+    end
+  end
+
   describe 'when user is a project admin' do
     let (:project) { FactoryBot.create(:project) }
     let (:another_project) { FactoryBot.create(:project) }
@@ -70,6 +122,23 @@ RSpec.describe API::V1::PermissionFormsController, type: :controller do
       expect(response).to have_http_status(:forbidden)
       expect(Portal::PermissionForm.count).to eq(1)
     end
+
+    it 'GET search_teachers returns a list of teachers from the project' do
+      teacher1 = FactoryBot.create(:teacher, user: FactoryBot.create(:user, login: 'test_teacher1'))
+      cohort = FactoryBot.create(:admin_cohort, project: project)
+      teacher1.cohorts << cohort
+
+      teacher2 = FactoryBot.create(:teacher, user: FactoryBot.create(:user, login: 'test_teacher2'))
+
+      get :search_teachers, params: { name: 'test_teacher' }
+
+      expect(response).to have_http_status(:ok)
+      body = JSON.parse(response.body)
+      expect(body.size).to eq(1)
+      expect(JSON.parse(response.body)).to match([
+        hash_including('id' => teacher1.id)
+      ])
+    end
   end
 
   describe 'when user is a project researcher' do
@@ -87,6 +156,23 @@ RSpec.describe API::V1::PermissionFormsController, type: :controller do
       delete :destroy, params: { id: permission_form.id }
       expect(response).to have_http_status(:forbidden)
       expect(Portal::PermissionForm.count).to eq(1)
+    end
+
+    it 'GET search_teachers returns a list of teachers from the project' do
+      teacher1 = FactoryBot.create(:teacher, user: FactoryBot.create(:user, login: 'test_teacher1'))
+      cohort = FactoryBot.create(:admin_cohort, project: project)
+      teacher1.cohorts << cohort
+
+      teacher2 = FactoryBot.create(:teacher, user: FactoryBot.create(:user, login: 'test_teacher2'))
+
+      get :search_teachers, params: { name: 'test_teacher' }
+
+      expect(response).to have_http_status(:ok)
+      body = JSON.parse(response.body)
+      expect(body.size).to eq(1)
+      expect(JSON.parse(response.body)).to match([
+        hash_including('id' => teacher1.id)
+      ])
     end
   end
 
