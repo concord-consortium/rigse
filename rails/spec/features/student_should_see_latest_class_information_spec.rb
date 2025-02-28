ae334637269fd254585a4d59fccd4d75c063675a
Ethan McElroy <emcelroy@concord.org>
2025-02-28 23:28:12 +0000

    chore: temporarily disable feature tests

--- rails/spec/features/student_should_see_latest_class_information_spec.rb
+++ rails/spec/features/student_should_see_latest_class_information_spec.rb
@@ -1,36 +1,36 @@
-require 'spec_helper'
+# require 'spec_helper'
 
-RSpec.feature 'Student should see latest class information', :WebDriver => true do
-  before do
-    generate_default_settings_with_factories
-  end
+# RSpec.feature 'Student should see latest class information', :WebDriver => true do
+#   before do
+#     generate_default_settings_with_factories
+#   end
 
-  scenario 'Student should see the updated class name', js: true do
-    create_class_in_js_context
-    login_as('taylor')
-    visit root_path
-    expect(page).to have_content('Basic Electronics')
-  end
+#   scenario 'Student should see the updated class name', js: true do
+#     create_class_in_js_context
+#     login_as('taylor')
+#     visit root_path
+#     expect(page).to have_content('Basic Electronics')
+#   end
 
-  scenario 'Student should see all the updated information of a class', js: true do
-    create_class_in_js_context
-    login_as('taylor')
-    visit root_path
-    first(:link, 'Basic Electronics').click
-    expect(page).to have_content('Class Word: betrx')
-    expect(page).to have_content('NON LINEAR DEVICES')
-    expect(page).to have_content('STATIC DISCIPLINE')
-  end
+#   scenario 'Student should see all the updated information of a class', js: true do
+#     create_class_in_js_context
+#     login_as('taylor')
+#     visit root_path
+#     first(:link, 'Basic Electronics').click
+#     expect(page).to have_content('Class Word: betrx')
+#     expect(page).to have_content('NON LINEAR DEVICES')
+#     expect(page).to have_content('STATIC DISCIPLINE')
+#   end
 
-  # moved from before method as this needs to run within the js context to wait for React to render
-  def create_class_in_js_context
-    login_as('teacher')
-    clazz = Portal::Clazz.find_by_name('Mathematics')
-    visit edit_portal_clazz_path(clazz)
-    fill_in('portal_clazz_name', :with => 'Basic Electronics')
-    fill_in('portal_clazz_description' , :with => 'This is a biology class')
-    fill_in('portal_clazz_class_word' , :with => 'betrx')
-    click_button('Save')
-    expect(page).not_to have_content('Save')
-  end
-end
+#   # moved from before method as this needs to run within the js context to wait for React to render
+#   def create_class_in_js_context
+#     login_as('teacher')
+#     clazz = Portal::Clazz.find_by_name('Mathematics')
+#     visit edit_portal_clazz_path(clazz)
+#     fill_in('portal_clazz_name', :with => 'Basic Electronics')
+#     fill_in('portal_clazz_description' , :with => 'This is a biology class')
+#     fill_in('portal_clazz_class_word' , :with => 'betrx')
+#     click_button('Save')
+#     expect(page).not_to have_content('Save')
+#   end
+# end
