715fdef490c2daa1dd208957f7a352f19d1e897b
Ethan McElroy <emcelroy@concord.org>
2025-03-10 23:53:45 +0000

    chore: specify binary for new driver

--- rails/spec/support/capybara_initializer.rb
+++ rails/spec/support/capybara_initializer.rb
@@ -67,6 +67,7 @@ class CapybaraInitializer
 
   Capybara.register_driver :headless_chrome do |app|
     options = Selenium::WebDriver::Chrome::Options.new
+    options.binary = "/usr/bin/chromium"
     options.add_argument('--headless')
     options.add_argument('--disable-gpu')
     options.add_argument('--no-sandbox')
