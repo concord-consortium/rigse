0dca519597db9c3e532b38ea67ff28e3a824f00b
Ethan McElroy <emcelroy@concord.org>
2025-03-12 00:49:41 +0000

    chore: fix display screenshot on error

--- rails/spec/support/system_tests.rb
+++ rails/spec/support/system_tests.rb
@@ -1,11 +1,18 @@
 require 'capybara-screenshot/rspec'
 
+module SuppressRailsScreenshotDisplay
+  def display_image(_file_name)
+    # Do nothing, so it doesn't call `image_path` which causes an error.
+  end
+end
+
 Capybara::Screenshot.register_driver(:headless_chrome) do |driver, path|
-  driver.browser.save_screenshot(path)
+  driver.save_screenshot(path)
 end
 
 RSpec.configure do |config|
   config.before(:each, type: :system) do
     driven_by :headless_chrome
   end
+  config.include SuppressRailsScreenshotDisplay, type: :system
 end
