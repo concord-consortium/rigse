78cd1220a63cf810b49b4e92d356ecff90cf1f56
Doug Martin <dmartin@concord.org>
2025-08-25 10:14:08 -0400

    feat: Add EIN to footer [RIGSE-238]
    This commit updates the footer component as follows:
    
    - Added EIN information
    - Changed "organization" to "charity"

--- rails/react-components/src/library/components/page-footer.tsx
+++ rails/react-components/src/library/components/page-footer.tsx
@@ -17,7 +17,9 @@ const PageFooter = Component({
       <div id="footer">
         <div className="footer-inner">
           <p>
-            © { year } <a href="https://concord.org" id="footer_cc_link">The Concord Consortium</a>. All Rights Reserved. The Concord Consortium is a 501(c)(3) nonprofit organization registered in the U.S.
+            © { year } <a href="https://concord.org" id="footer_cc_link">The Concord Consortium</a>. All Rights Reserved.
+            <br/>
+            The Concord Consortium is a 501(c)(3) nonprofit charity registered in the U.S. under EIN 04-3254131.
             <br />
             <a href="https://concord.org/privacy-policy" id="privacy-policy-link" target="_blank" rel="noopener noreferrer">Privacy Policy</a> · Questions/Feedback: <a href="mailto:help@concord.org?subject=STEM%20Resource%20Finder%20question">Send us an email</a>
             <br />
